fef0228c4387b5e6083f39bbc1972d208c9cd842
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2013-11-14 22:32:49 +0100

    Change logs Level at runtime (admin only)
    --
    Inspired of https://github.com/yesnault/springAngularGradle

--- app/templates/src/main/webapp/scripts/_controllers.js
+++ app/templates/src/main/webapp/scripts/_controllers.js
@@ -113,3 +113,28 @@
             $location.path('');
         });
 });
+
+<%= baseName %>App.controller('LogsController', function LogsController($scope, LogsService) {
+    $scope.findAll = function () {
+        LogsService.findAll().
+            success(function (loggers) {
+                $scope.loggers = loggers;
+            })
+            .error(function (resp) {
+                console.log("Error with LogsService.findAll" + resp);
+            });
+    }
+
+    $scope.changeLevel = function(loggerName, newLevel) {
+        LogsService.changeLevel(loggerName, newLevel).
+            success(function(loggers) {
+                console.log("Success changing Level for " + loggerName + " to " + newLevel);
+                $scope.findAll();
+            }).
+            error(function(resp) {
+                console.log("Error with LogsService.changeLevel : " + resp)
+            });
+    }
+
+    $scope.findAll();
+});
