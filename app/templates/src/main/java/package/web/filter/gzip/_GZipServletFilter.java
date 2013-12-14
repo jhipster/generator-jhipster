88038652b395f3f78e415f4f442f30088ac5c08b
Jérôme Mirc <jerome.mirc@gmail.com>
2013-12-14 07:13:33 -0500

    generator-jhipster-53 http-auth-interceptor does not work correctly

---
+++ app/templates/src/main/java/package/web/filter/gzip/_GZipServletFilter.java
@@ -0,0 +1,39 @@
+package <%=packageName%>.web.filter.gzip;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GZipServletFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest  httpRequest  = (HttpServletRequest)  request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if ( acceptsGZipEncoding(httpRequest) ) {
+            httpResponse.addHeader("Content-Encoding", "gzip");
+            GZipServletResponseWrapper gzipResponse =
+                    new GZipServletResponseWrapper(httpResponse);
+            chain.doFilter(request, gzipResponse);
+            gzipResponse.close();
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    private boolean acceptsGZipEncoding(HttpServletRequest httpRequest) {
+        String acceptEncoding = httpRequest.getHeader("Accept-Encoding");
+        return acceptEncoding != null && acceptEncoding.contains("gzip");
+    }
+}
