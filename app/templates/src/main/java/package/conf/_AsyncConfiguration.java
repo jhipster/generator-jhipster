e94e2589d4febc5fd2312cfc023641fd25f3aa18
Jérôme Mirc <jerome.mirc@gmail.com>
2013-12-28 07:54:27 -0500

    generator-jhipster-80 Migrate to Spring Boot Merge issue

--- app/templates/src/main/java/package/conf/_AsyncConfiguration.java
+++ app/templates/src/main/java/package/conf/_AsyncConfiguration.java
@@ -27,13 +27,6 @@ public class AsyncConfiguration implements AsyncConfigurer, EnvironmentAware  {
         this.env = new RelaxedPropertyResolver(environment, "async.");
     }
 
-    private RelaxedPropertyResolver env;
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.env = new RelaxedPropertyResolver(environment, "async.");
-    }
-
     @Override
     public Executor getAsyncExecutor() {
         log.debug("Creating Async Task Executor");
