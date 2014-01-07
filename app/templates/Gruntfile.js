7c2fedafdfb293b3b3fdbcc301eab0fb39e6d558
Julien Dubois <jdubois@ippon.fr>
2014-01-07 14:48:32 +0100

    Allow remote grunt server access

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -70,8 +70,8 @@ module.exports = function (grunt) {
       ],
       options: {
         port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        // Change this to 'localhost' to deny access to the server from outside.
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
