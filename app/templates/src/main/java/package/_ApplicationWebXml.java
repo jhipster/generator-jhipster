1c9deee7689d3ae2ddf84c165360053e8aef0086
Julien Dubois <jdubois@ippon.fr>
2014-01-27 21:53:35 +0100

    Corrected application builder to use the « dev » profiles in WAR files by default

--- app/templates/src/main/java/package/_ApplicationWebXml.java
+++ app/templates/src/main/java/package/_ApplicationWebXml.java
@@ -10,6 +10,6 @@ public class ApplicationWebXml extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.main(Application.class);
     }
 }
