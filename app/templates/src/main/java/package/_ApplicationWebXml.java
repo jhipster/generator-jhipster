11aff1db66cb154e6f70f7ff1e100b2d630f02e2
Jérôme Mirc <jerome.mirc@gmail.com>
2014-01-27 22:43:37 -0500

    Fix the issue to not be able to run the war in tomcat. Still have an issue. Open a ticket on spring-boot (https://github.com/spring-projects/spring-boot/issues/278)

--- app/templates/src/main/java/package/_ApplicationWebXml.java
+++ app/templates/src/main/java/package/_ApplicationWebXml.java
@@ -1,5 +1,8 @@
 package <%=packageName%>;
 
+import <%=packageName%>.config.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.SpringBootServletInitializer;
 
@@ -8,9 +11,27 @@ import org.springframework.boot.web.SpringBootServletInitializer;
  */
 public class ApplicationWebXml extends SpringBootServletInitializer {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        application.sources(Application.class);
-        return application.main(Application.class);
+        application.profiles(addDefaultProfile())
+                .sources(Application.class);
+    }
+
+    /**
+     * Set a default profile if it has not been set
+     *
+     * Please use -Dspring.active.profile=dev
+     */
+    private String addDefaultProfile() {
+        String profile = System.getProperty("spring.active.profile");
+        if (profile != null) {
+            log.debug("Running with Spring profile(s) : {}", profile);
+            return profile;
+        }
+
+        log.debug("No Spring profile configured, running with default configuration");
+        return Constants.SPRING_PROFILE_DEVELOPMENT;
     }
 }
