f2d455921a4a92ae21cbb00788234c1444f55bd1
Julien Dubois <jdubois@ippon.fr>
2014-01-28 21:55:30 +0100

    Remove the Atmosphere call to Google Analytics (see #110 )

--- app/templates/src/main/java/package/config/_WebConfigurer.java
+++ app/templates/src/main/java/package/config/_WebConfigurer.java
@@ -12,6 +12,7 @@ import <%=packageName%>.web.filter.StaticResourcesProductionFilter;
 import <%=packageName%>.web.filter.gzip.GZipServletFilter;
 import <%=packageName%>.web.servlet.HealthCheckServlet;
 import org.atmosphere.cache.UUIDBroadcasterCache;
+import org.atmosphere.cpr.AtmosphereFramework;
 import org.atmosphere.cpr.AtmosphereServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,9 +21,11 @@ import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.util.ReflectionUtils;
 
 import javax.inject.Inject;
 import javax.servlet.*;
+import java.lang.reflect.Field;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;<% if (clusteredHttpSession == 'hazelcast') { %>
@@ -218,8 +221,12 @@ public class WebConfigurer implements ServletContextInitializer {
      */
     private void initAtmosphereServlet(ServletContext servletContext) {
         log.debug("Registering Atmosphere Servlet");
+        AtmosphereServlet servlet = new AtmosphereServlet();
+        Field frameworkField = ReflectionUtils.findField(AtmosphereServlet.class, "framework");
+        ReflectionUtils.makeAccessible(frameworkField);
+        ReflectionUtils.setField(frameworkField, servlet, new NoAnalyticsAtmosphereFramework());
         ServletRegistration.Dynamic atmosphereServlet =
-                servletContext.addServlet("atmosphereServlet", new AtmosphereServlet());
+                servletContext.addServlet("atmosphereServlet", servlet);
 
         atmosphereServlet.setInitParameter("org.atmosphere.cpr.packages", "com.mycompany.myapp.web.websocket");
         atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcasterCacheClass", UUIDBroadcasterCache.class.getName());
@@ -230,5 +237,23 @@ public class WebConfigurer implements ServletContextInitializer {
         atmosphereServlet.addMapping("/websocket/*");
         atmosphereServlet.setLoadOnStartup(3);
         atmosphereServlet.setAsyncSupported(true);
+    }
+
+    /**
+     * Atmosphere sends tracking data to Google Analytics, which is a potential security issue.
+     * <p>
+     * If you want to send this data, please use directly the AtmosphereFramework class.
+     * </p>
+     */
+    public class NoAnalyticsAtmosphereFramework extends AtmosphereFramework {
+
+        public NoAnalyticsAtmosphereFramework() {
+            super();
+        }
+
+        @Override
+        protected void analytics() {
+            // noop
+        }
     }
 }
