41c0d4d67f7a87cf248ef6f738a9b26a8822f9b4
mkh1973 <m.kofler.haeusler@me.com>
2014-02-09 21:34:18 +0100

    Fix bug regarding setting the smtp host.

--- app/templates/src/main/java/package/config/_MailConfiguration.java
+++ app/templates/src/main/java/package/config/_MailConfiguration.java
@@ -14,6 +14,20 @@ import java.util.Properties;
 @Configuration
 public class MailConfiguration implements EnvironmentAware {
 
+  private static final String ENV_SPRING_MAIL = "spring.mail.";
+  private static final String DEFAULT_HOST = "127.0.0.1";
+  private static final String PROP_HOST = "host";
+  private static final String DEFAULT_PROP_HOST = "localhost";
+  private static final String PROP_PORT = "port";
+  private static final String PROP_USER = "user";
+  private static final String PROP_PASSWORD = "password";
+  private static final String PROP_PROTO = "protocol";
+  private static final String PROP_TLS = "tls";
+  private static final String PROP_AUTH = "auth";
+  private static final String PROP_SMTP_AUTH = "mail.smtp.auth";
+  private static final String PROP_STARTTLS = "mail.smtp.starttls.enable";
+  private static final String PROP_TRANSPORT_PROTO = "mail.transport.protocol";
+
   private final Logger log = LoggerFactory.getLogger(MailConfiguration.class);
 
   private RelaxedPropertyResolver env;
@@ -29,28 +43,30 @@ public class MailConfiguration implements EnvironmentAware {
   @Bean
   public JavaMailSenderImpl javaMailSender() {
     log.debug("Configuring mail server");
-    String host = env.getProperty("host", "localhost");
-    int port = env.getProperty("port", Integer.class, 0);
-    String user = env.getProperty("user");
-    String password = env.getProperty("password");
-    String protocol = env.getProperty("protocol");
-    Boolean tls = env.getProperty("tls", Boolean.class, false);
+    String host = env.getProperty(PROP_HOST, DEFAULT_PROP_HOST);
+    int port = env.getProperty(PROP_PORT, Integer.class, 0);
+    String user = env.getProperty(PROP_USER);
+    String password = env.getProperty(PROP_PASSWORD);
+    String protocol = env.getProperty(PROP_PROTO);
+    Boolean tls = env.getProperty(PROP_TLS, Boolean.class, false);
+    Boolean auth = env.getProperty(PROP_AUTH, Boolean.class, false);
 
     JavaMailSenderImpl sender = new JavaMailSenderImpl();
-    if (host != null && !host.equals("")) {
-      log.warn("Warning! Your SMTP server is not configured. We will try to use one on localhost.");
-      log.debug("Did you configure your SMTP settings in your application.yml?");
-      sender.setHost("127.0.0.1");
-    } else {
-      sender.setHost(host);
+    if (host != null && !host.isEmpty()) {
+        sender.setHost(host);
+      } else {
+        log.warn("Warning! Your SMTP server is not configured. We will try to use one on localhost.");
+        log.debug("Did you configure your SMTP settings in your application.yml?");
+        sender.setHost(DEFAULT_HOST);
     }
     sender.setPort(port);
     sender.setUsername(user);
     sender.setPassword(password);
 
     Properties sendProperties = new Properties();
-    sendProperties.setProperty("smtp.starttls.enable", tls ? "true" : "false");
-    sendProperties.setProperty("mail.transport.protocol", protocol);
+    sendProperties.setProperty(PROP_SMTP_AUTH, auth.toString());
+    sendProperties.setProperty(PROP_STARTTLS, tls.toString());
+    sendProperties.setProperty(PROP_TRANSPORT_PROTO, protocol);
     sender.setJavaMailProperties(sendProperties);
     return sender;
   }
