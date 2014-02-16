75a583eb9b5f100d58d7edfc147892c310b84dfd
Jérôme Mirc <jerome.mirc@gmail.com>
2014-02-16 11:30:55 -0500

    Remove spaces

--- app/templates/src/main/webapp/scripts/_services.js
+++ app/templates/src/main/webapp/scripts/_services.js
@@ -58,14 +58,12 @@
                 var promise = $http.get('app/rest/audits/all').then(function (response) {
                     return response.data;
                 });
-
                 return promise;
             },
             findByDates: function(fromDate, toDate) {
                 var promise = $http.get('app/rest/audits/byDates', {params: {fromDate: fromDate, toDate: toDate}}).then(function (response) {
                     return response.data;
                 });
-
                 return promise;
             }
         }
