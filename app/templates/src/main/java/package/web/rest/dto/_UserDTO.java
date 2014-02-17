ec953c0a280407799c675e533ef83e8665aebb52
Julien Dubois <jdubois@ippon.fr>
2014-02-17 23:17:49 +0100

    formatting + added toString

--- app/templates/src/main/java/package/web/rest/dto/_UserDTO.java
+++ app/templates/src/main/java/package/web/rest/dto/_UserDTO.java
@@ -4,15 +4,17 @@ import java.util.Map;
 
 public class UserDTO {
 
-  private String login;
-  private String firstName;
-  private String lastName;
-  private String email;
-  private Map<String, Boolean> roles;
+    private final String login;
+    
+    private final String firstName;
+    
+    private final String lastName;
+    
+    private final String email;
 
-  public UserDTO() {}
+    private final Map<String, Boolean> roles;
 
-  public UserDTO(String login, String firstName, String lastName, String email, Map<String, Boolean> roles) {
+    public UserDTO(String login, String firstName, String lastName, String email, Map<String, Boolean> roles) {
         this.login = login;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -24,39 +26,29 @@ public class UserDTO {
         return login;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Map<String, Boolean> getRoles() {
         return roles;
     }
 
-    public void setRoles(Map<String, Boolean> roles) {
-        this.roles = roles;
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "email='" + email + '\'' +
+                ", login='" + login + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
