147192a30090a51529256fe538c5e157e3233a5a
G.J.R. Timmer <gjr.timmer@gmail.com>
2014-03-12 14:03:42 +0100

    Fix CacheManager NullPointer Exception
    CacheManager throws NullPointer exception when adding a new entity to
    the application.
    
    Correlation with MySQL / JPA MetaModel

--- app/templates/src/main/java/package/config/_CacheConfiguration.java
+++ app/templates/src/main/java/package/config/_CacheConfiguration.java
@@ -30,7 +30,7 @@ import java.util.SortedSet;
 
 @Configuration
 @EnableCaching
-@AutoConfigureAfter(MetricsConfiguration.class)
+@AutoConfigureAfter(value = {MetricsConfiguration.class, DatabaseConfiguration.class})
 public class CacheConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);<% if (hibernateCache == 'hazelcast' || clusteredHttpSession == 'hazelcast') { %>  
@@ -69,7 +69,13 @@ public class CacheConfiguration {
         log.debug("Registring Ehcache Metrics gauges");
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
         for (EntityType<?> entity : entities) {
-            String name = entity.getJavaType().getName();
+            
+            String name = entity.getName();
+            if ( name == null ) {
+                name = entity.getJavaType().getName();
+            }
+            Assert.notNull(name, "entity cannot exist without a identifier");
+            
             net.sf.ehcache.Cache cache = cacheManager.getCache(name);
             if (cache != null) {
                 cache.getCacheConfiguration().setTimeToLiveSeconds(env.getProperty("cache.timeToLiveSeconds", Integer.class, 3600));
