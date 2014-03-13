f3d3065d92d5d77ac4dc198deca6fcb4e81eaa09
Jérôme Mirc <jerome.mirc@gmail.com>
2014-03-13 07:09:33 -0400

    Update _UserResource.java

--- app/templates/src/main/java/package/web/rest/_UserResource.java
+++ app/templates/src/main/java/package/web/rest/_UserResource.java
@@ -3,6 +3,7 @@ package <%=packageName%>.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import <%=packageName%>.domain.User;
 import <%=packageName%>.repository.UserRepository;
+import <%=packageName%>.security.AuthoritiesConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
