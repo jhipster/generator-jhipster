a412341acbfaa2fcd7c0d95911d7ca0d84fe9fb3
Julien Dubois <jdubois@auchan.net>
2014-03-28 17:42:53 +0100

    Removed PNG files from imagemin

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -221,7 +221,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: 'src/main/webapp/images',
-          src: '{,*/}*.{png,jpg,jpeg}',
+          src: '{,*/}*.{jpg,jpeg}', // we don't optimize PNG files as it doesn't work on Linux. If you are not on Linux, feel free to use '{,*/}*.{png,jpg,jpeg}'
           dest: '<%%= yeoman.dist %>/images'
         }]
       }
