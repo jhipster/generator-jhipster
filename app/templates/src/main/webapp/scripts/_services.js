3b0e957063196c024b1c6434e47490aa765538ac
Chris Bonham <bonhamcm@thirdeyeconsulting.com>
2014-04-18 15:17:49 -0400

    Authentication error not setting attribute

--- app/templates/src/main/webapp/scripts/_services.js
+++ app/templates/src/main/webapp/scripts/_services.js
@@ -121,6 +121,7 @@
                         authService.loginConfirmed(data);
                     });
                 }).error(function (data, status, headers, config) {
+                    $rootScope.authenticationError = data.message;
                     Session.destroy();
                 });
             },
