269b6fdce0fd76b2899e4b1e41d6fe3fa8327dd3
Lukas Peleska <git@luaks.net>
2014-04-26 14:08:45 +0200

    Added a the possibility to create jhipster with gulp. Created a basic gulpfile with usemin and sass.

---
+++ app/templates/gulpfile.js
@@ -0,0 +1,103 @@
+var gulp = require('gulp'),
+  prefix = require('gulp-autoprefixer'),
+  minifyCss = require('gulp-minify-css'),
+  usemin = require('gulp-usemin'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-ruby-sass'),
+  minifyHtml = require('gulp-minify-html'),
+  jshint = require('gulp-jshint');
+  rev = require('gulp-rev'),
+  connect = require('gulp-connect'),
+  proxy = require('proxy-middleware');
+
+var yeoman = {
+    app: 'src/main/webapp/',//require('./bower.json').appPath || 'app',
+    dist: 'src/main/webapp/dist',
+    tmp: '.tmp/'<% if(useCompass) { %>,
+    scss: 'src/main/scss/'<% } %>
+}
+
+gulp.task('clean', function(){})
+gulp.task('test', function(){})
+
+<% if(useCompass) { %>
+gulp.task('sass', function() {
+	gulp.src(yeoman.scss + '{,*/}*.scss').
+	  pipe(sass({loadPath: [yeoman.app + 'bower_components']})).
+	  pipe(gulp.dest(yeoman.tmp + 'styles'));
+});
+<% } %>
+
+gulp.task('styles',<% if(useCompass) { %> ['sass'],<% } %> function() {
+    return gulp.src(yeoman.app + '{,*/}*.css').
+	pipe(gulp.dest(yeoman.tmp));
+});
+
+gulp.task('connect', ['styles'], function() {
+  connect.server(
+    {
+      root: [app, tmp],
+      livereload: true,
+      middleware: function(connect, o) {
+	return [
+	  (function() {
+	    var url = require('url');
+	    var proxy = require('proxy-middleware');
+	    var options = url.parse('http://localhost:8080/app');
+	    options.route = '/app';
+	    return proxy(options);
+	  })(),
+	  (function() {
+	    var url = require('url');
+	    var proxy = require('proxy-middleware');
+	    var options = url.parse('http://localhost:8080/metrics');
+	    options.route = '/metrics';
+	    return proxy(options);
+	  })(),
+	  (function() {
+	    var url = require('url');
+	    var proxy = require('proxy-middleware');
+	    var options = url.parse('http://localhost:8080/dump');
+	    options.route = '/dump';
+	    return proxy(options);
+	  })()<% if (devDatabaseType == 'h2Memory') { %>,
+	  (function() {
+	    var url = require('url');
+	    var proxy = require('proxy-middleware');
+	    var options = url.parse('http://localhost:8080/console');
+	    options.route = '/console';
+	    return proxy(options);
+	  })()<% } %>
+	];
+      }
+    }
+  );
+});
+
+gulp.task('build', ['clean', 'usemin']);
+
+gulp.task('usemin', ['styles'], function(){
+    gulp.src(yeoman.app + '{,*/}*.html')
+        .pipe(usemin({
+            css: [
+                prefix.apply(),
+                minifyCss(),
+                rev(),
+                'concat'
+            ],
+            html: [
+                minifyHtml({empty: true, conditionals:true})
+            ],
+            js: [
+                uglify(),
+                rev(),
+                'concat'
+            ]
+        }))
+        .pipe(gulp.dest(yeoman.dist));
+});
+
+gulp.task('default', function() {
+    gulp.run('test');
+    gulp.run('build');
+});
