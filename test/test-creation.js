431fea849684e366d8d025825ba0554bf4e53d50
Jan Marten <jan.marten@gmx.de>
2014-05-11 14:52:57 +0200

    Added hazelcast test

--- test/test-creation.js
+++ test/test-creation.js
@@ -160,4 +160,46 @@ describe('jhipster generator', function () {
       done();
     });
   });
+
+  it('creates expected files with hibernateCache "hazelcast"', function (done) {
+
+    var expectedAdditionalFiles = [
+      resourceDir + 'config/liquibase/changelog/db-changelog-001.xml',
+      resourceDir + 'config/liquibase/master.xml',
+      resourceDir + 'config/liquibase/users.csv',
+      resourceDir + 'config/liquibase/authorities.csv',
+      resourceDir + 'config/liquibase/users_authorities.csv',
+      webappDir + 'images/glyphicons-halflings.png',
+      webappDir + 'images/glyphicons-halflings-white.png',
+      webappDir + 'styles/bootstrap.css',
+      webappDir + 'styles/main.css',
+      webappDir + 'fonts/glyphicons-halflings-regular.eot',
+      webappDir + 'fonts/glyphicons-halflings-regular.svg',
+      webappDir + 'fonts/glyphicons-halflings-regular.ttf',
+      webappDir + 'fonts/glyphicons-halflings-regular.woff',
+      javaPackageDir + 'config/hazelcast/HazelcastCacheRegionFactory.java',
+      javaPackageDir + 'config/hazelcast/package-info.java'
+    ];
+
+    var expected = defaultFiles.concat(expectedAdditionalFiles);
+
+    helpers.mockPrompt(this.app, {
+      'baseName': 'jhipster',
+      'packageName': 'com.mycompany.myapp',
+      'javaVersion': '7',
+      'authenticationType': 'token',
+      'databaseType': 'sql',
+      'hibernateCache': 'hazelcast',
+      'clusteredHttpSession': 'no',
+      'websocket': 'no',
+      'prodDatabaseType': 'mysql',
+      'devDatabaseType': 'h2Memory',
+      'useCompass': false
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
 });
