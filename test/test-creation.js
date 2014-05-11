861d0d7573a9ff167e8e0e2c0d8e6de70c7c0212
Jan Marten <jan.marten@gmx.de>
2014-05-11 10:13:37 +0200

    Added test for default configuration

--- test/test-creation.js
+++ test/test-creation.js
@@ -20,14 +20,48 @@ describe('jhipster generator', function () {
   });
 
   it('creates expected files', function (done) {
+
+    var resourceDir = 'src/main/resources/';
+    var webappDir = 'src/main/webapp/'
+
     var expected = [
       // add files you expect to exist here.
       '.jshintrc',
-      '.editorconfig'
+      '.editorconfig',
+      'bower.json',
+      'package.json',
+      'pom.xml',
+      '.bowerrc',
+      '.gitignore',
+      'README.md',
+      '.yo-rc.json',
+      resourceDir + 'config/liquibase/changelog/db-changelog-001.xml',
+      resourceDir + 'config/liquibase/master.xml',
+      resourceDir + 'config/liquibase/users.csv',
+      resourceDir + 'config/liquibase/authorities.csv',
+      resourceDir + 'config/liquibase/users_authorities.csv',
+      webappDir + 'images/glyphicons-halflings.png', 
+      webappDir + 'images/glyphicons-halflings-white.png', 
+      webappDir + 'styles/bootstrap.css', 
+      webappDir + 'styles/main.css', 
+      webappDir + 'fonts/glyphicons-halflings-regular.eot',
+      webappDir + 'fonts/glyphicons-halflings-regular.svg',
+      webappDir + 'fonts/glyphicons-halflings-regular.ttf',
+      webappDir + 'fonts/glyphicons-halflings-regular.woff'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'baseName': 'jhipster',
+      'packageName': 'com.mycompany.myapp',
+      'javaVersion': '7',
+      'authenticationType': 'cookie',
+      'databaseType': 'sql',
+      'hibernateCache': 'no',
+      'clusteredHttpSession': 'no',
+      'websocket': 'no',
+      'prodDatabaseType': 'mysql',
+      'devDatabaseType': 'h2Memory',
+      'useCompass': false
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
