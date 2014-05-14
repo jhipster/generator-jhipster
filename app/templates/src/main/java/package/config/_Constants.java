611f4413b4308ebf17179975782e16f50256c9ee
Michael Kofler-Häusler <m.kofler.haeusler@me.com>
2014-05-14 11:57:48 +0200

    Update _Constants.java

--- app/templates/src/main/java/package/config/_Constants.java
+++ app/templates/src/main/java/package/config/_Constants.java
@@ -3,13 +3,10 @@ package <%=packageName%>.config;
 /**
  * Application constants.
  */
-public final class Constants {
+public Interface Constants {
 
     public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
     public static final String SYSTEM_ACCOUNT = "system";
-
-    private Constants() {
-    }
 
 }
