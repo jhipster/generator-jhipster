d719aaa6a413abeba24ee1cb15e0a855543b36de
Jérôme Mirc <jerome.mirc@gmail.com>
2014-08-18 10:39:29 -0400

    bug fix #491

--- app/templates/src/main/java/package/domain/_PersistentAuditEvent.java
+++ app/templates/src/main/java/package/domain/_PersistentAuditEvent.java
@@ -27,7 +27,7 @@ public class PersistentAuditEvent  {
     @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "event_id")<% } %><% if (databaseType == 'nosql') { %>
     @Field("event_id")<% } %>
-    private long id;
+    private Long id;
 
     @NotNull
     private String principal;
@@ -46,11 +46,11 @@ public class PersistentAuditEvent  {
     @CollectionTable(name="T_PERSISTENT_AUDIT_EVENT_DATA", joinColumns=@JoinColumn(name="event_id"))<% } %>
     private Map<String, String> data = new HashMap<>();
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
