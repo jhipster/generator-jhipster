5575c02e7d5cd58e3c6ef110485dd97b0148d964
Julien Dubois <julien.dubois@gmail.com>
2014-08-30 12:27:16 +0200

    Fix #479

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -24,16 +24,16 @@ module.exports = function (grunt) {
         files: ['src/main/scss/{,*/}*.{scss,sass}'],
         tasks: ['compass:server', 'autoprefixer']
       },<% } %>
-    styles: {
+      styles: {
         files: ['src/main/webapp/styles/{,*/}*.css'],
         tasks: ['copy:styles', 'autoprefixer']
       },
-    livereload: {
+      livereload: {
         options: {
           livereload: 35729
         },
         files: [
-          'src/main/webapp/{,*/}*.html',
+          'src/main/webapp/**/*.html',
           '.tmp/styles/{,*/}*.css',
           '{.tmp/,}src/main/webapp/scripts/{,*/}*.js',
           'src/main/webapp/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
