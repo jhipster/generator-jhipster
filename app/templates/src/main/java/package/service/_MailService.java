686e0dc4ca850c0a950497472ff8003b53bb323b
Jan Marten <jan.marten@gmx.de>
2014-09-02 18:27:04 +0200

    Fix indentation and remove trailing whitespaces

--- app/templates/src/main/java/package/service/_MailService.java
+++ app/templates/src/main/java/package/service/_MailService.java
@@ -50,7 +50,7 @@ public class MailService {
     public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
         log.debug("Send e-mail[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
                 isMultipart, isHtml, to, subject, content);
-        
+
         // Prepare message using a Spring helper
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
