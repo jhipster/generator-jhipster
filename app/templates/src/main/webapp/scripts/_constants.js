48b6fead31705c1171eb604020bd981b9c117770
Jan Marten <jan.marten@gmx.de>
2014-09-02 18:26:39 +0200

    Fix indentation and remove trailing whitespaces

--- app/templates/src/main/webapp/scripts/_constants.js
+++ app/templates/src/main/webapp/scripts/_constants.js
@@ -8,8 +8,8 @@
         user: 'ROLE_USER'
     });
 
-/* 
-Languages codes are ISO_639-1 codes, see http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes 
+/*
+Languages codes are ISO_639-1 codes, see http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 They are written in English to avoid character encoding issues (not a perfect solution)
 */
 <%= angularAppName %>.constant('LANGUAGES', {
