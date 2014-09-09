bf111fe5b962a2db7cbce5ee6789e9e7e0653d7d
Julien Dubois <julien.dubois@gmail.com>
2014-09-09 18:12:03 +0200

    Formatting

--- app/templates/src/main/java/package/config/_OAuth2ServerConfiguration.java
+++ app/templates/src/main/java/package/config/_OAuth2ServerConfiguration.java
@@ -41,44 +41,43 @@ public class OAuth2ServerConfiguration {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             http
-                    .exceptionHandling()
-                    .authenticationEntryPoint(authenticationEntryPoint)
-                    .and()
-                    .logout()
-                    .logoutUrl("/app/logout")
-                    .logoutSuccessHandler(ajaxLogoutSuccessHandler)
-                    .and()
-                    .csrf()
-                    .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
-                    .disable()
-                    .headers()
-                    .frameOptions().disable()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/views/**").permitAll()
-                    .antMatchers("/app/rest/authenticate").permitAll()
-                    .antMatchers("/app/rest/register").permitAll()
-                    .antMatchers("/app/rest/logs/**").hasAnyAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/app/**").authenticated()
-                    .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/websocket/**").permitAll()
-                    .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/health/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/dump/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/shutdown/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/beans/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/info/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/autoconfig/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/env/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/api-docs/**").hasAuthority(AuthoritiesConstants.ADMIN)
-                    .antMatchers("/protected/**").authenticated();
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .logout()
+                .logoutUrl("/app/logout")
+                .logoutSuccessHandler(ajaxLogoutSuccessHandler)
+                .and()
+                .csrf()
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
+                .disable()
+                .headers()
+                .frameOptions().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/views/**").permitAll()
+                .antMatchers("/app/rest/authenticate").permitAll()
+                .antMatchers("/app/rest/register").permitAll()
+                .antMatchers("/app/rest/logs/**").hasAnyAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/app/**").authenticated()
+                .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/websocket/**").permitAll()
+                .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/health/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/dump/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/shutdown/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/beans/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/info/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/autoconfig/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/env/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/api-docs/**").hasAuthority(AuthoritiesConstants.ADMIN)
+                .antMatchers("/protected/**").authenticated();
 
         }
-
     }
 
     @Configuration
@@ -97,7 +96,7 @@ public class OAuth2ServerConfiguration {
 
         @Bean
         public TokenStore tokenStore() {
-            <% if (databaseType == 'sql') { %>return new JdbcTokenStore(dataSource);<% } %><% if (databaseType == 'nosql') { %>return new InMemoryTokenStore();<% } %>
+            <% if (databaseType == 'sql') { %>return new JdbcTokenStore(dataSource);<% } else { %>return new InMemoryTokenStore();<% } %>
         }
 
         @Inject
@@ -106,6 +105,7 @@ public class OAuth2ServerConfiguration {
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints)
                 throws Exception {
+
             endpoints
                     .tokenStore(tokenStore())
                     .authenticationManager(authenticationManager);
@@ -114,13 +114,13 @@ public class OAuth2ServerConfiguration {
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients
-                    .inMemory()
-                    .withClient(propertyResolver.getProperty(PROP_CLIENTID))
-                    .scopes("read", "write")
-                    .authorities(AuthoritiesConstants.ADMIN, AuthoritiesConstants.USER)
-                    .authorizedGrantTypes("password")
-                    .secret(propertyResolver.getProperty(PROP_SECRET))
-                    .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
+                .inMemory()
+                .withClient(propertyResolver.getProperty(PROP_CLIENTID))
+                .scopes("read", "write")
+                .authorities(AuthoritiesConstants.ADMIN, AuthoritiesConstants.USER)
+                .authorizedGrantTypes("password")
+                .secret(propertyResolver.getProperty(PROP_SECRET))
+                .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
         }
 
         @Override
