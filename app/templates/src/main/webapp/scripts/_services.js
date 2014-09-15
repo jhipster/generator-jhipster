51cc16efb922803054989a853e3c52a0afc0720d
Julien Dubois <julien.dubois@gmail.com>
2014-09-15 18:29:37 +0200

    Corrected language issue mentioned in #573

--- app/templates/src/main/webapp/scripts/_services.js
+++ app/templates/src/main/webapp/scripts/_services.js
@@ -8,6 +8,9 @@
                 if (language == undefined) {
                     language = $translate.storage().get('NG_TRANSLATE_LANG_KEY');
                 }
+                if (language == undefined) {
+                    language = 'en';
+                }
 
                 var promise =  $http.get('/i18n/' + language + '.json').then(function(response) {
                     return LANGUAGES;
