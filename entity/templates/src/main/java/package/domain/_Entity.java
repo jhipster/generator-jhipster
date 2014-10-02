3bc58c5f3a1bbfe6f710b4fce24f9fe742d8882f
Julien Dubois <julien.dubois@gmail.com>
2014-10-02 18:34:29 +0200

    Corrected the equals/hascode of generated entities
    Check the ID value and not just the reference
    
    Fix #616

--- entity/templates/src/main/java/package/domain/_Entity.java
+++ entity/templates/src/main/java/package/domain/_Entity.java
@@ -80,9 +80,7 @@ public class <%= entityClass %> implements Serializable {
 
         <%= entityClass %> <%= entityInstance %> = (<%= entityClass %>) o;
 
-        if <% if (databaseType == 'sql') { %>(id != <%= entityInstance %>.id)<% } %><% if (databaseType == 'nosql') { %>(!id.equals(<%= entityInstance %>.id))<% } %> {
-            return false;
-        }
+        if (id != null ? !id.equals(<%= entityInstance %>.id) : <%= entityInstance %>.id != null) return false;
 
         return true;
     }
