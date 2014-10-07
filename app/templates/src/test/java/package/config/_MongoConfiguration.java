ad90b4abc15c7a39f69443634f2beac31851cd4f
Romain Fromi <Romain.Fromi@nespresso.com>
2014-10-07 16:57:27 +0200

    Using embedmongo-maven-plugin to download and start MongoDB during pre-integration-test phase

--- app/templates/src/test/java/package/config/_MongoConfiguration.java
+++ app/templates/src/test/java/package/config/_MongoConfiguration.java
@@ -1,12 +1,12 @@
 package <%=packageName%>.config;
 
 import com.mongodb.Mongo;
-import cz.jirutka.spring.embedmongo.EmbeddedMongoBuilder;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 
 @Configuration
 public class MongoConfiguration {
@@ -14,21 +14,6 @@ public class MongoConfiguration {
     @Bean
     public Mongo mongo() throws IOException {
         System.setProperty("DB.TRACE","true");
-        return new EmbeddedMongoBuilder()
-                .version("2.6.0")
-                .bindIp("127.0.0.1")
-                .port(allocateRandomPort())
-                .build();
-    }
-
-    public static int allocateRandomPort() {
-        try {
-            ServerSocket server = new ServerSocket(0);
-            int port = server.getLocalPort();
-            server.close();
-            return port;
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to acquire a random free port", e);
-        }
+        return MongodForTestsFactory.with(Version.Main.PRODUCTION).newMongo();
     }
 }
