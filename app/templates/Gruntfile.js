dfd3a0c2c8d769e2aa5527688851ad9cd8edba85
Jérôme Mirc <jerome.mirc@gmail.com>
2014-11-02 05:15:33 -0500

    bug fix #721

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -69,18 +69,25 @@ module.exports = function (grunt) {
                     changeOrigin: false
                 },
                 {
-                context: '/dump',
-                host: 'localhost',
-                port: 8080,
-                https: false,
-                changeOrigin: false
+                    context: '/dump',
+                    host: 'localhost',
+                    port: 8080,
+                    https: false,
+                    changeOrigin: false
                 },
                 {
-                context: '/api-docs',
-                host: 'localhost',
-                port: 8080,
-                https: false,
-                changeOrigin: false
+                    context: '/health',
+                    host: 'localhost',
+                    port: 8080,
+                    https: false,
+                    changeOrigin: false
+                },
+                {
+                    context: '/api-docs',
+                    host: 'localhost',
+                    port: 8080,
+                    https: false,
+                    changeOrigin: false
                 }<% if (authenticationType == 'token') { %>,
                 {
                     context: '/oauth/token',
