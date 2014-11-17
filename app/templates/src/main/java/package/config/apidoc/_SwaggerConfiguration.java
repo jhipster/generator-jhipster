802a737ea9f1fdc83e299825ce281c26a65aa17e
Julien Dubois <julien.dubois@gmail.com>
2014-11-17 18:13:04 +0100

    Added more logs and documented the Swagger performance issue
    See #762

--- app/templates/src/main/java/package/config/apidoc/_SwaggerConfiguration.java
+++ app/templates/src/main/java/package/config/apidoc/_SwaggerConfiguration.java
@@ -4,16 +4,29 @@ import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
 import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 import com.wordnik.swagger.model.ApiInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 
+/**
+ * Swagger configuration.
+ *
+ * Warning! When having a lot of REST endpoints, Swagger can become a performance issue. In that
+ * case, you can use a specific Spring profile for this class, so that only front-end developers
+ * have access to the Swagger view.
+ */
 @Configuration
 @EnableSwagger
 public class SwaggerConfiguration implements EnvironmentAware {
+
+    private final Logger log = LoggerFactory.getLogger(SwaggerConfiguration.class);
+
     public static final String DEFAULT_INCLUDE_PATTERN = "/app/rest/.*";
 
     private RelaxedPropertyResolver propertyResolver;
@@ -24,18 +37,26 @@ public class SwaggerConfiguration implements EnvironmentAware {
     }
 
     /**
-     * Swagger Spring MVC configuration
+     * Swagger Spring MVC configuration.
      */
     @Bean
     public SwaggerSpringMvcPlugin swaggerSpringMvcPlugin(SpringSwaggerConfig springSwaggerConfig) {
-        return new SwaggerSpringMvcPlugin(springSwaggerConfig)
-                .apiInfo(apiInfo())
-                .genericModelSubstitutes(ResponseEntity.class)
-                .includePatterns(DEFAULT_INCLUDE_PATTERN);
+        log.debug("Starting Swagger");
+        StopWatch watch = new StopWatch();
+        watch.start();
+        SwaggerSpringMvcPlugin swaggerSpringMvcPlugin = new SwaggerSpringMvcPlugin(springSwaggerConfig)
+            .apiInfo(apiInfo())
+            .genericModelSubstitutes(ResponseEntity.class)
+            .includePatterns(DEFAULT_INCLUDE_PATTERN);
+
+        swaggerSpringMvcPlugin.build();
+        watch.stop();
+        log.debug("Started Swagger in {} ms", watch.getTotalTimeMillis());
+        return swaggerSpringMvcPlugin;
     }
 
     /**
-     * API Info as it appears on the swagger-ui page
+     * API Info as it appears on the swagger-ui page.
      */
     private ApiInfo apiInfo() {
         return new ApiInfo(
