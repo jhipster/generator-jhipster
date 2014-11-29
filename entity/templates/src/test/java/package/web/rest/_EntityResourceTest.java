2668b8d91a5f8cb8821ec2df4ab9a10917329680
Julien Dubois <julien.dubois@gmail.com>
2014-11-29 09:26:43 +0100

    Better formatting

--- entity/templates/src/test/java/package/web/rest/_EntityResourceTest.java
+++ entity/templates/src/test/java/package/web/rest/_EntityResourceTest.java
@@ -39,30 +39,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class <%= entityClass %>ResourceTest {<% if (fieldsContainDateTime == true) { %>
-   private static final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss");<% } %>
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss");<% } %>
 <% for (fieldId in fields) {
     var defaultValueName = 'DEFAULT_' + fields[fieldId].fieldNameUnderscored.toUpperCase();
     var updatedValueName = 'UPDATED_' + fields[fieldId].fieldNameUnderscored.toUpperCase();
     if (fields[fieldId].fieldType == 'String') { %>
     private static final String <%=defaultValueName %> = "SAMPLE_TEXT";
-    private static final String <%=updatedValueName %> = "UPDATED_TEXT";
-    <% } else if (fields[fieldId].fieldType == 'Integer') { %>
+    private static final String <%=updatedValueName %> = "UPDATED_TEXT";<% } else if (fields[fieldId].fieldType == 'Integer') { %>
+
     private static final Integer <%=defaultValueName %> = 0;
-    private static final Integer <%=updatedValueName %> = 1;
-    <% } else if (fields[fieldId].fieldType == 'Long') { %>
+    private static final Integer <%=updatedValueName %> = 1;<% } else if (fields[fieldId].fieldType == 'Long') { %>
+
     private static final Long <%=defaultValueName %> = 0L;
-    private static final Long <%=updatedValueName %> = 1L;
-    <% } else if (fields[fieldId].fieldType == 'BigDecimal') { %>
+    private static final Long <%=updatedValueName %> = 1L;<% } else if (fields[fieldId].fieldType == 'BigDecimal') { %>
+
     private static final BigDecimal <%=defaultValueName %> = BigDecimal.ZERO;
-    private static final BigDecimal <%=updatedValueName %> = BigDecimal.ONE;
-    <% } else if (fields[fieldId].fieldType == 'LocalDate') { %>
+    private static final BigDecimal <%=updatedValueName %> = BigDecimal.ONE;<% } else if (fields[fieldId].fieldType == 'LocalDate') { %>
+
     private static final LocalDate <%=defaultValueName %> = new LocalDate(0L);
-    private static final LocalDate <%=updatedValueName %> = new LocalDate();
-    <% } else if (fields[fieldId].fieldType == 'DateTime') { %>
+    private static final LocalDate <%=updatedValueName %> = new LocalDate();<% } else if (fields[fieldId].fieldType == 'DateTime') { %>
+
     private static final DateTime <%=defaultValueName %> = new DateTime(0L);
     private static final DateTime <%=updatedValueName %> = new DateTime().withMillisOfSecond(0);
-    private static final String <%=defaultValueName %>_STR = dateTimeFormatter.print(<%= defaultValueName %>);
-    <% } else if (fields[fieldId].fieldType == 'Boolean') { %>
+    private static final String <%=defaultValueName %>_STR = dateTimeFormatter.print(<%= defaultValueName %>);<% } else if (fields[fieldId].fieldType == 'Boolean') { %>
+
     private static final Boolean <%=defaultValueName %> = false;
     private static final Boolean <%=updatedValueName %> = true;<% } } %>
 
