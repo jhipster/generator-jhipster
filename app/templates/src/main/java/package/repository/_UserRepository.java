d95eb53239dadc86197af946aa6fd3ce20ec8f10
Zsombor Gegesy <gzsombor@gmail.com>
2014-12-12 12:53:10 +0100

    Simplify UserDetailsService, fix UserRepository to use JpaRepository or MongoRepository with the correct ID type. (so no need to declare a separate save method, as it is already provided by Jpa and Mongo Repository)

--- app/templates/src/main/java/package/repository/_UserRepository.java
+++ app/templates/src/main/java/package/repository/_UserRepository.java
@@ -2,10 +2,9 @@ package <%=packageName%>.repository;
 
 import <%=packageName%>.domain.User;
 
-import org.joda.time.DateTime;<% if (databaseType == 'sql') { %><% if (javaVersion == '8') { %>
-import org.springframework.data.repository.Repository;
-import java.util.Optional;<% } else {%>
-import org.springframework.data.jpa.repository.JpaRepository;<%}%>
+import org.joda.time.DateTime;<% if (databaseType == 'sql') { %>
+import org.springframework.data.jpa.repository.JpaRepository;<% if (javaVersion == '8') { %>
+import java.util.Optional;<% } %>
 import org.springframework.data.jpa.repository.Query;<% } %><% if (databaseType == 'mongodb') { %>
 import org.springframework.data.mongodb.repository.MongoRepository;<% } %>
 
@@ -17,7 +16,7 @@ import java.util.Optional;<%}%>
  */<% } %><% if (databaseType == 'mongodb') { %>/**
  * Spring Data MongoDB repository for the User entity.
  */<% } %><% if (javaVersion == '8') { %>
-public interface UserRepository extends <% if (databaseType == 'sql') { %>Repository<% } %><% if (databaseType == 'mongodb') { %>MongoRepository<% } %><User, String> {
+public interface UserRepository extends <% if (databaseType == 'sql') { %>JpaRepository<User, Long><% } %><% if (databaseType == 'mongodb') { %>MongoRepository<User, String><% } %> {
 
     Optional<User> findOneByActivationKey(String activationKey);
 
@@ -26,16 +25,10 @@ public interface UserRepository extends <% if (databaseType == 'sql') { %>Reposi
     Optional<User> findOneByEmail(String email);
 
     Optional<User> findOneByLogin(String login);
-<% if (databaseType == 'sql') { %>
-    Optional<User> save(User t);
-<% } else if (databaseType == 'mongodb') { %>
-    @SuppressWarnings("unchecked")
-    User save(User t);
-<% } %>
     void delete(User t);
 
 }<% } else { %>
-public interface UserRepository extends <% if (databaseType == 'sql') { %>JpaRepository<% } %><% if (databaseType == 'mongodb') { %>MongoRepository<% } %><User, String> {
+public interface UserRepository extends <% if (databaseType == 'sql') { %>JpaRepository<User, Long><% } %><% if (databaseType == 'mongodb') { %>MongoRepository<User, String><% } %> {
 
     User findOneByActivationKey(String activationKey);
 
