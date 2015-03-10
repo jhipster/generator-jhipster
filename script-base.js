1deb384b81ee532e226458a48d3980d41358383a
Julien Dubois <julien.dubois@gmail.com>
2015-03-10 12:01:33 +0100

    Use UTC dates for timestamps
    - for people working in different timezones
    - to be consistent with the Maven timestamps (which are UTC based)

--- script-base.js
+++ script-base.js
@@ -82,12 +82,13 @@ Generator.prototype.addChangelogToLiquibase = function (changelogName) {
 // This generates a date to be used by Liquibase changelogs
 Generator.prototype.dateFormatForLiquibase = function () {
     var now = new Date();
-    var year = "" + now.getFullYear();
-    var month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
-    var day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
-    var hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
-    var minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
-    var second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+    var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+    var year = "" + now_utc.getFullYear();
+    var month = "" + (now_utc.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+    var day = "" + now_utc.getDate(); if (day.length == 1) { day = "0" + day; }
+    var hour = "" + now_utc.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+    var minute = "" + now_utc.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+    var second = "" + now_utc.getSeconds(); if (second.length == 1) { second = "0" + second; }
     return year + "" + month + "" + day + "" + hour + "" + minute + "" + second;
 };
 
