a30c096318db7ad9889a533a0f6e4ea2c92d650b
Frederik Hahne <atomfrede@gmail.com>
2015-04-06 13:17:49 +0200

    Frontend files get generated
    Update #508

---
+++ app/templates/src/main/webapp/scripts/app/account/reset/finish/_reset.finish.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('jhipsterApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('finishReset', {
+                parent: 'account',
+                url: '/reset/finish?key',
+                data: {
+                    roles: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/account/reset/finish/reset.finish.html',
+                        controller: 'ResetFinishController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('reset');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
---
+++ app/templates/src/main/webapp/scripts/app/account/reset/request/_reset.request.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('jhipsterApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('requestReset', {
+                parent: 'account',
+                url: '/reset/request',
+                data: {
+                    roles: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/account/reset/request/reset.request.html',
+                        controller: 'RequestResetController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('reset');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
