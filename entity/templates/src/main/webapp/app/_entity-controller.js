fc3a75480fe0d0839b17d9bf7ed635616b5a8639
saki <saki@chare.eu>
2015-04-06 14:59:27 +0200

    fix global leak (idx)

--- entity/templates/src/main/webapp/app/_entity-controller.js
+++ entity/templates/src/main/webapp/app/_entity-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('<%=angularAppName%>')
-    .controller('<%= entityClass %>Controller', function ($scope<% for (idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %><% if (pagination != 'no') { %>, ParseLinks<% } %>) {
+    .controller('<%= entityClass %>Controller', function ($scope<% for (var idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %><% if (pagination != 'no') { %>, ParseLinks<% } %>) {
         $scope.<%= entityInstance %>s = [];<% for (idx in differentTypes) { if (differentTypes[idx] != entityClass) { %>
         $scope.<%= differentTypes[idx].toLowerCase() %>s = <%= differentTypes[idx] %>.query();<% } } %><% if (pagination == 'pager' || pagination == 'pagination') { %>
         $scope.page = 1;
--- entity/templates/src/main/webapp/app/_entity-detail-controller.js
+++ entity/templates/src/main/webapp/app/_entity-detail-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('<%=angularAppName%>')
-    .controller('<%= entityClass %>DetailController', function ($scope, $stateParams<% for (idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %>) {
+    .controller('<%= entityClass %>DetailController', function ($scope, $stateParams<% for (var idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %>) {
         $scope.<%= entityInstance %> = {};
         $scope.load = function (id) {
             <%= entityClass %>.get({id: id}, function(result) {
