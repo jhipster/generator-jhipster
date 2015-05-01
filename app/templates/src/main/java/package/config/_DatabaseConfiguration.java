c945a18ff70d0351e666476e992601c3c6631825
chris-bailey <chris@chrisbaileydeveloper.com>
2015-05-01 11:36:58 -0400

    Word concatenation fix

--- app/templates/src/main/java/package/config/_DatabaseConfiguration.java
+++ app/templates/src/main/java/package/config/_DatabaseConfiguration.java
@@ -81,7 +81,7 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
         log.debug("Configuring Datasource");
         if (propertyResolver.getProperty("url") == null && propertyResolver.getProperty("databaseName") == null) {
             log.error("Your database connection pool configuration is incorrect! The application" +
-                    "cannot start. Please check your Spring profile, current profiles are: {}",
+                    " cannot start. Please check your Spring profile, current profiles are: {}",
                     Arrays.toString(env.getActiveProfiles()));
 
             throw new ApplicationContextException("Database connection pool is not configured correctly");
