b064da40f9f376226eaa4b92b9dd63403e6a7f56
Thibaut <thibaut@alantaya.com>
2015-05-22 18:04:00 +0200

    support LiquiBase context functionnality
    - Support context functionnality for each profile: test, devlopment and production. More informations: http://www.liquibase.org/documentation/contexts.html.
    - Tested with Java 8, SQL and maven.
    
    close #1507

--- app/templates/src/main/java/package/config/_DatabaseConfiguration.java
+++ app/templates/src/main/java/package/config/_DatabaseConfiguration.java
@@ -55,7 +55,8 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);<% if (databaseType == 'sql') { %>
 
-    private RelaxedPropertyResolver propertyResolver;
+    private RelaxedPropertyResolver dataSourcePropertyResolver;
+    private RelaxedPropertyResolver liquiBasePropertyResolver;
 
     private Environment env;
 
@@ -71,7 +72,8 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
     @Override
     public void setEnvironment(Environment env) {
         this.env = env;
-        this.propertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
+        this.dataSourcePropertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
+        this.liquiBasePropertyResolver = new RelaxedPropertyResolver(env, "liquiBase.");
     }
 
     @Bean(destroyMethod = "shutdown")
@@ -79,7 +81,7 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
     @Profile("!" + Constants.SPRING_PROFILE_CLOUD)
     public DataSource dataSource() {
         log.debug("Configuring Datasource");
-        if (propertyResolver.getProperty("url") == null && propertyResolver.getProperty("databaseName") == null) {
+        if (dataSourcePropertyResolver.getProperty("url") == null && dataSourcePropertyResolver.getProperty("databaseName") == null) {
             log.error("Your database connection pool configuration is incorrect! The application" +
                     " cannot start. Please check your Spring profile, current profiles are: {}",
                     Arrays.toString(env.getActiveProfiles()));
@@ -87,21 +89,21 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
             throw new ApplicationContextException("Database connection pool is not configured correctly");
         }
         HikariConfig config = new HikariConfig();
-        config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
-        if(StringUtils.isEmpty(propertyResolver.getProperty("url"))) {
-            config.addDataSourceProperty("databaseName", propertyResolver.getProperty("databaseName"));
-            config.addDataSourceProperty("serverName", propertyResolver.getProperty("serverName"));
+        config.setDataSourceClassName(dataSourcePropertyResolver.getProperty("dataSourceClassName"));
+        if(StringUtils.isEmpty(dataSourcePropertyResolver.getProperty("url"))) {
+            config.addDataSourceProperty("databaseName", dataSourcePropertyResolver.getProperty("databaseName"));
+            config.addDataSourceProperty("serverName", dataSourcePropertyResolver.getProperty("serverName"));
         } else {
-            config.addDataSourceProperty("url", propertyResolver.getProperty("url"));
+            config.addDataSourceProperty("url", dataSourcePropertyResolver.getProperty("url"));
         }
-        config.addDataSourceProperty("user", propertyResolver.getProperty("username"));
-        config.addDataSourceProperty("password", propertyResolver.getProperty("password"));
+        config.addDataSourceProperty("user", dataSourcePropertyResolver.getProperty("username"));
+        config.addDataSourceProperty("password", dataSourcePropertyResolver.getProperty("password"));
 <% if (prodDatabaseType == 'mysql' || devDatabaseType == 'mysql') { %>
         //MySQL optimizations, see https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
-        if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(propertyResolver.getProperty("dataSourceClassName"))) {
-            config.addDataSourceProperty("cachePrepStmts", propertyResolver.getProperty("cachePrepStmts", "true"));
-            config.addDataSourceProperty("prepStmtCacheSize", propertyResolver.getProperty("prepStmtCacheSize", "250"));
-            config.addDataSourceProperty("prepStmtCacheSqlLimit", propertyResolver.getProperty("prepStmtCacheSqlLimit", "2048"));
+        if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourcePropertyResolver.getProperty("dataSourceClassName"))) {
+            config.addDataSourceProperty("cachePrepStmts", dataSourcePropertyResolver.getProperty("cachePrepStmts", "true"));
+            config.addDataSourceProperty("prepStmtCacheSize", dataSourcePropertyResolver.getProperty("prepStmtCacheSize", "250"));
+            config.addDataSourceProperty("prepStmtCacheSqlLimit", dataSourcePropertyResolver.getProperty("prepStmtCacheSqlLimit", "2048"));
         }<% } %>
         if (metricRegistry != null) {
             config.setMetricRegistry(metricRegistry);
@@ -114,9 +116,9 @@ public class DatabaseConfiguration <% if (databaseType == 'sql') { %>implements 
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
         liquibase.setChangeLog("classpath:config/liquibase/master.xml");
-        liquibase.setContexts("development, production");
+        liquibase.setContexts(liquiBasePropertyResolver.getProperty("context", "development, production"));
         if (env.acceptsProfiles(Constants.SPRING_PROFILE_FAST)) {
-            if ("org.h2.jdbcx.JdbcDataSource".equals(propertyResolver.getProperty("dataSourceClassName"))) {
+            if ("org.h2.jdbcx.JdbcDataSource".equals(dataSourcePropertyResolver.getProperty("dataSourceClassName"))) {
                 liquibase.setShouldRun(true);
                 log.warn("Using '{}' profile with H2 database in memory is not optimal, you should consider switching to" +
                     " MySQL or Postgresql to avoid rebuilding your database upon each start.", Constants.SPRING_PROFILE_FAST);
