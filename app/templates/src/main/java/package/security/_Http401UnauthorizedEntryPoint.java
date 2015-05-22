7bf36177e7d337ca457103ae508fe41c2b827e43
Thibaut <thibaut@alantaya.com>
2015-05-22 19:09:48 +0200

    missing @Override
    - Tested with Java 8, SQL and maven
    
    close #1510

--- app/templates/src/main/java/package/security/_Http401UnauthorizedEntryPoint.java
+++ app/templates/src/main/java/package/security/_Http401UnauthorizedEntryPoint.java
@@ -22,6 +22,7 @@ public class Http401UnauthorizedEntryPoint implements AuthenticationEntryPoint {
     /**
      * Always returns a 401 error code to the client.
      */
+    @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2) throws IOException,
             ServletException {
 
--- app/templates/src/main/java/package/security/_SpringSecurityAuditorAware.java
+++ app/templates/src/main/java/package/security/_SpringSecurityAuditorAware.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
 
+    @Override
     public String getCurrentAuditor() {
         String userName = SecurityUtils.getCurrentLogin();
         return (userName != null ? userName : Constants.SYSTEM_ACCOUNT);
