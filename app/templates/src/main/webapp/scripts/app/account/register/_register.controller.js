13bd1cfe515f135efa59b5d60b8036c872739b13
Deepu KS <d4udts@gmail.com>
2015-05-29 19:25:58 +0800

    make translation an option during generation
    changes to have an option during generation and supporting changes to
    strip unwanted translation directives and filters fromhtml and js files
    
    2nd PR for the translation changes
    
    Fix #1416

--- app/templates/src/main/webapp/scripts/app/account/register/_register.controller.js
+++ app/templates/src/main/webapp/scripts/app/account/register/_register.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('<%=angularAppName%>')
-    .controller('RegisterController', function ($scope, $translate, $timeout, Auth) {
+    .controller('RegisterController', function ($scope, <% if (enableTranslation){ %>$translate, <% } %>$timeout, Auth) {
         $scope.success = null;
         $scope.error = null;
         $scope.doNotMatch = null;
@@ -13,7 +13,7 @@ angular.module('<%=angularAppName%>')
             if ($scope.registerAccount.password !== $scope.confirmPassword) {
                 $scope.doNotMatch = 'ERROR';
             } else {
-                $scope.registerAccount.langKey = $translate.use();
+                $scope.registerAccount.langKey = <% if (enableTranslation){ %>$translate.use()<% }else {%> 'en' <% } %>;
                 $scope.doNotMatch = null;
                 $scope.error = null;
                 $scope.errorUserExists = null;
