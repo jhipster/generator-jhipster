2c88ebe1b6c5c24f180967bd42694a494e95b79f
Julien Dubois <julien.dubois@gmail.com>
2015-05-31 12:08:09 +0200

    Wording

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,7 +5,7 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
 
-describe('jhipster generator', function () {
+describe('JHipster generator', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
