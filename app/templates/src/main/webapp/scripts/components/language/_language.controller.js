5e1f03c10a1c64a25c2dec3f577908e3b2544f4b
sl <sl@sl.telenet.be>
2015-06-12 20:55:07 +0200

    'change displayed translated language name in native language name'

--- app/templates/src/main/webapp/scripts/components/language/_language.controller.js
+++ app/templates/src/main/webapp/scripts/components/language/_language.controller.js
@@ -10,4 +10,28 @@ angular.module('<%=angularAppName%>')
         Language.getAll().then(function (languages) {
             $scope.languages = languages;
         });
+    })
+    .filter('getLang', function () {
+        return function (lang) {
+            return {
+                "en": "English",
+                "fr": "Français",
+                "de": "Deutsch",
+                "it": "Italiano",
+                "ru": "Русский",
+                "tr": "Türkçe",
+                "ca": "Català",
+                "da": "Dansk",
+                "es": "Español",
+                "hu": "Magyar",
+                "ja": "日本語",
+                "kr": "한국어",
+                "pl": "Polski",
+                "pt-br": "Português (Brasil)",
+                "ro": "Română",
+                "sv": "Svenska",
+                "zh-cn": "中文（简体）",
+                "zh-tw": "繁體中文",
+            }[lang];
+        }
     });
