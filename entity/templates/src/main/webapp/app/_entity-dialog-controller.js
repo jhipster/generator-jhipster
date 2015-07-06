ca6cdca073bfe0c8d0a0dccae610742265f1e5b3
Julien Dubois <julien.dubois@gmail.com>
2015-07-06 17:18:36 +0200

    Formatting

--- entity/templates/src/main/webapp/app/_entity-dialog-controller.js
+++ entity/templates/src/main/webapp/app/_entity-dialog-controller.js
@@ -4,28 +4,28 @@ angular.module('<%=angularAppName%>').controller('<%= entityClass %>DialogContro
     ['$scope', '$stateParams', '$modalInstance', 'entity', '<%= entityClass %>'<% for (idx in differentTypes) { if (differentTypes[idx] != entityClass) {%>, '<%= differentTypes[idx] %>'<% } } %>,
         function($scope, $stateParams, $modalInstance, entity, <%= entityClass %><% for (idx in differentTypes) { if (differentTypes[idx] != entityClass) {%>, <%= differentTypes[idx] %><% } } %>) {
 
-      $scope.<%= entityInstance %> = entity;<% for (idx in differentTypes) { if (differentTypes[idx] != entityClass) { %>
-      $scope.<%= differentTypes[idx].toLowerCase() %>s = <%= differentTypes[idx] %>.query();<% } } %>
-      $scope.load = function(id) {
-          <%= entityClass %>.get({id : id}, function(result) {
+        $scope.<%= entityInstance %> = entity;<% for (idx in differentTypes) { if (differentTypes[idx] != entityClass) { %>
+        $scope.<%= differentTypes[idx].toLowerCase() %>s = <%= differentTypes[idx] %>.query();<% } } %>
+        $scope.load = function(id) {
+            <%= entityClass %>.get({id : id}, function(result) {
                 $scope.<%= entityInstance %> = result;
-          });
-      };
+            });
+        };
 
-      var onSaveFinished = function (result) {
-        $scope.$emit('<%=angularAppName%>:<%= entityInstance %>Update', result);
-        $modalInstance.close(result);
-      };
+        var onSaveFinished = function (result) {
+            $scope.$emit('<%=angularAppName%>:<%= entityInstance %>Update', result);
+            $modalInstance.close(result);
+        };
 
-      $scope.save = function () {
-        if ($scope.<%= entityInstance %>.id != null) {
-          <%= entityClass %>.update($scope.<%= entityInstance %>, onSaveFinished);
-        } else {
-          <%= entityClass %>.save($scope.<%= entityInstance %>, onSaveFinished);
-        }
-      };
+        $scope.save = function () {
+            if ($scope.<%= entityInstance %>.id != null) {
+                <%= entityClass %>.update($scope.<%= entityInstance %>, onSaveFinished);
+            } else {
+                <%= entityClass %>.save($scope.<%= entityInstance %>, onSaveFinished);
+            }
+        };
 
-      $scope.clear = function() {
-        $modalInstance.dismiss('cancel');
-      };
+        $scope.clear = function() {
+            $modalInstance.dismiss('cancel');
+        };
 }]);
