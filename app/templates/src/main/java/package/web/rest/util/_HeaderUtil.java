95d7540651076e7a8e6618c98916818da7441a3b
Deepu KS <d4udts@gmail.com>
2015-08-01 18:49:11 +0800

    change notification to use filed names from entity
    change error and success notification message to use translation content
    from entity.json files and concat message for no translation case

--- app/templates/src/main/java/package/web/rest/util/_HeaderUtil.java
+++ app/templates/src/main/java/package/web/rest/util/_HeaderUtil.java
@@ -14,17 +14,27 @@ public class HeaderUtil {
         headers.add("X-<%=angularAppName%>-params", param);
         return headers;
     }
-
+    <%
+    if(enableTranslation) {
+        var createdMessage = '"' + angularAppName + '." + entityName + ".created"';
+        var updatedMessage = '"' + angularAppName + '." + entityName + ".updated"';
+        var deletedMessage = '"' + angularAppName + '." + entityName + ".deleted"';
+    } else {
+        var createdMessage = '"A new " + entityName + " is created with identifier " + param';
+        var updatedMessage = '"A " + entityName + " is updated with identifier " + param';
+        var deletedMessage = '"A " + entityName + " is deleted with identifier " + param';
+    }
+    %>
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
-        return createAlert("<%= angularAppName %>." + entityName + ".created", param);
+        return createAlert(<%- createdMessage %>, param);
     }
 
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
-        return createAlert("<%= angularAppName %>." + entityName + ".updated", param);
+        return createAlert(<%- updatedMessage %>, param);
     }
 
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
-        return createAlert("<%= angularAppName %>." + entityName + ".deleted", param);
+        return createAlert(<%- deletedMessage %>, param);
     }
 
 }
