d44c397a51ab19627443d3489453627868d13f68
Julien Dubois <julien.dubois@gmail.com>
2015-10-05 17:11:56 +0200

    Update the Heroku sub-generator to use the new Spring configuration meta-data

--- heroku/templates/src/main/java/package/config/_HerokuDatabaseConfiguration.java
+++ heroku/templates/src/main/java/package/config/_HerokuDatabaseConfiguration.java
@@ -3,36 +3,22 @@ package <%=packageName%>.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;<% if (hibernateCache == 'hazelcast') { %>
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;<% if (hibernateCache == 'hazelcast') { %>
 import org.springframework.cache.CacheManager;<% } %>
-import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.ApplicationContextException;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 @Profile(Constants.SPRING_PROFILE_HEROKU)
-public class HerokuDatabaseConfiguration implements EnvironmentAware {
+public class HerokuDatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(HerokuDatabaseConfiguration.class);
 
-    private RelaxedPropertyResolver dataSourcePropertyResolver;
-
-    private RelaxedPropertyResolver jhipsterPropertyResolver;
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.dataSourcePropertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
-        this.jhipsterPropertyResolver = new RelaxedPropertyResolver(environment, "jhipster.datasource.");
-    }
-
     @Bean
-    public DataSource dataSource(<% if (hibernateCache == 'hazelcast') { %>CacheManager cacheManager<% } %>) {
+    public DataSource dataSource(DataSourceProperties dataSourceProperties, JHipsterProperties jHipsterProperties<% if (hibernateCache == 'hazelcast') { %>, CacheManager cacheManager<% } %>) {
         log.debug("Configuring Heroku Datasource");
 
         String herokuUrl = System.getenv("JDBC_DATABASE_URL");
@@ -40,13 +26,13 @@ public class HerokuDatabaseConfiguration implements EnvironmentAware {
 	    HikariConfig config = new HikariConfig();
 
 	    //MySQL optimizations, see https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
-	    if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourcePropertyResolver.getProperty("driver-class-name"))) {
-                config.addDataSourceProperty("cachePrepStmts", jhipsterPropertyResolver.getProperty("cachePrepStmts", "true"));
-                config.addDataSourceProperty("prepStmtCacheSize", jhipsterPropertyResolver.getProperty("prepStmtCacheSize", "250"));
-                config.addDataSourceProperty("prepStmtCacheSqlLimit", jhipsterPropertyResolver.getProperty("prepStmtCacheSqlLimit", "2048"));
+	    if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourceProperties.getDriverClassName())) {
+                config.addDataSourceProperty("cachePrepStmts", jHipsterProperties.getDatasource().isCachePrepStmts());
+                config.addDataSourceProperty("prepStmtCacheSize", jHipsterProperties.getDatasource().getPrepStmtCacheSize());
+                config.addDataSourceProperty("prepStmtCacheSqlLimit", jHipsterProperties.getDatasource().getPrepStmtCacheSqlLimit());
             }
 
-            config.setDataSourceClassName(dataSourcePropertyResolver.getProperty("driver-class-name"));
+            config.setDataSourceClassName(dataSourceProperties.getDriverClassName());
             config.addDataSourceProperty("url", herokuUrl);
             return new HikariDataSource(config);
         } else {
