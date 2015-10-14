1ee524e6a61c67233c192cc1e1d675e857f37cb0
cbornet <cbornet@hotmail.com>
2015-10-14 15:05:25 +0200

    use autoconfigured emebedded mongodb

--- app/templates/src/test/java/package/web/rest/_AuditResourceTest.java
+++ app/templates/src/test/java/package/web/rest/_AuditResourceTest.java
@@ -1,8 +1,6 @@
 package <%=packageName%>.web.rest;
 
-import <%=packageName%>.Application;<% if (databaseType == 'mongodb') { %>
-import <%=packageName%>.config.MongoConfiguration;
-import org.springframework.context.annotation.Import;<% } %>
+import <%=packageName%>.Application;
 import <%=packageName%>.config.audit.AuditEventConverter;
 import <%=packageName%>.domain.PersistentAuditEvent;
 import <%=packageName%>.repository.PersistenceAuditEventRepository;
@@ -35,8 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-@IntegrationTest<% if (databaseType == 'mongodb') { %>
-@Import(MongoConfiguration.class)<% } %><% if (databaseType == 'sql') { %>
+@IntegrationTest<% if (databaseType == 'sql') { %>
 @Transactional<% } %>
 public class AuditResourceTest {
 
