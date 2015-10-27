3d01826b77af5bdefe95806d43587543b47446d4
cbornet <cbornet@hotmail.com>
2015-10-27 14:01:20 +0100

    keep config metrics folder only for cassandra
    Use the autoconfigured DataSourceHealthIndicator from spring boot 1.3

--- app/index.js
+++ app/index.js
@@ -684,14 +684,11 @@ JhipsterGenerator.prototype.app = function app() {
     this.template('src/main/java/package/config/locale/_package-info.java', javaDir + 'config/locale/package-info.java', this, {});
     this.template('src/main/java/package/config/locale/_AngularCookieLocaleResolver.java', javaDir + 'config/locale/AngularCookieLocaleResolver.java', this, {});
 
-    this.template('src/main/java/package/config/metrics/_package-info.java', javaDir + 'config/metrics/package-info.java', this, {});
     if (this.databaseType == 'cassandra') {
+        this.template('src/main/java/package/config/metrics/_package-info.java', javaDir + 'config/metrics/package-info.java', this, {});
+        this.template('src/main/java/package/config/metrics/_JHipsterHealthIndicatorConfiguration.java', javaDir + 'config/metrics/JHipsterHealthIndicatorConfiguration.java', this, {});
         this.template('src/main/java/package/config/metrics/_CassandraHealthIndicator.java', javaDir + 'config/metrics/CassandraHealthIndicator.java', this, {});
     }
-    if (this.databaseType == 'sql' || this.databaseType == 'mongodb') {
-        this.template('src/main/java/package/config/metrics/_DatabaseHealthIndicator.java', javaDir + 'config/metrics/DatabaseHealthIndicator.java', this, {});
-    }
-    this.template('src/main/java/package/config/metrics/_JHipsterHealthIndicatorConfiguration.java', javaDir + 'config/metrics/JHipsterHealthIndicatorConfiguration.java', this, {});
 
     if (this.hibernateCache == "hazelcast") {
         this.template('src/main/java/package/config/hazelcast/_HazelcastCacheRegionFactory.java', javaDir + 'config/hazelcast/HazelcastCacheRegionFactory.java', this, {});
@@ -1186,6 +1183,9 @@ JhipsterGenerator.prototype.app = function app() {
     // Remove old files, from previous JHipster versions
     removefile(javaDir + 'config/MailConfiguration.java');
     removefile(javaDir + 'config/metrics/JavaMailHealthIndicator.java');
+    if (this.databaseType == 'sql' || this.databaseType == 'mongodb') {
+        removefolder(javaDir + 'config/metrics');
+    }
 
     removefile(javaDir + 'domain/util/CustomLocalDateSerializer.java');
     removefile(javaDir + 'domain/util/CustomDateTimeSerializer.java');
