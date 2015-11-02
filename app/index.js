a0bd694249c179c19147a86032c70d072c3efcb0
amit2103 <amit.pandey2103@gmail.com>
2015-11-02 20:08:57 +0530

    Removed unnecessary file
    Fix for getting UserID from NOSQL and RDBMS
    Fix for #1730

--- app/index.js
+++ app/index.js
@@ -505,14 +505,12 @@ JhipsterGenerator.prototype.app = function app() {
         this.libFolder = 'lib/oracle/ojdbc/' + this.ojdbcVersion + '/';
         mkdirp(this.libFolder);
     }
-
     if (this.databaseType === 'cassandra' || this.databaseType === 'mongodb') {
         this.pkType = 'String';
     } else {
         this.pkType = 'Long';
     }
 
-    // Create application
     this.template('_package.json', 'package.json', this, {});
     this.template('_bower.json', 'bower.json', this, {});
     this.template('_README.md', 'README.md', this, {});
@@ -788,7 +786,7 @@ JhipsterGenerator.prototype.app = function app() {
     this.template('src/main/java/package/service/util/_RandomUtil.java', javaDir + 'service/util/RandomUtil.java', this, {});
 
     this.template('src/main/java/package/web/filter/_package-info.java', javaDir + 'web/filter/package-info.java', this, {});
-    this.template('src/main/java/package/web/filter/_CachingHttpHeadersFilter.java', javaDir + 'web/filter/CachingHttpHeadersFilter.java', this, {});
+    this.template('src/main/java/package/web/filter/_CachingHttpHeadersFilter.java', javaDir + 'web/filter/CachingHttp<ersFilter.java', this, {});
     this.template('src/main/java/package/web/filter/_StaticResourcesProductionFilter.java', javaDir + 'web/filter/StaticResourcesProductionFilter.java', this, {});
     if (this.authenticationType == 'session') {
         this.template('src/main/java/package/web/filter/_CsrfCookieGeneratorFilter.java', javaDir + 'web/filter/CsrfCookieGeneratorFilter.java', this, {});
