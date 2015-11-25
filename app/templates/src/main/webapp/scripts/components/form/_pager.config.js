5de3ff0e2b754f10dce69172bed00b6cd9901f77
Gerald Humphries <geraldhumphries@gmail.com>
2015-11-25 12:54:33 -0500

    change previous and next text for pager to arrows

--- app/templates/src/main/webapp/scripts/components/form/_pager.config.js
+++ app/templates/src/main/webapp/scripts/components/form/_pager.config.js
@@ -3,4 +3,6 @@
 angular.module('<%=angularAppName%>')
     .config(function (pagerConfig) {
         pagerConfig.itemsPerPage = 20;
+        pagerConfig.previousText = '«';
+        pagerConfig.nextText = '»';
     });
