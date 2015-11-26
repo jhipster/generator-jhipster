1aad76b53cf3f518bc6cf2c969617cfba983e0b6
sendilkumarn <sendilkumarn@live.com>
2015-11-26 11:27:48 +0800

    added angular-ui bootstrap 14.3 updates

--- app/templates/src/main/webapp/scripts/app/admin/user-management/_user-management.js
+++ app/templates/src/main/webapp/scripts/app/admin/user-management/_user-management.js
@@ -100,8 +100,8 @@ angular.module('<%=angularAppName%>')
                 data: {
                     authorities: ['ROLE_ADMIN'],
                 },
-                onEnter: ['$stateParams', '$state', '$modal', function($stateParams, $state, $modal) {
-                    $modal.open({
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $modal) {
+                    $uibModal.open({
                         templateUrl: 'scripts/app/admin/user-management/user-management-delete-dialog.html',
                         controller: 'user-managementDeleteController',
                         size: 'md',
