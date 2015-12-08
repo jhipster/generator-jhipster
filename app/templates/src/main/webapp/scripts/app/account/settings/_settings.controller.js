668a4de1fd7276e49558c87c46b98a86e99d04f5
WB <xjiaoyang@users.noreply.github.com>
2015-12-08 10:52:47 +0800

    PR for issue #2386
    a separate variable for storing account data instead of using the same global

--- app/templates/src/main/webapp/scripts/app/account/settings/_settings.controller.js
+++ app/templates/src/main/webapp/scripts/app/account/settings/_settings.controller.js
@@ -5,15 +5,15 @@ angular.module('<%=angularAppName%>')
         $scope.success = null;
         $scope.error = null;
         Principal.identity(true).then(function(account) {
-            $scope.settingsAccount = account;
+            $scope.settingsAccount = copyAccount(account);
         });
 
         $scope.save = function () {
             Auth.updateAccount($scope.settingsAccount).then(function() {
                 $scope.error = null;
                 $scope.success = 'OK';
-                Principal.identity().then(function(account) {
-                    $scope.settingsAccount = account;
+                Principal.identity(true).then(function(account) {
+                    $scope.settingsAccount = copyAccount(account);
                 });<% if (enableTranslation){ %>
                 Language.getCurrent().then(function(current) {
                     if ($scope.settingsAccount.langKey !== current) {
@@ -25,4 +25,21 @@ angular.module('<%=angularAppName%>')
                 $scope.error = 'ERROR';
             });
         };
+        
+        /**
+         * a separate variable for storing account data instead of using the same global
+         * @param account
+         */
+        var copyAccount = function (account) {
+            return {
+                activated: account.activated,
+                authorities: account.authorities.slice(0),
+                email: account.email,
+                firstName: account.firstName,
+                langKey: account.langKey,
+                lastName: account.lastName,
+                login: account.login,
+                password: null
+            }
+        }
     });
