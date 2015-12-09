4378362192d5775517002e0b087d87c7e1371fe3
Julien Dubois <julien.dubois@gmail.com>
2015-12-09 14:22:35 -0500

    Indentation

--- app/templates/src/main/webapp/scripts/components/util/_date-util.service.js
+++ app/templates/src/main/webapp/scripts/components/util/_date-util.service.js
@@ -2,25 +2,25 @@
 
 angular.module('<%=angularAppName%>')
     .service('DateUtils', function ($filter) {
-      this.convertLocaleDateToServer = function(date) {
+    this.convertLocaleDateToServer = function(date) {
         if (date) {
-          return $filter('date')(date, 'yyyy-MM-dd');
+            return $filter('date')(date, 'yyyy-MM-dd');
         } else {
-          return null;
+            return null;
         }
-      };
-      this.convertLocaleDateFromServer = function(date) {
+    };
+    this.convertLocaleDateFromServer = function(date) {
         if (date) {
-          var dateString = date.split("-");
-          return new Date(dateString[0], dateString[1] - 1, dateString[2]);
+            var dateString = date.split("-");
+            return new Date(dateString[0], dateString[1] - 1, dateString[2]);
         }
         return null;
-      };
-      this.convertDateTimeFromServer = function(date) {
+    };
+    this.convertDateTimeFromServer = function(date) {
         if (date) {
-          return new Date(date);
+            return new Date(date);
         } else {
-          return null;
+            return null;
         }
-      }
-    });
+    }
+});
