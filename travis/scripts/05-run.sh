9f98958a7e4a67f6767d693aa786cf82b1b84749
Pascal Grimaud <pascalgrimaud@gmail.com>
2015-12-13 23:10:35 +0100

    Add protractor test in travis build to samples/app-websocket

--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -5,6 +5,11 @@ set -ev
 #--------------------------------------------------
 cd $HOME/$JHIPSTER
 if [ $RUNTASK == 1 ]; then
+  if [ $PROTRACTOR == 1 ]; then
+    export DISPLAY=:99.0
+    /etc/init.d/xvfb start
+  fi
+  
   if [ $JHIPSTER != "app-gradle" ]; then
     mvn -P$PROFILE &
     if [ $PROFILE == "dev" ]; then
@@ -20,4 +25,8 @@ if [ $RUNTASK == 1 ]; then
     # curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
     # fuser -k 8080/tcp ; sleep 10
   fi
+fi
+
+if [ $PROTRACTOR == 1 ]; then
+  grunt itest
 fi
