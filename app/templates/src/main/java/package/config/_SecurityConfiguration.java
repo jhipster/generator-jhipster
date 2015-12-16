f1e2452af34131eaf7dfa9b86dee05260e34fca8
Julien Dubois <julien.dubois@gmail.com>
2015-12-16 10:20:32 +0100

    Correct fail build

--- app/templates/src/main/java/package/config/_SecurityConfiguration.java
+++ app/templates/src/main/java/package/config/_SecurityConfiguration.java
@@ -14,8 +14,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;<% if (authenticationType == 'xauth' || authenticationType == 'oauth2') { %>
 import org.springframework.security.config.http.SessionCreationPolicy;<% } %><% if (clusteredHttpSession == 'hazelcast') { %>
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.userdetails.UserDetailsService;<% } %>
+import org.springframework.security.core.session.SessionRegistry;<% } %>
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
