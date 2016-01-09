a97fef370e784e32e506685f64828dfc00874480
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-01-09 17:09:04 +0100

    Travis : use correct profil to start java application

--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -11,11 +11,11 @@ if [ $RUN_APP == 1 ]; then
     fi
     mvn package -DskipTests=true -P$PROFILE
     mv target/*.war target/app.war
-    java -jar target/app.war &
+    java -jar target/app.war --spring.profiles.active=$PROFILE &
   else
     ./gradlew bootRepackage -P$PROFILE -x test
     mv build/libs/*.war build/libs/app.war
-    java -jar build/libs/app.war &
+    java -jar build/libs/app.war --spring.profiles.active=$PROFILE &
   fi
   sleep 40
   #-------------------------------------------------------------------------------
