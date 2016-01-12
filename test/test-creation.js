3dc280e7c607fb1a5e4e9ca210a7666f8c7a1dfd
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-01-12 02:27:49 +0100

    Yeoman : fix deprecated warning - add 2 dependencies : yeoman-assert, yeoman-test

--- test/test-creation.js
+++ test/test-creation.js
@@ -2,8 +2,8 @@
 'use strict';
 
 var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
 var os = require('os');
 
 describe('JHipster generator', function () {
