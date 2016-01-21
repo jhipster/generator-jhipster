19cbfd07b9a18231b5b01668c6c6393aed44244e
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-01-21 19:41:03 +0100

    Add tests for entity : dto, service and pagination

--- test/test-entity.js
+++ test/test-entity.js
@@ -7,7 +7,7 @@ var helpers = require('yeoman-test');
 var fse = require('fs-extra');
 
 describe('JHipster generator entity', function () {
-    describe('default configuration', function () {
+    describe('no dto, no service, no pagination', function () {
         before(function (done) {
             helpers.run(require.resolve('../entity'))
                 .inTmpDir(function (dir) {
@@ -20,6 +20,269 @@ describe('JHipster generator entity', function () {
                     dto: 'no',
                     service: 'no',
                     pagination: 'no'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('with dto, no service, no pagination', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'mapstruct',
+                    service: 'no',
+                    pagination: 'no'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                'src/main/java/com/mycompany/myapp/web/rest/dto/FooDTO.java',
+                'src/main/java/com/mycompany/myapp/web/rest/mapper/FooMapper.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('no dto, with serviceClass, no pagination', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'no',
+                    service: 'serviceClass',
+                    pagination: 'no'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                'src/main/java/com/mycompany/myapp/service/FooService.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('no dto, with serviceImpl, no pagination', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'no',
+                    service: 'serviceImpl',
+                    pagination: 'no'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                'src/main/java/com/mycompany/myapp/service/FooService.java',
+                'src/main/java/com/mycompany/myapp/service/impl/FooServiceImpl.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('no dto, no service, with pager', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'no',
+                    service: 'no',
+                    pagination: 'pager'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('no dto, no service, with pagination', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'no',
+                    service: 'no',
+                    pagination: 'pagination'
+                })
+                .on('end', done);
+        });
+
+        it('creates expected default files', function () {
+            assert.file([
+                '.jhipster/Foo.json',
+                'src/main/java/com/mycompany/myapp/domain/Foo.java',
+                'src/main/java/com/mycompany/myapp/repository/FooRepository.java',
+                'src/main/java/com/mycompany/myapp/web/rest/FooResource.java',
+                // 'src/main/resources/config/liquibase/changelog/20160120213555_added_entity_Foo.xml',
+                'src/main/webapp/scripts/app/entities/foo/foos.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.html',
+                'src/main/webapp/scripts/app/entities/foo/foo.js',
+                'src/main/webapp/scripts/app/entities/foo/foo.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-delete-dialog.controller.js',
+                'src/main/webapp/scripts/app/entities/foo/foo-detail.controller.js',
+                'src/test/javascript/spec/app/entities/foo/foo-detail.controller.spec.js',
+                'src/main/webapp/scripts/components/entities/foo/foo.service.js',
+                // 'src/main/webapp/i18n/en/foo.json',
+                // 'src/main/webapp/i18n/fr/foo.json',
+                'src/test/java/com/mycompany/myapp/web/rest/FooResourceIntTest.java',
+                'src/test/gatling/simulations/FooGatlingTest.scala',
+            ])
+        });
+    });
+
+    describe('no dto, no service, with infinite-scroll', function () {
+        before(function (done) {
+            helpers.run(require.resolve('../entity'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir)
+                })
+                .withArguments(['foo'])
+                .withPrompts({
+                    fieldAdd: false,
+                    relationshipAdd: false,
+                    dto: 'no',
+                    service: 'no',
+                    pagination: 'infinite-scroll'
                 })
                 .on('end', done);
         });
