5ad4e5d38f721b0650955c2d8ec895d62bac6ccb
Deepu k Sasidharan <d4udts@gmail.com>
2016-01-27 19:37:30 +0800

    remove profile fast from cache and application files

--- app/templates/src/main/java/package/config/_Constants.java
+++ app/templates/src/main/java/package/config/_Constants.java
@@ -8,7 +8,6 @@ public final class Constants {
     // Spring profile for development, production and "fast", see http://jhipster.github.io/profiles.html
     public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
-    public static final String SPRING_PROFILE_FAST = "fast";
     // Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
     public static final String SPRING_PROFILE_CLOUD = "cloud";
     // Spring profile used when deploying to Heroku
