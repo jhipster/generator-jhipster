b548fd656dd99031909a41ebd7a00c7adf797e0f
Mike Halma <michael.halma@closebrothers.com>
2016-01-29 16:20:30 +0000

    fix the Cassandra Cucumber tests
    spin up the in memopry cassandra database before running the cucumber
    tests

--- app/templates/src/test/java/package/cucumber/_CucumberTest.java
+++ app/templates/src/test/java/package/cucumber/_CucumberTest.java
@@ -2,11 +2,12 @@ package <%=packageName%>.cucumber;
 
 import org.junit.runner.RunWith;
 
+import <%=packageName%>.AbstractCassandraTest;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/features")
-public class CucumberTest {
+public class CucumberTest extends AbstractCassandraTest {
 
 }
