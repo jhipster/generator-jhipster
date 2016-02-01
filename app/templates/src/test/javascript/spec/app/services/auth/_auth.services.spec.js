e3ca7f48dee195014574b0f03d5056791f1bef69
Julien Dubois <julien.dubois@gmail.com>
2016-02-01 18:42:04 +0100

    Fix JWT client-side tests

--- app/templates/src/test/javascript/spec/app/services/auth/_auth.services.spec.js
+++ app/templates/src/test/javascript/spec/app/services/auth/_auth.services.spec.js
@@ -1,9 +1,7 @@
 'use strict';
 
 describe('Service Tests', function () {
-    <%_ if (authenticationType == 'session') { _%>
     beforeEach(mockApiAccountCall);
-    <%_ } _%>
     <%_ if (enableTranslation) { _%>
     beforeEach(mockI18nCalls);
     <%_ } _%>
@@ -18,9 +16,7 @@ describe('Service Tests', function () {
             authService = Auth;
             spiedAuthServerProvider = AuthServerProvider;
 
-            <%_ if (authenticationType == 'session' || authenticationType == 'oauth2') { _%>
             $httpBackend.expectPOST(/api\/logout\?cacheBuster=\d+/).respond(200, '');
-            <%_ } _%>
         }));
         //make sure no expectations were missed in your tests.
         //(e.g. expectGET or expectPOST)
@@ -28,7 +24,6 @@ describe('Service Tests', function () {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
         });
-        <% if (authenticationType == 'session' || authenticationType == 'oauth2') { %>
         it('should call backend on logout then call authServerProvider.logout', function(){
             //GIVEN
             //Set spy
@@ -43,20 +38,6 @@ describe('Service Tests', function () {
             //THEN
             expect(spiedAuthServerProvider.logout).toHaveBeenCalled();
             expect(spiedLocalStorageService.clearAll).toHaveBeenCalled();
-        });<% } %><% if (authenticationType == 'jwt') { %>
-          it('should call LocalStorageService.clearAll on logout', function(){
-            //GIVEN
-            //Set spy
-            spyOn(spiedLocalStorageService, "clearAll").and.callThrough();
-
-            //WHEN
-            authService.logout();
-            //flush the backend to "execute" the request to do the expectedGET assertion.
-            $httpBackend.flush();
-
-            //THEN
-            expect(spiedLocalStorageService.clearAll).toHaveBeenCalled();
-          });<% } %>
-
+        });
     });
 });
