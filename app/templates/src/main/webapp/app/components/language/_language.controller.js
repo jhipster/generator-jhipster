13a61b65ce7e81e150f03153af939e6bb8c24151
Pierre Besson <pbesson@ippon.fr>
2016-02-01 15:02:48 +0100

    clean up client code in /app/components/language following jshint guidelines

--- app/templates/src/main/webapp/app/components/language/_language.controller.js
+++ app/templates/src/main/webapp/app/components/language/_language.controller.js
@@ -14,28 +14,28 @@ angular.module('<%=angularAppName%>')
     .filter('findLanguageFromKey', function () {
         return function (lang) {
             return {
-                "ca": "Català",
-                "da": "Dansk",
-                "de": "Deutsch",
-                "en": "English",
-                "es": "Español",
-                "fr": "Français",
-                "gl": "Galego",
-                "hu": "Magyar",
-                "it": "Italiano",
-                "ja": "日本語",
-                "ko": "한국어",
-                "nl": "Nederlands",
-                "pl": "Polski",
-                "pt-br": "Português (Brasil)",
-                "pt-pt": "Português",
-                "ro": "Română",
-                "ru": "Русский",
-                "sv": "Svenska",
-                "ta": "தமிழ்",
-                "tr": "Türkçe",
-                "zh-cn": "中文（简体）",
-                "zh-tw": "繁體中文"
+                'ca': 'Català',
+                'da': 'Dansk',
+                'de': 'Deutsch',
+                'en': 'English',
+                'es': 'Español',
+                'fr': 'Français',
+                'gl': 'Galego',
+                'hu': 'Magyar',
+                'it': 'Italiano',
+                'ja': '日本語',
+                'ko': '한국어',
+                'nl': 'Nederlands',
+                'pl': 'Polski',
+                'pt-br': 'Português (Brasil)',
+                'pt-pt': 'Português',
+                'ro': 'Română',
+                'ru': 'Русский',
+                'sv': 'Svenska',
+                'ta': 'தமிழ்',
+                'tr': 'Türkçe',
+                'zh-cn': '中文（简体）',
+                'zh-tw': '繁體中文'
             }[lang];
-        }
+        };
     });
