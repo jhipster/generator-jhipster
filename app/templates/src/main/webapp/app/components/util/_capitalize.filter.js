6a39ebbfbaf08ccf64d8f39a0617ca1f019474c8
Pierre Besson <pbesson@ippon.fr>
2016-02-01 15:01:15 +0100

    clean up client code in /app/components/util following jshint guidelines

--- app/templates/src/main/webapp/app/components/util/_capitalize.filter.js
+++ app/templates/src/main/webapp/app/components/util/_capitalize.filter.js
@@ -3,8 +3,9 @@
 angular.module('<%=angularAppName%>')
     .filter('capitalize', function () {
         return function (input, scope) {
-            if (input != null)
+            if (input !== null) {
                 input = input.toLowerCase();
+            }
             return input.substring(0, 1).toUpperCase() + input.substring(1);
-        }
+        };
     });
