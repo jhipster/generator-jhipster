6cd2ac7836a46d5362cbfd2d33e5bd9037760893
Deepu k Sasidharan <d4udts@gmail.com>
2016-02-03 15:28:27 +0800

    remove unused var in gulpfile

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,7 +3,6 @@
 'use strict';
 
 var gulp = require('gulp'),
-//    gutil = require('gulp-util'),
     prefix = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
     usemin = require('gulp-usemin'),
