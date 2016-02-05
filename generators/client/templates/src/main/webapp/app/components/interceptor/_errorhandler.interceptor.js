85acdb9ad59e8141dc779bc154aa953aa5d0e3cc
Julien Dubois <julien.dubois@gmail.com>
2016-02-05 12:56:06 +0100

    Check when no data is returned, which is the case with JWT expired token

--- generators/client/templates/src/main/webapp/app/components/interceptor/_errorhandler.interceptor.js
+++ generators/client/templates/src/main/webapp/app/components/interceptor/_errorhandler.interceptor.js
@@ -4,9 +4,9 @@ angular.module('<%=angularAppName%>')
     .factory('errorHandlerInterceptor', function ($q, $rootScope) {
         return {
             'responseError': function (response) {
-                if (!(response.status === 401 && response.data.path.indexOf('/api/account') === 0 )){
-	                $rootScope.$emit('<%=angularAppName%>.httpError', response);
-	            }
+                if (!(response.status === 401 && (response.data === '' || response.data.path.indexOf('/api/account') === 0 ))) {
+                    $rootScope.$emit('<%=angularAppName%>.httpError', response);
+                }
                 return $q.reject(response);
             }
         };
