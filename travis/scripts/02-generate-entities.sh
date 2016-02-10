aac7ba6c3771f4390e2c80216d185a6f6af0a8d8
Anders Steiner <anders.steiner@gmail.com>
2016-02-10 20:22:04 +0100

    add entity field type, validation and option tests to travic ci testing, fix #2830

--- travis/scripts/02-generate-entities.sh
+++ travis/scripts/02-generate-entities.sh
@@ -16,3 +16,45 @@ fi
 if [ -a .jhipster/Operation.json ]; then
   yo jhipster:entity Operation --force --no-insight
 fi
+if [ -a .jhipster/FieldTestEntity.json ]; then
+  yo jhipster:entity FieldTestEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestInfiniteScrollEntity.json ]; then
+  yo jhipster:entity FieldTestInfiniteScrollEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestInfiniteScrollEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestMapstructEntity.json ]; then
+  yo jhipster:entity FieldTestMapstructEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestMapstructEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestPagerEntity.json ]; then
+  yo jhipster:entity FieldTestPagerEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestPagerEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestPaginationEntity.json ]; then
+  yo jhipster:entity FieldTestPaginationEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestPaginationEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestServiceClassEntity.json ]; then
+  yo jhipster:entity FieldTestServiceClassEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestServiceClassEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
+if [ -a .jhipster/FieldTestServiceImplEntity.json ]; then
+  yo jhipster:entity FieldTestServiceImplEntity --force --no-insight
+  if [ $JHIPSTER == "app-cassandra" ]; then
+    cat src/main/resources/config/cql/*_added_entity_FieldTestServiceImplEntity.cql >> src/main/resources/config/cql/create-tables.cql
+  fi
+fi
