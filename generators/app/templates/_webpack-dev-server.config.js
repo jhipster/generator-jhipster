08b6c545dba5006215331e6eb3b05f70f845651f
stormik <yuri.kushch@gmail.com>
2016-02-15 00:54:26 +0200

    Add webpack prod profile
    Remove gulp routine

---
+++ generators/app/templates/_webpack-dev-server.config.js
@@ -0,0 +1,63 @@
+var webpack = require('webpack');
+var path = require('path');
+var buildPath = path.resolve(__dirname, 'build');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+var config = {
+  //Entry points to the project
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/main/webapp/app/app.jsx')
+  ],
+  //Config options on how to interpret requires imports
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+    //node_modules: ["web_modules", "node_modules"]  (Default Settings)
+  },
+  //Server Configuration options
+  devServer:{
+    contentBase: 'src/main/webapp',  //Relative directory for base of server
+    devtool: 'eval',
+    hot: true,        //Live-reload
+    inline: true,
+    port: 3000        //Port Number
+  },
+  devtool: 'eval',
+  output: {
+    path: buildPath,    //Path of output file
+    filename: 'app.js'
+  },
+  plugins: [
+    //Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    //Allows error warnings but does not stop compiling. Will remove when eslint is added
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    //Loaders to interpret non-vanilla javascript code as well as most other extensions including images and text.
+    preLoaders: [
+      {
+        //Eslint loader
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        include: [path.resolve(__dirname, "src/main/webapp/app")],
+        exclude: [nodeModulesPath]
+      }
+    ],
+    loaders: [
+      {
+        //React-hot loader and
+        test: /\.(js|jsx)$/,  //All .js and .jsx files
+        loaders: ['react-hot', 'babel'], //react-hot is like browser sync and babel loads jsx and es6-7
+        exclude: [nodeModulesPath]
+      }
+    ]
+  },
+  //eslint config options. Part of the eslint-loader package
+  eslint: {
+    configFile: '.eslintrc'
+  }
+};
+
+module.exports = config;
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@
   gulp.task('test', ['pre-test'], function (cb) {
     var mochaErr;
 
-    gulp.src('test/**/*.js')
+    gulp.src('test/*.js')
       .pipe(plumber())
       .pipe(mocha({reporter: 'spec'}))
       .on('error', function (err) {
