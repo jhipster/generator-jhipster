fc4cc39e0b06c5ade659695063d2c60b0c1e8a7f
Gael Marziou <gael.marziou@free.fr>
2016-02-16 00:35:08 +0100

    Fix  cucumber compilation error

--- generators/server/templates/src/test/java/package/cucumber/stepdefs/_StepDefs.java
+++ generators/server/templates/src/test/java/package/cucumber/stepdefs/_StepDefs.java
@@ -1,13 +1,11 @@
 package <%=packageName%>.cucumber.stepdefs;
 
-import <%=packageName%>.Application;
+import <%=packageName%>.<%= mainClass %>;
 
 import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.ResultActions;
-
-import <%=packageName%>.<%= mainClass %>;
 
 @WebAppConfiguration
 @ContextConfiguration(classes = <%= mainClass %>.class, loader = SpringApplicationContextLoader.class)
