bdb7135ab153ef1adac09ff9c46de4f49df963fc
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-02-17 12:11:47 -0700

    add mapping to ignore unmapped UserDTO properties, use stream map instead of a loop for String/Authority Set conversion
    Fix #2907

--- generators/server/templates/src/main/java/package/web/rest/mapper/_UserMapper.java
+++ generators/server/templates/src/main/java/package/web/rest/mapper/_UserMapper.java
@@ -1,14 +1,13 @@
 package <%=packageName%>.web.rest.mapper;
-
 <% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
 import <%=packageName%>.domain.Authority;<% } %>
 import <%=packageName%>.domain.User;
 import <%=packageName%>.web.rest.dto.UserDTO;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Mapper for the entity User and its DTO UserDTO.
@@ -20,6 +19,15 @@ public interface UserMapper {
 
     List<UserDTO> usersToUserDTOs(List<User> users);
 
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedBy", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "activationKey", ignore = true)
+    @Mapping(target = "resetKey", ignore = true)
+    @Mapping(target = "resetDate", ignore = true)
+    @Mapping(target = "persistentTokens", ignore = true)
     User userDTOToUser(UserDTO userDTO);
 
     List<User> userDTOsToUsers(List<UserDTO> userDTOs);
@@ -33,21 +41,16 @@ public interface UserMapper {
         return user;
     }
 <% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
-    default Set<String> stringsFromAuthorities(Set<Authority> authorities) {
-        Set<String> auths = new HashSet<>();
-        for(Authority auth : authorities){
-            auths.add(auth.getName());
-        }
-        return auths;
+        default Set<String> stringsFromAuthorities (Set < Authority > authorities) {
+        return authorities.stream().map(Authority::getName)
+            .collect(Collectors.toSet());
     }
 
     default Set<Authority> authoritiesFromStrings(Set<String> strings) {
-        Set<Authority> auths = new HashSet<>();
-        for(String auth : strings){
-            Authority a = new Authority();
-            a.setName(auth);
-            auths.add(a);
-        }
-        return auths;
+        return strings.stream().map(string -> {
+            Authority auth = new Authority();
+            auth.setName(string);
+            return auth;
+        }).collect(Collectors.toSet());
     }<% } %>
 }
