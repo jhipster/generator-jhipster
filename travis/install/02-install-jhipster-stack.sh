ad9bfb9f753135ab566f831685640a06d17d3efe
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-02-21 19:41:11 +0100

    Formatting with ShellCheck: double quote to prevent globbing and word splitting

--- travis/install/02-install-jhipster-stack.sh
+++ travis/install/02-install-jhipster-stack.sh
@@ -9,7 +9,7 @@ npm install -g gulp-cli
 #-------------------------------------------------------------------------------
 # Install the latest version of JHipster
 #-------------------------------------------------------------------------------
-cd $TRAVIS_BUILD_DIR/
+cd "$TRAVIS_BUILD_DIR"/
 npm install
 npm link
 npm test
--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -3,27 +3,27 @@ set -ev
 #-------------------------------------------------------------------------------
 # Start the application
 #-------------------------------------------------------------------------------
-cd $HOME/$JHIPSTER
-if [ $RUN_APP == 1 ]; then
-  if [ $JHIPSTER != "app-gradle" ]; then
-    if [ $JHIPSTER == 'app-cassandra' ]; then
+cd "$HOME"/"$JHIPSTER"
+if [ "$RUN_APP" == 1 ]; then
+  if [ "$JHIPSTER" != "app-gradle" ]; then
+    if [ "$JHIPSTER" == 'app-cassandra' ]; then
       chmod -R 777 src/main/resources/config/cql/
       docker cp src/main/resources/config/cql/ samplecassandra-dev-cassandra:/
       docker exec -it samplecassandra-dev-cassandra init
     fi
-    mvn package -DskipTests=true -P$PROFILE
+    mvn package -DskipTests=true -P"$PROFILE"
     mv target/*.war target/app.war
-    java -jar target/app.war --spring.profiles.active=$PROFILE &
+    java -jar target/app.war --spring.profiles.active="$PROFILE" &
   else
-    ./gradlew bootRepackage -P$PROFILE -x test
+    ./gradlew bootRepackage -P"$PROFILE" -x test
     mv build/libs/*.war build/libs/app.war
-    java -jar build/libs/app.war --spring.profiles.active=$PROFILE &
+    java -jar build/libs/app.war --spring.profiles.active="$PROFILE" &
   fi
   sleep 40
   #-------------------------------------------------------------------------------
   # Launch protractor tests
   #-------------------------------------------------------------------------------
-  if [ $PROTRACTOR == 1 ]; then
+  if [ "$PROTRACTOR" == 1 ]; then
     gulp itest
   fi
 fi
