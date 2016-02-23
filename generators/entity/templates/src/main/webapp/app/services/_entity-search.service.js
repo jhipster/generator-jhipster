6a43e85ee3a7ef0e69236fd2c23f3c2964d0859b
William Marques <wmarques@ippon.fr>
2016-02-23 12:06:12 +0100

    refactored factories

--- generators/entity/templates/src/main/webapp/app/services/_entity-search.service.js
+++ generators/entity/templates/src/main/webapp/app/services/_entity-search.service.js
@@ -1,10 +1,15 @@
 (function() {
     'use strict';
 
-    angular.module('<%=angularAppName%>')
-        .factory('<%= entityClass %>Search', function ($resource) {
-            return $resource('api/_search/<%= entityApiUrl %>/:query', {}, {
-                'query': { method: 'GET', isArray: true}
-            });
+    angular
+        .module('<%=angularAppName%>')
+        .factory('<%= entityClass %>Search', <%= entityClass %>Search);
+
+    <%= entityClass %>Search.$inject = ['$resource'];
+
+    function <%= entityClass %>Search($resource) {
+        return $resource('api/_search/<%= entityApiUrl %>/:query', {}, {
+            'query': { method: 'GET', isArray: true}
         });
+    }
 })();
--- generators/entity/templates/src/main/webapp/app/services/_entity.service.js
+++ generators/entity/templates/src/main/webapp/app/services/_entity.service.js
@@ -1,34 +1,39 @@
 (function() {
     'use strict';
 
-    angular.module('<%=angularAppName%>')
-        .factory('<%= entityClass %>', function ($resource, DateUtils) {
-            return $resource('api/<%= entityApiUrl %>/:id', {}, {
-                'query': { method: 'GET', isArray: true},
-                'get': {
-                    method: 'GET',
-                    transformResponse: function (data) {
-                        data = angular.fromJson(data);<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
-                        data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateFromServer(data.<%=fields[idx].fieldName%>);<% }if (fields[idx].fieldType == 'ZonedDateTime' || fields[idx].fieldType == 'Date') { %>
-                        data.<%=fields[idx].fieldName%> = DateUtils.convertDateTimeFromServer(data.<%=fields[idx].fieldName%>);<% } }%>
-                        return data;
-                    }
-                },<% if (fieldsContainLocalDate == true) { %>
-                'update': {
-                    method: 'PUT',
-                    transformRequest: function (data) {<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
-                        data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateToServer(data.<%=fields[idx].fieldName%>);<% } }%>
-                        return angular.toJson(data);
-                    }
-                },
-                'save': {
-                    method: 'POST',
-                    transformRequest: function (data) {<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
-                        data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateToServer(data.<%=fields[idx].fieldName%>);<% } }%>
-                        return angular.toJson(data);
-                    }
-                }<% } else { %>
-                'update': { method:'PUT' }<% } %>
-            });
+    angular
+        .module('<%=angularAppName%>')
+        .factory('<%= entityClass %>', <%= entityClass %>);
+
+    <%= entityClass %>.$inject = ['$resource', 'DateUtils'];
+
+    function <%= entityClass %> ($resource, DateUtils) {
+        return $resource('api/<%= entityApiUrl %>/:id', {}, {
+            'query': { method: 'GET', isArray: true},
+            'get': {
+                method: 'GET',
+                transformResponse: function (data) {
+                    data = angular.fromJson(data);<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateFromServer(data.<%=fields[idx].fieldName%>);<% }if (fields[idx].fieldType == 'ZonedDateTime' || fields[idx].fieldType == 'Date') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertDateTimeFromServer(data.<%=fields[idx].fieldName%>);<% } }%>
+                    return data;
+                }
+            },<% if (fieldsContainLocalDate == true) { %>
+            'update': {
+                method: 'PUT',
+                transformRequest: function (data) {<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateToServer(data.<%=fields[idx].fieldName%>);<% } }%>
+                    return angular.toJson(data);
+                }
+            },
+            'save': {
+                method: 'POST',
+                transformRequest: function (data) {<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertLocaleDateToServer(data.<%=fields[idx].fieldName%>);<% } }%>
+                    return angular.toJson(data);
+                }
+            }<% } else { %>
+            'update': { method:'PUT' }<% } %>
         });
+    }
 })();
