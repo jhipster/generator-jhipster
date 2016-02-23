8580b5fd2f8d1d82becde600b91d30bc402a6f7e
William Marques <wmarques@ippon.fr>
2016-02-23 15:20:29 +0100

    refactored activate

--- generators/client/templates/src/main/webapp/app/account/activate/_activate.state.js
+++ generators/client/templates/src/main/webapp/app/account/activate/_activate.state.js
@@ -1,26 +1,29 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('<%=angularAppName%>')
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state('activate', {
-                parent: 'account',
-                url: '/activate?key',
-                data: {
-                    authorities: [],
-                    pageTitle: 'activate.title'
-                },
-                views: {
-                    'content@': {
-                        templateUrl: 'app/account/activate/activate.html',
-                        controller: 'ActivationController'
+    angular.module('<%=angularAppName%>')
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('activate', {
+                    parent: 'account',
+                    url: '/activate?key',
+                    data: {
+                        authorities: [],
+                        pageTitle: 'activate.title'
+                    },
+                    views: {
+                        'content@': {
+                            templateUrl: 'app/account/activate/activate.html',
+                            controller: 'ActivationController',
+                            controllerAs: 'vm'
+                        }
+                    },
+                    resolve: {
+                        translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                            $translatePartialLoader.addPart('activate');
+                            return $translate.refresh();
+                        }]
                     }
-                },
-                resolve: {
-                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                        $translatePartialLoader.addPart('activate');
-                        return $translate.refresh();
-                    }]
-                }
-            });
-    });
+                });
+        });
+})();
