fe92dc4cc2619c12ed4f4312a42162d537a87af2
Deepu k Sasidharan <d4udts@gmail.com>
2016-02-24 12:08:55 +0800

    correct indent

--- generators/entity/templates/src/main/webapp/app/entities/_entity-management.controller.js
+++ generators/entity/templates/src/main/webapp/app/entities/_entity-management.controller.js
@@ -10,11 +10,11 @@
     function <%= entityClass %>ManagementController ($scope, $state<% if (fieldsContainBlob) { %>, DataUtils<% } %>, <%= entityClass %><% if (searchEngine == 'elasticsearch') { %>, <%= entityClass %>Search<% } %><% if (pagination != 'no') { %>, ParseLinks<% } %> <%_ if (pagination == 'pager' || pagination == 'pagination'){ %>, AlertService, pagingParams, paginationConstants<% } %>) {
         var vm = this;
         <%_ if (pagination == 'pagination' || pagination == 'pager') { _%>
-    <%- include('pagination-template'); -%>
+<%- include('pagination-template'); -%>
         <%_ } else if (pagination == 'infinite-scroll') { _%>
-    <%- include('infinite-scroll-template'); -%>
+<%- include('infinite-scroll-template'); -%>
         <%_ } else { _%>
-    <%- include('no-pagination-template'); -%>
+<%- include('no-pagination-template'); -%>
         <% } %>
     }
 })();
