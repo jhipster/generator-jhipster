162ccf16ec3a49879bec6981a027c4549a7bf895
William Marques <wmarques@ippon.fr>
2016-02-25 11:34:07 +0100

    log refactor

--- generators/client/templates/src/main/webapp/app/admin/logs/_logs.state.js
+++ generators/client/templates/src/main/webapp/app/admin/logs/_logs.state.js
@@ -1,26 +1,33 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('<%=angularAppName%>')
-    .config(function ($stateProvider) {
+    angular
+        .module('<%=angularAppName%>')
+        .config(stateConfig);
+        
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
         $stateProvider
-            .state('logs', {
-                parent: 'admin',
-                url: '/logs',
-                data: {
-                    authorities: ['ROLE_ADMIN'],
-                    pageTitle: 'logs.title'
-                },
-                views: {
-                    'content@': {
-                        templateUrl: 'app/admin/logs/logs.html',
-                        controller: 'LogsController'
-                    }
-                },
-                resolve: {
-                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                        $translatePartialLoader.addPart('logs');
-                        return $translate.refresh();
-                    }]
+        .state('logs', {
+            parent: 'admin',
+            url: '/logs',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'logs.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/admin/logs/logs.html',
+                    controller: 'LogsController'
                 }
-            });
-    });
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('logs');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
