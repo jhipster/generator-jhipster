94483a12d2ef9b39356cc02d9b85df404818dfab
Julien Dubois <julien.dubois@gmail.com>
2016-02-25 14:10:17 +0100

    Clean up the project before Travis build
    See https://github.com/jhipster/generator-jhipster/issues/2984#issuecomment-188779591

--- travis/scripts/01-generate-project.sh
+++ travis/scripts/01-generate-project.sh
@@ -10,6 +10,9 @@ rm -Rf "$HOME"/"$JHIPSTER"/node_modules/.bin/*grunt*
 rm -Rf "$HOME"/"$JHIPSTER"/node_modules/*grunt*
 rm -Rf "$HOME"/"$JHIPSTER"/node_modules/*phantom*
 
+rm -Rf "$HOME"/"$JHIPSTER"/src
+rm -Rf "$HOME"/"$JHIPSTER"/target
+
 npm link generator-jhipster
 yo jhipster --force --no-insight
 ls -al "$HOME"/"$JHIPSTER"
