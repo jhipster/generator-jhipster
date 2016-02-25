aa0060b19f98f4f74efce9ce3f02e142a2dbebd0
William Marques <wmarques@ippon.fr>
2016-02-25 17:55:36 +0100

    adding new language files

---
+++ generators/client/templates/src/main/webapp/app/components/language/_language.constants.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+    
+    angular
+        .module('<%=angularAppName%>')
+    /*
+     Languages codes are ISO_639-1 codes, see http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+     They are written in English to avoid character encoding issues (not a perfect solution)
+     */
+        .constant('LANGUAGES', [
+            'en', 'fr'
+            // jhipster-needle-006 - JHipster will add new languages here
+        ]
+    );
+})();
