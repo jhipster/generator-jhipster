170ea77043a80edf23c7496248944e1e83671113
Cyril Beslay <cicoub13@gmail.com>
2016-02-28 16:37:14 +0100

    fix entityTranslationKey when using angular-suffix during generation
    fix #3024

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -1508,7 +1508,7 @@ module.exports = EntityGenerator.extend({
             } else if (databaseType == 'cassandra') {
                 this.entityUrlType = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}';
             }
-            this.entityTranslationKey = this.entityInstance;
+            this.entityTranslationKey = _s.camelize(this.entityStateName);
 
             this.differentTypes = [this.entityClass];
             if (this.relationships == undefined) {
