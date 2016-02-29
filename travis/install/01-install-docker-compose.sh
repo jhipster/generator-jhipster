18d0429596bb70b592a19dac3f0effe459e040fe
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-02-29 02:40:42 +0100

    Upgrade docker-compose to 1.6.2

--- travis/install/01-install-docker-compose.sh
+++ travis/install/01-install-docker-compose.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -ev
 #-------------------------------------------------------------------------------
+# Upgrade Docker
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
 # Install docker-compose
 #-------------------------------------------------------------------------------
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-"$(uname -s)"-"$(uname -m)" > docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-"$(uname -s)"-"$(uname -m)" > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
