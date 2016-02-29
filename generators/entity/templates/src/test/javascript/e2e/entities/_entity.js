602101b414bb51a6c72db1f083b96814e3143421
Deepu k Sasidharan <d4udts@gmail.com>
2016-02-29 23:58:03 +0800

    add entity e2e tests for protractor

--- generators/entity/templates/src/test/javascript/e2e/entities/_entity.js
+++ generators/entity/templates/src/test/javascript/e2e/entities/_entity.js
@@ -21,43 +21,15 @@ describe('<%= entityClass %> e2e test', function () {
         element(by.css('button[type=submit]')).click();
     });
 
-    beforeEach(function () {
-        accountMenu.click();
+    it('should load <%= entityClassPlural %>', function () {
+        entityMenu.click();
+        element(by.css('[ui-sref="<%= entityStateName %>"]')).click();
+        expect(element.all(by.css('h2')).first().getText()).toMatch(/<%= entityClassPlural %>/);
     });
 
-    it('should load user management', function () {
-        element(by.css('[ui-sref="user-management"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Users/);
-    });
-
-    it('should load metrics', function () {
-        element(by.css('[ui-sref="metrics"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Application Metrics/);
-    });
-
-    it('should load health', function () {
-        element(by.css('[ui-sref="health"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Health Checks/);
-    });
-
-    it('should load configuration', function () {
-        element(by.css('[ui-sref="configuration"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Configuration/);
-    });
-
-    it('should load audits', function () {
-        element(by.css('[ui-sref="audits"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Audits/);
-    });
-
-    it('should load logs', function () {
-        element(by.css('[ui-sref="logs"]')).click();
-        expect(element.all(by.css('h2')).first().getText()).toMatch(/Logs/);
-    });
-
-    it('should load api docs', function () {
-        element(by.css('[ui-sref="docs"]')).click();
-        expect(element.all(by.css('iframe')).isDisplayed()).toBeTruthy();
+    it('should load create <%= entityClass %> dialog', function () {
+        element(by.css('[ui-sref="<%= entityStateName %>.new"]')).click();
+        expect(element.all(by.css('h4')).first().getText()).toMatch(/Create or edit a <%= entityClass %>/);
     });
 
     afterAll(function () {
