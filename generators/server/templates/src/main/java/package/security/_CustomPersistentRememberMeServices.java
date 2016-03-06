d359ac5779e88dd9df97795a8a5cd59ee87b9d5e
Vivek More <vivz.thats.me@gmail.com>
2016-03-06 15:21:18 -0500

    update references to rememberMe key
    update references in
    - test configuration (application.yml) template
    - service configuration (CustomPersistentRememberMeServices.java) template
    - security configuration (SecurtyConfiguration.java) template

--- generators/server/templates/src/main/java/package/security/_CustomPersistentRememberMeServices.java
+++ generators/server/templates/src/main/java/package/security/_CustomPersistentRememberMeServices.java
@@ -75,7 +75,7 @@ public class CustomPersistentRememberMeServices extends
     public CustomPersistentRememberMeServices(Environment env, org.springframework.security.core.userdetails
         .UserDetailsService userDetailsService) {
 
-        super(env.getProperty("jhipster.security.rememberme.key"), userDetailsService);
+        super(env.getProperty("jhipster.security.rememberMe.key"), userDetailsService);
         random = new SecureRandom();
     }
 
