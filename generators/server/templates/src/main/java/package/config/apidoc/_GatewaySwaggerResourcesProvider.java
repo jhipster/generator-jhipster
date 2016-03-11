ab9f1de88ea5a5f54db944e9861cbe54b4bd9ab9
Julien Dubois <julien.dubois@gmail.com>
2016-03-11 14:26:23 +0100

    Upgrade to Spring Cloud 1.1.0.M5
    Fix #3130

--- generators/server/templates/src/main/java/package/config/apidoc/_GatewaySwaggerResourcesProvider.java
+++ generators/server/templates/src/main/java/package/config/apidoc/_GatewaySwaggerResourcesProvider.java
@@ -1,18 +1,19 @@
 package <%=packageName%>.config.apidoc;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.zuul.filters.ProxyRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
-
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Retrieves all registered microservices Swagger resources.
@@ -24,7 +25,7 @@ public class GatewaySwaggerResourcesProvider implements SwaggerResourcesProvider
     private final Logger log = LoggerFactory.getLogger(GatewaySwaggerResourcesProvider.class);
 
     @Inject
-    private ProxyRouteLocator routeLocator;
+    private RouteLocator routeLocator;
 
     @Inject
     private DiscoveryClient discoveryClient;
@@ -37,9 +38,9 @@ public class GatewaySwaggerResourcesProvider implements SwaggerResourcesProvider
         resources.add(swaggerResource("default", "/v2/api-docs"));
 
         //Add the registered microservices swagger docs as additional swagger resources
-        Map<String, String> routes = routeLocator.getRoutes();
-        routes.forEach((path, serviceId) -> {
-            resources.add(swaggerResource(serviceId, path.replace("**","v2/api-docs")));
+        List<Route> routes = routeLocator.getRoutes();
+        routes.forEach(route -> {
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs")));
         });
 
         return resources;
@@ -52,5 +53,4 @@ public class GatewaySwaggerResourcesProvider implements SwaggerResourcesProvider
         swaggerResource.setSwaggerVersion("2.0");
         return swaggerResource;
     }
-
 }
