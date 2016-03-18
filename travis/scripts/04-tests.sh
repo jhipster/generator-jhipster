36b2a61c3a07715e0d8f2b2a4709e487b1cd1544
Julien Dubois <julien.dubois@gmail.com>
2016-03-18 18:00:37 +0100

    Try to fix the Travis test

--- travis/scripts/04-tests.sh
+++ travis/scripts/04-tests.sh
@@ -5,7 +5,8 @@ set -ev
 #--------------------------------------------------
 cd "$HOME"/"$JHIPSTER"
 if [ "$JHIPSTER" != "app-gradle" ]; then
-  mvn test
+  MYSQL_HOST=$(docker inspect --format '{{.NetworkSettings.Networks.docker_default.IPAddress}}' tmp-mysql)
+  mvn test --spring.datasource.url=jdbc:mysql://${MYSQL_HOST}:3306/tmp?useUnicode=true&characterEncoding=utf8&useSSL=false
 else
   ./gradlew test
 fi
