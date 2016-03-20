409273c0d7baeb76e95ed68c679d5bca4c210248
MathieuAA <mathieu.aa@free.fr>
2016-03-20 10:31:56 +0100

    Updated to take arguments

--- module/index.js
+++ module/index.js
@@ -3,7 +3,7 @@
 var JDLParser = require('../lib/reader/jdl_reader');
 
 module.exports = (function () {
-  var self = {};
-  self.parse = JDLParser.read(process.argv.slice(2));
-  return self;
+  return {
+    parse: JDLParser.read
+  };
 })();
