ab1dfffea3c4722433f57bb80a6498f7e96dfc72
Gustavo Andrade <gustavoandrade82@gmail.com>
2016-03-22 21:36:20 -0300

    Added JSR-303 validation rules to saveAccount method. Fix #3042

--- generators/server/templates/src/main/java/package/web/rest/dto/_UserDTO.java
+++ generators/server/templates/src/main/java/package/web/rest/dto/_UserDTO.java
@@ -13,17 +13,10 @@ import java.util.stream.Collectors;
  */
 public class UserDTO {
 
-    public static final int PASSWORD_MIN_LENGTH = 4;
-    public static final int PASSWORD_MAX_LENGTH = 100;
-
     @Pattern(regexp = "^[a-z0-9]*$")
     @NotNull
     @Size(min = 1, max = 50)
     private String login;
-
-    @NotNull
-    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH)
-    private String password;
 
     @Size(max = 50)
     private String firstName;
@@ -46,28 +39,23 @@ public class UserDTO {
     }
 
     public UserDTO(User user) {
-        this(user.getLogin(), null, user.getFirstName(), user.getLastName(),
+        this(user.getLogin(), user.getFirstName(), user.getLastName(),
             user.getEmail(), user.getActivated(), user.getLangKey(),<% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
             user.getAuthorities().stream().map(Authority::getName)
                 .collect(Collectors.toSet()));<% } else { %>
             user.getAuthorities());<% } %>
     }
 
-    public UserDTO(String login, String password, String firstName, String lastName,
+    public UserDTO(String login, String firstName, String lastName,
         String email, boolean activated, String langKey, Set<String> authorities) {
 
-        this.login = login;
-        this.password = password;
+        this.login = login;        
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.activated = activated;
         this.langKey = langKey;
         this.authorities = authorities;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public String getLogin() {
@@ -102,7 +90,6 @@ public class UserDTO {
     public String toString() {
         return "UserDTO{" +
             "login='" + login + '\'' +
-            ", password='" + password + '\'' +
             ", firstName='" + firstName + '\'' +
             ", lastName='" + lastName + '\'' +
             ", email='" + email + '\'' +
