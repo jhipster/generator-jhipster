73074ce69c68af8e0518adb3271c13146f44e28e
William Marques <wmarques@ippon.fr>
2016-03-29 17:02:13 +0200

    removing expose, updating docker-compose subgen

--- generators/server/templates/src/main/docker/mongodb/scripts/init_replicaset.js
+++ generators/server/templates/src/main/docker/mongodb/scripts/init_replicaset.js
@@ -4,8 +4,8 @@ if(status.errmsg === 'no replset config has been received') {
 }
 for (var i = 1; i <= param; i++) {
     if(i!==1)
-        rs.add("docker_<%= baseName.toLowerCase() %>-mongodb-node_" + i + ":27017");
+        rs.add(folder+"_<%= baseName.toLowerCase() %>-mongodb-node_" + i + ":27017");
 }
 cfg = rs.conf();
-cfg.members[0].host = "docker_<%= baseName.toLowerCase() %>-mongodb-node_1:27017";
+cfg.members[0].host = folder+"_<%= baseName.toLowerCase() %>-mongodb-node_1:27017";
 rs.reconfig(cfg);
