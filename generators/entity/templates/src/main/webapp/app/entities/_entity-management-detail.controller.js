ea0d2d52656193bbee20f99f7f54a51bd2bd4522
Deepu k Sasidharan <d4udts@gmail.com>
2016-03-31 17:49:08 +0800

    remove unused method in entity client

--- generators/entity/templates/src/main/webapp/app/entities/_entity-management-detail.controller.js
+++ generators/entity/templates/src/main/webapp/app/entities/_entity-management-detail.controller.js
@@ -10,11 +10,7 @@
     function <%= entityAngularJSName %>DetailController($scope, $rootScope, $stateParams<% if (fieldsContainBlob) { %>, DataUtils<% } %>, entity<% for (idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %>) {
         var vm = this;
         vm.<%= entityInstance %> = entity;
-        vm.load = function (id) {
-            <%= entityClass %>.get({id: id}, function(result) {
-                vm.<%= entityInstance %> = result;
-            });
-        };
+        
         var unsubscribe = $rootScope.$on('<%=angularAppName%>:<%= entityInstance %>Update', function(event, result) {
             vm.<%= entityInstance %> = result;
         });
