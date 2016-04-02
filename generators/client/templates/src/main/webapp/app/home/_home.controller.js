601ddbf820a6c0efc28074aed5d24abeb9300185
Jon Ruddell <jonathan.ruddell@gmail.com>
2016-04-01 22:06:58 -0600

    fix login dialog register link to close the dialog
    change the register translation to use ng-click instead of ui-sref, add register function in home controller so changed register link can function correctly
    
    
    
    Fix #3312

--- generators/client/templates/src/main/webapp/app/home/_home.controller.js
+++ generators/client/templates/src/main/webapp/app/home/_home.controller.js
@@ -5,14 +5,15 @@
         .module('<%=angularAppName%>')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
 
-    function HomeController ($scope, Principal, LoginService) {
+    function HomeController ($scope, Principal, LoginService, $state) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
+        vm.register = register;
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
@@ -24,6 +25,9 @@
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
+        }
+        function register () {
+            $state.go('register');
         }
     }
 })();
