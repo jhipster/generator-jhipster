3aa80b36dc4afda1e7c48eb92a0be1776b7204ca
Pierre Besson <pbesson@ippon.fr>
2016-04-06 11:01:31 +0200

    fix forgotten packageName

--- generators/server/templates/src/main/java/package/config/_UaaConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_UaaConfiguration.java
@@ -1,6 +1,6 @@
 package <%=packageName%>.config;
 
-import de.stytex.foobar.security.AuthoritiesConstants;
+import <%=packageName%>.security.AuthoritiesConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
