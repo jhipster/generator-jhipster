5e252ea4a10478b1074fb3d08003d90d6524a94a
David Steiman <adinatbust@gmail.com>
2016-04-11 17:35:32 +0200

    removes useless comment

--- generators/server/templates/src/main/java/package/gateway/_TokenRelayFilter.java
+++ generators/server/templates/src/main/java/package/gateway/_TokenRelayFilter.java
@@ -12,7 +12,6 @@ public class TokenRelayFilter extends ZuulFilter {
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
 
-        // Alter ignored headers as per: https://gitter.im/spring-cloud/spring-cloud?at=56fea31f11ea211749c3ed22
         Set<String> headers = (Set<String>) ctx.get("ignoredHeaders");
         // We need our JWT tokens relayed to resource servers
         headers.remove("authorization");
