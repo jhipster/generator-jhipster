57a080a5e4708d1764c20db262ee030f9297cabb
William Marques <wmarques@ippon.fr>
2016-04-19 14:30:09 +0200

    same regexp as userjava

--- generators/server/templates/src/main/java/package/web/rest/_UserResource.java
+++ generators/server/templates/src/main/java/package/web/rest/_UserResource.java
@@ -214,7 +214,7 @@ public class UserResource {
      * @param login the login of the user to find
      * @return the ResponseEntity with status 200 (OK) and with body the "login" user, or with status 404 (Not Found)
      */
-    @RequestMapping(value = "/users/{login:[_'.@a-z0-9-]+}",
+    @RequestMapping(value = "/users/{login:^[a-z0-9]*$|anonymousUser}",
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
@@ -231,7 +231,7 @@ public class UserResource {
      * @param login the login of the user to delete
      * @return the ResponseEntity with status 200 (OK)
      */
-    @RequestMapping(value = "/users/{login:[_'.@a-z0-9-]+}",
+    @RequestMapping(value = "/users/{login:^[a-z0-9]*$|anonymousUser}",
         method = RequestMethod.DELETE,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
