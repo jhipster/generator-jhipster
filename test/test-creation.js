04ad2b19006023e2670f0fc707e8d187936ce9aa
Davy De Waele <ddewaele@gmail.com>
2016-04-26 17:53:29 +0200

    removed development_ribbon.png from expected list

--- test/test-creation.js
+++ test/test-creation.js
@@ -306,7 +306,6 @@ const expectedFiles = {
         CLIENT_TEST_SRC_DIR + 'spec/app/account/reset/request/reset.request.controller.spec.js',
         CLIENT_TEST_SRC_DIR + 'spec/app/services/auth/auth.services.spec.js',
         CLIENT_MAIN_SRC_DIR + 'content/css/documentation.css',
-        CLIENT_MAIN_SRC_DIR + 'content/images/development_ribbon.png',
         CLIENT_MAIN_SRC_DIR + 'content/images/hipster.png',
         CLIENT_MAIN_SRC_DIR + 'content/images/hipster2x.png'
     ],
