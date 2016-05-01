b6ce34691fc4d56ad90c9e7a9ad379f706caca54
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 14:05:32 +0200

    Travis: retry curl application before launching protractor tests

--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-set -ev
+
+launchProtractor() {
+    retryCount=1
+    maxRetry=10
+    httpUrl="http://localhost:8080"
+
+    rep=$(curl -v "$httpUrl")
+    status=$?
+    while [ "$status" -ne 0 ] && [ "$retryCount" -gt "$maxRetry" ]; do
+        echo "[$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
+        retryCount=$((retryCount+1))
+        sleep 10
+        rep=$(curl -v "$httpUrl")
+        status=$?
+    done
+
+    if [ "$status" -ne 0 ]; then
+      echo "[$(date)] Not connected after" $retryCount " retries. Abort protractor tests."
+      exit 0
+    fi
+
+    echo "[$(date)] Connected to application. Start protractor tests."
+    gulp itest --no-notification
+}
+
 #-------------------------------------------------------------------------------
 # Start the application
 #-------------------------------------------------------------------------------
@@ -14,11 +38,11 @@ if [ "$RUN_APP" == 1 ]; then
     mv build/libs/*.war build/libs/app.war
     java -jar build/libs/app.war --spring.profiles.active="$PROFILE" &
   fi
-  sleep 40
+  sleep 20
   #-------------------------------------------------------------------------------
   # Launch protractor tests
   #-------------------------------------------------------------------------------
   if [ "$PROTRACTOR" == 1 ]; then
-    gulp itest --no-notification
+    launchProtractor
   fi
 fi
