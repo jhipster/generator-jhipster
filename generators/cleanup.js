61aa01c83806ac9fa827f057cdbe57783dbf3fed
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-05-04 16:49:54 -0600

    change removeFile to removefile to call the correct function

--- generators/cleanup.js
+++ generators/cleanup.js
@@ -12,7 +12,7 @@ module.exports = {
 function cleanupOldFiles(generator, javaDir, testDir) {
     if (generator.isJhipsterVersionLessThan('3.1.1')) {
         //removeFile and removeFolder methods should be called here for files and folders to cleanup
-        generator.removeFile(ANGULAR_DIR + 'blocks/config/uib-pager.config.js');
-        generator.removeFile(ANGULAR_DIR + 'blocks/config/uib-pagination.config.js');
+        generator.removefile(ANGULAR_DIR + 'blocks/config/uib-pager.config.js');
+        generator.removefile(ANGULAR_DIR + 'blocks/config/uib-pagination.config.js');
     }
 }
