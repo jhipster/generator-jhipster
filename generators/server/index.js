9810022bedc8cc274c5195cd02e5561848e13ce3
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-11 07:32:59 +0200

    Rename to insert_default_users.cql as it's not template file

--- generators/server/index.js
+++ generators/server/index.js
@@ -1018,10 +1018,10 @@ module.exports = JhipsterServerGenerator.extend({
                 this.template(SERVER_MAIN_RES_DIR + 'config/cql/_create-keyspace-prod.cql', SERVER_MAIN_RES_DIR + 'config/cql/create-keyspace-prod.cql', this, {});
                 this.template(SERVER_MAIN_RES_DIR + 'config/cql/_create-keyspace.cql', SERVER_MAIN_RES_DIR + 'config/cql/create-keyspace.cql', this, {});
                 this.template(SERVER_MAIN_RES_DIR + 'config/cql/_drop-keyspace.cql', SERVER_MAIN_RES_DIR + 'config/cql/drop-keyspace.cql', this, {});
-                this.template(SERVER_MAIN_RES_DIR + 'config/cql/_create-tables.cql', SERVER_MAIN_RES_DIR + 'config/cql/create-tables.cql');
+                this.template(SERVER_MAIN_RES_DIR + 'config/cql/_create-tables.cql', SERVER_MAIN_RES_DIR + 'config/cql/create-tables.cql', this, {});
                 this.copy(SERVER_MAIN_RES_DIR + 'config/cql/changelog/README.md', SERVER_MAIN_RES_DIR + 'config/cql/changelog/README.md');
                 if (this.applicationType !== 'microservice') {
-                    this.copy(SERVER_MAIN_RES_DIR + 'config/cql/changelog/_insert_default_users.cql', SERVER_MAIN_RES_DIR + 'config/cql/changelog/00000000000000_insert_default_users.cql');
+                    this.copy(SERVER_MAIN_RES_DIR + 'config/cql/changelog/insert_default_users.cql', SERVER_MAIN_RES_DIR + 'config/cql/changelog/00000000000000_insert_default_users.cql');
                 }
             }
         },
