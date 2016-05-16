39c4f2af0a241cdd3b969d250980c389d2fb9b44
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-16 21:46:12 +0200

    Travis: cache improvements - upgrade docker 1.10 to 1.11 - upgrade node 4.4.3 to 4.4.4 - use maven wrapper to be consistent with gradle wrapper - use new repo jhipster-travis-cache - use m2 for gradle - remove the cache for node_modules: doesn't work well with npm3

--- travis/install/04-cache.sh
+++ travis/install/04-cache.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 set -ev
 #-------------------------------------------------------------------------------
-# Use jhipster-travis-build that contain .m2 and node_modules
+# Use jhipster-travis-cache that contain .m2 and node_modules
 #-------------------------------------------------------------------------------
 cd "$TRAVIS_BUILD_DIR"/
-git clone https://github.com/jhipster/jhipster-travis-build.git
-if [ "$JHIPSTER" != 'app-gradle' ]; then
-  rm -Rf "$HOME"/.m2/repository/
-  mv "$TRAVIS_BUILD_DIR"/jhipster-travis-build/repository "$HOME"/.m2/
-  ls -al "$HOME"/.m2/
-  ls -al "$HOME"/.m2/repository/
-fi
-if [ "$JHIPSTER_NODE_CACHE" == 1 ]; then
-  mv "$TRAVIS_BUILD_DIR"/jhipster-travis-build/node_modules/ "$JHIPSTER_SAMPLES/$JHIPSTER"/
-  ls -al "$JHIPSTER_SAMPLES"/"$JHIPSTER"/
-fi
+git clone -b $JHIPSTER_CACHE_BRANCH $JHIPSTER_CACHE_REPO
+
+rm -Rf "$HOME"/.m2/repository/
+mv "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/repository "$HOME"/.m2/
+
 #-------------------------------------------------------------------------------
 # Use phantomjs cache
 #-------------------------------------------------------------------------------
-tar -xvf "$TRAVIS_BUILD_DIR"/jhipster-travis-build/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C "$TRAVIS_BUILD_DIR"/jhipster-travis-build/
+tar -xvf "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/
 sudo mkdir -p /usr/local/phantomjs-2.1.1/
-sudo mv "$TRAVIS_BUILD_DIR"/jhipster-travis-build/phantomjs-2.1.1-linux-x86_64/LICENSE.BSD /usr/local/phantomjs-2.1.1/
-sudo mv "$TRAVIS_BUILD_DIR"/jhipster-travis-build/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/phantomjs-2.1.1/
+sudo mv "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/phantomjs-2.1.1-linux-x86_64/LICENSE.BSD /usr/local/phantomjs-2.1.1/
+sudo mv "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/phantomjs-2.1.1/
 sudo rm /usr/local/phantomjs
 sudo ln -sf /usr/local/phantomjs-2.1.1/ /usr/local/phantomjs
 ls -l /usr/local/
