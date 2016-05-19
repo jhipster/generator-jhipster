53945a6b75a1649937c9f69cf7836bbfc5b31d81
Deepu k Sasidharan <d4udts@gmail.com>
2016-05-19 17:35:07 +0800

    revert temporary workaround done for yo version issue

--- travis/install/02-install-jhipster-stack.sh
+++ travis/install/02-install-jhipster-stack.sh
@@ -7,8 +7,7 @@ npm install -g npm
 #-------------------------------------------------------------------------------
 # Install yeoman, bower and gulp
 #-------------------------------------------------------------------------------
-# setting yeoman version as temp workaround for yeoman issue https://github.com/yeoman/yo/issues/437
-npm install -g yo@1.8.1
+npm install -g yo
 npm install -g bower
 npm install -g gulp-cli
 #-------------------------------------------------------------------------------
