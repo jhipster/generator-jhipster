6bf13425422ba1fae3663758e225c94c65200cc9
Deepu k Sasidharan <d4udts@gmail.com>
2016-05-25 23:18:12 +0800

    add missing inject fix #3645

--- generators/client/templates/src/main/webapp/app/layouts/navbar/_active-menu.directive.js
+++ generators/client/templates/src/main/webapp/app/layouts/navbar/_active-menu.directive.js
@@ -5,6 +5,8 @@
         .module('<%=angularAppName%>')
         .directive('activeMenu', activeMenu);
 
+    activeMenu.$inject = ['$translate', '$locale', 'tmhDynamicLocale'];
+
     function activeMenu($translate, $locale, tmhDynamicLocale) {
         var directive = {
             restrict: 'A',
