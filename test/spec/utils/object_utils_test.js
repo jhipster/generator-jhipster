ab41f4f2e8b8737ecdacac3e196957202ac385a8
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-06-04 17:22:03 +0200

    Added ::values function

--- test/spec/utils/object_utils_test.js
+++ test/spec/utils/object_utils_test.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const expect = require('chai').expect,
-    merge = require('../../../lib/utils/object_utils').merge;
+    fail = expect.fail,
+    merge = require('../../../lib/utils/object_utils').merge,
+    values = require('../../../lib/utils/object_utils').values;
 
 describe('ObjectUtils', function () {
   describe('::merge', function () {
@@ -50,6 +52,34 @@ describe('ObjectUtils', function () {
         expect(
             object2
         ).to.deep.equal({b: 3, c: 4});
+      });
+    });
+  });
+  describe('::values', function () {
+    describe('when passing a nil object', function () {
+      it('fails', function () {
+        try {
+          values(null);
+          fail();
+        } catch(error) {
+          expect(error.name).to.eq('NullPointerException');
+        }
+        try {
+          values(undefined);
+          fail();
+        } catch(error) {
+          expect(error.name).to.eq('NullPointerException');
+        }
+      });
+    });
+    describe('when passing a valid object', function() {
+      it("returns its keys' values", function() {
+        expect(values({
+          a: 42,
+          b: 'A string',
+          c: [1,2,3,4,5],
+          d: {d1: '', d2: 'something'}
+        })).to.deep.eq([42, 'A string', [1,2,3,4,5], {d1: '', d2: 'something'}]);
       });
     });
   });
