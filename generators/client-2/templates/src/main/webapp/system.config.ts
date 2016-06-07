05f3bae2905b0f950d566b005ec15207dba52681
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-07 16:57:39 +0800

    polish

--- generators/client-2/templates/src/main/webapp/system.config.ts
+++ generators/client-2/templates/src/main/webapp/system.config.ts
@@ -12,7 +12,7 @@
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app': { main: 'app.main.js',  defaultExtension: 'js' },
+        'app': { main: 'app.main.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' }
     };
     var ngPackageNames = [
