d8d14e9f259d18c41b1d511ae4e9bb153a992e3a
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-06-07 21:25:55 +0200

    Added validations

---
+++ test/spec/core/jhipster/validations_test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const expect = require('chai').expect,
+    VALIDATIONS = require('../../../../lib/core/jhipster/validations').VALIDATIONS,
+    exists = require('../../../../lib/core/jhipster/validations').exists,
+    needsValue = require('../../../../lib/core/jhipster/validations').needsValue;
+
+describe('VALIDATIONS', function () {
+  describe('::exists', function () {
+    describe('when checking for a valid validation', function () {
+      it('returns true', function () {
+        expect(exists(VALIDATIONS.MAXBYTES)).to.be.true;
+      });
+    });
+    describe('when checking for an invalid validation', function () {
+      it('returns false', function () {
+        expect(exists('NOTHING')).to.be.false;
+      });
+    });
+  });
+  describe('::needsValue', function() {
+    describe('when checking whether a validation needs a value', function() {
+      it('returns so', function() {
+        expect(needsValue(VALIDATIONS.MAXLENGTH)).to.be.true;
+        expect(needsValue(VALIDATIONS.REQUIRED)).to.be.false;
+      });
+    });
+  });
+});
