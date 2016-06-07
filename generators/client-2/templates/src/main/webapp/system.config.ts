aa23f74d56509779bd43d930c760120b2a595aa7
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-07 15:53:18 +0800

    fix systemJS imports

--- generators/client-2/templates/src/main/webapp/system.config.ts
+++ generators/client-2/templates/src/main/webapp/system.config.ts
@@ -1,52 +1,45 @@
-/***********************************************************************************************
- * User Configuration.
- **********************************************************************************************/
-/** Map relative paths to URLs. */
-const map: any = {
-};
-
-/** User packages configuration. */
-const packages: any = {
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-/***********************************************************************************************
- * Everything underneath this line is managed by the CLI.
- **********************************************************************************************/
-const barrels: string[] = [
-  // Angular specific barrels.
-  '@angular/core',
-  '@angular/common',
-  '@angular/compiler',
-  '@angular/http',
-  '@angular/router',
-  '@angular/platform-browser',
-  '@angular/platform-browser-dynamic',
-
-  // Thirdparty barrels.
-  'rxjs',
-
-  // App specific barrels.
-  'app'
-];
-
-const cliSystemConfigPackages: any = {};
-barrels.forEach((barrelName: string) => {
-  cliSystemConfigPackages[barrelName] = { main: 'index' };
-});
-
-/** Type declaration for ambient System. */
-declare var System: any;
-
-// Apply the CLI SystemJS configuration.
-System.config({
-  map: {
-    '@angular': 'vendor/@angular',
-    'rxjs': 'vendor/rxjs',
-    'main': 'main.js'
-  },
-  packages: cliSystemConfigPackages
-});
-
-// Apply the user's configuration.
-System.config({ map, packages });
+/**
+* System configuration for Angular 2 samples
+* Adjust as necessary for your application needs.
+*/
+(function(global) {
+    // map tells the System loader where to look for things
+    var map = {
+        'app': 'app', // 'dist',
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'app.main.js'
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app': { main: 'app.main.js',  defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' }
+    };
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'upgrade',
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/'+ pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/'+ pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+    };
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+        map: map,
+        packages: packages
+    }
+    System.config(config);
+})(this);
