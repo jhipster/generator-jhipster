591069d9c313ed2c7c034dea7a996969bc7c0fc4
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-07 19:04:16 +0800

    add missing file

---
+++ generators/client-2/templates/src/main/webapp/system.config.js
@@ -0,0 +1,45 @@
+/**
+* System configuration for Angular 2 samples
+* Adjust as necessary for your application needs.
+*/
+(function(global) {
+    // map tells the System loader where to look for things
+    var map = {
+        'app': 'app', // 'dist',
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'app.main.js'
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app': { main: 'app.main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' }
+    };
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'upgrade',
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/'+ pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/'+ pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+    };
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+        map: map,
+        packages: packages
+    }
+    System.config(config);
+})(this);
