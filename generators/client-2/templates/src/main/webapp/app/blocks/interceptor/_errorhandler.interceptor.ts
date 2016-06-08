e45276c78541d8f326e54f77ce08794913527751
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:10:40 +0800

    add common module

--- generators/client-2/templates/src/main/webapp/app/blocks/interceptor/_errorhandler.interceptor.ts
+++ generators/client-2/templates/src/main/webapp/app/blocks/interceptor/_errorhandler.interceptor.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .factory('errorHandlerInterceptor', errorHandlerInterceptor);
 
     errorHandlerInterceptor.$inject = ['$q', '$rootScope'];
