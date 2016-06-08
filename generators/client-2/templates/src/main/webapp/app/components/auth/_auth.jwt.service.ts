338b63635adfe80b7758885ed0193ebf1007ef4f
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:13:05 +0800

    add common module for auth

--- generators/client-2/templates/src/main/webapp/app/components/auth/_auth.jwt.service.ts
+++ generators/client-2/templates/src/main/webapp/app/components/auth/_auth.jwt.service.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .factory('AuthServerProvider', AuthServerProvider);
 
     AuthServerProvider.$inject = ['$http', '$localStorage', '$sessionStorage', '$q'];
