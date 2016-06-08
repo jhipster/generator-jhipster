9fddf63a83a91b3079e85db11271a23abef0aaae
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:14:21 +0800

    add common module for language

--- generators/client-2/templates/src/main/webapp/app/components/language/_language.filter.ts
+++ generators/client-2/templates/src/main/webapp/app/components/language/_language.filter.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .filter('findLanguageFromKey', findLanguageFromKey);
 
     function findLanguageFromKey() {
