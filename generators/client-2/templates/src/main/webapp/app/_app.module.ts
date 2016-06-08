a88125b3aca329493662eb935d3de6231697116a
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 13:15:36 +0800

    create account module

--- generators/client-2/templates/src/main/webapp/app/_app.module.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.module.ts
@@ -16,7 +16,8 @@
             'ui.router',
             'infinite-scroll',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
-            'angular-loading-bar'
+            'angular-loading-bar',
+            '<%=angularAppName%>.account'
         ])
         .run(run);
 
--- generators/client-2/templates/src/main/webapp/app/account/settings/_settings.controller.ts
+++ generators/client-2/templates/src/main/webapp/app/account/settings/_settings.controller.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.account')
         .controller('SettingsController', SettingsController);
 
     SettingsController.$inject = ['Principal', 'Auth'<% if (enableTranslation){ %>, '<%=jhiPrefixCapitalized%>LanguageService', '$translate'<% } %>];
