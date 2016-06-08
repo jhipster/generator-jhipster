de6fe6b047b8b15995b10425a6f8b14502346314
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:13:46 +0800

    add common module for form

--- generators/client-2/templates/src/main/webapp/app/components/form/_show-validation.directive.ts
+++ generators/client-2/templates/src/main/webapp/app/components/form/_show-validation.directive.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .directive('showValidation', showValidation);
 
     function showValidation () {
