82b5669bf3ca3fabe75f738e7801f4f03e58b50e
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:13:46 +0800

    add common module for form

--- generators/client-2/templates/src/main/webapp/app/components/form/_pagination.constants.ts
+++ generators/client-2/templates/src/main/webapp/app/components/form/_pagination.constants.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .constant('paginationConstants', {
             'itemsPerPage': 20
         });
