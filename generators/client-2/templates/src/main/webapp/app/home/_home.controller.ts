48d72248a6b42d83c7b56063531beaebd2edb466
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-08 15:18:01 +0800

    add common module for home

--- generators/client-2/templates/src/main/webapp/app/home/_home.controller.ts
+++ generators/client-2/templates/src/main/webapp/app/home/_home.controller.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>')
+        .module('<%=angularAppName%>.common')
         .controller('HomeController', HomeController);
 
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
