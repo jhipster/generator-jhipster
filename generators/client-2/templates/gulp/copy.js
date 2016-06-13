44556523618e5824e323635fbd8d6201eb231e8c
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-13 16:53:36 +0800

    move copy tasks to file

---
+++ generators/client-2/templates/gulp/copy.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp'),
+    rev = require('gulp-rev'),
+    plumber = require('gulp-plumber'),
+    handleErrors = require('./handleErrors'),
+    es = require('event-stream'),
+    flatten = require('gulp-flatten'),
+    changed = require('gulp-changed');
+
+var handleErrors = require('./gulp/handleErrors');
+var config = require('./config');
+
+module.exports = {<% if(enableTranslation) { /* copy i18n folders only if translation is enabled */ %>
+    i18n: copyI18n,<% } %>
+    fonts: copyFonts,
+    common: copyCommon,
+    deps: copyDeps
+}
+<% if(enableTranslation) { %>
+function copyI18n() {
+    return gulp.src(config.app + 'i18n/**')
+        .pipe(plumber({errorHandler: handleErrors}))
+        .pipe(changed(config.dist + 'i18n/'))
+        .pipe(gulp.dest(config.dist + 'i18n/'));
+}
+<% } %>
+function copyFonts() {
+    return es.merge(<% if(!useSass) { %>gulp.src(config.bower + 'bootstrap/fonts/*.*')
+        .pipe(plumber({errorHandler: handleErrors}))
+        .pipe(changed(config.dist + 'content/fonts/'))
+        .pipe(rev())
+        .pipe(gulp.dest(config.dist + 'content/fonts/'))
+        .pipe(rev.manifest(config.revManifest, {
+            base: config.dist,
+            merge: true
+        }))
+        .pipe(gulp.dest(config.dist)),<% } %>
+        gulp.src(config.app + 'content/**/*.{woff,woff2,svg,ttf,eot,otf}')
+        .pipe(plumber({errorHandler: handleErrors}))
+        .pipe(changed(config.dist + 'content/fonts/'))
+        .pipe(flatten())
+        .pipe(rev())
+        .pipe(gulp.dest(config.dist + 'content/fonts/'))
+        .pipe(rev.manifest(config.revManifest, {
+            base: config.dist,
+            merge: true
+        }))
+        .pipe(gulp.dest(config.dist))
+    );
+}
+
+function copyCommon() {
+    return gulp.src([config.app + 'robots.txt', config.app + 'favicon.ico', config.app + '.htaccess'], { dot: true })
+        .pipe(plumber({errorHandler: handleErrors}))
+        .pipe(changed(config.dist))
+        .pipe(gulp.dest(config.dist));
+}
+
+//copy npm dependencies to vendor folder
+//TODO optimize to copy only required minified files
+function copyDeps(){
+    return gulp.src([
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.js',
+        'node_modules/@angular/**/*.js',
+        'node_modules/rxjs/**/*.js'
+    ], { base: 'node_modules' })
+    .pipe(gulp.dest(config.dist + 'vendor'));
+}
