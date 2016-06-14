c77ce504829c6479aafb72a2a5f8c4b24d14016d
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-14 14:55:04 +0800

    polish

--- generators/server/index.js
+++ generators/server/index.js
@@ -925,6 +925,7 @@ module.exports = JhipsterServerGenerator.extend({
     },
 
     end: function () {
+
         if (this.prodDatabaseType === 'oracle') {
             this.log('\n\n');
             this.warning(chalk.yellow.bold('You have selected Oracle database.\n') + 'Please rename ' +
