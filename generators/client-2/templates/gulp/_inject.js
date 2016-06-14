ccfcb98e81b1bf1f0559dca5a9599e3f12d75d42
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-14 17:26:02 +0800

    polish

--- generators/client-2/templates/gulp/_inject.js
+++ generators/client-2/templates/gulp/_inject.js
@@ -18,17 +18,22 @@ module.exports = {
     test: test,
     troubleshoot: troubleshoot
 }
-
+// TODO temp filter out ng2 files, need to completeley remove this task
 function app() {
-    return gulp.src(config.app + 'index.html')
-        .pipe(inject(gulp.src(config.dist + 'app/**/*.js')
+    return gulp.src(config.dist + 'index.html')
+        .pipe(inject(gulp.src([config.dist + 'app/**/*.js',
+            '!' + config.dist + 'app/**/upgrade_adapter.js',
+            '!' + config.dist + 'app/**/profile-info.js',
+            '!' + config.dist + 'app/**/profile.service.js',
+            '!' + config.dist + 'app/**/page-ribbon.component.js',
+            '!' + config.dist + 'app/**/app.main.js'])
             .pipe(naturalSort())
             .pipe(angularFilesort()), {relative: true}))
         .pipe(gulp.dest(config.dist));
 }
 
 function vendor() {
-    var stream = gulp.src(config.app + 'index.html')
+    var stream = gulp.src(config.dist + 'index.html')
         .pipe(plumber({errorHandler: handleErrors}))
         .pipe(inject(gulp.src(bowerFiles(), {read: false}), {
             name: 'bower',
@@ -60,7 +65,7 @@ function test() {
 
 function troubleshoot() {
     /* this task removes the troubleshooting content from index.html*/
-    return gulp.src(config.app + 'index.html')
+    return gulp.src(config.dist + 'index.html')
         .pipe(plumber({errorHandler: handleErrors}))
         /* having empty src as we dont have to read any files*/
         .pipe(inject(gulp.src('', {read: false}), {
--- generators/client-2/templates/src/main/webapp/app/components/_common.module.ts
+++ generators/client-2/templates/src/main/webapp/app/components/_common.module.ts
@@ -1,13 +1,13 @@
-(function() {
-    'use strict';
+import { PageRibbonComponent } from './profiles/page-ribbon.component';
+import { upgradeAdapter } from '../upgrade_adapter';
 
-    angular
-        .module('<%=angularAppName%>.common', [
-            'ngStorage', <% if (enableTranslation) { %>
-            'tmh.dynamicLocale',
-            'pascalprecht.translate', <% } %>
-            'ngResource',
-            'ui.bootstrap',
-            'ui.router'
-        ]);
-})();
+angular
+    .module('<%=angularAppName%>.common', [
+        'ngStorage', <% if (enableTranslation) { %>
+        'tmh.dynamicLocale',
+        'pascalprecht.translate', <% } %>
+        'ngResource',
+        'ui.bootstrap',
+        'ui.router'
+    ])
+    .directive('pageRibbon', upgradeAdapter.downgradeNg2Component(PageRibbonComponent));
