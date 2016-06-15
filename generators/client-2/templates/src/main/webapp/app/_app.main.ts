bd931573505afc7ab8ea55214d3237e5ea8b8424
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-15 12:47:39 +0800

    remove gulp inject:app

--- generators/client-2/templates/src/main/webapp/app/_app.main.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.main.ts
@@ -1,3 +1,4 @@
 import { upgradeAdapter } from './upgrade_adapter';
+import './app.module';
 
 upgradeAdapter.bootstrap(document.body, ['<%=angularAppName%>.app'], {strictDi: true});
--- generators/client-2/templates/src/main/webapp/app/admin/_admin.module.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/_admin.module.ts
@@ -1,13 +1,9 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('<%=angularAppName%>.admin', [
-            'ngStorage', <% if (enableTranslation) { %>
-            'tmh.dynamicLocale',
-            'pascalprecht.translate', <% } %>
-            'ngResource',
-            'ui.bootstrap',
-            'ui.router'
-        ]);
-})();
+angular
+    .module('<%=angularAppName%>.admin', [
+        'ngStorage', <% if (enableTranslation) { %>
+        'tmh.dynamicLocale',
+        'pascalprecht.translate', <% } %>
+        'ngResource',
+        'ui.bootstrap',
+        'ui.router'
+    ]);
