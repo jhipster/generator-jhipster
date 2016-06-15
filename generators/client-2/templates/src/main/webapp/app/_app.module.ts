32f0e7bc38612269dda127aaae279e3f6dda0ef5
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-15 13:34:02 +0800

    import config and interceptors

--- generators/client-2/templates/src/main/webapp/app/_app.module.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.module.ts
@@ -2,9 +2,25 @@ import './components/common.module';
 import './account/account.module';
 import './admin/admin.module';
 import './entities/entity.module';
-import { AppStateConfig } from './app.state';
+
 import { StateHandler } from './blocks/handlers/state.handler';
 import { TranslationHandler } from './blocks/handlers/translation.handler';
+
+import { AlertServiceConfig } from './blocks/config/alert.config';
+import { CompileServiceConfig } from './blocks/config/compile.config';
+import { HttpConfig } from './blocks/config/http.config';
+import { LocalStorageConfig } from './blocks/config/localstorage.config';
+import { TranslationConfig } from './blocks/config/translation.config';
+import { PagerConfig } from './blocks/config/uib-pager.config';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { AppStateConfig } from './app.state';
+
+import { TranslationStorageProvider } from './blocks/config/translation-storage.provider';
+
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
 
 angular
     .module('<%=angularAppName%>.app', [
@@ -27,7 +43,19 @@ angular
         '<%=angularAppName%>.admin',
         '<%=angularAppName%>.entity'
     ])
+    .config(AlertServiceConfig)
+    .config(CompileServiceConfig)
+    .config(HttpConfig)
+    .config(LocalStorageConfig)
+    .config(TranslationConfig)
+    .config(PagerConfig)
+    .config(PaginationConfig)
     .config(AppStateConfig)
+    .factory('TranslationStorageProvider', TranslationStorageProvider)
+    .factory('AuthExpiredInterceptor', AuthExpiredInterceptor)
+    .factory('AuthInterceptor', AuthInterceptor);
+    .factory('ErrorHandlerInterceptor', ErrorHandlerInterceptor);
+    .factory('NotificationInterceptor', NotificationInterceptor);
     .run(run);
 
 run.$inject = ['StateHandler'<% if (enableTranslation) { %>, 'TranslationHandler'<% } %>];
