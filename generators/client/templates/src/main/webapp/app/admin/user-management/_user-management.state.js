972c5bba235bcded74decdb8dbf5ec91dc3a3489
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-06-14 22:48:07 -0700

    set up sorting and pagination for user-management to match entities

--- generators/client/templates/src/main/webapp/app/admin/user-management/_user-management.state.js
+++ generators/client/templates/src/main/webapp/app/admin/user-management/_user-management.state.js
@@ -11,7 +11,7 @@
         $stateProvider
         .state('user-management', {
             parent: 'admin',
-            url: '/user-management',
+            url: '/user-management?page&sort',
             data: {
                 authorities: ['ROLE_ADMIN'],
                 pageTitle: 'user-management.home.title'
@@ -23,11 +23,31 @@
                     controllerAs: 'vm'
                 }
             },
+            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                }
+            },
             resolve: {
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort)
+                    };
+                }]<%_ if (enableTranslation){ _%>,
                 translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                     $translatePartialLoader.addPart('user-management');
                     return $translate.refresh();
                 }]
+            <%_ } _%>
+                
             }
         })
         .state('user-management-detail', {
