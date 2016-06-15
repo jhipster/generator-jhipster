4cf512e88a908737b3d160ae875e9d61ba3da174
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-15 13:11:59 +0800

    polish

--- generators/client-2/templates/src/main/webapp/app/home/_home.controller.ts
+++ generators/client-2/templates/src/main/webapp/app/home/_home.controller.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>.common')
+        .module('<%=angularAppName%>.app')
         .controller('HomeController', HomeController);
 
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
--- generators/client-2/templates/src/main/webapp/app/layouts/navbar/_active-menu.directive.ts
+++ generators/client-2/templates/src/main/webapp/app/layouts/navbar/_active-menu.directive.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>.common')
+        .module('<%=angularAppName%>.app')
         .directive('activeMenu', activeMenu);
 
     activeMenu.$inject = ['$translate', '$locale', 'tmhDynamicLocale'];
