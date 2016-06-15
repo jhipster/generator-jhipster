8965267dbc5e178476414b884f734abc1d1f248f
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-15 13:11:59 +0800

    polish

--- generators/client-2/templates/src/main/webapp/app/layouts/navbar/_navbar.controller.ts
+++ generators/client-2/templates/src/main/webapp/app/layouts/navbar/_navbar.controller.ts
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('<%=angularAppName%>.common')
+        .module('<%=angularAppName%>.app')
         .controller('NavbarController', NavbarController);
 
     NavbarController.$inject = ['$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
