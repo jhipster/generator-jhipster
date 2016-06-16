e200fd65bced674d7b7826a53346e09ac4f6e64f
SendilKumar N <sendilkumarn@live.com>
2016-06-17 01:30:52 +0800

    state ang2 transformation

--- generators/client-2/templates/src/main/webapp/app/admin/logs/_logs.state.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/logs/_logs.state.ts
@@ -1,33 +1,26 @@
-(function() {
-    'use strict';
+stateConfig.$inject = ['$stateProvider'];
 
-    angular
-        .module('<%=angularAppName%>.admin')
-        .config(stateConfig);
-
-    stateConfig.$inject = ['$stateProvider'];
-
-    function stateConfig($stateProvider) {
-        $stateProvider.state('logs', {
-            parent: 'admin',
-            url: '/logs',
-            data: {
-                authorities: ['ROLE_ADMIN'],
-                pageTitle: 'logs.title'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/admin/logs/logs.html',
-                    controller: 'LogsController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('logs');
-                    return $translate.refresh();
-                }]
+export function stateConfig($stateProvider) {
+    $stateProvider.state('logs', {
+        parent: 'admin',
+        url: '/logs',
+        data: {
+            authorities: ['ROLE_ADMIN'],
+            pageTitle: 'logs.title'
+        },
+        views: {
+            'content@': {
+                templateUrl: 'app/admin/logs/logs.html',
+                controller: 'LogsController',
+                controllerAs: 'vm'
             }
-        });
-    }
-})();
+        },
+        resolve: {
+            translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                $translatePartialLoader.addPart('logs');
+                return $translate.refresh();
+            }]
+        }
+    });
+}
+
