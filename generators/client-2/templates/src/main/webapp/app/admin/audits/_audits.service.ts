c420081402c7c1baa25610a9d061661435ef7a9f
SendilKumar N <sendilkumarn@live.com>
2016-06-17 11:39:53 +0800

    exported simple services

--- generators/client-2/templates/src/main/webapp/app/admin/audits/_audits.service.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/audits/_audits.service.ts
@@ -1,25 +1,20 @@
-(function() {
-    'use strict';
+import {Injectable} from '@angular/core';
 
-    angular
-        .module('<%=angularAppName%>.admin')
-        .factory('AuditsService', AuditsService);
+AuditsService.$inject = ['$resource'];
 
-    AuditsService.$inject = ['$resource'];
+@Injectable()
+export function AuditsService ($resource) {
+    var service = $resource(<% if(authenticationType === 'uaa') { %>'<%= uaaBaseName.toLowerCase() %>/api/audits/:id'<%} else { %>'management/jhipster/audits/:id'<% } %>, {}, {
+        'get': {
+            method: 'GET',
+            isArray: true
+        },
+        'query': {
+            method: 'GET',
+            isArray: true,
+            params: {fromDate: null, toDate: null}
+        }
+    });
 
-    function AuditsService ($resource) {
-        var service = $resource(<% if(authenticationType === 'uaa') { %>'<%= uaaBaseName.toLowerCase() %>/api/audits/:id'<%} else { %>'management/jhipster/audits/:id'<% } %>, {}, {
-            'get': {
-                method: 'GET',
-                isArray: true
-            },
-            'query': {
-                method: 'GET',
-                isArray: true,
-                params: {fromDate: null, toDate: null}
-            }
-        });
-
-        return service;
-    }
-})();
+    return service;
+}
