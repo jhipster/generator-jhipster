71291891f0f98b3e4f03be272c4468687686fddf
Deepu k Sasidharan <d4udts@gmail.com>
2016-06-17 13:48:11 +0800

    polish [ci skip]

--- generators/client-2/templates/src/main/webapp/app/_app.main.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.main.ts
@@ -1,4 +1,6 @@
 import { upgradeAdapter } from './upgrade_adapter';
+import { HTTP_PROVIDERS } from '@angular/http';
 import './app.module';
 
 upgradeAdapter.bootstrap(document.body, ['<%=angularAppName%>.app'], {strictDi: true});
+upgradeAdapter.addProvider(HTTP_PROVIDERS);
