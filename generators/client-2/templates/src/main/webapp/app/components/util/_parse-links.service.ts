c4b9b098fc30ec2034e26c697137836a46c5f89f
SendilKumar N <sendilkumarn@live.com>
2016-06-19 10:57:21 +0800

    parse-links update

--- generators/client-2/templates/src/main/webapp/app/components/util/_parse-links.service.ts
+++ generators/client-2/templates/src/main/webapp/app/components/util/_parse-links.service.ts
@@ -1,46 +1,32 @@
-(function(){
-    'use strict';
+export class ParseLinks {
 
-    angular
-        .module('<%=angularAppName%>.common')
-        .factory('ParseLinks', ParseLinks);
-
-    function ParseLinks () {
-
-        var service = {
-            parse : parse
-        };
-
-        return service;
-
-        function parse(header) {
-            if (header.length === 0) {
-                throw new Error('input must not be of zero length');
-            }
-
-            // Split parts by comma
-            var parts = header.split(',');
-            var links = {};
-            // Parse each part into a named link
-            angular.forEach(parts, function(p) {
-                var section = p.split(';');
-                if (section.length !== 2) {
-                    throw new Error('section could not be split on ";"');
-                }
-                var url = section[0].replace(/<(.*)>/, '$1').trim();
-                var queryString = {};
-                url.replace(
-                    new RegExp('([^?=&]+)(=([^&]*))?', 'g'),
-                    function($0, $1, $2, $3) { queryString[$1] = $3; }
-                );
-                var page = queryString.page;
-                if (angular.isString(page)) {
-                    page = parseInt(page);
-                }
-                var name = section[1].replace(/rel="(.*)"/, '$1').trim();
-                links[name] = page;
-            });
-            return links;
+    parse(header: string): any {
+        if (header.length === 0) {
+            throw new Error('input must not be of zero length');
         }
+
+        // Split parts by comma
+        let parts: string[] = header.split(',');
+        let links: any = {};
+        // Parse each part into a named link
+        parts.forEach( (p) => {
+            let section: string[] = p.split(';');
+            if (section.length !== 2) {
+                throw new Error('section could not be split on ";"');
+            }
+            let url: string = section[0].replace(/<(.*)>/, '$1').trim();
+            let queryString: any = {};
+            url.replace(
+                new RegExp('([^?=&]+)(=([^&]*))?', 'g'),
+                ($0, $1, $2, $3) => { return queryString[$1] = $3; }
+            );
+            let page: any = queryString.page;
+            if ( typeof page === "string") {
+                page = parseInt(page);
+            }
+            let name: string = section[1].replace(/rel="(.*)"/, '$1').trim();
+            links[name] = page;
+        });
+        return links;
     }
-})();
+}
