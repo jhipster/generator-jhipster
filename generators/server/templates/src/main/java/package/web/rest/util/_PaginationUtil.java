aaca7b13561df3fef87aa23422fecc60d8e354a4
Julien Dubois <julien.dubois@gmail.com>
2016-06-23 23:15:08 +0200

    remove unused import

--- generators/server/templates/src/main/java/package/web/rest/util/_PaginationUtil.java
+++ generators/server/templates/src/main/java/package/web/rest/util/_PaginationUtil.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
