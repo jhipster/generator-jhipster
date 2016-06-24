feff94d6c6220fddc051163375510632f7c998e4
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-06-25 00:49:00 +0200

    Fixed tests

--- test/spec/parser/jdl_parser_test.js
+++ test/spec/parser/jdl_parser_test.js
@@ -18,6 +18,7 @@ describe('JDLParser', function () {
         it('fails', function () {
           try {
             JDLParser.parse(null, 'sql');
+            fail();
           } catch (error) {
             expect(error.name).to.eq('NullPointerException');
           }
@@ -29,6 +30,7 @@ describe('JDLParser', function () {
             JDLParser.parse({
               notNull: 42
             }, null);
+            fail();
           } catch (error) {
             expect(error.name).to.eq('NullPointerException');
           }
@@ -40,6 +42,7 @@ describe('JDLParser', function () {
             JDLParser.parse({
               notNull: 42
             }, 'WRONG');
+            fail();
           } catch (error) {
             expect(error.name).to.eq('IllegalArgumentException');
           }
