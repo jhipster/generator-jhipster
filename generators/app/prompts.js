68d125521775201295a9e9d322f9e9fe2b1f8955
SendilKumar N <sendilkumarn@live.com>
2016-06-27 18:12:51 +0800

    minor chore fixes

--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -150,7 +150,7 @@ function askForClient() {
             },
             {
                 value: 'angular2',
-                name: '[BETA] Angular 2.x (rc2)'
+                name: '[BETA] Angular 2.x (rc3)'
             }
         ],
         default: 'angular1'
