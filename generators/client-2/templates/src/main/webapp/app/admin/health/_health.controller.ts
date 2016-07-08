7d258f053d7339b5fd2348b1d0cbf3be9b5afffd
William Marques <wmarques@ippon.fr>
2016-07-08 14:55:38 +0200

    refactor health service

--- generators/client-2/templates/src/main/webapp/app/admin/health/_health.controller.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/health/_health.controller.ts
@@ -1,63 +1,54 @@
-(function() {
-    'use strict';
 
-    angular
-        .module('<%=angularAppName%>.admin')
-        .controller('<%=jhiPrefixCapitalized%>HealthCheckController', <%=jhiPrefixCapitalized%>HealthCheckController);
+<%=jhiPrefixCapitalized%>HealthCheckController.$inject = ['<%=jhiPrefixCapitalized%>HealthService', '$uibModal'];
 
-    <%=jhiPrefixCapitalized%>HealthCheckController.$inject = ['<%=jhiPrefixCapitalized%>HealthService', '$uibModal'];
+export function <%=jhiPrefixCapitalized%>HealthCheckController (<%=jhiPrefixCapitalized%>HealthService, $uibModal) {
+    var vm = this;
 
-    function <%=jhiPrefixCapitalized%>HealthCheckController (<%=jhiPrefixCapitalized%>HealthService, $uibModal) {
-        var vm = this;
+    vm.updatingHealth = true;
+    vm.getLabelClass = getLabelClass;
+    vm.refresh = refresh;
+    vm.showHealth = showHealth;
+    vm.baseName = <%=jhiPrefixCapitalized%>HealthService.getBaseName;
+    vm.subSystemName = <%=jhiPrefixCapitalized%>HealthService.getSubSystemName;
 
-        vm.updatingHealth = true;
-        vm.getLabelClass = getLabelClass;
-        vm.refresh = refresh;
-        vm.showHealth = showHealth;
-        vm.baseName = <%=jhiPrefixCapitalized%>HealthService.getBaseName;
-        vm.subSystemName = <%=jhiPrefixCapitalized%>HealthService.getSubSystemName;
+    vm.refresh();
 
-        vm.refresh();
-
-        function getLabelClass (statusState) {
-            if (statusState === 'UP') {
-                return 'label-success';
-            } else {
-                return 'label-danger';
-            }
+    function getLabelClass (statusState) {
+        if (statusState === 'UP') {
+            return 'label-success';
+        } else {
+            return 'label-danger';
         }
-
-        function refresh () {
-            vm.updatingHealth = true;
-            <%=jhiPrefixCapitalized%>HealthService.checkHealth().then(function (response) {
-                vm.healthData = <%=jhiPrefixCapitalized%>HealthService.transformHealthData(response);
-                vm.updatingHealth = false;
-            }, function (response) {
-                vm.healthData =  <%=jhiPrefixCapitalized%>HealthService.transformHealthData(response.data);
-                vm.updatingHealth = false;
-            });
-        }
-
-        function showHealth (health) {
-            $uibModal.open({
-                templateUrl: 'app/admin/health/health.modal.html',
-                controller: 'HealthModalController',
-                controllerAs: 'vm',
-                size: 'lg',
-                resolve: {
-                    currentHealth: function() {
-                        return health;
-                    },
-                    baseName: function() {
-                        return vm.baseName;
-                    },
-                    subSystemName: function() {
-                        return vm.subSystemName;
-                    }
-
-                }
-            });
-        }
-
     }
-})();
+
+    function refresh () {
+        vm.updatingHealth = true;
+
+        <%=jhiPrefixCapitalized%>HealthService.checkHealth().subscribe(health => {
+            vm.healthData = <%=jhiPrefixCapitalized%>HealthService.transformHealthData(health);
+            vm.updatingHealth = false;
+        });
+    }
+
+    function showHealth (health) {
+        $uibModal.open({
+            templateUrl: 'app/admin/health/health.modal.html',
+            controller: 'HealthModalController',
+            controllerAs: 'vm',
+            size: 'lg',
+            resolve: {
+                currentHealth: function() {
+                    return health;
+                },
+                baseName: function() {
+                    return vm.baseName;
+                },
+                subSystemName: function() {
+                    return vm.subSystemName;
+                }
+
+            }
+        });
+    }
+
+}
