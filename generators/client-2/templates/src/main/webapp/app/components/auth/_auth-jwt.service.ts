ebccc087212720dae6a83e43cf92a6c00bbaf2ba
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-07-12 09:51:57 -0700

    remove undefined token checks
    Fix #3651

--- generators/client-2/templates/src/main/webapp/app/components/auth/_auth-jwt.service.ts
+++ generators/client-2/templates/src/main/webapp/app/components/auth/_auth-jwt.service.ts
@@ -3,7 +3,6 @@ AuthServerProvider.$inject = ['$http', '$localStorage', '$sessionStorage', '$q']
 export function AuthServerProvider ($http, $localStorage, $sessionStorage, $q) {
     var service = {
         getToken: getToken,
-        hasValidToken: hasValidToken,
         login: login,
         loginWithToken: loginWithToken,
         storeAuthenticationToken: storeAuthenticationToken,
@@ -14,11 +13,6 @@ export function AuthServerProvider ($http, $localStorage, $sessionStorage, $q) {
 
     function getToken () {
         return $localStorage.authenticationToken || $sessionStorage.authenticationToken;
-    }
-
-    function hasValidToken () {
-        var token = this.getToken();
-        return token && token.expires && token.expires > new Date().getTime();
     }
 
     function login (credentials) {
