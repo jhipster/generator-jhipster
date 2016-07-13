f593b31857447d6a73b0b67deeeaaaccfc0fbf0f
Deepu k Sasidharan <d4udts@gmail.com>
2016-07-13 07:21:11 +0530

    add test for multiple jdl

--- test/test-import-jdl.js
+++ test/test-import-jdl.js
@@ -19,7 +19,7 @@ describe('JHipster generator import jdl', function () {
 
         it('creates entity json files', function () {
             assert.file([
-                '.jhipster/department.json',
+                '.jhipster/Department.json',
                 '.jhipster/JobHistory.json',
                 '.jhipster/Job.json',
                 '.jhipster/Employee.json',
@@ -27,6 +27,32 @@ describe('JHipster generator import jdl', function () {
                 '.jhipster/Task.json',
                 '.jhipster/Country.json',
                 '.jhipster/Region.json'
+            ]);
+        });
+    });
+
+    describe('imports a JDL model from multiple files', function () {
+        beforeEach(function (done) {
+            helpers.run(require.resolve('../generators/import-jdl'))
+                .inTmpDir(function (dir) {
+                    fse.copySync(path.join(__dirname, '../test/templates/import-jdl'), dir);
+                })
+                .withArguments(['jdl.jdl', 'jdl2.jdl'])
+                .on('end', done);
+        });
+
+        it('creates entity json files', function () {
+            assert.file([
+                '.jhipster/Department.json',
+                '.jhipster/JobHistory.json',
+                '.jhipster/Job.json',
+                '.jhipster/Employee.json',
+                '.jhipster/Location.json',
+                '.jhipster/Task.json',
+                '.jhipster/Country.json',
+                '.jhipster/Region.json',
+                '.jhipster/DepartmentAlt.json',
+                '.jhipster/JobHistoryAlt.json'
             ]);
         });
     });
