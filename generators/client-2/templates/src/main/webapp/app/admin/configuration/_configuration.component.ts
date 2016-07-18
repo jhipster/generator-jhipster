b5b019f4750bd3f3588efccea331953a845460d9
William Marques <wmarques@ippon.fr>
2016-07-18 11:23:43 +0200

    sorting and filtering

--- generators/client-2/templates/src/main/webapp/app/admin/configuration/_configuration.component.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/configuration/_configuration.component.ts
@@ -11,14 +11,18 @@ export class <%=jhiPrefixCapitalized%>ConfigurationComponent {
     configuration:any = null;
     configKeys:any[];
     filter: string;
+    orderProp: string;
+    reverse: boolean;
 
     constructor(private <%=jhiPrefix%>ConfigurationService:<%=jhiPrefixCapitalized%>ConfigurationService){
         this.configKeys = [];
         this.filter = '';
+        this.orderProp = 'prefix';
+        this.reverse = false;
     }
 
     getConfigs(): any[] {
-        return this.filterConfig(this.configuration);
+        return this.sortConfig(this.filterConfig(this.configuration));
     }
 
 
@@ -44,5 +48,21 @@ export class <%=jhiPrefixCapitalized%>ConfigurationComponent {
         return configArray.filter(config => {
             return config.prefix.indexOf(this.filter) >= 0;
         });
+    }
+
+    private sortConfig(configArray: any[]) {
+        configArray = configArray.slice(0).sort((a, b) => {
+            if (a[this.orderProp] < b[this.orderProp]) {
+              return -1;
+            } else if ([b[this.orderProp] < a[this.orderProp]]) {
+              return 1;
+            } else {
+              return 0;
+            }
+        });
+
+        if(this.reverse) configArray.reverse();
+
+        return configArray;
     }
 }
