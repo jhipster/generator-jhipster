96b8ea15781627d3ad5a316edd6ca3bfbfddaae0
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-07-22 11:57:46 -0700

    add swagger ui gulp task and changes to angular-2 branch

--- generators/server/templates/src/main/java/package/config/_Constants.java
+++ generators/server/templates/src/main/java/package/config/_Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
     // Spring profile used when deploying to Heroku
     public static final String SPRING_PROFILE_HEROKU = "heroku";
     // Spring profile used to disable swagger
-    public static final String SPRING_PROFILE_NO_SWAGGER = "no-swagger";
+    public static final String SPRING_PROFILE_SWAGGER = "swagger";
     // Spring profile used to disable running liquibase
     public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
 
