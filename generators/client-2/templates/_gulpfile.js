d0556aec0812f14b6178dee19cfd95992f5feab9
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-07-22 11:57:46 -0700

    add swagger ui gulp task and changes to angular-2 branch

--- generators/client-2/templates/_gulpfile.js
+++ generators/client-2/templates/_gulpfile.js
@@ -10,6 +10,7 @@ var gulp = require('gulp'),<% if(useSass) { %>
     imagemin = require('gulp-imagemin'),
     ngConstant = require('gulp-ng-constant'),
     rename = require('gulp-rename'),
+    replace = require('gulp-replace'),
     eslint = require('gulp-eslint'),<% if (testFrameworks.indexOf('protractor') > -1) { %>
     argv = require('yargs').argv,
     gutil = require('gulp-util'),
@@ -120,7 +121,7 @@ gulp.task('inject:test', inject.test);
 
 gulp.task('inject:troubleshoot', inject.troubleshoot);
 
-gulp.task('assets:prod', ['images', 'styles', 'html'], build);
+gulp.task('assets:prod', ['images', 'styles', 'html', 'swagger-ui'], build);
 
 gulp.task('html', function () {
     return gulp.src(config.app + 'app/**/*.html')
@@ -131,6 +132,22 @@ gulp.task('html', function () {
             moduleSystem: 'IIFE'
         }))
         .pipe(gulp.dest(config.tmp));
+});
+
+gulp.task('swagger-ui', function () {
+    return es.merge(
+        gulp.src([config.bower + 'swagger-ui/dist/**',
+             '!' + config.bower + 'swagger-ui/dist/index.html',
+             '!' + config.bower + 'swagger-ui/dist/swagger-ui.min.js',
+             '!' + config.bower + 'swagger-ui/dist/swagger-ui.js'])
+            .pipe(gulp.dest(config.dist + 'swagger-ui/')),
+        gulp.src(config.app + 'swagger-ui/index.html')
+            .pipe(replace('../bower_components/swagger-ui/dist/', ''))
+            .pipe(replace('swagger-ui.js', 'lib/swagger-ui.min.js'))
+            .pipe(gulp.dest(config.dist + 'swagger-ui/')),
+        gulp.src(config.bower  + 'swagger-ui/dist/swagger-ui.min.js')
+            .pipe(gulp.dest(config.dist + 'swagger-ui/lib/'))
+    );
 });
 
 gulp.task('ngconstant:dev', function () {
