6762243a80990351317907e21cae385f4f89d864
Sukein Alex <Sukein.Alex@wanari.com>
2016-07-27 17:18:38 +0200

    added imports

--- generators/server/templates/src/main/java/package/web/rest/errors/_ExceptionTranslator.java
+++ generators/server/templates/src/main/java/package/web/rest/errors/_ExceptionTranslator.java
@@ -2,6 +2,8 @@ package <%=packageName%>.web.rest.errors;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 <%_ if (databaseType != 'no' && databaseType != 'cassandra') { _%>
 import org.springframework.dao.ConcurrencyFailureException;
