3c4c21a102cef82d94e6db2f550378f317d41f1e
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-08-01 23:52:55 +0200

    Cleaned up tests

--- test/spec/utils/string_utils_test.js
+++ test/spec/utils/string_utils_test.js
@@ -6,22 +6,22 @@ const expect = require('chai').expect,
 describe('::isNilOrEmpty', function() {
   describe('when passing a nil object', function() {
     it('returns true', function() {
-      expect(isNilOrEmpty(null)).to.true;
+      expect(isNilOrEmpty(null)).to.be.true;
     });
   });
   describe('when passing an undefined object', function() {
     it('returns true', function() {
-      expect(isNilOrEmpty(undefined)).to.true;
+      expect(isNilOrEmpty(undefined)).to.be.true;
     });
   });
   describe('when passing an empty string', function() {
     it('returns true', function() {
-      expect(isNilOrEmpty('')).to.true;
+      expect(isNilOrEmpty('')).to.be.true;
     });
   });
   describe('when passing a valid string', function() {
     it('returns false', function() {
-      expect(isNilOrEmpty('ABC')).to.false;
+      expect(isNilOrEmpty('ABC')).to.be.false;
     });
   });
 });
