cd39aa011d3a4e2ec416ea2c182f71dee7a33c0c
Deepu k Sasidharan <d4udts@gmail.com>
2016-08-02 22:52:29 +0530

    change reserved keyword check, pass type

--- test/spec/core/jhipster/reserved_keywords_test.js
+++ test/spec/core/jhipster/reserved_keywords_test.js
@@ -8,19 +8,33 @@ describe('ReservedKeywords', function () {
     describe('when passing a nil or empty keyword', function () {
       it('returns false', function () {
         expect(isReserved()).to.be.false;
-        expect(isReserved('')).to.be.false;
+        expect(isReserved('','')).to.be.false;
+        expect(isReserved('ValidKeyword')).to.be.false;
       });
     });
     describe('when passing a valid keyword', function() {
       it('returns false', function() {
-        expect(isReserved('ValidKeyword')).to.be.false;
+        expect(isReserved('ValidKeyword', 'JHIPSTER')).to.be.false;
+        expect(isReserved('ACCOUNT', 'JAVA')).to.be.false;
       });
     });
-    describe('when passing an invalid keyword, no matter the case', function() {
+    describe('when passing an invalid jhipster keyword, no matter the case', function() {
       it('returns true', function() {
-        expect(isReserved('Account')).to.be.true;
-        expect(isReserved('account')).to.be.true;
-        expect(isReserved('ACCOUNT')).to.be.true;
+        expect(isReserved('Account', 'JHIPSTER')).to.be.true;
+        expect(isReserved('account', 'jhipster')).to.be.true;
+        expect(isReserved('ACCOUNT', 'JHIPSTER')).to.be.true;
+        expect(isReserved('ACCOUNT', 'jhipster')).to.be.true;
+      });
+    });
+    describe('when passing an invalid keyword for diffrent types', function() {
+      it('returns true', function() {
+        expect(isReserved('ACCOUNT', 'jhipster')).to.be.true;
+        expect(isReserved('SUPER', 'JAVA')).to.be.true;
+        expect(isReserved('ACCESSIBLE', 'MYSQL')).to.be.true;
+        expect(isReserved('ANALYSE', 'POSTGRESQL')).to.be.true;
+        expect(isReserved('ADD', 'CASSANDRA')).to.be.true;
+        expect(isReserved('ACTIVATE', 'ORACLE')).to.be.true;
+        expect(isReserved('DOCUMENT', 'MONGODB')).to.be.true;
       });
     });
   });
