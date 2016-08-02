9e760d97951f5ffe3aa3d7ed023d592cb074fd41
Luca Orlandi <luca.orlandi@gmail.com>
2016-08-02 19:11:10 +0200

    Close #3921 - cause and exception are missing in log

--- generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
+++ generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
@@ -34,10 +34,10 @@ public class LoggingAspect {
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
         if (env.acceptsProfiles(Constants.SPRING_PROFILE_DEVELOPMENT)) {
             log.error("Exception in {}.{}() with cause = {} and exception {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), e.getCause(), e);
+                joinPoint.getSignature().getName(), e.getCause().getMessage(), e.getMessage());
         } else {
             log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), e.getCause());
+                joinPoint.getSignature().getName(), e.getCause().getMessage());
         }
     }
 
