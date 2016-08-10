53f0e37cfcd55973e768ec47a5479b98c778698d
William Marques <wmarques@ippon.fr>
2016-08-10 16:46:02 +0200

    Add Forms Dependency. Refactor Register Page

--- generators/client-2/templates/src/main/webapp/app/account/_account.module.ts
+++ generators/client-2/templates/src/main/webapp/app/account/_account.module.ts
@@ -4,6 +4,17 @@ import { Password } from './password/password.service';
 import { PasswordResetInit } from './reset/request/reset-request.service';
 import { PasswordResetFinish } from './reset/finish/reset-finish.service';
 
+import { upgradeAdapter } from "../upgrade_adapter";
+
+import { RegisterStateConfig } from "./register/register.state";
+import { AccountStateConfig } from "./account.state";
+
+import { RegisterComponent } from "./register/register.controller";
+
+upgradeAdapter.upgradeNg1Provider('Auth');
+upgradeAdapter.upgradeNg1Provider('$translate');
+upgradeAdapter.upgradeNg1Provider('LoginService');
+
 angular
     .module('<%=angularAppName%>.account', [
         'ngStorage', <% if (enableTranslation) { %>
@@ -13,6 +24,9 @@ angular
         'ui.bootstrap',
         'ui.router'
     ])
+    .config(AccountStateConfig)
+    .config(RegisterStateConfig)
+    .directive('register', <angular.IDirectiveFactory> upgradeAdapter.downgradeNg2Component(RegisterComponent))
     .factory('Register', Register)
     .factory('Activate', Activate)
     .factory('Password', Password)
