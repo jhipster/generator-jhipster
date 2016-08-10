702c5068170740ab6c019e84f7b6ba5d8044af25
William Marques <wmarques@ippon.fr>
2016-08-10 16:46:02 +0200

    Add Forms Dependency. Refactor Register Page

--- generators/client-2/templates/src/main/webapp/app/_app.main.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.main.ts
@@ -1,8 +1,11 @@
 import { upgradeAdapter } from './upgrade_adapter';
 import { HTTP_PROVIDERS, XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import './app.module';
 
 upgradeAdapter.addProvider(HTTP_PROVIDERS);
+upgradeAdapter.addProvider(disableDeprecatedForms());
+upgradeAdapter.addProvider(provideForms());
 upgradeAdapter.addProvider({
    provide: XSRFStrategy, useValue:  new CookieXSRFStrategy('CSRF-TOKEN', 'X-CSRF-TOKEN')
 });
