daaa33a45a1dffd9b490da06a6769ba34ae9e373
Boris Moers <boris@moers.ch>
2016-08-11 15:20:32 +0200

    undo enum

--- generators/entity/templates/src/main/java/package/domain/enumeration/_Enum.java
+++ generators/entity/templates/src/main/java/package/domain/enumeration/_Enum.java
@@ -5,11 +5,4 @@ package <%=packageName%>.domain.enumeration;
  */
 public enum <%= enumName %> {
     <%- enumValues %>;
-    <% if (enumValues.indexOf("(")!=-1){ %>
-    private Object value;
-		private <%= enumName %> (Object value){this.value = value;}
-    
-		@com.fasterxml.jackson.annotation.JsonValue
-    public String toString(){return String.valueOf(this.value);}
-    <% } %>
 }
