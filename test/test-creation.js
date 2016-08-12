4ee6c7ff52a97b7135b5ea0ac4a7de4abb2473af
Deepu k Sasidharan <d4udts@gmail.com>
2016-08-12 10:53:12 +0530

    polish

--- test/test-creation.js
+++ test/test-creation.js
@@ -331,7 +331,7 @@ const expectedFiles = {
         'gulp/config.js',
         'gulp/serve.js',
         'gulp/utils.js',
-        'gulp/handleErrors.js',
+        'gulp/handle-errors.js',
         CLIENT_MAIN_SRC_DIR + 'favicon.ico',
         CLIENT_MAIN_SRC_DIR + 'robots.txt',
         CLIENT_MAIN_SRC_DIR + 'i18n/en/activate.json',
