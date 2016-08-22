9969809c352b746f303f2bde351d429542993522
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-08-22 12:53:02 -0700

    fix aws error message and display generator-jhipster paths depending on project type

--- generators/aws/lib/aws.js
+++ generators/aws/lib/aws.js
@@ -7,18 +7,26 @@ var S3 = require('./s3.js'),
 var Aws, generator;
 
 var AwsFactory = module.exports = function AwsFactory(generatorRef) {
+    generator = generatorRef;
     try {
         Aws = require('aws-sdk');
-        generator = generatorRef;
     } catch (e) {
+        var windowsPath;
+        var linuxPath;
+        var applicationType = generator.config.get('applicationType');
+        if (applicationType === 'monolith' || applicationType === 'gateway') {
+            windowsPath = 'cd node_modules\\generator-jhipster\n';
+            linuxPath = 'cd node_modules/generator-jhipster\n';
+        } else {
+            windowsPath = 'cd %USERPROFILE%\\AppData\\Roaming\\npm\\node_modules\\generator-jhipster\n';
+            linuxPath = 'cd /usr/local/lib/node_modules/generator-jhipster\n';
+        }
         generator.env.error(chalk.red(
             'You don\'t have the AWS SDK installed. Please install it in the JHipster generator directory.\n\n') +
             chalk.yellow('WINDOWS\n') +
-            chalk.green('cd %USERPROFILE%\\AppData\\Roaming\\npm\\node_modules\\generator-jhipster\n' +
-            'npm install aws-sdk progress node-uuid\n\n') +
+            chalk.green(windowsPath + 'npm install aws-sdk progress node-uuid\n\n') +
             chalk.yellow('LINUX / MAC\n') +
-            chalk.green('cd /usr/local/lib/node_modules/generator-jhipster\n' +
-            'npm install aws-sdk progress node-uuid')
+            chalk.green(linuxPath + 'npm install aws-sdk progress node-uuid')
         );
     }
 };
