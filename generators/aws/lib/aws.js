ba46b2d6de10293f401b8f1ea71763918d59f755
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-08-24 15:03:11 -0700

    polish and eslint issues

--- generators/aws/lib/aws.js
+++ generators/aws/lib/aws.js
@@ -1,7 +1,6 @@
 'use strict';
 var S3 = require('./s3.js'),
     Rds = require('./rds.js'),
-    chalk = require('chalk'),
     shelljs = require('shelljs'),
     os = require('os'),
     Eb = require('./eb.js');
@@ -12,17 +11,16 @@ var AwsFactory = module.exports = function AwsFactory(generatorRef, cb) {
     generator = generatorRef;
     try {
         Aws = require('aws-sdk');
-        generator.log("Found AWS dependencies");
-        cb()
+        cb();
     } catch (e) {
-        generator.log("Installing AWS dependencies into your JHipster folder");
+        generator.log('Installing AWS dependencies into your JHipster folder');
         var jhipsterPath;
         var applicationType = generator.config.get('applicationType');
         if (applicationType === 'monolith' || applicationType === 'gateway') {
             if (os.platform() === 'win32') {
-                jhipsterPath = 'node_modules\\generator-jhipster\n';
+                jhipsterPath = 'node_modules\\generator-jhipster';
             } else {
-                jhipsterPath = 'node_modules/generator-jhipster\n';
+                jhipsterPath = 'node_modules/generator-jhipster';
             }
         } else {
             var prefix = shelljs.exec('npm config get prefix', {silent:true}).trim();
@@ -32,7 +30,7 @@ var AwsFactory = module.exports = function AwsFactory(generatorRef, cb) {
                 jhipsterPath = prefix + '/lib/node_modules/generator-jhipster';
             }
         }
-        shelljs.exec('npm install aws-sdk progress node-uuid --prefix ' + jhipsterPath, {silent:false}, function (code, msg, err) {
+        shelljs.exec('npm install aws-sdk progress node-uuid --prefix ' + jhipsterPath, {silent:true}, function (code, msg, err) {
             if (code !== 0) generator.error('Something went wrong while installing:\n' + err);
             Aws = require('aws-sdk');
             cb();
