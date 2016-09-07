6e8b540099cfb30312903d910765e89b799bb736
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-07 23:44:21 +0530

    ng2: add main module

--- generators/client-2/templates/src/main/webapp/app/_upgrade_adapter.ts
+++ generators/client-2/templates/src/main/webapp/app/_upgrade_adapter.ts
@@ -1,3 +1,5 @@
 import { UpgradeAdapter } from "@angular/upgrade";
+import { forwardRef } from '@angular/core';
+import { <%=angularAppName%>AppModule } from './app.module';
 
-export const upgradeAdapter = new UpgradeAdapter();
+export const upgradeAdapter = new UpgradeAdapter(forwardRef(() => '<%=angularAppName%>AppModule'));
