6a6be30a52a7e0bb84c9909114dad134092638a3
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-08 10:32:41 +0530

    ng2: add common module declarations

--- generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
+++ generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
@@ -4,16 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ProfileService } from './profiles/profile.service';
+import { PageRibbonComponent } from './profiles/page-ribbon.component';
+import { CapitalizePipe } from './util/capitalize.pipe';
+import { TruncateCharactersPipe } from './util/truncate-characters.pipe';
+import { TruncateWordsPipe } from './util/truncate-words.pipe';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
+    declarations: [
+        PageRibbonComponent,
+        CapitalizePipe,
+        TruncateCharactersPipe,
+        TruncateWordsPipe
+    ],
     providers: [
         ProfileService,
         {
             provide: XSRFStrategy,
             useValue:  new CookieXSRFStrategy('CSRF-TOKEN', 'X-CSRF-TOKEN')
         }
-    ],
-    declarations: []
+    ]
 })
 export class <%=angularAppName%>CommonModule {}
