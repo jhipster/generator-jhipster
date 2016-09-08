42c512f5ce52d684128a1750306d6ef0867fc916
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-08 09:53:17 +0530

    ng2: polish: correct imports

--- generators/client-2/templates/src/main/webapp/app/_upgrade_adapter.ts
+++ generators/client-2/templates/src/main/webapp/app/_upgrade_adapter.ts
@@ -1,5 +1,5 @@
 import { UpgradeAdapter } from "@angular/upgrade";
 import { forwardRef } from '@angular/core';
-import { <%=angularAppName%>AppModule } from './app.module';
+import { <%=angularAppName%>AppModule } from './app.ng2module';
 
 export const upgradeAdapter = new UpgradeAdapter(forwardRef(() => <%=angularAppName%>AppModule));
