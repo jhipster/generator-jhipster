7ef2c68abd7e53ac5ed04400a10beff41527e4a7
SendilKumar N <sendilkumarn@live.com>
2016-09-08 15:29:50 +0800

    ng2(common): polish

--- generators/client-2/templates/src/main/webapp/app/components/_common.module.ts
+++ generators/client-2/templates/src/main/webapp/app/components/_common.module.ts
@@ -39,6 +39,6 @@ angular
     .factory('LoginService', LoginService)
     .factory('Principal', Principal)
     .factory('ProfileService',upgradeAdapter.downgradeNg2Provider(ProfileService))
-    .provider('AlertService', AlertService) <% if (enableTranslation) { %>
+    .factory('AlertService', AlertService) <% if (enableTranslation) { %>
     .factory('<%=jhiPrefixCapitalized%>LanguageService', <%=jhiPrefixCapitalized%>LanguageService)<% } %>
     .directive('pageRibbon',  <angular.IDirectiveFactory> upgradeAdapter.downgradeNg2Component(PageRibbonComponent));
