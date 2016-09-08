8b92bb8cdc17a6abeb0648f3a4cd97ebf5089551
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-08 09:53:17 +0530

    ng2: polish: correct imports

--- generators/client-2/templates/src/main/webapp/app/_app.ng2module.ts
+++ generators/client-2/templates/src/main/webapp/app/_app.ng2module.ts
@@ -3,13 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { <%=angularAppName%>CommonModule } from './components/common.module';
-import { <%=angularAppName%>AdminModule } from './admin/admin.module';
+import { <%=angularAppName%>CommonModule } from './components/common.ng2module';
+import { <%=angularAppName%>AdminModule } from './admin/admin.ng2module';
+import { <%=angularAppName%>AccountModule } from './admin/account.ng2module';
 
 import {FindLanguageFromKeyPipe} from "./components/language/language.pipe";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, <%=angularAppName%>CommonModule, <%=angularAppName%>AdminModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        <%=angularAppName%>CommonModule,
+        <%=angularAppName%>AdminModule,
+        <%=angularAppName%>AccountModule
+    ],
     providers: [{
         provide: XSRFStrategy, useValue:  new CookieXSRFStrategy('CSRF-TOKEN', 'X-CSRF-TOKEN')
     }],
