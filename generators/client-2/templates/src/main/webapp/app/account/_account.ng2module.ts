43472753195937005dbb24c1738dd48c249149fb
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-09 23:28:32 +0530

    ng2: (account) set default module

--- generators/client-2/templates/src/main/webapp/app/account/_account.ng2module.ts
+++ generators/client-2/templates/src/main/webapp/app/account/_account.ng2module.ts
@@ -1,10 +1,18 @@
-import { XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
+import { HttpModule, XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { <%=angular2AppName%>CommonModule } from '../components/common.ng2module';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        <%=angular2AppName%>CommonModule
+    ],
     declarations: [
 
     ],
@@ -13,6 +21,7 @@ import { NgModule } from '@angular/core';
             provide: XSRFStrategy,
             useValue:  new CookieXSRFStrategy('CSRF-TOKEN', 'X-CSRF-TOKEN')
         }
-    ]
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class <%=angular2AppName%>AccountModule {}
