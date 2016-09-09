1f746507f95b493be862f3c590e49c165b4404fc
SendilKumar N <sendilkumarn@live.com>
2016-09-09 14:28:08 +0800

    feat(ng2): added system js imports for ng-bootstrap

--- generators/client-2/templates/src/main/webapp/system.config.js
+++ generators/client-2/templates/src/main/webapp/system.config.js
@@ -10,12 +10,14 @@
         'app': 'app', // 'dist',
         '@angular': 'vendor/@angular',
         'rxjs': 'vendor/rxjs',
-        'main': 'app.main'
+        'main': 'app.main',
+        '@ng-bootstrap': 'vendor/@ng-bootstrap'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'app.main' },
-        'rxjs': {}
+        'rxjs': {},
+        '@ng-bootstrap/ng-bootstrap': {main: 'index.js', defaultExtension: 'js'}
     };
     var ngPackageNames = [
         'common',
@@ -28,6 +30,27 @@
         'router',
         'upgrade',
     ];
+    // Packages related to ngbootstrap 
+    var ngBootstrapPackageNames = [
+        'accordion',
+        'alert',
+        'radio',
+        'carousel',
+        'collapse',
+        'datepicker',
+        'dropdown',
+        'modal',
+        'pagination',
+        'popover',
+        'progressbar',
+        'rating',
+        'tabset',
+        'timepicker',
+        'tooltip',
+        'typeahead',
+        'util'
+    ];
+
     // Individual files (~300 requests):
     function packIndex(pkgName) {
         packages['@angular/'+ pkgName] = { main: 'index' };
@@ -36,10 +59,15 @@
     function packUmd(pkgName) {
         packages['@angular/'+ pkgName] = { main: '/bundles/' + pkgName + '.umd' };
     };
+    // ng-bootstrap index packing
+    function ngBootstrapPackIndex(pkgName) {
+        packages['@ng-bootstrap/ng-bootstrap/' + pkgName] = {main: 'index.js', defaultExtension: 'js'};
+    };
     // Most environments should use UMD; some (Karma) need the individual index files
     var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
     // Add package entries for angular packages
     ngPackageNames.forEach(setPackageConfig);
+    ngBootstrapPackageNames.forEach(ngBootstrapPackIndex);
     var config = {
         map: map,
         packages: packages
