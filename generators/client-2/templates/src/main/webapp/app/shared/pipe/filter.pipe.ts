372db40ee023fff4c52a0a08ac40336cb4b315dc
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-10 15:28:23 +0530

    ng2: (shared) polish

--- generators/client-2/templates/src/main/webapp/app/shared/pipe/filter.pipe.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/pipe/filter.pipe.ts
@@ -1,8 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-    name: 'filter'
-})
+@Pipe({name: 'filter'})
 export class FilterPipe implements PipeTransform {
     transform(input: Array<any>, filterValue: string, field: string): any {
         return input.filter(val => {
--- generators/client-2/templates/src/main/webapp/app/shared/pipe/translate.pipe.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/pipe/translate.pipe.ts
@@ -1,8 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 //TODO dummy translate pipe
-@Pipe({
-    name: 'translate'
-})
+@Pipe({name: 'translate'})
 export class TranslatePipe implements PipeTransform {
     transform(input: any, filterValue: string, field: string): any {
         return input;
