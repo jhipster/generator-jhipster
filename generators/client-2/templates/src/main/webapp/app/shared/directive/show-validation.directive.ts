c994ec548bc875d30fb7de130e2ae4b87730f0cf
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-10 21:11:55 +0530

    ng2: (shared) upgrade showvalidation directives

--- generators/client-2/templates/src/main/webapp/app/shared/directive/show-validation.directive.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/directive/show-validation.directive.ts
@@ -1,27 +1,23 @@
-export function showValidation () {
-    var directive = {
-        restrict: 'A',
-        require: 'form',
-        link: linkFunc
-    };
+import { Directive, ElementRef, Input, Renderer } from '@angular/core';
 
-    return directive;
+@Directive({ selector: 'form[show-validation]' })
+export class ShowValidation {
+    constructor(el: ElementRef, renderer: Renderer) {
+       $(el.nativeElement).find('.form-group').each(function() {
+           var $formGroup = $(this);
+           var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
 
-    function linkFunc (scope, element) {
-        element.find('.form-group').each(function() {
-            var $formGroup = angular.element(this);
-            var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
-
-            if ($inputs.length > 0) {
-                $inputs.each(function() {
-                    var $input = angular.element(this);
-                    scope.$watch(function() {
-                        return $input.hasClass('ng-invalid') && $input.hasClass('ng-dirty');
-                    }, function(isInvalid) {
-                        $formGroup.toggleClass('has-error', isInvalid);
-                    });
-                });
-            }
-        });
+           if ($inputs.length > 0) {
+               $inputs.each(function() {
+                   var $input = $(this);
+                   //TODO this logic needs to be adapted
+                   /*scope.$watch(function() {
+                       return $input.hasClass('ng-invalid') && $input.hasClass('ng-dirty');
+                   }, function(isInvalid) {
+                       $formGroup.toggleClass('has-error', isInvalid);
+                   });*/
+               });
+           }
+       });
     }
 }
