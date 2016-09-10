af90bcd7abf5ffb1c37dd0cc257118d3f52be0e8
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-10 13:52:11 +0530

    enchance: (config) add method to get active profiles

--- generators/server/templates/src/main/java/package/config/_DefaultProfileUtil.java
+++ generators/server/templates/src/main/java/package/config/_DefaultProfileUtil.java
@@ -1,9 +1,9 @@
 package <%=packageName%>.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.Environment;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Utility class to load a Spring profile to be used as default
@@ -28,5 +28,18 @@ public final class DefaultProfileUtil {
         */
         defProperties.put(SPRING_PROFILE_DEFAULT, Constants.SPRING_PROFILE_DEVELOPMENT);
         app.setDefaultProperties(defProperties);
+    }
+
+    /**
+     * Get the profiles that are applied else get default profiles.
+     * @param env
+     * @return
+     */
+    public static String[] getActiveProfiles(Environment env) {
+        String[] profiles = env.getActiveProfiles();
+        if (profiles.length == 0) {
+            return env.getDefaultProfiles();
+        }
+        return profiles;
     }
 }
