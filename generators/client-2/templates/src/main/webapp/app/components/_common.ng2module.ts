45cef73bb04dee790aa2343d54f9701e7de7965f
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-10 15:12:15 +0530

    ng2: (shared) correct imports

--- generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
+++ generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { ProfileService } from './profiles/profile.service';
 import { PageRibbonComponent } from './profiles/page-ribbon.component';
 
-import { TruncateCharactersPipe } from './util/truncate-characters.pipe';
-import { TruncateWordsPipe } from './util/truncate-words.pipe';
-import { CapitalizePipe } from './util/capitalize.pipe';
+import { TruncateCharactersPipe } from '../shared/truncate-characters.pipe';
+import { TruncateWordsPipe } from '../shared/truncate-words.pipe';
+import { CapitalizePipe } from '../shared/capitalize.pipe';
 import { FilterPipe } from '../shared/filter.pipe';
 import { OrderByPipe } from '../shared/order-by.pipe';
 import { TranslatePipe } from '../shared/translate.pipe';
