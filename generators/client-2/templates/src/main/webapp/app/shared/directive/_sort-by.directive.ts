4c02f79eeae8d080ae3bfc2406af9aab0c3126f6
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-10 15:15:35 +0530

    ng2: (shared) move common directive

---
+++ generators/client-2/templates/src/main/webapp/app/shared/directive/_sort-by.directive.ts
@@ -0,0 +1,20 @@
+angular
+    .module('<%=angularAppName%>.common')
+    .directive('jhSortBy', jhSortBy);
+
+function jhSortBy() {
+    var directive = {
+        restrict: 'A',
+        scope: false,
+        require: '^jhSort',
+        link: linkFunc
+    };
+
+    return directive;
+
+    function linkFunc(scope, element, attrs, parentCtrl) {
+        element.bind('click', function () {
+            parentCtrl.sort(attrs.jhSortBy);
+        });
+    }
+}
