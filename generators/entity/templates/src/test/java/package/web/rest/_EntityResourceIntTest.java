22fc8574be9a0ed2ea5b66f706239c389f39407e
David Steiman <d.steiman@stytex.de>
2016-09-11 11:33:45 +0200

    exclude uaa related test support for non uaa apps

--- generators/entity/templates/src/test/java/package/web/rest/_EntityResourceIntTest.java
+++ generators/entity/templates/src/test/java/package/web/rest/_EntityResourceIntTest.java
@@ -2,8 +2,9 @@ package <%=packageName%>.web.rest;
 <% if (databaseType == 'cassandra') { %>
 import <%=packageName%>.AbstractCassandraTest;<% } %>
 import <%=packageName%>.<%= mainClass %>;
+<% if (authenticationType == 'uaa') { %>
 import <%=packageName%>.config.SecurityBeanOverrideConfiguration;
-
+<% } %>
 import <%=packageName%>.domain.<%= entityClass %>;
 <%_ for (idx in relationships) { // import entities in required relationships
         var relationshipValidate = relationships[idx].relationshipValidate;
@@ -58,7 +59,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @see <%= entityClass %>Resource
  */
 @RunWith(SpringRunner.class)
+<% if (authenticationType === 'uaa') { %>
 @SpringBootTest(classes = {<%= mainClass %>.class, SecurityBeanOverrideConfiguration.class})
+<% } else { %>
+@SpringBootTest(classes = <%= mainClass %>.class)
+<% } %>
 public class <%= entityClass %>ResourceIntTest <% if (databaseType == 'cassandra') { %>extends AbstractCassandraTest <% } %>{<%_ if (fieldsContainZonedDateTime == true) { _%>
 
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZone(ZoneId.of("Z"));<%_ } _%>
