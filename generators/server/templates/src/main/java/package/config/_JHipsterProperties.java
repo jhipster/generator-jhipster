e6c980e512580673eed2b27c872050615b28a39a
Julien Dubois <julien.dubois@gmail.com>
2016-09-12 12:07:11 +0200

    formatting

--- generators/server/templates/src/main/java/package/config/_JHipsterProperties.java
+++ generators/server/templates/src/main/java/package/config/_JHipsterProperties.java
@@ -190,6 +190,7 @@ public class JHipsterProperties {
         }
         <%_ } _%>
         <%_ if (clusteredHttpSession == 'hazelcast' || hibernateCache == 'hazelcast') { _%>
+
         public Hazelcast getHazelcast() {
             return hazelcast;
         }
@@ -227,13 +228,10 @@ public class JHipsterProperties {
 
         private final RememberMe rememberMe = new RememberMe();
         <%_ } _%>
-
-        <%_ if((applicationType === 'microservice' || applicationType === 'uaa') && authenticationType === 'uaa') { _%>
+        <%_ if ((applicationType === 'microservice' || applicationType === 'uaa') && authenticationType === 'uaa') { _%>
 
         private final LoadBalancedResourceDetails clientAuthorization = new LoadBalancedResourceDetails();
-
         <%_ } _%>
-
         <%_ if (authenticationType === 'oauth2' || authenticationType === 'jwt' || authenticationType === 'uaa') { _%>
 
         private final Authentication authentication = new Authentication();
@@ -244,35 +242,34 @@ public class JHipsterProperties {
             return rememberMe;
         }
         <%_ } _%>
-
         <%_ if (authenticationType === 'oauth2' || authenticationType === 'jwt' || authenticationType === 'uaa') { _%>
+
         public Authentication getAuthentication() {
             return authentication;
         }
+        <%_ if ((applicationType === 'microservice' || applicationType === 'uaa') && authenticationType === 'uaa') { _%>
 
-        <%_ if((applicationType === 'microservice' || applicationType === 'uaa') && authenticationType === 'uaa') { _%>
         public LoadBalancedResourceDetails getClientAuthorization() {
             return clientAuthorization;
         }
-
         <%_ } _ %>
-
         public static class Authentication {
-
             <%_ if (authenticationType == 'oauth2') { _%>
-            private final Oauth oauth = new Oauth();
 
+            private final Oauth oauth = new Oauth();
             <%_ } _%>
             <%_ if (authenticationType == 'jwt' || authenticationType == 'uaa') { _%>
-            private final Jwt jwt = new Jwt();
 
+            private final Jwt jwt = new Jwt();
             <%_ } _%>
             <%_ if (authenticationType == 'oauth2') { _%>
+
             public Oauth getOauth() {
                 return oauth;
             }
             <%_ } _%>
             <%_ if (authenticationType == 'jwt' || authenticationType == 'uaa') { _%>
+
             public Jwt getJwt() {
                 return jwt;
             }
@@ -319,6 +316,7 @@ public class JHipsterProperties {
                 private String secret;
 
                 private long tokenValidityInSeconds = 1800;
+
                 private long tokenValidityInSecondsForRememberMe = 2592000;
 
                 public String getSecret() {
@@ -348,8 +346,8 @@ public class JHipsterProperties {
             <%_ } _%>
         }
         <%_ } _%>
-
         <%_ if (authenticationType == 'session') { _%>
+
         public static class RememberMe {
 
             @NotNull
@@ -634,8 +632,8 @@ public class JHipsterProperties {
 
             public void setQueueSize(int queueSize) { this.queueSize = queueSize; }
         }
-
     <%_ if (applicationType == 'gateway' || applicationType == 'microservice') { _%>
+
         private final SpectatorMetrics spectatorMetrics = new SpectatorMetrics();
 
         public SpectatorMetrics getSpectatorMetrics() { return spectatorMetrics; }
@@ -650,8 +648,8 @@ public class JHipsterProperties {
         }
     <%_ } _%>
     }
-
     <%_ if (enableSocialSignIn) { _%>
+
     public static class Social {
 
         private String redirectAfterSignIn = "/#/home";
@@ -664,9 +662,9 @@ public class JHipsterProperties {
             this.redirectAfterSignIn = redirectAfterSignIn;
         }
     }
-
     <%_ } _%>
     <%_ if (applicationType == 'gateway') { _%>
+
     public static class Gateway {
 
         private final RateLimiting rateLimiting = new RateLimiting();
