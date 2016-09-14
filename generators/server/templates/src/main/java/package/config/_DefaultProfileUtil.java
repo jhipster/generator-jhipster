fb71497c0b970740fad5eb1b4b6d49ca625e18bd
Julien Dubois <julien.dubois@gmail.com>
2016-09-14 19:02:59 +0200

    Add Swagger profile by default

--- generators/server/templates/src/main/java/package/config/_DefaultProfileUtil.java
+++ generators/server/templates/src/main/java/package/config/_DefaultProfileUtil.java
@@ -3,8 +3,6 @@ package <%=packageName%>.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.core.env.Environment;
 
-import java.util.*;
-
 /**
  * Utility class to load a Spring profile to be used as default
  * when there is no <code>spring.profiles.active</code> set in the environment or as command line argument.
@@ -20,14 +18,7 @@ public final class DefaultProfileUtil {
      * @param app the spring application
      */
     public static void addDefaultProfile(SpringApplication app) {
-        Map<String, Object> defProperties =  new HashMap<>();
-        /*
-        * The default profile to use when no other profiles are defined
-        * This cannot be set in the <code>application.yml</code> file.
-        * See https://github.com/spring-projects/spring-boot/issues/1219
-        */
-        defProperties.put(SPRING_PROFILE_DEFAULT, Constants.SPRING_PROFILE_DEVELOPMENT);
-        app.setDefaultProperties(defProperties);
+        app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT, Constants.SPRING_PROFILE_SWAGGER);
     }
 
     /**
