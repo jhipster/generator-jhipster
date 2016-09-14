e8e053f18b075187463964821c85f2cf7a358e4d
William Marques <wmarques@ippon.fr>
2016-09-14 17:46:03 +0200

    migrate alert service

--- generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
+++ generators/client-2/templates/src/main/webapp/app/components/_common.ng2module.ts
@@ -5,7 +5,7 @@ import { <%=angular2AppName%>SharedModule } from '../shared/shared.ng2module';
 import { AlertService } from "./alert/alert.service";
 import { ProfileService } from './profiles/profile.service';
 import { PageRibbonComponent } from './profiles/page-ribbon.component';
-import { jhiAlertComponent } from "./alert/alert.directive";
+import { jhiAlertComponent } from "./alert/alert.component";
 
 @NgModule({
     imports: [
--- generators/client-2/templates/src/main/webapp/app/components/alert/_alert.service.ts
+++ generators/client-2/templates/src/main/webapp/app/components/alert/_alert.service.ts
@@ -1,133 +1,116 @@
-export function AlertService () {
-    this.toast = false;
-    /*jshint validthis: true */
-    this.$get = getService;
+import {Injectable, Sanitizer, SecurityContext} from '@angular/core';
 
-    this.showAsToast = function(isToast) {
-        this.toast = isToast;
-    };
+@Injectable()
+export class AlertService {
 
-    getService.$inject = ['$timeout', '$sce'<% if (enableTranslation) { %>, '$translate'<% } %>];
+    private alertId: number;
+    private alerts: any[];
+    private timeout: number;
+    private toast: boolean;
 
-    function getService ($timeout, $sce<% if (enableTranslation) { %>,$translate<% } %>) {
-        var toast = this.toast,
-            alertId = 0, // unique id for each alert. Starts from 0.
-            alerts = [],
-            timeout = 5000; // default timeout
-
-        return {
-            factory: factory,
-            isToast: isToast,
-            add: addAlert,
-            closeAlert: closeAlert,
-            closeAlertByIndex: closeAlertByIndex,
-            clear: clear,
-            get: get,
-            success: success,
-            error: error,
-            info: info,
-            warning : warning
-        };
-
-        function isToast() {
-            return toast;
-        }
-
-        function clear() {
-            alerts = [];
-        }
-
-        function get() {
-            return alerts;
-        }
-
-        function success(msg, params, position) {
-            return this.add({
-                type: 'success',
-                msg: msg,
-                params: params,
-                timeout: timeout,
-                toast: toast,
-                position: position
-            });
-        }
-
-        function error(msg, params, position) {
-            return this.add({
-                type: 'danger',
-                msg: msg,
-                params: params,
-                timeout: timeout,
-                toast: toast,
-                position: position
-            });
-        }
-
-        function warning(msg, params, position) {
-            return this.add({
-                type: 'warning',
-                msg: msg,
-                params: params,
-                timeout: timeout,
-                toast: toast,
-                position: position
-            });
-        }
-
-        function info(msg, params, position) {
-            return this.add({
-                type: 'info',
-                msg: msg,
-                params: params,
-                timeout: timeout,
-                toast: toast,
-                position: position
-            });
-        }
-
-        function factory(alertOptions) {
-            var alert = {
-                type: alertOptions.type,
-                msg: $sce.trustAsHtml(alertOptions.msg),
-                id: alertOptions.alertId,
-                timeout: alertOptions.timeout,
-                toast: alertOptions.toast,
-                position: alertOptions.position ? alertOptions.position : 'top right',
-                scoped: alertOptions.scoped,
-                close: function (alerts) {
-                    return closeAlert(this.id, alerts);
-                }
-            };
-            if(!alert.scoped) {
-                alerts.push(alert);
-            }
-            return alert;
-        }
-
-        function addAlert(alertOptions, extAlerts) {
-            alertOptions.alertId = alertId++;
-        <%_ if (enableTranslation) { _%>
-            alertOptions.msg = $translate.instant(alertOptions.msg, alertOptions.params);
-        <%_ } _%>
-            var that = this;
-            var alert = this.factory(alertOptions);
-            if (alertOptions.timeout && alertOptions.timeout > 0) {
-                $timeout(function () {
-                    that.closeAlert(alertOptions.alertId, extAlerts);
-                }, alertOptions.timeout);
-            }
-            return alert;
-        }
-
-        function closeAlert(id, extAlerts) {
-            var thisAlerts = extAlerts ? extAlerts : alerts;
-            return closeAlertByIndex(thisAlerts.map(function(e) { return e.id; }).indexOf(id), thisAlerts);
-        }
-
-        function closeAlertByIndex(index, thisAlerts) {
-            return thisAlerts.splice(index, 1);
-        }
+    constructor(private sanitizer: Sanitizer) {
+        this.alertId = 0; // unique id for each alert. Starts from 0.
+        this.alerts = [];
+        this.timeout = 5000; // default timeout
+        this.toast = false;
     }
 
+    clear() {
+        this.alerts = [];
+    }
 
+    get(): any {
+        return this.alerts;
+    }
+
+    success(msg, params, position): any {
+        return this.addAlert({
+            type: 'success',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    error(msg, params, position): any {
+        return this.addAlert({
+            type: 'danger',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    warning(msg, params, position): any {
+        return this.addAlert({
+            type: 'warning',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    info(msg, params, position): any {
+        return this.addAlert({
+            type: 'info',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    factory(alertOptions): any {
+        var alert = {
+            type: alertOptions.type,
+            msg: this.sanitizer.sanitize(SecurityContext.HTML, alertOptions.msg),
+            id: alertOptions.alertId,
+            timeout: alertOptions.timeout,
+            toast: alertOptions.toast,
+            position: alertOptions.position ? alertOptions.position : 'top right',
+            scoped: alertOptions.scoped,
+            close: function (alerts) {
+                return this.closeAlert(this.id, alerts);
+            }
+        };
+        if (!alert.scoped) {
+            this.alerts.push(alert);
+        }
+        return alert;
+    }
+
+    addAlert(alertOptions, extAlerts): any {
+        alertOptions.alertId = this.alertId++;
+        //alertOptions.msg = $translate.instant(alertOptions.msg, alertOptions.params);
+        var that = this;
+        var alert = this.factory(alertOptions);
+        if (alertOptions.timeout && alertOptions.timeout > 0) {
+            setTimeout(() => {
+                that.closeAlert(alertOptions.alertId, extAlerts);
+            }, alertOptions.timeout);
+        }
+        return alert;
+    }
+
+    closeAlert(id, extAlerts): any {
+        var thisAlerts = extAlerts ? extAlerts : this.alerts;
+        return this.closeAlertByIndex(thisAlerts.map(function (e) {
+            return e.id;
+        }).indexOf(id), thisAlerts);
+    }
+
+    closeAlertByIndex(index, thisAlerts): any {
+        return thisAlerts.splice(index, 1);
+    }
+
+    showAsToast(isToast: boolean) {
+        this.toast = isToast;
+    }
 }
