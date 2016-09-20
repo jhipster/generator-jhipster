81bab444bd536c427e9b2b5b3c8f80567e0fa3a7
Julien Dubois <julien.dubois@gmail.com>
2016-09-20 22:32:03 +0200

    Formatting

--- generators/server/templates/src/test/java/package/config/elasticsearch/_IndexReinitializer.java
+++ generators/server/templates/src/test/java/package/config/elasticsearch/_IndexReinitializer.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class IndexReinitializer {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Inject
     private ElasticsearchTemplate elasticsearchTemplate;
 
