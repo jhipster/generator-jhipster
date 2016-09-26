1af9fbd4902271217087c0b4e12b6b4390441fcf
Deepu k Sasidharan <d4udts@gmail.com>
2016-09-26 17:02:31 +0530

    ng2: admin: migrate metrics components

---
+++ generators/client-2/templates/src/main/webapp/app/admin/metrics/_metrics-modal.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from "@angular/core";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: '<%=jhiPrefix%>-metrics-modal',
+    templateUrl: 'app/admin/metrics/metrics-modal.html',
+    inputs: ['threadDump', 'dismiss']
+})
+export class <%=jhiPrefixCapitalized%>MetricsMonitoringModalComponent implements OnInit {
+
+    threadDump: any;
+    threadDumpAll: number = 0;
+    threadDumpBlocked: number = 0;
+    threadDumpRunnable: number = 0;
+    threadDumpTimedWaiting: number = 0;
+    threadDumpWaiting: number = 0;
+
+    ngOnInit() {
+        this.threadDump.forEach((value) => {
+            if (value.threadState === 'RUNNABLE') {
+                this.threadDumpRunnable += 1;
+            } else if (value.threadState === 'WAITING') {
+                this.threadDumpWaiting += 1;
+            } else if (value.threadState === 'TIMED_WAITING') {
+                this.threadDumpTimedWaiting += 1;
+            } else if (value.threadState === 'BLOCKED') {
+                this.threadDumpBlocked += 1;
+            }
+        });
+
+        this.threadDumpAll = this.threadDumpRunnable + this.threadDumpWaiting +
+            this.threadDumpTimedWaiting + this.threadDumpBlocked;
+    }
+
+    getLabelClass (threadState) {
+        if (threadState === 'RUNNABLE') {
+            return 'label-success';
+        } else if (threadState === 'WAITING') {
+            return 'label-info';
+        } else if (threadState === 'TIMED_WAITING') {
+            return 'label-warning';
+        } else if (threadState === 'BLOCKED') {
+            return 'label-danger';
+        }
+    }
+}
---
+++ generators/client-2/templates/src/main/webapp/app/admin/metrics/_metrics.component.ts
@@ -0,0 +1,61 @@
+import { Component, Inject, OnInit, TemplateRef } from "@angular/core";
+import { <%=jhiPrefixCapitalized%>MetricsService } from "./metrics.service";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: '<%=jhiPrefix%>-metrics',
+    templateUrl: 'app/admin/metrics/metrics.html',
+})
+export class <%=jhiPrefixCapitalized%>MetricsMonitoringComponent implements OnInit {
+    metrics: any = {};
+    cachesStats: any = {};
+    servicesStats: any = {};
+    updatingMetrics: boolean = true;
+    threadDump: any;
+
+    constructor(private modalService: NgbModal, private metricsService:<%=jhiPrefixCapitalized%>MetricsService) {}
+
+    ngOnInit() {
+        this.refresh();
+    }
+
+    refresh () {
+        this.updatingMetrics = true;
+        this.metricsService.getMetrics().subscribe((metrics) => {
+            this.metrics = metrics;
+            this.updatingMetrics = false;
+            this.servicesStats = {};
+            this.cachesStats = {};
+            Object.keys(metrics.timers).forEach((key) => {
+                let value = metrics.timers[key];
+                if (key.indexOf('web.rest') !== -1 || key.indexOf('service') !== -1) {
+                    this.servicesStats[key] = value;
+                }
+                if (key.indexOf('net.sf.ehcache.Cache') !== -1) {
+                    // remove gets or puts
+                    let index = key.lastIndexOf('.');
+                    let newKey = key.substr(0, index);
+
+                    // Keep the name of the domain
+                    index = newKey.lastIndexOf('.');
+                    this.cachesStats[newKey] = {
+                        'name': newKey.substr(index + 1),
+                        'value': value
+                    };
+                }
+            });
+        });
+    }
+
+    refreshThreadDumpData (metricsModal: TemplateRef<any>) {
+        this.metricsService.threadDump().subscribe((data) => {
+            this.threadDump = data;
+            this.modalService.open(metricsModal, { size: 'lg'}).result.then((result) => {
+                console.log(`Closed with: ${result}`);
+            }, (reason) => {
+                console.log(`Dismissed ${reason}`);
+            });
+        });
+    }
+
+}
