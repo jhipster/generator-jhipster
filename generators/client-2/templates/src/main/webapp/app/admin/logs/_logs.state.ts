04a7ecd1bd7ad26e2884b25195d9947987babc7f
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-01 15:43:39 +0530

    ng2: (admin) add logs state

--- generators/client-2/templates/src/main/webapp/app/admin/logs/_logs.state.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/logs/_logs.state.ts
@@ -1,23 +1,20 @@
-LogsStateConfig.$inject = ['$stateProvider'];
+import { LogsComponent } from './logs.component';
 
-export function LogsStateConfig($stateProvider) {
-    $stateProvider.state('logs', {
-        parent: 'admin',
-        url: '/logs',
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'logs.title'
-        },
-        views: {
-            'content@': {
-                template: '<<%=jhiPrefix%>-logs></<%=jhiPrefix%>-logs>'
-            }
-        },
-        resolve: {
-            translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                $translatePartialLoader.addPart('logs');
-                return $translate.refresh();
-            }]
-        }
-    });
+export const logsState = {
+    name: 'logs',
+    parent: 'admin',
+    url: '/logs',
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'logs.title'
+    },
+    views: {
+        'content@': { component: LogsComponent }
+    },
+    resolve: {
+        translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+            $translatePartialLoader.addPart('logs');
+            return $translate.refresh();
+        }]
+    }
 }
