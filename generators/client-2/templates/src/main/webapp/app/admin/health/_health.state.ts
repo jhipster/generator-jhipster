cc8aebf04b7448542dc2e9f75d61921d04771be0
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-01 15:41:05 +0530

    ng2: (admin) add health state

--- generators/client-2/templates/src/main/webapp/app/admin/health/_health.state.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/health/_health.state.ts
@@ -1,23 +1,20 @@
-HealthStateConfig.$inject = ['$stateProvider'];
+import { <%=jhiPrefixCapitalized%>HealthCheckComponent } from './health.component';
 
-export function HealthStateConfig($stateProvider) {
-    $stateProvider.state('<%=jhiPrefix%>-health', {
-        parent: 'admin',
-        url: '/health',
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'health.title'
-        },
-        views: {
-            'content@': {
-                template: '<<%=jhiPrefix%>-health></<%=jhiPrefix%>-health>'
-            }
-        },
-        resolve: {
-            translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                $translatePartialLoader.addPart('health');
-                return $translate.refresh();
-            }]
-        }
-    });
+export const healthState = {
+    name: '<%=jhiPrefix%>-health',
+    parent: 'admin',
+    url: '/health',
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'health.title'
+    },
+    views: {
+        'content@': { component: <%=jhiPrefixCapitalized%>HealthCheckComponent }
+    },
+    resolve: {
+        translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+            $translatePartialLoader.addPart('health');
+            return $translate.refresh();
+        }]
+    }
 }
