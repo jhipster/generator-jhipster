54b2f1827fc829aad18db8e8c3f60c65e69c8583
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-01 15:34:41 +0530

    ng2: (admin) add audits state

--- generators/client-2/templates/src/main/webapp/app/admin/audits/_audits.state.ts
+++ generators/client-2/templates/src/main/webapp/app/admin/audits/_audits.state.ts
@@ -1,23 +1,20 @@
-AuditStateConfig.$inject = ['$stateProvider'];
+import { AuditsComponent } from './audits.component';
 
-export function AuditStateConfig($stateProvider) {
-    $stateProvider.state('audits', {
-        parent: 'admin',
-        url: '/audits',
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'audits.title'
-        },
-        views: {
-            'content@': {
-                template: '<<%=jhiPrefix%>-audit></<%=jhiPrefix%>-audit>'
-            }
-        },
-        resolve: {
-            translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                $translatePartialLoader.addPart('audits');
-                return $translate.refresh();
-            }]
-        }
-    });
+export const auditState = {
+    name: 'audits',
+    parent: 'admin',
+    url: '/audits',
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'audits.title'
+    },
+    views: {
+        'content@': { component: AuditsComponent }
+    },
+    resolve: {
+        translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+            $translatePartialLoader.addPart('audits');
+            return $translate.refresh();
+        }]
+    }
 }
