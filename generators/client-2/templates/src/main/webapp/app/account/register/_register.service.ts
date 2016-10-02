393d9ac5adb6208a1b3402887421de97ebfe1e42
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-02 13:16:32 +0530

    ng2: (account) migrate register service

--- generators/client-2/templates/src/main/webapp/app/account/register/_register.service.ts
+++ generators/client-2/templates/src/main/webapp/app/account/register/_register.service.ts
@@ -1,5 +1,14 @@
-Register.$inject = ['$resource'];
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
-export function Register ($resource) {
-    return $resource(<% if(authenticationType === 'uaa') { %>'<%= uaaBaseName.toLowerCase() %>/api/register'<%} else { %>'api/register'<% } %>, {}, {});
+
+@Injectable()
+export class Register {
+
+    constructor (private http: Http) {}
+
+    save(account: any): Observable<any> {
+        return this.http.post(<% if(authenticationType === 'uaa') { %>'<%= uaaBaseName.toLowerCase() %>/api/register'<%} else { %>'api/register'<% } %>, account).map((res: Response) => res.json());
+    }
 }
