2c8d97744e7fc02422a6bee80274b2fdd5cb4a2a
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-05 17:35:00 +0530

    ng2: (admin) migrate tracker rename file

---
+++ generators/client-2/templates/src/main/webapp/app/admin/tracker/_tracker.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { <%=jhiPrefixCapitalized%>TrackerService } from './tracker.service';
+
+@Component({
+    selector: '<%=jhiPrefix%>-tracker',
+    templateUrl: './tracker.html'
+})
+export class <%=jhiPrefixCapitalized%>TrackerController implements OnInit {
+
+    constructor(private trackerService: <%=jhiPrefixCapitalized%>TrackerService){}
+
+    activities: any[] = [];
+
+    showActivity (activity: any) {
+        let existingActivity: boolean = false;
+        for (let index = 0; index < this.activities.length; index++) {
+            if(this.activities[index].sessionId === activity.sessionId) {
+                existingActivity = true;
+                if ( activity.page === 'logout' ) {
+                    this.activities.splice(index, 1);
+                } else {
+                    this.activities[index] = activity;
+                }
+            }
+        }
+        if (!existingActivity && (activity.page !== 'logout')) {
+            this.activities.push(activity);
+        }
+    }
+
+    ngOnInit() {
+        this.trackerService.receive().then(
+            ((activity) => {
+                this.showActivity(activity);
+            })
+        );
+    }
+
+}
