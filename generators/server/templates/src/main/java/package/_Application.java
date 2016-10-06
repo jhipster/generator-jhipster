855e91c333f79ab4665fa223b05252f3a3031ebe
Julien Dubois <julien.dubois@gmail.com>
2016-10-06 21:47:29 +0200

    [Sonar] remove hard-coded IP

--- generators/server/templates/src/main/java/package/_Application.java
+++ generators/server/templates/src/main/java/package/_Application.java
@@ -87,7 +87,7 @@ public class <%= mainClass %> {
         Environment env = app.run(args).getEnvironment();
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application '{}' is running! Access URLs:\n\t" +
-                "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                "Local: \t\thttp://localhost:{}\n\t" +
                 "External: \thttp://{}:{}\n----------------------------------------------------------",
             env.getProperty("spring.application.name"),
             env.getProperty("server.port"),
