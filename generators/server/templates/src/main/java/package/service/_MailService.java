88cf06e5532173f501332f5541340b4695fa3469
Julien Dubois <julien.dubois@gmail.com>
2016-10-06 21:59:23 +0200

    [Sonar] Log exceptions

--- generators/server/templates/src/main/java/package/service/_MailService.java
+++ generators/server/templates/src/main/java/package/service/_MailService.java
@@ -61,7 +61,7 @@ public class MailService {
             javaMailSender.send(mimeMessage);
             log.debug("Sent e-mail to User '{}'", to);
         } catch (Exception e) {
-            log.warn("E-mail could not be sent to user '{}', exception is: {}", to, e.getMessage());
+            log.warn("E-mail could not be sent to user '{}'", to, e);
         }
     }
 
