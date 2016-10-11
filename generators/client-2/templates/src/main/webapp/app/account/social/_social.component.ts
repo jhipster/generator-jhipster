09f3339318698548ec8c476272f9847aa17b50f7
040796 <flavien.cathala@gmail.com>
2016-10-11 12:17:58 +0200

    commit moved files

---
+++ generators/client-2/templates/src/main/webapp/app/account/social/_social.component.ts
@@ -0,0 +1,35 @@
+import { Inject, Component, Input, OnInit } from '@angular/core';
+import { SocialService } from './social.service';
+
+@Component({
+    selector: 'jh-social',
+    templateUrl: 'app/account/social/social.html'
+})
+export class JhSocialComponent implements OnInit {
+    @Input() provider: string;
+    label: string;
+    providerSetting: string;
+    providerURL: string;
+    csrf: string;
+
+        constructor (
+                    <% if (enableTranslation){ %>
+                        @Inject('$translatePartialLoader') private $translatePartialLoader,
+                        @Inject('$translate') private $translate, <% } %>
+                        //$filter,
+                        private SocialService: SocialService)
+        {}
+
+        ngOnInit() {
+            <% if (enableTranslation){ %>
+            this.$translatePartialLoader.addPart('social');
+            this.$translate.refresh();
+            <% } %>
+            //this.label = $filter('capitalize')(this.provider);
+            this.label = this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
+            this.providerSetting = this.SocialService.getProviderSetting(this.provider);
+            this.providerURL = this.SocialService.getProviderURL(this.provider);
+            this.csrf = this.SocialService.getCSRF();
+        }
+
+}
