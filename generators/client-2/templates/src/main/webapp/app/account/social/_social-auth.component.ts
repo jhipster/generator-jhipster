da5d7c19da4f537b633eb013a826f945ee06b7dc
040796 <flavien.cathala@gmail.com>
2016-10-12 09:49:37 +0200

    loginWithToken fixed

--- generators/client-2/templates/src/main/webapp/app/account/social/_social-auth.component.ts
+++ generators/client-2/templates/src/main/webapp/app/account/social/_social-auth.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { StateService } from 'ui-router-ng2';
 import { AuthService } from '../../shared';
+import { LoginService } from '../../shared';
 
 @Component({
     selector: '<%=jhiPrefix%>-register',
     templateUrl: 'app/account/register/register.html'
 })
-    export class SocialAuthComponent implements OnInit {
+export class SocialAuthComponent implements OnInit {
 
-        constructor (private $state: StateService,
-                    @Inject('$cookies') private $cookies,
-                    private Auth: AuthService
-                   ) {}
+    constructor (private $state: StateService,
+                @Inject('$cookies') private $cookies,
+                private Auth: AuthService,
+                private loginService: LoginService
+                ) {}
 
-        ngOnInit() {
-            let token = this.$cookies.get('social-authentication');
-            this.Auth.loginWithToken(token, false).then(() => {
-                this.$cookies.remove('social-authentication');
-                this.Auth.authorize(true);
-            }, () => {
-                this.$state.go('social-register', {'success': 'false'});
-            });
-        }
+    ngOnInit() {
+        let token = this.$cookies.get('social-authentication');
+        this.loginService.loginWithToken(token, false).then(() => {
+            this.$cookies.remove('social-authentication');
+            this.Auth.authorize(true);
+         }, () => {
+            this.$state.go('social-register', {'success': 'false'});
+        });
     }
+}
