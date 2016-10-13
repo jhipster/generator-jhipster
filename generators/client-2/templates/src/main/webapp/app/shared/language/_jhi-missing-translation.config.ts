7cf91d64af28b2abf2abe0037a581dc73ee547c0
Deepu k Sasidharan <d4udts@gmail.com>
2016-10-13 23:08:27 +0530

    ng2: (i18n) polish

--- generators/client-2/templates/src/main/webapp/app/shared/language/_jhi-missing-translation.config.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/language/_jhi-missing-translation.config.ts
@@ -2,6 +2,6 @@ import {MissingTranslationHandler} from 'ng2-translate/ng2-translate';
 
 export class <%=jhiPrefixCapitalized%>MissingTranslationHandler implements MissingTranslationHandler {
     handle(key: string) {
-        return 'err-no-translation-found';
+        return `translation-not-found[${key}]`;
     }
 }
