2ed890725a5cc1d2bd8d36d57a7df2342f0d0cd3
040796 <flavien.cathala@gmail.com>
2016-10-17 17:06:05 +0200

    comments removed

--- generators/client-2/templates/src/main/webapp/app/account/social/_social-register.component.ts
+++ generators/client-2/templates/src/main/webapp/app/account/social/_social-register.component.ts
@@ -14,12 +14,9 @@ export class SocialRegisterComponent implements OnInit  {
     {}
 
     ngOnInit() {
-        //let success = this.$stateParams.success;
         this.success = this.$stateParams.success;
         this.error = !this.success;
         this.provider = this.$stateParams.provider;
-        //this.providerLabel = $filter('capitalize')(this.provider);
         this.providerLabel = this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
-        //this.success = this.$stateParams.success;
     }
 }
