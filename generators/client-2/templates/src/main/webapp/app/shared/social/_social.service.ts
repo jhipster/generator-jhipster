f3efb0442b42e49c8d710a80960b07f0361187ed
040796 <flavien.cathala@gmail.com>
2016-10-17 17:08:29 +0200

    comments removed

--- generators/client-2/templates/src/main/webapp/app/shared/social/_social.service.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/social/_social.service.ts
@@ -19,18 +19,4 @@ export class SocialService {
         return 'signin/' + provider;
     }
 
-    /*getCSRF () {
-        var name = 'CSRF-TOKEN=';
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) !== -1) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return '';
-    }*/
 }
