423bbaf03c83313d327b6c84e6b77ad6787de9e4
Nikola Kološnjaji <nkolosnjaji@tmns.com>
2016-10-23 11:24:40 +0200

    Use $cookies to retrive CSRF cookie token value

--- generators/client/templates/src/main/webapp/app/account/social/_social.service.js
+++ generators/client/templates/src/main/webapp/app/account/social/_social.service.js
@@ -5,9 +5,9 @@
         .module('<%=angularAppName%>')
         .factory('SocialService', SocialService);
 
-    SocialService.$inject = ['$document'];
+    SocialService.$inject = ['$document', '$http', '$cookies'];
 
-    function SocialService ($document) {
+    function SocialService ($document, $http, $cookies) {
         var socialService = {
             getProviderSetting: getProviderSetting,
             getProviderURL: getProviderURL,
@@ -31,19 +31,7 @@
         }
 
         function getCSRF () {
-            /* globals document */
-            var name = 'XSRF-TOKEN=';
-            var ca = $document[0].cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) === ' ') {
-                    c = c.substring(1);
-                }
-                if (c.indexOf(name) !== -1) {
-                    return c.substring(name.length, c.length);
-                }
-            }
-            return '';
+            return $cookies.get($http.defaults.xsrfCookieName);
         }
     }
 })();
