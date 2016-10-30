426031660125ba3c3bd96c9a599477d380a47fc0
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-10-30 15:13:02 +0100

    Added the ::camelCase function + tests

--- test/spec/utils/string_utils_test.js
+++ test/spec/utils/string_utils_test.js
@@ -1,27 +1,62 @@
 'use strict';
 
 const expect = require('chai').expect,
+    fail = expect.fail,
+    camelCase = require('../../../lib/utils/string_utils').camelCase,
     isNilOrEmpty = require('../../../lib/utils/string_utils').isNilOrEmpty;
 
-describe('::isNilOrEmpty', function() {
-  describe('when passing a nil object', function() {
-    it('returns true', function() {
-      expect(isNilOrEmpty(null)).to.be.true;
+describe('StringUtils', () => {
+  describe('::camelCase', () => {
+    describe('when passing a valid string', () => {
+      it('camel-cases it', () => {
+        expect(camelCase('entity')).to.eq('entity');
+        expect(camelCase('Entity')).to.eq('entity');
+        expect(camelCase('EntityA')).to.eq('entityA');
+        expect(camelCase('EntityAN')).to.eq('entityAN');
+        expect(camelCase('Entity_AN')).to.eq('entityAN');
+        expect(camelCase('_entity_AN')).to.eq('entityAN');
+        expect(camelCase('_entit--y_AN---')).to.eq('entityAN');
+        expect(camelCase('En tity_AN ')).to.eq('entityAN');
+      });
+    });
+    describe('when passing an invalid parameter', () => {
+      describe('as it is nil', () => {
+        it('fails', () => {
+          try {
+            camelCase();
+            fail();
+          } catch (error) {
+            expect(error.name).to.eq('NullPointerException');
+          }
+        });
+      });
+      describe('as it is empty', () => {
+        it('returns it', () => {
+          expect(camelCase('')).to.eq('');
+        });
+      });
     });
   });
-  describe('when passing an undefined object', function() {
-    it('returns true', function() {
-      expect(isNilOrEmpty(undefined)).to.be.true;
+  describe('::isNilOrEmpty', () => {
+    describe('when passing a nil object', () => {
+      it('returns true', () => {
+        expect(isNilOrEmpty(null)).to.be.true;
+      });
     });
-  });
-  describe('when passing an empty string', function() {
-    it('returns true', function() {
-      expect(isNilOrEmpty('')).to.be.true;
+    describe('when passing an undefined object', () => {
+      it('returns true', () => {
+        expect(isNilOrEmpty(undefined)).to.be.true;
+      });
     });
-  });
-  describe('when passing a valid string', function() {
-    it('returns false', function() {
-      expect(isNilOrEmpty('ABC')).to.be.false;
+    describe('when passing an empty string', () => {
+      it('returns true', () => {
+        expect(isNilOrEmpty('')).to.be.true;
+      });
+    });
+    describe('when passing a valid string', () => {
+      it('returns false', () => {
+        expect(isNilOrEmpty('ABC')).to.be.false;
+      });
     });
   });
 });
