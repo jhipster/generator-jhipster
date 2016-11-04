8b0e6d6d8fc31346a0f55b55a54ed41457ca6123
Matt Raible <matt@raibledesigns.com>
2016-11-04 14:07:19 -0600

    Fix compiler error
    jhi-missing-translation.config.ts(3,14): error TS2420: Class 'JhiMissingTranslationHandler' incorrectly implements interface 'MissingTranslationHandler'.

--- generators/client-2/templates/src/main/webapp/app/shared/language/_jhi-missing-translation.config.ts
+++ generators/client-2/templates/src/main/webapp/app/shared/language/_jhi-missing-translation.config.ts
@@ -1,7 +1,7 @@
-import {MissingTranslationHandler} from 'ng2-translate/ng2-translate';
+import { MissingTranslationHandler, MissingTranslationHandlerParams } from 'ng2-translate';
 
 export class <%=jhiPrefixCapitalized%>MissingTranslationHandler implements MissingTranslationHandler {
-    handle(key: string) {
-        return `translation-not-found[${key}]`;
+    handle(params: MissingTranslationHandlerParams): any {
+        return `translation-not-found[${params.key}`;
     }
 }
