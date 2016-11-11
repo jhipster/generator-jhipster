cd449c9840f3e5690a1cdcbc22e0ce70b42017bf
Henri Tremblay <henri.tremblay@gmail.com>
2016-11-11 09:49:22 +0100

    Clean and build samples

--- build-samples.sh
+++ build-samples.sh
@@ -8,15 +8,38 @@ function ctrl_c() {
     exit 1
 }
 
-for dir in $(ls -1 travis/samples); do
-	echo "*********************** Running $dir"
-	pushd "travis/samples/$dir"
-	npm link generator-jhipster
-    yo jhipster --force
-    if [ -f pom.xml ]; then
-        ./mvnw verify
-    else
-        ./gradlew test
-    fi
-	popd
-done
+function usage() {
+    me=$(basename "$0")
+    echo
+    echo "Usage: $me build|clean"
+    echo
+    exit 2
+}
+
+if [ "$1" = "build" ]; then
+
+    for dir in $(ls -1 travis/samples); do
+        echo "*********************** Building $dir"
+        pushd "travis/samples/$dir"
+        npm link generator-jhipster
+        yo jhipster --force
+        if [ -f pom.xml ]; then
+            ./mvnw verify
+        else
+            ./gradlew test
+        fi
+        popd
+    done
+
+elif [ "$1" = "clean" ]; then
+
+    for dir in $(ls -1 travis/samples); do
+        echo "*********************** Cleaning $dir"
+        pushd "travis/samples/$dir"
+        ls -a | grep -v .yo-rc.json | xargs rm -rf | true
+        popd
+    done
+
+else
+    usage
+fi
