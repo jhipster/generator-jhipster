5c08e0389a5e18806f962b4973acd99887bcf3e7
040796 <flavien.cathala@gmail.com>
2016-11-16 15:57:56 +0100

    clean

--- generators/client-2/templates/src/main/webapp/app/_vendor.ts
+++ generators/client-2/templates/src/main/webapp/app/_vendor.ts
@@ -1,12 +1,4 @@
 import 'angular';
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
-import '@angular/core';
-import '@angular/common';
-import '@angular/forms';
-import '@angular/http';
-import '@angular/router';
-import '@angular/upgrade';
 
 import 'angular-aria';
 import 'angular-dynamic-locale';
@@ -18,6 +10,3 @@ import 'angular-bootstrap-npm';
 import 'jquery/dist/jquery';
 import 'ng-file-upload';
 import 'ng-infinite-scroll';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
