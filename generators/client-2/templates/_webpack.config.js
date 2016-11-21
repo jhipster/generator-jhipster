5fd3cc751a4d03a99960407b7863c095f314d119
040796 <flavien.cathala@gmail.com>
2016-11-21 13:52:58 +0100

    copyplugin
    fully fonctionnal

--- generators/client-2/templates/_webpack.config.js
+++ generators/client-2/templates/_webpack.config.js
@@ -1,15 +1,17 @@
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: {
-        'main': './src/main/webapp/app/app.main',
         'polyfills': './src/main/webapp/app/polyfills',
-        'vendor': './src/main/webapp/app/vendor'
+        'vendor': './src/main/webapp/app/vendor',
+        'main': './src/main/webapp/app/app.main'
     },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js', '.css', '.scss'],
         modules: ['node_modules']
     },
     output: {
-        path: './src/main/webapp/bin',
+        path: './target/www',
         filename: '[name].bundle.js',
         chunkFilename: '[id].chunk.js'
     },
@@ -18,6 +20,7 @@ module.exports = {
             test: /[\/]angular\.js$/,
             loader: "exports?angular"
         },
+            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
         {
             test: /\.ts$/,
             loaders: [
@@ -30,6 +33,25 @@ module.exports = {
             test: /\.html$/,
             loader: 'raw-loader',
             exclude: ['./src/main/webapp/index.html']
-        }]
-    }
+        },
+            { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff2" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/main/webapp/index.html'},
+            { from: './src/main/webapp/content', to: 'content'},
+            { from: './src/main/webapp/i18n', to: 'i18n'}
+        ]),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
  };
