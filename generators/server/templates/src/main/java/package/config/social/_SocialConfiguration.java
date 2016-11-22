9c9d81ad308b3e6a694078d57eace89635e251b4
Luca Orlandi <luca.orlandi@gmail.com>
2016-11-22 16:27:27 +0100

    Fixed missing imports

--- generators/server/templates/src/main/java/package/config/social/_SocialConfiguration.java
+++ generators/server/templates/src/main/java/package/config/social/_SocialConfiguration.java
@@ -14,7 +14,9 @@ import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurer;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.social.connect.web.SignInAdapter;
@@ -23,6 +25,7 @@ import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
 import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 // jhipster-needle-add-social-connection-factory-import-package
+
 
 import javax.inject.Inject;
 
