73a5d87bc6d41021561046eb6dd0498edeafc27a
VictorADS <victor.ads75@gmail.com>
2016-12-14 14:10:17 +0100

    Finish moving to bs4 in admins

--- generators/client/templates/angular/src/main/webapp/app/admin/metrics/_metrics-modal.component.ts
+++ generators/client/templates/angular/src/main/webapp/app/admin/metrics/_metrics-modal.component.ts
@@ -33,15 +33,15 @@ export class <%=jhiPrefixCapitalized%>MetricsMonitoringModalComponent implements
             this.threadDumpTimedWaiting + this.threadDumpBlocked;
     }
 
-    getLabelClass (threadState) {
+    getTagClass (threadState) {
         if (threadState === 'RUNNABLE') {
-            return 'label-success';
+            return 'tag-success';
         } else if (threadState === 'WAITING') {
-            return 'label-info';
+            return 'tag-info';
         } else if (threadState === 'TIMED_WAITING') {
-            return 'label-warning';
+            return 'tag-warning';
         } else if (threadState === 'BLOCKED') {
-            return 'label-danger';
+            return 'tag-danger';
         }
     }
 }
