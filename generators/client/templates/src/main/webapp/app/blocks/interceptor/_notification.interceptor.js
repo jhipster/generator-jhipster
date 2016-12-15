3eecf5b7968010b9eaa4b463f580c54fcfd83446
Robert Ohajda <rohajda@users.noreply.github.com>
2016-12-15 16:59:28 +0100

    Cherry-pick correction from #4687
    Fix #4680 on the 3.x maintenance branch

--- generators/client/templates/src/main/webapp/app/blocks/interceptor/_notification.interceptor.js
+++ generators/client/templates/src/main/webapp/app/blocks/interceptor/_notification.interceptor.js
@@ -16,7 +16,7 @@
 
         function response (response) {
             var headers = Object.keys(response.headers()).filter(function (header) {
-                return header.endsWith('app-alert') || header.endsWith('app-params')
+                return header.indexOf('app-alert', header.length - 'app-alert'.length) !== -1 || header.indexOf('app-params', header.length - 'app-params'.length) !== -1;
             }).sort();
             var alertKey = response.headers(headers[0]);
             if (angular.isString(alertKey)) {
