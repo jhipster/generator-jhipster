40eb3b894acc623016e54ca9675d2a029c344b67
VictorADS <victor.ads75@gmail.com>
2016-12-16 17:31:45 +0100

    Refactoring

--- generators/client/templates/angular/src/main/webapp/app/shared/service/_pagination-util.service.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/service/_pagination-util.service.ts
@@ -6,14 +6,20 @@ export class PaginationUtil  {
     parseAscending (sort: string) {
         let sortArray = sort.split(',');
         if (sortArray.length > 1){
-            return sort.split(',').slice(-1)[0] === 'asc';
+            return sortArray.slice(-1)[0] === 'asc';
         } else {
-            sortArray = sort.split('%2C');
-            if (sortArray.length > 1){
-                return sort.split('%2C').slice(-1)[0] === 'asc';
-            }
-            return true;
+            return this.parseAscendingEncoded(sort);
         }
+    }
+
+    parseAscendingEncoded (sort: string) {
+        console.log("Je parse encoded");
+        let sortArray = sort.split('%2C');
+        if (sortArray.length > 1){
+            return sortArray.slice(-1)[0] === 'asc';
+        }
+        // default to true if no sort defined
+        return true;
     }
 
     // query params are strings, and need to be parsed
