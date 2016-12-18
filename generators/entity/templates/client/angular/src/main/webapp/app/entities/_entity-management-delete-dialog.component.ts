941825107d21fc482d984f70ec0dcefac85f7fbd
deepu105 <d4udts@gmail.com>
2016-12-18 18:31:24 +0100

    add ng2 entity files

---
+++ generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management-delete-dialog.component.ts
@@ -0,0 +1,24 @@
+angular
+    .module('<%=angularAppName%>')
+    .controller('<%= entityAngularJSName %>DeleteController',<%= entityAngularJSName %>DeleteController);
+
+<%= entityAngularJSName %>DeleteController.$inject = ['$uibModalInstance', 'entity', '<%= entityClass %>'];
+
+function <%= entityAngularJSName %>DeleteController($uibModalInstance, entity, <%= entityClass %>) {
+    var vm = this;
+
+    vm.<%= entityInstance %> = entity;
+    vm.clear = clear;
+    vm.confirmDelete = confirmDelete;
+
+    function clear () {
+        $uibModalInstance.dismiss('cancel');
+    }
+
+    function confirmDelete (id) {
+        <%= entityClass %>.delete({id: id},
+            function () {
+                $uibModalInstance.close(true);
+            });
+    }
+}
---
+++ generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity.service.ts
@@ -0,0 +1,46 @@
+<%_ var hasDate = false;
+    if (fieldsContainZonedDateTime || fieldsContainLocalDate) {
+        hasDate = true;
+    }
+_%>
+angular
+    .module('<%=angularAppName%>')
+    .factory('<%= entityClass %>', <%= entityClass %>);
+
+<%= entityClass %>.$inject = ['$resource'<% if (hasDate) { %>, 'DateUtils'<% } %>];
+
+function <%= entityClass %> ($resource<% if (hasDate) { %>, DateUtils<% } %>) {
+    var resourceUrl = <% if (applicationType == 'gateway' && locals.microserviceName) {%> '<%= microserviceName.toLowerCase() %>/' +<% } %> 'api/<%= entityApiUrl %>/:id';
+
+    return $resource(resourceUrl, {}, {
+        'query': { method: 'GET', isArray: true},
+        'get': {
+            method: 'GET',
+            transformResponse: function (data) {
+                if (data) {
+                    data = angular.fromJson(data);<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertLocalDateFromServer(data.<%=fields[idx].fieldName%>);<% }if (fields[idx].fieldType == 'ZonedDateTime') { %>
+                    data.<%=fields[idx].fieldName%> = DateUtils.convertDateTimeFromServer(data.<%=fields[idx].fieldName%>);<% } }%>
+                }
+                return data;
+            }
+        },<% if (fieldsContainLocalDate) { %>
+        'update': {
+            method: 'PUT',
+            transformRequest: function (data) {
+                var copy = angular.copy(data);<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                copy.<%=fields[idx].fieldName%> = DateUtils.convertLocalDateToServer(copy.<%=fields[idx].fieldName%>);<% } }%>
+                return angular.toJson(copy);
+            }
+        },
+        'save': {
+            method: 'POST',
+            transformRequest: function (data) {
+                var copy = angular.copy(data);<% for (idx in fields) { if (fields[idx].fieldType == 'LocalDate') { %>
+                copy.<%=fields[idx].fieldName%> = DateUtils.convertLocalDateToServer(copy.<%=fields[idx].fieldName%>);<% } }%>
+                return angular.toJson(copy);
+            }
+        }<% } else { %>
+        'update': { method:'PUT' }<% } %>
+    });
+}
