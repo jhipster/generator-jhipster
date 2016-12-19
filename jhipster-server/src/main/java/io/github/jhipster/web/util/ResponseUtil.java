5d46d2ac64936f0b6733216d9ca55ad5c52d0b06
Julien Dubois <julien.dubois@gmail.com>
2016-12-19 23:13:23 +0100

    Reformat & add licence header

--- jhipster-server/src/main/java/io/github/jhipster/web/util/ResponseUtil.java
+++ jhipster-server/src/main/java/io/github/jhipster/web/util/ResponseUtil.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.jhipster.web.util;
 
 import java.util.Optional;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 /**
  * Utility class for ResponseEntity creation.
@@ -28,7 +42,7 @@ public final class ResponseUtil {
      */
     public static <X> ResponseEntity<X> wrapOrNotFound(Optional<X> maybeResponse, HttpHeaders header) {
         return maybeResponse.map(response -> ResponseEntity.ok().headers(header).body(response))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
