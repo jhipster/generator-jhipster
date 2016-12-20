ba3ee22067c3ba3e5aae4f78ca3c2cc5ebc363d5
VictorADS <victor.ads75@gmail.com>
2016-12-20 12:05:56 +0100

    Fix for pagination

--- generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management.component.ts
+++ generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management.component.ts
@@ -6,6 +6,7 @@ import { StateService } from 'ui-router-ng2';
 import { <%= entityClass %> } from './<%= entityFileName %>.model';
 import { <%= entityClass %>Service } from './<%= entityFileName %>.service';
 import { EventManager, AlertService, ITEMS_PER_PAGE, ParseLinks, Principal, PaginationUtil } from '../../shared';
+import { PaginationConfig } from "../../blocks/config/uib-pagination.config";
 
 @Component({
     selector: '<%= entityFileName %>-mgmt',
