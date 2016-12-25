31843f635d1f217481dfab7e736f5b681545d1a3
SendilKumar N <sendilkumarn@live.com>
2016-12-25 18:37:12 +0800

    added sort logic from sort by directive

--- generators/client/templates/angular/src/main/webapp/app/shared/directive/_sort-by.directive.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/directive/_sort-by.directive.ts
@@ -5,15 +5,36 @@ import { <%=jhiPrefixCapitalized%>SortDirective } from './sort.directive';
     selector: '[<%=jhiPrefix%>SortBy]'
 })
 export class <%=jhiPrefixCapitalized%>SortByDirective {
+
     @Input() <%=jhiPrefix%>SortBy: string;
 
     <%=jhiPrefix%>Sort: <%=jhiPrefixCapitalized%>SortDirective;
 
-    constructor(@Host() <%=jhiPrefix%>Sort: <%=jhiPrefixCapitalized%>SortDirective) {
+    constructor(@Host() <%=jhiPrefix%>Sort: <%=jhiPrefixCapitalized%>SortDirective,  private el: ElementRef, private _renderer: Renderer) {
         this.<%=jhiPrefix%>Sort = <%=jhiPrefix%>Sort;
     }
 
     @HostListener('click') onClick() {
-        this.<%=jhiPrefix%>Sort.sort(this.<%=jhiPrefix%>SortBy);
+    	if (this.<%=jhiPrefix%>Sort.predicate && this.<%=jhiPrefix%>Sort.predicate !== '_score') {
+        	this.<%=jhiPrefix%>Sort.sort(this.<%=jhiPrefix%>SortBy);
+        	this.applyClass();
+        }
+    }
+
+    applyClass () {
+        let childSpan = this.el.nativeElement.children[1],
+            sortDesc = 'fa-sort-desc',
+            add = 'fa-sort-asc';
+        if (!this.<%=jhiPrefix%>Sort.ascending) {
+            add = sortDesc;
+        }
+        this.resetClasses(childSpan);
+        this._renderer.setElementClass(childSpan, add, true);
+    }
+
+    resetClasses (childSpan) {
+        let remove = childSpan.className;
+        let classesToRemove = remove.split(' ');
+        this._renderer.setElementClass(childSpan, classesToRemove[1], false);
     }
 }
