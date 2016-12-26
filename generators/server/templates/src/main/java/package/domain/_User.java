2f6a874864474242488467fd2ce8e87f9e6158e3
Ramzi Maalej <ramzi.maalej@gmail.com>
2016-12-26 18:27:19 -0500

    Change backend to expose profile photo as imageUrl

--- generators/server/templates/src/main/java/package/domain/_User.java
+++ generators/server/templates/src/main/java/package/domain/_User.java
@@ -97,6 +97,12 @@ public class User<% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
     @Column(name = "lang_key")<% } %>
     private String langKey;
 
+    @Size(max = 256)<% if (databaseType == 'sql') { %>
+    @Column(name = "image_url", length = 256)<% } %><% if (databaseType == 'mongodb') { %>
+    @Field("image_url")<% } %><% if (databaseType == 'cassandra') { %>
+    @Column(name = "image_url")<% } %>
+    private String imageUrl;
+
     @Size(max = 20)<% if (databaseType == 'sql') { %>
     @Column(name = "activation_key", length = 20)<% } %><% if (databaseType == 'mongodb') { %>
     @Field("activation_key")<% } %><% if (databaseType == 'cassandra') { %>
@@ -182,6 +188,14 @@ public class User<% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public boolean getActivated() {
@@ -278,6 +292,7 @@ public class User<% if (databaseType == 'sql' || databaseType == 'mongodb') { %>
             ", firstName='" + firstName + '\'' +
             ", lastName='" + lastName + '\'' +
             ", email='" + email + '\'' +
+            ", imageUrl='" + imageUrl + '\'' +
             ", activated='" + activated + '\'' +
             ", langKey='" + langKey + '\'' +
             ", activationKey='" + activationKey + '\'' +
