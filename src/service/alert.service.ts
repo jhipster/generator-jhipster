da9e2aea66c5679c66a75305b7279faa81717dd0
Deepu K Sasidharan <d4udts@gmail.com>
2017-01-08 13:25:14 +0100

    add alert service

---
+++ src/service/alert.service.ts
@@ -0,0 +1,119 @@
+import { Injectable, Sanitizer, SecurityContext } from '@angular/core';
+import { TranslateService } from 'ng2-translate/ng2-translate';
+import { ConfigHelper } from '../helper';
+
+@Injectable()
+export class AlertService {
+
+    private alertId: number;
+    private alerts: any[];
+    private timeout: number;
+    private i18nEnabled: boolean;
+
+    constructor(private sanitizer: Sanitizer, private translateService: TranslateService, private toast: boolean) {
+        this.i18nEnabled = ConfigHelper.getConfig().i18nEnabled;
+        this.alertId = 0; // unique id for each alert. Starts from 0.
+        this.alerts = [];
+        this.timeout = 5000; // default timeout
+    }
+
+    clear() {
+        this.alerts = [];
+    }
+
+    get(): any {
+        return this.alerts;
+    }
+
+    success(msg, params?, position?): any {
+        return this.addAlert({
+            type: 'success',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    error(msg, params?, position?): any {
+        return this.addAlert({
+            type: 'danger',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    warning(msg, params?, position?): any {
+        return this.addAlert({
+            type: 'warning',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    info(msg, params?, position?): any {
+        return this.addAlert({
+            type: 'info',
+            msg: msg,
+            params: params,
+            timeout: this.timeout,
+            toast: this.toast,
+            position: position
+        }, []);
+    }
+
+    factory(alertOptions): any {
+        let alert = {
+            type: alertOptions.type,
+            msg: this.sanitizer.sanitize(SecurityContext.HTML, alertOptions.msg),
+            id: alertOptions.alertId,
+            timeout: alertOptions.timeout,
+            toast: alertOptions.toast,
+            position: alertOptions.position ? alertOptions.position : 'top right',
+            scoped: alertOptions.scoped,
+            close: (alerts) => {
+                return this.closeAlert(alertOptions.alertId, alerts);
+            }
+        };
+        if (!alert.scoped) {
+            this.alerts.push(alert);
+        }
+        return alert;
+    }
+
+    addAlert(alertOptions, extAlerts): any {
+        alertOptions.alertId = this.alertId++;
+        if (this.i18nEnabled && alertOptions.msg) {
+            alertOptions.msg = this.translateService.instant(alertOptions.msg, alertOptions.params);
+        }
+        let alert = this.factory(alertOptions);
+        if (alertOptions.timeout && alertOptions.timeout > 0) {
+            setTimeout(() => {
+                this.closeAlert(alertOptions.alertId, extAlerts);
+            }, alertOptions.timeout);
+        }
+        return alert;
+    }
+
+    closeAlert(id, extAlerts?): any {
+        let thisAlerts = extAlerts ? extAlerts : this.alerts;
+        return this.closeAlertByIndex(thisAlerts.map(function (e) {
+            return e.id;
+        }).indexOf(id), thisAlerts);
+    }
+
+    closeAlertByIndex(index, thisAlerts): any {
+        return thisAlerts.splice(index, 1);
+    }
+
+    isToast(): boolean {
+        return this.toast;
+    }
+}
---
+++ tests/service/alert.service.spec.ts
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TestBed, inject } from '@angular/core/testing';
+import { Sanitizer } from '@angular/core';
+import { TranslateService } from 'ng2-translate/ng2-translate';
+
+import { AlertService } from '../../src/service/alert.service';
+import { ConfigHelper } from '../../src/helper';
+
+class MockTranslateService extends TranslateService {
+    instant(msg, params) {
+        return msg;
+    }
+}
+
+function mockAlertService(sanitizer: Sanitizer) {
+    return new AlertService(sanitizer, new MockTranslateService(null, null, null), false);
+}
+
+ConfigHelper.setModuleConfigOptions({
+    i18nEnabled: false
+});
+
+describe('Alert service test', () => {
+
+    describe('Alert Service Test', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                providers: [{
+                    provide: AlertService, useFactory: mockAlertService, deps: [Sanitizer]
+                }]
+            });
+        });
+
+        it('should produce a proper alert object and fetch it', inject([AlertService], (service: AlertService) => {
+            expect(service.addAlert({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                params: {},
+                timeout: 3000,
+                toast: true,
+                position: 'top left'
+            }, [])).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                id: 0,
+                timeout: 3000,
+                toast: true,
+                position: 'top left',
+                scoped: undefined
+            }));
+
+            expect(service.get().length).toBe(1);
+            expect(service.get()).toContain(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                id: 0,
+                timeout: 3000,
+                toast: true,
+                position: 'top left',
+                scoped: undefined
+            }));
+        }));
+
+        it('should produce an alert object with correct id', inject([AlertService], (service: AlertService) => {
+            service.info('Hello Jhipster info');
+            expect(service.success('Hello Jhipster success')).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster success',
+                id: 1
+            }));
+
+            expect(service.get().length).toBe(2);
+            expect(service.get()).toContain(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster success',
+                id: 1
+            }));
+        }));
+
+        it('should close an alert correctly', inject([AlertService], (service: AlertService) => {
+            service.info('Hello Jhipster info');
+            service.info('Hello Jhipster info 2');
+            expect(service.success('Hello Jhipster success')).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster success',
+                id: 2
+            }));
+
+            expect(service.get().length).toBe(3);
+            service.closeAlert(1);
+            expect(service.get().length).toBe(2);
+            expect(service.get()).not.toContain(jasmine.objectContaining({
+                type: 'info',
+                msg: 'Hello Jhipster info 2',
+                id: 1
+            }));
+            service.closeAlert(2);
+            expect(service.get().length).toBe(1);
+            expect(service.get()).not.toContain(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster success',
+                id: 2
+            }));
+            service.closeAlert(0);
+            expect(service.get().length).toBe(0);
+            expect(service.get()).not.toContain(jasmine.objectContaining({
+                type: 'info',
+                msg: 'Hello Jhipster info',
+                id: 0
+            }));
+        }));
+
+        it('should clear alerts', inject([AlertService], (service: AlertService) => {
+            service.info('Hello Jhipster info');
+            service.error('Hello Jhipster info');
+            service.success('Hello Jhipster info');
+            expect(service.get().length).toBe(3);
+            service.clear();
+            expect(service.get().length).toBe(0);
+        }));
+
+        it('should produce a scoped alert', inject([AlertService], (service: AlertService) => {
+            expect(service.addAlert({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                params: {},
+                timeout: 3000,
+                toast: true,
+                position: 'top left',
+                scoped: true
+            }, [])).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                id: 0,
+                timeout: 3000,
+                toast: true,
+                position: 'top left',
+                scoped: true
+            }));
+
+            expect(service.get().length).toBe(0);
+        }));
+
+        it('should produce a success message', inject([AlertService], (service: AlertService) => {
+            expect(service.success('Hello Jhipster')).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster'
+            }));
+        }));
+
+        it('should produce a success message with custom position', inject([AlertService], (service: AlertService) => {
+            expect(service.success('Hello Jhipster', {}, 'bottom left')).toEqual(jasmine.objectContaining({
+                type: 'success',
+                msg: 'Hello Jhipster',
+                position: 'bottom left',
+            }));
+        }));
+
+        it('should produce a error message', inject([AlertService], (service: AlertService) => {
+            expect(service.error('Hello Jhipster')).toEqual(jasmine.objectContaining({
+                type: 'danger',
+                msg: 'Hello Jhipster'
+            }));
+        }));
+
+        it('should produce a warning message', inject([AlertService], (service: AlertService) => {
+            expect(service.warning('Hello Jhipster')).toEqual(jasmine.objectContaining({
+                type: 'warning',
+                msg: 'Hello Jhipster'
+            }));
+        }));
+
+        it('should produce a info message', inject([AlertService], (service: AlertService) => {
+            expect(service.info('Hello Jhipster')).toEqual(jasmine.objectContaining({
+                type: 'info',
+                msg: 'Hello Jhipster'
+            }));
+        }));
+    });
+});
