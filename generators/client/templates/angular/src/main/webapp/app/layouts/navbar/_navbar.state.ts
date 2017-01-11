5dcfcebaa071957557b26f524a62c289bb266a06
SendilKumar N <sendilkumarn@live.com>
2017-01-11 20:12:20 +0800

    linting and merge fixes

--- generators/client/templates/angular/src/main/webapp/app/layouts/navbar/_navbar.state.ts
+++ generators/client/templates/angular/src/main/webapp/app/layouts/navbar/_navbar.state.ts
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { Ng2StateDeclaration } from "ui-router-ng2";
-import { JhiLanguageService } from 'ng-jhipster';
-import { NavbarComponent } from './layouts';
-import { AuthService } from './shared';
-
-export const appState: Ng2StateDeclaration = {
-    name: 'app',
-    abstract: true,
-    views: {
-        'navbar@': { component: NavbarComponent }
-    },
-    resolve: [
-        {
-            token: 'authorize',
-            deps: [AuthService],
-            resolveFn: (auth) => auth.authorize()
-        }<% if (enableTranslation) { %>,
-        {
-            token: 'translate',
-            deps: [JhiLanguageService],
-            resolveFn: (languageService: JhiLanguageService) => languageService.setLocations([])
-        }<% } %>
-    ]
-};
-=======
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
 
@@ -52,4 +26,3 @@ export const navbarRoute: Routes = [
     outlet: 'navbar'
   }
 ];
->>>>>>> Changed from state to route - layouts
