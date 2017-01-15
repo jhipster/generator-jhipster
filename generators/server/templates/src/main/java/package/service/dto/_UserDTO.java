68b70c34794c9828cea421cc17b7032e242994d4
Clément Tamisier <clement.tamisier@gmail.com>
2017-01-15 22:06:30 +0100

    fix many-to-many relationship when User is the target Entity

--- generators/server/templates/src/main/java/package/service/dto/_UserDTO.java
+++ generators/server/templates/src/main/java/package/service/dto/_UserDTO.java
@@ -109,6 +109,10 @@ public class UserDTO {
         return login;
     }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public String getFirstName() {
         return firstName;
     }
