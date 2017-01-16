17dc8b93e17a11aa9e7f2ccf1fdb9ee3c893cbad
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-01-16 10:11:10 -0800

    fix indentation for eslint [ci skip]

--- generators/client/templates/angularjs/src/main/webapp/app/admin/user-management/_user-management.state.js
+++ generators/client/templates/angularjs/src/main/webapp/app/admin/user-management/_user-management.state.js
@@ -112,26 +112,26 @@
             }]
         })
         .state('user-management-detail', {
-                    parent: 'user-management',
-                    url: '/user/:login',
-                    data: {
-                        authorities: ['ROLE_ADMIN'],
-                        pageTitle: 'user-management.detail.title'
-                    },
-                    views: {
-                        'content@': {
-                            templateUrl: 'app/admin/user-management/user-management-detail.html',
-                            controller: 'UserManagementDetailController',
-                            controllerAs: 'vm'
-                        }
-                    },
-                    resolve: {
-                        translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                            $translatePartialLoader.addPart('user-management');
-                            return $translate.refresh();
-                        }]
-                    }
-                })
+            parent: 'user-management',
+            url: '/user/:login',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'user-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/admin/user-management/user-management-detail.html',
+                    controller: 'UserManagementDetailController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('user-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
         .state('user-management.delete', {
             url: '/{login}/delete',
             data: {
