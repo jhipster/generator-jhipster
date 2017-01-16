5cb6baa0225e0239641d1d0ca5b90cdaca72bcc8
Julien Dubois <julien.dubois@gmail.com>
2017-01-16 17:55:10 +0100

    Try to have Sonarqube understand the Karma test coverage

--- travis/scripts/06-sonar.sh
+++ travis/scripts/06-sonar.sh
@@ -6,6 +6,10 @@
 cd "$HOME"/app
 if [ "$JHIPSTER" == "app-default-from-scratch" ]; then
     if [ "$TRAVIS_REPO_SLUG" = "jhipster/generator-jhipster" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-        ./mvnw clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
+        ./mvnw clean
+        if [ -f "gulpfile.js" ]; then
+            gulp test --no-notification
+        fi
+        ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
     fi
 fi
