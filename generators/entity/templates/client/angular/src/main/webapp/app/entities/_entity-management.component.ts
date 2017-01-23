acdfb74cb7952e32dcae34cfd1a1ae9709f3ef01
Julien Dubois <julien.dubois@gmail.com>
2017-01-23 12:10:14 +0100

    Fix lint warning

--- generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management.component.ts
+++ generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management.component.ts
@@ -15,11 +15,11 @@ import { PaginationConfig } from '../../blocks/config/uib-pagination.config';
 })
 export class <%= entityAngularJSName %>Component implements OnInit {
     <%_ if (pagination === 'pagination' || pagination === 'pager') { _%>
-    <%- include('pagination-template'); -%>
+<%- include('pagination-template'); -%>
     <%_ } else if (pagination === 'infinite-scroll') { _%>
-    <%- include('infinite-scroll-template'); -%>
+<%- include('infinite-scroll-template'); -%>
     <%_ } else if (pagination === 'no') { _%>
-    <%- include('no-pagination-template'); -%>
+<%- include('no-pagination-template'); -%>
     <%_ } _%>
     ngOnInit() {
         this.loadAll();
