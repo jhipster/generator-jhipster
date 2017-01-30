e995539a78204f42f96ddcdc31809a22859af39b
Julien Dubois <julien.dubois@gmail.com>
2017-01-30 09:34:02 +0100

    Change the order of authentication questions, so JWT is on top of OAuth2 as it’s far more popular

--- generators/server/prompts.js
+++ generators/server/prompts.js
@@ -68,12 +68,12 @@ function askForServerSideOpts() {
                     name: 'HTTP Session Authentication (stateful, default Spring Security mechanism)'
                 },
                 {
-                    value: 'oauth2',
-                    name: 'OAuth2 Authentication (stateless, with an OAuth2 server implementation)'
-                },
-                {
                     value: 'jwt',
                     name: 'JWT authentication (stateless, with a token)'
+                },
+                {
+                    value: 'oauth2',
+                    name: 'OAuth2 Authentication (stateless, with an OAuth2 server implementation)'
                 }
             ],
             default: 0
