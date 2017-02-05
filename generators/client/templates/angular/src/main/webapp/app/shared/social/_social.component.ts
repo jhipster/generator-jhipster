caa8128c5f25e755c096460d014cea15a99b6f91
Eelco Brolman <e.brolman@telecats.nl>
2017-02-05 02:44:25 +0100

    added the register location to the languageService in social
    Fixes #5102

--- generators/client/templates/angular/src/main/webapp/app/shared/social/_social.component.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/social/_social.component.ts
@@ -21,7 +21,7 @@ export class <%=jhiPrefixCapitalized%>SocialComponent implements OnInit {
     ) {}
 
     ngOnInit() {
-        this.languageService.setLocations(['social']);
+        this.languageService.setLocations(['social', 'register']);
         this.label = this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
         this.providerSetting = this.socialService.getProviderSetting(this.provider);
         this.providerURL = this.socialService.getProviderURL(this.provider);
