4fe4199721908c5d3cd831e78e8cd2e75ca59649
Mohamed <mohamed.aitabderrahman@gmail.com>
2017-02-06 15:44:46 +0000

    add SessionsComponent karma spec

--- generators/client/files-angular.js
+++ generators/client/files-angular.js
@@ -413,6 +413,7 @@ const files = {
                 'spec/app/account/password/_password.component.spec.ts',
                 'spec/app/account/password/_password-strength-bar.component.spec.ts',
                 'spec/app/account/register/_register.component.spec.ts',
+                'spec/app/account/sessions/_sessions.component.spec.ts',
                 'spec/app/account/settings/_settings.component.spec.ts',
                 'spec/app/admin/health/_health.component.spec.ts',
                 'spec/helpers/_spyobject.ts',
