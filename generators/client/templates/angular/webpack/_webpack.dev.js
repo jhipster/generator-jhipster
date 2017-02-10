539cb7b19dba0ae06037b96cc3f8f0963eb91b21
Hippolyte Durix <hdurix@ippon.fr>
2017-02-10 22:36:52 +0100

    polish webpack configuration files
    correct indentation & js style

--- generators/client/templates/angular/webpack/_webpack.dev.js
+++ generators/client/templates/angular/webpack/_webpack.dev.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(ddlPath)) {
     execSync('webpack --config webpack/webpack.vendor.js');
 }
 
-module.exports = webpackMerge(commonConfig({env: ENV}), {
+module.exports = webpackMerge(commonConfig({ env: ENV }), {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './<%= BUILD_DIR %>www',
@@ -39,7 +39,7 @@ module.exports = webpackMerge(commonConfig({env: ENV}), {
         }<% } %>]
     },
     output: {
-        path: path.resolve('<%= BUILD_DIR %>www') ,
+        path: path.resolve('<%= BUILD_DIR %>www'),
         filename: '[name].bundle.js',
         chunkFilename: '[id].chunk.js'
     },
