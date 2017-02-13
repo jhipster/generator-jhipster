dc4aa0dd8600c2bc52fcfcffc9c3fdbe00808d21
atomfrede <atomfrede@gmail.com>
2017-02-13 07:05:12 +0100

    remove debugging console.log statements close #5179

--- generators/client/templates/angular/src/main/webapp/app/admin/metrics/_metrics.component.ts
+++ generators/client/templates/angular/src/main/webapp/app/admin/metrics/_metrics.component.ts
@@ -64,9 +64,9 @@ export class <%=jhiPrefixCapitalized%>MetricsMonitoringComponent implements OnIn
             const modalRef  = this.modalService.open(<%=jhiPrefixCapitalized%>MetricsMonitoringModalComponent, { size: 'lg'});
             modalRef.componentInstance.threadDump = data;
             modalRef.result.then((result) => {
-                console.log(`Closed with: ${result}`);
+                // Left blank intentionally, nothing to do here
             }, (reason) => {
-                console.log(`Dismissed ${reason}`);
+                // Left blank intentionally, nothing to do here
             });
         });
     }
--- generators/client/templates/angular/src/main/webapp/app/admin/user-management/_user-modal.service.ts
+++ generators/client/templates/angular/src/main/webapp/app/admin/user-management/_user-modal.service.ts
@@ -31,11 +31,9 @@ export class UserModalService {
         let modalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static'});
         modalRef.componentInstance.user = user;
         modalRef.result.then(result => {
-            console.log(`Closed with: ${result}`);
             this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true });
             this.isOpen = false;
         }, (reason) => {
-            console.log(`Dismissed ${reason}`);
             this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true });
             this.isOpen = false;
         });
