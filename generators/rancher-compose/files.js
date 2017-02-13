dfc956dce13f7070455066931bf4a87038a3cb12
Pierre Besson <pbesson@ippon.fr>
2017-02-13 14:26:26 +0100

    generate files at the root of the folder rather than in the rancher directory

--- generators/rancher-compose/files.js
+++ generators/rancher-compose/files.js
@@ -1,26 +1,17 @@
 'use strict';
 
-const mkdirp = require('mkdirp');
-
-/* Constants use throughout */
-const RANCHER_DIR = 'rancher/';
-
 module.exports = {
     writeFiles
 };
 
 function writeFiles() {
     return {
-        setUpRancherDir() {
-            mkdirp(RANCHER_DIR);
-        },
-
         writeRancherCompose: function() {
-            this.template('_rancher-compose.yml', RANCHER_DIR + 'rancher-compose.yml');
+            this.template('_rancher-compose.yml','rancher-compose.yml');
         },
 
         writeDockerCompose: function() {
-            this.template('_docker-compose.yml', RANCHER_DIR + 'docker-compose.yml');
+            this.template('_docker-compose.yml', 'docker-compose.yml');
         }
     };
 }
