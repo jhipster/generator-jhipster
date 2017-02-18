aee07a2e8c23c8e063a7189899929dcd960971b0
SendilKumar N <sendilkumarn@live.com>
2017-02-18 13:40:06 +0800

    renaming identity variable to remove the conflicts

--- generators/client/templates/angular/src/main/webapp/app/shared/auth/_principal.service.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/auth/_principal.service.ts
@@ -8,7 +8,7 @@ import { <%=jhiPrefixCapitalized%>TrackerService } from '../tracker/tracker.serv
 
 @Injectable()
 export class Principal {
-    private identity: any;
+    private identityObj: any;
     private authenticated = false;
     private authenticationState = new Subject<any>();
 
@@ -18,18 +18,18 @@ export class Principal {
     ) {}
 
     authenticate (identity) {
-        this.identity = identity;
+        this.identityObj = identity;
         this.authenticated = identity !== null;
-        this.authenticationState.next(this.identity);
+        this.authenticationState.next(this.identityObj);
     }
 
     hasAnyAuthority (authorities: string[]): Promise<boolean> {
-        if (!this.authenticated || !this.identity || !this.identity.authorities) {
+        if (!this.authenticated || !this.identityObj || !this.identityObj.authorities) {
             return Promise.resolve(false);
         }
 
         for (let i = 0; i < authorities.length; i++) {
-            if (this.identity.authorities.indexOf(authorities[i]) !== -1) {
+            if (this.identityObj.authorities.indexOf(authorities[i]) !== -1) {
                 return Promise.resolve(true);
             }
         }
@@ -51,38 +51,38 @@ export class Principal {
 
     identity (force?: boolean): Promise<any> {
         if (force === true) {
-            this.identity = undefined;
+            this.identityObj = undefined;
         }
 
-        // check and see if we have retrieved the identity data from the server.
+        // check and see if we have retrieved the identityObj     data from the server.
         // if we have, reuse it by immediately resolving
-        if (this.identity) {
-            return Promise.resolve(this.identity);
+        if (this.identityObj) {
+            return Promise.resolve(this.identityObj);
         }
 
-        // retrieve the identity data from the server, update the identity object, and then resolve.
+        // retrieve the identityObj data from the server, update the identity object, and then resolve.
         return this.account.get().toPromise().then(account => {
             if (account) {
-                this.identity = account;
+                this.identityObj = account;
                 this.authenticated = true;
                 <%_ if (websocket === 'spring-websocket') { _%>
                 this.trackerService.connect();
                 <%_ } _%>
             } else {
-                this.identity = null;
+                this.identityObj = null;
                 this.authenticated = false;
             }
-            this.authenticationState.next(this.identity);
-            return this.identity;
+            this.authenticationState.next(this.identityObj);
+            return this.identityObj;
         }).catch(err => {
             <%_ if (websocket === 'spring-websocket') { _%>
             if (this.trackerService.stompClient && this.trackerService.stompClient.connected) {
                 this.trackerService.disconnect();
             }
             <%_ } _%>
-            this.identity = null;
+            this.identityObj = null;
             this.authenticated = false;
-            this.authenticationState.next(this.identity);
+            this.authenticationState.next(this.identityObj);
             return null;
         });
     }
@@ -92,7 +92,7 @@ export class Principal {
     }
 
     isIdentityResolved (): boolean {
-        return this.identity !== undefined;
+        return this.identityObj !== undefined;
     }
 
     getAuthenticationState(): Observable<any> {
@@ -100,6 +100,6 @@ export class Principal {
     }
 
     getImageUrl(): String {
-        return this.isIdentityResolved () ? this.identity.imageUrl : null;
+        return this.isIdentityResolved () ? this.identityObj.imageUrl : null;
     }
 }
