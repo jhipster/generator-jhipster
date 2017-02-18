4ab9f76d8cf7b655f21236b01e721cd4f537aefb
Hippolyte Durix <hdurix@ippon.fr>
2017-02-18 15:01:30 +0100

    Filter anonymous user on the backend
    When getting all the users, the anonymous user is ignored

--- generators/server/templates/src/main/java/package/config/_Constants.java
+++ generators/server/templates/src/main/java/package/config/_Constants.java
@@ -9,6 +9,7 @@ public final class Constants {
     public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
 
     public static final String SYSTEM_ACCOUNT = "system";
+    public static final String ANONYMOUS_USER = "anonymoususer";
 
     private Constants() {
     }
