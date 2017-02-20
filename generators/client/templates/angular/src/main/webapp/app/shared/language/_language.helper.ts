11495631ff45c1f51f5083f2b6e8b9f970972467
SendilKumar N <sendilkumarn@live.com>
2017-02-20 15:06:22 +0800

    fixing translate to have default app name as title

--- generators/client/templates/angular/src/main/webapp/app/shared/language/_language.helper.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/language/_language.helper.ts
@@ -44,7 +44,7 @@ export class JhiLanguageHelper {
     }
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
-        let title: string = (routeSnapshot.data && routeSnapshot.data['pageTitle']) ? routeSnapshot.data['pageTitle'] : '';
+        let title: string = (routeSnapshot.data && routeSnapshot.data['pageTitle']) ? routeSnapshot.data['pageTitle'] : '<%= angularAppName %>';
         if (routeSnapshot.firstChild) {
             title = this.getPageTitle(routeSnapshot.firstChild) || title;
         }
