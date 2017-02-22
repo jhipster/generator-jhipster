952ca43aaca261f533eb20da64a2538447d99049
Henri Tremblay <henri.tremblay@gmail.com>
2017-02-21 21:14:16 -0500

    Hibernate factory preventing spontaneous cache creation

--- jhipster-server/src/main/java/io/github/jhipster/config/jcache/NoDefaultJCacheRegionFactory.java
+++ jhipster-server/src/main/java/io/github/jhipster/config/jcache/NoDefaultJCacheRegionFactory.java
@@ -22,25 +22,20 @@ import java.util.Properties;
 import org.hibernate.boot.spi.SessionFactoryOptions;
 import org.hibernate.cache.CacheException;
 import org.hibernate.cache.jcache.JCacheRegionFactory;
+import org.hibernate.cache.spi.CacheDataDescription;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 
+import javax.cache.Cache;
+
 /**
- * Special Hibernate region factory that will convert a Spring URI (e.g. classpath:ehcache.xml) to a real URI (e.g.
- * file://ehcache.xml).
+ * Extends the default {@code JCacheRegionFactory} but makes sure all caches are already existing to prevent
+ * spontaneous creation of badly configured caches (e.g. {@code new MutableConfiguration()}.
  */
-public class SpringCacheRegionFactory extends JCacheRegionFactory {
+public class NoDefaultJCacheRegionFactory extends JCacheRegionFactory {
 
     @Override
-    public void start(SessionFactoryOptions options, Properties properties) throws CacheException {
-        // Translate the Spring URI to a real URI
-        String uri = properties.getProperty(CONFIG_URI);
-        Resource resource = new DefaultResourceLoader().getResource(uri);
-        try {
-            properties.setProperty(CONFIG_URI, resource.getURI().toString());
-        } catch (IOException e) {
-            throw new CacheException(e);
-        }
-        super.start(options, properties);
+    protected Cache<Object, Object> createCache(String regionName, Properties properties, CacheDataDescription metadata) {
+        throw new IllegalStateException("All Hibernate caches should be created upfront. Please update CacheConfiguration.java with the missing cache");
     }
 }
