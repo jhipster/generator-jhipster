c3adf697f94c8f4ad428b7b50c07719da1fefc8e
Alexandru-Constantin Bledea <contact@apixandru.com>
2017-02-23 22:02:23 +0200

    fix typos

--- generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
+++ generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
@@ -35,7 +35,7 @@ public class LoggingAspect {
      */
     @Pointcut("within(<%=packageName%>.repository..*) || within(<%=packageName%>.service..*) || within(<%=packageName%>.web.rest..*)")
     public void loggingPointcut() {
-        // Method is empty as this is just a Poincut, the implementations are in the advices.
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
     /**
