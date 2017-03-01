6576238fe318fbeb12c1125948738d924c1f7b2c
Deepu K Sasidharan <d4udts@gmail.com>
2017-03-01 14:56:52 +0530

    es6 update modules

--- generators/modules/index.js
+++ generators/modules/index.js
@@ -1,13 +1,13 @@
 'use strict';
-var util = require('util'),
-    generator = require('yeoman-generator'),
-    _ = require('lodash'),
-    chalk = require('chalk'),
-    scriptBase = require('../generator-base');
+const util = require('util');
+const generator = require('yeoman-generator');
+const _ = require('lodash');
+const chalk = require('chalk');
+const scriptBase = require('../generator-base');
 
 const constants = require('../generator-constants');
 
-var ModulesGenerator = generator.extend({});
+const ModulesGenerator = generator.extend({});
 
 util.inherits(ModulesGenerator, scriptBase);
 
@@ -15,56 +15,56 @@ module.exports = ModulesGenerator.extend({
     constructor: function () {
         generator.apply(this, arguments);
 
-        var jhipsterVar = this.options.jhipsterVar;
-        var jhipsterFunc = this.options.jhipsterFunc;
-        if (jhipsterVar === undefined || jhipsterVar.moduleName === undefined) {
+        const jhipsterconst = this.options.jhipsterconst;
+        const jhipsterFunc = this.options.jhipsterFunc;
+        if (jhipsterconst === undefined || jhipsterconst.moduleName === undefined) {
             this.error(chalk.red('This sub-generator must be used by JHipster modules, and the module name is not defined.'));
         }
 
-        this.log('Composing JHipster configuration with module ' + chalk.red(jhipsterVar.moduleName));
+        this.log('Composing JHipster configuration with module ' + chalk.red(jhipsterconst.moduleName));
 
-        var baseName = this.config.get('baseName');
-        var packageName = this.config.get('packageName');
-        var packageFolder = this.config.get('packageFolder');
+        const baseName = this.config.get('baseName');
+        const packageName = this.config.get('packageName');
+        const packageFolder = this.config.get('packageFolder');
 
         if (!this.options.skipValidation && (baseName === undefined || packageName === undefined)) {
             this.log(chalk.red('ERROR! There is no existing JHipster configuration file in this directory.'));
-            this.error('JHipster ' + jhipsterVar.moduleName + ' is a JHipster module, and needs a .yo-rc.json configuration file made by JHipster.');
+            this.error('JHipster ' + jhipsterconst.moduleName + ' is a JHipster module, and needs a .yo-rc.json configuration file made by JHipster.');
         }
-        // add required JHipster variables
-        jhipsterVar.baseName = this.baseName = baseName;
-        jhipsterVar.packageName = packageName;
-        jhipsterVar.packageFolder = packageFolder;
+        // add required JHipster constiables
+        jhipsterconst.baseName = this.baseName = baseName;
+        jhipsterconst.packageName = packageName;
+        jhipsterconst.packageFolder = packageFolder;
 
-        jhipsterVar.jhipsterConfig = this.config.getAll();
-        jhipsterVar.applicationType = this.config.get('applicationType');
-        jhipsterVar.authenticationType = this.config.get('authenticationType');
-        jhipsterVar.hibernateCache = this.config.get('hibernateCache');
-        jhipsterVar.clusteredHttpSession = this.config.get('clusteredHttpSession');
-        jhipsterVar.websocket = this.config.get('websocket');
-        jhipsterVar.databaseType = this.config.get('databaseType');
-        jhipsterVar.devDatabaseType = this.config.get('devDatabaseType');
-        jhipsterVar.prodDatabaseType = this.config.get('prodDatabaseType');
-        jhipsterVar.searchEngine = this.config.get('searchEngine');
-        jhipsterVar.useSass = this.config.get('useSass');
-        jhipsterVar.buildTool = this.config.get('buildTool');
-        jhipsterVar.enableTranslation = this.config.get('enableTranslation');
-        jhipsterVar.nativeLanguage = this.config.get('nativeLanguage');
-        jhipsterVar.languages = this.config.get('languages');
-        jhipsterVar.enableSocialSignIn = this.config.get('enableSocialSignIn');
-        jhipsterVar.testFrameworks = this.config.get('testFrameworks');
-        jhipsterVar.jhiPrefix = this.config.get('jhiPrefix');
-        jhipsterVar.jhiPrefixCapitalized = _.upperFirst(jhipsterVar.jhiPrefix);
-        jhipsterVar.jhipsterVersion = this.config.get('jhipsterVersion');
-        jhipsterVar.serverPort = this.config.get('serverPort');
-        jhipsterVar.clientFramework = this.config.get('clientFramework');
+        jhipsterconst.jhipsterConfig = this.config.getAll();
+        jhipsterconst.applicationType = this.config.get('applicationType');
+        jhipsterconst.authenticationType = this.config.get('authenticationType');
+        jhipsterconst.hibernateCache = this.config.get('hibernateCache');
+        jhipsterconst.clusteredHttpSession = this.config.get('clusteredHttpSession');
+        jhipsterconst.websocket = this.config.get('websocket');
+        jhipsterconst.databaseType = this.config.get('databaseType');
+        jhipsterconst.devDatabaseType = this.config.get('devDatabaseType');
+        jhipsterconst.prodDatabaseType = this.config.get('prodDatabaseType');
+        jhipsterconst.searchEngine = this.config.get('searchEngine');
+        jhipsterconst.useSass = this.config.get('useSass');
+        jhipsterconst.buildTool = this.config.get('buildTool');
+        jhipsterconst.enableTranslation = this.config.get('enableTranslation');
+        jhipsterconst.nativeLanguage = this.config.get('nativeLanguage');
+        jhipsterconst.languages = this.config.get('languages');
+        jhipsterconst.enableSocialSignIn = this.config.get('enableSocialSignIn');
+        jhipsterconst.testFrameworks = this.config.get('testFrameworks');
+        jhipsterconst.jhiPrefix = this.config.get('jhiPrefix');
+        jhipsterconst.jhiPrefixCapitalized = _.upperFirst(jhipsterconst.jhiPrefix);
+        jhipsterconst.jhipsterVersion = this.config.get('jhipsterVersion');
+        jhipsterconst.serverPort = this.config.get('serverPort');
+        jhipsterconst.clientFramework = this.config.get('clientFramework');
 
-        jhipsterVar.angularAppName = this.getAngularAppName();
-        jhipsterVar.mainClassName = this.getMainClassName();
-        jhipsterVar.javaDir = constants.SERVER_MAIN_SRC_DIR + packageFolder + '/';
-        jhipsterVar.resourceDir = constants.SERVER_MAIN_RES_DIR;
-        jhipsterVar.webappDir = constants.CLIENT_MAIN_SRC_DIR;
-        jhipsterVar.CONSTANTS = constants;
+        jhipsterconst.angularAppName = this.getAngularAppName();
+        jhipsterconst.mainClassName = this.getMainClassName();
+        jhipsterconst.javaDir = constants.SERVER_MAIN_SRC_DIR + packageFolder + '/';
+        jhipsterconst.resourceDir = constants.SERVER_MAIN_RES_DIR;
+        jhipsterconst.webappDir = constants.CLIENT_MAIN_SRC_DIR;
+        jhipsterconst.CONSTANTS = constants;
 
         // alias fs and log methods so that we can use it in script-base when invoking functions from jhipsterFunc context in modules
         jhipsterFunc.fs = this.fs;
@@ -142,7 +142,7 @@ module.exports = ModulesGenerator.extend({
     },
 
     initializing: function () {
-        var insight = this.insight();
+        const insight = this.insight();
         insight.trackWithEvent('generator', 'modules');
 
         this.log('Reading the JHipster project configuration for your module');
