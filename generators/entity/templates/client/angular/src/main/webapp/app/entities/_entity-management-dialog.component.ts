af43e0458f018310d4b64e23e226bc9ef5d8fe1b
William Marques <wmarques@ippon.fr>
2017-03-08 00:26:35 +0100

    Fix rebase issue

--- generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management-dialog.component.ts
+++ generators/entity/templates/client/angular/src/main/webapp/app/entities/_entity-management-dialog.component.ts
@@ -136,11 +136,9 @@ export class <%= entityAngularName %>DialogComponent implements OnInit {
         if (this.<%= entityInstance %>.id !== undefined) {
             this.<%= entityInstance %>Service.update(this.<%= entityInstance %>)
                 .subscribe((res: <%= entityAngularName %>) =>
-                    this.onSaveSuccess(res), (res: Response) => this.onSaveError(res.json()));
+                    this.onSaveSuccess(res), (res: Response) => this.onSaveError(res));
         } else {
             this.<%= entityInstance %>Service.create(this.<%= entityInstance %>)
-                .subscribe((res: <%= entityAngularName %>) =>
-                    this.onSaveSuccess(res), (res: Response) => this.onSaveError(res.json()));
                 .subscribe((res: <%= entityAngularName %>) => 
                     this.onSaveSuccess(res), (res: Response) => this.onSaveError(res));
         }
