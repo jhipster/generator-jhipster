1c22504c88ef45e54d8216d57ea26193f8fbf7ca
cbornet <cbornet@hotmail.com>
2017-03-07 17:19:56 +0100

    Initialize Mongo using MongoTemplate

--- generators/server/templates/src/main/java/package/config/dbmigrations/_InitialSetupMigration.java
+++ generators/server/templates/src/main/java/package/config/dbmigrations/_InitialSetupMigration.java
@@ -1,11 +1,17 @@
 package <%=packageName%>.config.dbmigrations;
 
+import <%=packageName%>.domain.Authority;
+import <%=packageName%>.domain.User;
+import <%=packageName%>.security.AuthoritiesConstants;
+
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.time.ZonedDateTime;
 import java.util.*;
 
 /**
@@ -13,16 +19,6 @@ import java.util.*;
  */
 @ChangeLog(order = "001")
 public class InitialSetupMigration {
-
-    private Map<String, String>[] authoritiesUser = new Map[] { new HashMap<>() };
-
-    private Map<String, String>[] authoritiesAdminAndUser = new Map[] { new HashMap<>(), new HashMap<>() };
-
-    {
-        authoritiesUser[0].put("_id", "ROLE_USER");
-        authoritiesAdminAndUser[0].put("_id", "ROLE_USER");
-        authoritiesAdminAndUser[1].put("_id", "ROLE_ADMIN");
-    }
 
 <%_ if (databaseType === 'mongodb' && authenticationType === 'oauth2') { _%>
     private Map<String, String>[] grantAuthorities = new Map[] { new HashMap<>(), new HashMap<>() };
@@ -36,79 +32,72 @@ public class InitialSetupMigration {
 
 <%_ } _%>
     @ChangeSet(order = "01", author = "initiator", id = "01-addAuthorities")
-    public void addAuthorities(DB db) {
-        DBCollection authorityCollection = db.getCollection("jhi_authority");
-        authorityCollection.insert(
-            BasicDBObjectBuilder.start()
-                .add("_id", "ROLE_ADMIN")
-                .get());
-        authorityCollection.insert(
-            BasicDBObjectBuilder.start()
-                .add("_id", "ROLE_USER")
-                .get());
+    public void addAuthorities(MongoTemplate mongoTemplate) {
+        mongoTemplate.save(new Authority(AuthoritiesConstants.ADMIN));
+        mongoTemplate.save(new Authority(AuthoritiesConstants.USER));
     }
 
     @ChangeSet(order = "02", author = "initiator", id = "02-addUsers")
-    public void addUsers(DB db) {
-        DBCollection usersCollection = db.getCollection("jhi_user");
-        usersCollection.createIndex("login");
-        usersCollection.createIndex("email");
-        usersCollection.insert(BasicDBObjectBuilder.start()
-            .add("_id", "user-0")
-            .add("login", "system")
-            .add("password", "$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG")
-            .add("first_name", "")
-            .add("last_name", "System")
-            .add("email", "system@localhost")
-            .add("activated", "true")
-            .add("lang_key", "en")
-            .add("created_by", "system")
-            .add("created_date", new Date())
-            .add("authorities", authoritiesAdminAndUser)
-            .get()
-        );
-        usersCollection.insert(BasicDBObjectBuilder.start()
-            .add("_id", "user-1")
-            .add("login", "anonymoususer")
-            .add("password", "$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO")
-            .add("first_name", "Anonymous")
-            .add("last_name", "User")
-            .add("email", "anonymous@localhost")
-            .add("activated", "true")
-            .add("lang_key", "en")
-            .add("created_by", "system")
-            .add("created_date", new Date())
-            .add("authorities", new Map[]{})
-            .get()
-        );
-        usersCollection.insert(BasicDBObjectBuilder.start()
-            .add("_id", "user-2")
-            .add("login", "admin")
-            .add("password", "$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC")
-            .add("first_name", "admin")
-            .add("last_name", "Administrator")
-            .add("email", "admin@localhost")
-            .add("activated", "true")
-            .add("lang_key", "en")
-            .add("created_by", "system")
-            .add("created_date", new Date())
-            .add("authorities", authoritiesAdminAndUser)
-            .get()
-        );
-        usersCollection.insert(BasicDBObjectBuilder.start()
-            .add("_id", "user-3")
-            .add("login", "user")
-            .add("password", "$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K")
-            .add("first_name", "")
-            .add("last_name", "User")
-            .add("email", "user@localhost")
-            .add("activated", "true")
-            .add("lang_key", "en")
-            .add("created_by", "system")
-            .add("created_date", new Date())
-            .add("authorities", authoritiesUser)
-            .get()
-        );
+    public void addUsers(MongoTemplate mongoTemplate) {
+        Authority adminAuthority = new Authority(AuthoritiesConstants.ADMIN);
+        Authority userAuthority = new Authority(AuthoritiesConstants.USER);
+
+        User systemUser = new User();
+        systemUser.setId("user-0");
+        systemUser.setLogin("system");
+        systemUser.setPassword("$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG");
+        systemUser.setFirstName("");
+        systemUser.setLastName("System");
+        systemUser.setEmail("system@localhost");
+        systemUser.setActivated(true);
+        systemUser.setLangKey("en");
+        systemUser.setCreatedBy(systemUser.getLogin());
+        systemUser.setCreatedDate(ZonedDateTime.now());
+        systemUser.getAuthorities().add(adminAuthority);
+        systemUser.getAuthorities().add(userAuthority);
+        mongoTemplate.save(systemUser);
+
+        User anonymousUser = new User();
+        anonymousUser.setId("user-1");
+        anonymousUser.setLogin("anonymoususer");
+        anonymousUser.setPassword("$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO");
+        anonymousUser.setFirstName("Anonymous");
+        anonymousUser.setLastName("User");
+        anonymousUser.setEmail("anonymous@localhost");
+        anonymousUser.setActivated(true);
+        anonymousUser.setLangKey("en");
+        anonymousUser.setCreatedBy(systemUser.getLogin());
+        anonymousUser.setCreatedDate(ZonedDateTime.now());
+        mongoTemplate.save(anonymousUser);
+
+        User adminUser = new User();
+        adminUser.setId("user-2");
+        adminUser.setLogin("admin");
+        adminUser.setPassword("$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC");
+        adminUser.setFirstName("admin");
+        adminUser.setLastName("Administrator");
+        adminUser.setEmail("admin@localhost");
+        adminUser.setActivated(true);
+        adminUser.setLangKey("en");
+        adminUser.setCreatedBy(systemUser.getLogin());
+        adminUser.setCreatedDate(ZonedDateTime.now());
+        adminUser.getAuthorities().add(adminAuthority);
+        adminUser.getAuthorities().add(userAuthority);
+        mongoTemplate.save(adminUser);
+
+        User userUser = new User();
+        userUser.setId("user-3");
+        userUser.setLogin("user");
+        userUser.setPassword("$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K");
+        userUser.setFirstName("");
+        userUser.setLastName("User");
+        userUser.setEmail("user@localhost");
+        userUser.setActivated(true);
+        userUser.setLangKey("en");
+        userUser.setCreatedBy(systemUser.getLogin());
+        userUser.setCreatedDate(ZonedDateTime.now());
+        userUser.getAuthorities().add(userAuthority);
+        mongoTemplate.save(userUser);
     }
 <%_ if (enableSocialSignIn) { _%>
     @ChangeSet(author = "initiator", id = "03-addSocialUserConnection", order = "03")
