01c6f6cfe6a369955cb528e1dd22b807cffd66a3
Deepu K Sasidharan <d4udts@gmail.com>
2017-03-08 12:19:33 +0100

    improve build sample scripts

--- travis/build-samples.sh
+++ travis/build-samples.sh
@@ -18,14 +18,29 @@ function usage() {
 
 function buildProject() {
     dir=$1
+    JHIPSTER=$dir
+    APP_FOLDER="$JHIPSTER_SAMPLES/$dir"
+    echo "*********************** Copying entities for $dir"
+    source ./scripts/01-generate-entities.sh
     echo "*********************** Building $dir"
-    pushd "$mydir/samples/$dir"
-    npm link generator-jhipster
-    yo jhipster --force
-    if [ -f pom.xml ]; then
-        ./mvnw verify
-    else
-        ./gradlew test
+    pushd "$APP_FOLDER"
+    yarn link generator-jhipster
+    yo jhipster --force --no-insight --skip-checks --with-entities
+    echo "*********************** Testing $dir"
+    if [ -f "mvnw" ]; then
+        ./mvnw test \
+            -Dlogging.level.io.github.jhipster.sample=ERROR \
+            -Dlogging.level.io.github.jhipster.travis=ERROR
+    elif [ -f "gradlew" ]; then
+        ./gradlew test \
+            -Dlogging.level.io.github.jhipster.sample=ERROR \
+            -Dlogging.level.io.github.jhipster.travis=ERROR
+    fi
+    if [ -f "gulpfile.js" ]; then
+        gulp test --no-notification
+    fi
+    if [ -f "tsconfig.json" ]; then
+        yarn run test
     fi
     popd
 }
@@ -33,19 +48,20 @@ function buildProject() {
 function cleanProject() {
     dir=$1
     echo "*********************** Cleaning $dir"
-    pushd "$mydir/samples/$dir"
+    pushd "$JHIPSTER_SAMPLES/$dir"
     ls -a | grep -v .yo-rc.json | xargs rm -rf | true
     popd
 }
 
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+JHIPSTER_SAMPLES="$mydir/samples"
 
 if [ "$1" = "build" ]; then
     if [ "$2" != "" ]; then
         buildProject "$2"
     else
-        for dir in $(ls -1 "$mydir/samples"); do
-            if [ -f "$mydir/samples/$dir/.yo-rc.json" ]; then
+        for dir in $(ls -1 "$JHIPSTER_SAMPLES"); do
+            if [ -f "$JHIPSTER_SAMPLES/$dir/.yo-rc.json" ]; then
                 buildProject "$dir"
             else
                 echo "$dir: Not a JHipster project. Skipping"
@@ -56,8 +72,8 @@ elif [ "$1" = "clean" ]; then
     if [ "$2" != "" ]; then
         cleanProject "$2"
     else
-        for dir in $(ls -1 "$mydir/samples"); do
-            if [ -f "$mydir/samples/$dir/.yo-rc.json" ]; then
+        for dir in $(ls -1 "$JHIPSTER_SAMPLES"); do
+            if [ -f "$JHIPSTER_SAMPLES/$dir/.yo-rc.json" ]; then
                 cleanProject "$dir"
             else
                 echo "$dir: Not a JHipster project. Skipping"
