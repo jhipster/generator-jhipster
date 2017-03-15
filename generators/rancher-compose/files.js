58efc4c35a7839ab69cb225fd8d6e9c7fda68869
Pierre Besson <pbesson@ippon.fr>
2017-03-15 19:47:32 +0100

    add a 'registry-config' sidekick container to serve the registry config from a volume

--- generators/rancher-compose/files.js
+++ generators/rancher-compose/files.js
@@ -12,6 +12,13 @@ function writeFiles() {
 
         writeDockerCompose: function() {
             this.template('_docker-compose.yml', 'docker-compose.yml');
+        },
+
+        writeRegistrySidekickFiles: function() {
+            if (this.serviceDiscoveryType === 'eureka' || this.serviceDiscoveryType === 'consul') {
+                this.copy('registry-config-sidekick/Dockerfile', 'registry-config-sidekick/Dockerfile');
+                this.template('registry-config-sidekick/_application.yml', 'registry-config-sidekick/application.yml');
+            }
         }
     };
 }
