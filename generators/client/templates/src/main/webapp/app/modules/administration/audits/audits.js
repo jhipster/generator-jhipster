f6e697b97465e15d4c0c6dac011970b1de56f127
SendilKumar N <sendilkumarn@live.com>
2017-03-22 16:52:46 +0800

    Added fixed data table component
    Added route for audit
    
    Added table content cell component for datatable
    
    revamping audits
    
    linting
    
    removed data-table
    
    Adds Configuration
    
    changed from state

--- generators/client/templates/src/main/webapp/app/modules/administration/audits/audits.js
+++ generators/client/templates/src/main/webapp/app/modules/administration/audits/audits.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Translate from 'react-translate-component';
 
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+
 import { getAudits } from '../../reducers/administration';
 
 export class AuditsPage extends Component {
@@ -22,22 +24,38 @@ export class AuditsPage extends Component {
   }
 
   render() {
+    const showCheckboxes = false;
     const { audits, isFetching } = this.props;
     return (
       <div className="well">
         <div>
-          <h2 translate="health.title">Audits</h2>
-          <p>
-            <button type="button" onClick={() => this.getAuditList()} className={isFetching ? 'btn btn-danger' : 'btn btn-primary'} disabled={isFetching}>
-              <span className="glyphicon glyphicon-refresh" />&nbsp;
-              <Translate component="span" content="health.refresh.button" />
-            </button>
-          </p>
-          FIX ME datatable, pagination and filter by date
+          <h2 translate="audits.title">Audits</h2>
+          FIX ME pagination and filter by date and sorting
           <hr />
           <div className="row">
-            <div className="col-sm-10">
-              {JSON.stringify(audits)}
+            <div className="col-sm-12">
+              <Table>
+                <TableHeader
+                  displaySelectAll={showCheckboxes}
+                  adjustForCheckbox={showCheckboxes}>
+                  <TableRow>
+                    <TableHeaderColumn>Timestamp</TableHeaderColumn>
+                    <TableHeaderColumn>Principal</TableHeaderColumn>
+                    <TableHeaderColumn>Address</TableHeaderColumn>
+                    <TableHeaderColumn>Type</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={showCheckboxes}>
+                  {audits.map((row, index) => (
+                    <TableRow key={index}>
+                      <TableRowColumn>{row.timestamp}</TableRowColumn>
+                      <TableRowColumn>{row.principal}</TableRowColumn>
+                      <TableRowColumn>{row.data.remoteAddress}</TableRowColumn>
+                      <TableRowColumn>{row.type}</TableRowColumn>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </div>
           </div>
         </div>
