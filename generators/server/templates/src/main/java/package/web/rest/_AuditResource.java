298d1d4c812647683ec831b75b2cb51542961d77
Julien Dubois <julien.dubois@gmail.com>
2017-03-23 09:57:54 +0100

    Remove unused imports

--- generators/server/templates/src/main/java/package/web/rest/_AuditResource.java
+++ generators/server/templates/src/main/java/package/web/rest/_AuditResource.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.util.List;
 
