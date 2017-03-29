3609489c0cfd14f9f28a46555f099adcbc29b539
Julien Dubois <julien.dubois@gmail.com>
2017-03-29 19:02:04 +0200

    Change code to make Sonar happy
    See #5489

--- generators/server/templates/src/main/java/package/web/rest/errors/_CustomParameterizedException.java
+++ generators/server/templates/src/main/java/package/web/rest/errors/_CustomParameterizedException.java
@@ -24,13 +24,13 @@ public class CustomParameterizedException extends RuntimeException {
     private static final String PARAM = "param";
 
     private final String message;
-    private Map<String, String> paramMap;
+
+    private final Map<String, String> paramMap = new HashMap<>();
 
     public CustomParameterizedException(String message, String... params) {
         super(message);
         this.message = message;
         if (params != null && params.length > 0) {
-            this.paramMap = new HashMap<>();
             for (int i = 0; i < params.length; i++) {
                 paramMap.put(PARAM + i, params[i]);
             }
@@ -40,11 +40,10 @@ public class CustomParameterizedException extends RuntimeException {
     public CustomParameterizedException(String message, Map<String, String> paramMap) {
         super(message);
         this.message = message;
-        this.paramMap = paramMap;
+        this.paramMap.putAll(paramMap);
     }
 
     public ParameterizedErrorVM getErrorVM() {
         return new ParameterizedErrorVM(message, paramMap);
     }
-
 }
