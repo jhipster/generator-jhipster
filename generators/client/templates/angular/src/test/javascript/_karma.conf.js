effa8ff2b5ae527e6a8fd8f2a115100824ed6b0a
Julien Dubois <julien.dubois@gmail.com>
2017-04-04 00:17:29 +0200

    Add Karma notifier

--- generators/client/templates/angular/src/test/javascript/_karma.conf.js
+++ generators/client/templates/angular/src/test/javascript/_karma.conf.js
@@ -87,11 +87,17 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'junit', 'progress', 'karma-remap-istanbul'],
+        reporters: ['dots', 'junit', 'progress', 'karma-remap-istanbul', 'notify'],
 
         junitReporter: {
             outputFile: '../../../../<%= BUILD_DIR %>test-results/karma/TESTS-results.xml'
         },
+
+        notifyReporter: {
+            reportEachFailure: true, // Default: false, will notify on every failed sepc
+            reportSuccess: true // Default: true, will notify when a suite was successful
+        },
+
 
         remapIstanbulReporter: {
             reports: { // eslint-disable-line
