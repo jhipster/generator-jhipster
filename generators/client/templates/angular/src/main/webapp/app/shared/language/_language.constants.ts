038a65052931c41750828e6e2b831724696e254b
Julien Dubois <julien.dubois@gmail.com>
2017-04-12 18:46:20 +0200

    Add Apache 2 license headers on all template files

--- generators/client/templates/angular/src/main/webapp/app/shared/language/_language.constants.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/language/_language.constants.ts
@@ -1,3 +1,21 @@
+<%#
+ Copyright 2013-2017 the original author or authors.
+
+ This file is part of the JHipster project, see https://jhipster.github.io/
+ for more information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-%>
 /*
     Languages codes are ISO_639-1 codes, see http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     They are written in English to avoid character encoding issues (not a perfect solution)
