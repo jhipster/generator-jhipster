e522a8063d1abd49e56711788418dbb13eea6869
SendilKumar N <sendilkumarn@live.com>
2017-04-19 23:15:52 +0800

    minor linting

--- generators/client/templates/angular/src/main/webapp/app/blocks/interceptor/_auth-expired.interceptor.ts
+++ generators/client/templates/angular/src/main/webapp/app/blocks/interceptor/_auth-expired.interceptor.ts
@@ -70,6 +70,7 @@ export class AuthExpiredInterceptor extends HttpInterceptor {
         });
     }
 <%_ } if (authenticationType === 'session') { _%>
+    
     responseIntercept(observable: Observable<Response>): Observable<Response> {
         return <Observable<Response>> observable.catch((error) => {
             <%_ // TODO this is ng1 way...the ng2 would be more like someRouterService.subscribe(url).forEach.. this needs to be updated _%>
