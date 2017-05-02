d9bc71c847653b8733063c8a4c5b822d3e72027e
cbornet <cbornet@hotmail.com>
2017-05-02 17:08:36 +0200

    Use Instant as type for tracker activity's timestamp

--- generators/server/templates/src/main/java/package/web/websocket/_ActivityService.java
+++ generators/server/templates/src/main/java/package/web/websocket/_ActivityService.java
@@ -18,7 +18,6 @@
 -%>
 package <%=packageName%>.web.websocket;
 
-import <%=packageName%>.security.SecurityUtils;
 import <%=packageName%>.web.websocket.dto.ActivityDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,13 +30,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.security.Principal;
-import java.util.Calendar;
+import java.time.Instant;
 
 import static <%=packageName%>.config.WebsocketConfiguration.IP_ADDRESS;
 
@@ -45,8 +39,6 @@ import static <%=packageName%>.config.WebsocketConfiguration.IP_ADDRESS;
 public class ActivityService implements ApplicationListener<SessionDisconnectEvent> {
 
     private static final Logger log = LoggerFactory.getLogger(ActivityService.class);
-
-    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final SimpMessageSendingOperations messagingTemplate;
 
@@ -57,12 +49,10 @@ public class ActivityService implements ApplicationListener<SessionDisconnectEve
     @SubscribeMapping("/topic/activity")
     @SendTo("/topic/tracker")
     public ActivityDTO sendActivity(@Payload ActivityDTO activityDTO, StompHeaderAccessor stompHeaderAccessor, Principal principal) {
-        activityDTO.setUserLogin(SecurityUtils.getCurrentUserLogin());
         activityDTO.setUserLogin(principal.getName());
         activityDTO.setSessionId(stompHeaderAccessor.getSessionId());
         activityDTO.setIpAddress(stompHeaderAccessor.getSessionAttributes().get(IP_ADDRESS).toString());
-        Instant instant = Instant.ofEpochMilli(Calendar.getInstance().getTimeInMillis());
-        activityDTO.setTime(dateTimeFormatter.format(ZonedDateTime.ofInstant(instant, ZoneOffset.systemDefault())));
+        activityDTO.setTime(Instant.now());
         log.debug("Sending user tracking data {}", activityDTO);
         return activityDTO;
     }
