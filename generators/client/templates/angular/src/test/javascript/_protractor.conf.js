b28aa2e608a93a1d9921e6128a5d154c32ef31ef
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-05-02 19:10:42 +0200

    Change jasmineNodeOpts defaultTimeoutInterval from 30000 to 60000

--- generators/client/templates/angular/src/test/javascript/_protractor.conf.js
+++ generators/client/templates/angular/src/test/javascript/_protractor.conf.js
@@ -42,7 +42,7 @@ exports.config = {
 
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000
+        defaultTimeoutInterval: 60000
     },
 
     beforeLaunch: function() {
