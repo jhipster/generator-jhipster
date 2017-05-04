88d3c3486a032c060c9a5610c810371febf6d01d
Srinivasa Vasu <svasu@redhat.com>
2017-05-04 07:13:44 +0530

    update to prodDatabaseType check

--- generators/kubernetes/files.js
+++ generators/kubernetes/files.js
@@ -11,8 +11,8 @@ function writeFiles() {
                 this.app = this.appConfigs[i];
                 this.template('_deployment.yml', `${appName}/${appName}-deployment.yml`);
                 this.template('_service.yml', `${appName}/${appName}-service.yml`);
-
-                if (this.app.prodDatabaseType) {
+                // prodDatabaseType is string type. If we choose microservices with no DB, it is trying to move _no.yml
+                if (this.app.prodDatabaseType !== 'no') {
                     this.template(`db/_${this.app.prodDatabaseType}.yml`, `${appName}/${appName}-${this.app.prodDatabaseType}.yml`);
                 }
                 if (this.app.searchEngine === 'elasticsearch') {
