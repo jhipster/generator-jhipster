1fec912212aa68410354c41a9e642e45b76101a9
Markus Öllinger <oellinger.markus@gmail.com>
2017-05-05 21:27:19 +0200

    change pointcut to include annotations
    Fixes #5715

--- generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
+++ generators/server/templates/src/main/java/package/aop/logging/_LoggingAspect.java
@@ -51,9 +51,9 @@ public class LoggingAspect {
     /**
      * Pointcut that matches all repositories, services and Web REST endpoints.
      */
-    @Pointcut("(within(<%=packageName%>.repository..*) && bean(*Repository))"+
-                  " || (within(<%=packageName%>.service..*) && bean(*Service))"+
-                  " || (within(<%=packageName%>.web.rest..*) && bean(*Resource))")
+    @Pointcut("(within(io.github.jhipster.travis.repository..*) && @annotation(org.springframework.stereotype.Repository))"+
+                  " || (within(io.github.jhipster.travis.service..*) && @annotation(org.springframework.stereotype.Service))"+
+                  " || (within(io.github.jhipster.travis.web.rest..*) && @annotation(org.springframework.web.bind.annotation.RestController))")
     public void loggingPointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
