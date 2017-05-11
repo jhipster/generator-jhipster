db2f30c231b31542b190a62f9f2f46b283a633f9
cbornet <cbornet@hotmail.com>
2017-05-11 05:02:56 +0000

    Change audit event date type from LocalDateTime to Instant

--- generators/server/templates/src/main/java/package/service/_AuditEventService.java
+++ generators/server/templates/src/main/java/package/service/_AuditEventService.java
@@ -20,13 +20,13 @@ package <%=packageName%>.service;
 
 import <%=packageName%>.config.audit.AuditEventConverter;
 import <%=packageName%>.repository.PersistenceAuditEventRepository;
-import java.time.LocalDateTime;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;<% if (databaseType == 'sql') { %>
 import org.springframework.transaction.annotation.Transactional;<% } %>
 
+import java.time.Instant;
 import java.util.Optional;
 
 /**
@@ -56,7 +56,7 @@ public class AuditEventService {
             .map(auditEventConverter::convertToAuditEvent);
     }
 
-    public Page<AuditEvent> findByDates(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable) {
+    public Page<AuditEvent> findByDates(Instant fromDate, Instant toDate, Pageable pageable) {
         return persistenceAuditEventRepository.findAllByAuditEventDateBetween(fromDate, toDate, pageable)
             .map(auditEventConverter::convertToAuditEvent);
     }
