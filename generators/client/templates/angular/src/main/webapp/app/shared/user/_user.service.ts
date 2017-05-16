54f086f02890b91e765416a4628b3971d856f834
Deepu K Sasidharan <d4udts@gmail.com>
2017-05-16 21:44:20 +0200

    polish user service

--- generators/client/templates/angular/src/main/webapp/app/shared/user/_user.service.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/user/_user.service.ts
@@ -17,10 +17,12 @@
  limitations under the License.
 -%>
 import { Injectable } from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import { User } from './user.model';
+import { ResponseWrapper } from '../model/response-wrapper.model';
+import { createRequestOption } from '../model/request-util';
 
 @Injectable()
 export class UserService {
@@ -40,21 +42,10 @@ export class UserService {
         return this.http.get(`${this.resourceUrl}/${login}`).map((res: Response) => res.json());
     }
 
-    query(req?: any): Observable<Response> {
-        const params: URLSearchParams = new URLSearchParams();
-        if (req) {
-            params.set('page', req.page);
-            params.set('size', req.size);
-            if (req.sort) {
-                params.paramsMap.set('sort', req.sort);
-            }
-        }
-
-        const options = {
-            search: params
-        };
-
-        return this.http.get(this.resourceUrl, options);
+    query(req?: any): Observable<ResponseWrapper> {
+        const options = createRequestOption(req);
+        return this.http.get(this.resourceUrl, options)
+            .map((res: Response) => this.convertResponse(res));
     }
 
     delete(login: string): Observable<Response> {
@@ -72,4 +63,8 @@ export class UserService {
 <%_ } _%>
     }
 
+    private convertResponse(res: Response): ResponseWrapper {
+        const jsonResponse = res.json();
+        return new ResponseWrapper(res.headers, jsonResponse);
+    }
 }
