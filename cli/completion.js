1f58ade5f2d30eea58a66304165d14537aa7cdbb
Deepu K Sasidharan <d4udts@gmail.com>
2017-05-18 09:17:42 +0200

    remove alias from autocompletion

--- cli/completion.js
+++ cli/completion.js
@@ -13,10 +13,9 @@ module.exports.init = (program, binName) => {
         .action((opts) => {});
     // General handler. Gets called on `jhipster <tab>` and `jhipster stuff ... <tab>`
     const commands = program.commands.map(d => d._name);
-    const alias = program.commands.map(d => d._alias);
     const opts = program.options.map(d => d.long);
     const optShort = program.options.map(d => d.short);
-    const allOpts = [].concat(commands, alias, optShort, opts).filter(d => d !== undefined);
+    const allOpts = [].concat(commands, optShort, opts).filter(d => d !== undefined);
 
     tabtab.on(binName, (data, done) => done(null, allOpts));
     tabtab.start();
