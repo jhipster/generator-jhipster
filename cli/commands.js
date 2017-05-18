af07cc44c3b446b22e3e7b1cbc39a8a7e69d72fd
Julien Dubois <julien.dubois@gmail.com>
2017-05-18 23:15:50 +0200

    Remove command aliases, and re-word the descriptions

--- cli/commands.js
+++ cli/commands.js
@@ -1,76 +1,62 @@
 module.exports = {
     app: {
         default: true,
-        alias: 'a',
-        desc: 'Creates a new JHipster application based on the selected options'
+        desc: 'Create a new JHipster application based on the selected options'
     },
     aws: {
-        desc: 'Initializes a AWS app and generates a WAR file that is ready to push to AWS.'
+        desc: 'Deploy the current application to Amazon Web Services'
     },
     'ci-cd': {
-        alias: 'ci',
-        desc: 'Creates pipeline scripts for various CI/CD tools based on the selected options'
+        desc: 'Create pipeline scripts for popular Continuous Integration/Continuous Deployment tools'
     },
     client: {
-        desc: 'Creates a new JHipster client side application based on the selected options'
+        desc: 'Create a new JHipster client-side application based on the selected options'
     },
     cloudfoundry: {
-        alias: 'cf',
-        desc: 'Generates a `deploy/cloudfoundry` folder with a specific manifest.yml to deploy to Cloud Foundry'
+        desc: 'Generate a `deploy/cloudfoundry` folder with a specific manifest.yml to deploy to Cloud Foundry'
     },
     'docker-compose': {
-        alias: 'dc',
-        desc: 'Creates all required Docker deployment configuration for the selected applications'
+        desc: 'Create all required Docker deployment configuration for the selected applications'
     },
     entity: {
-        alias: 'e',
         argument: ['name'],
-        desc: 'Creates a new JHipster entity: JPA entity, Spring server side components and Angular client side components'
+        desc: 'Create a new JHipster entity: JPA entity, Spring server-side components and Angular client-side components'
     },
     'export-jdl': {
-        alias: 'ejdl',
         argument: ['jdlFile'],
-        desc: 'Creates JDL from the json entities'
+        desc: 'Create a JDL file from the existing entities'
     },
     heroku: {
-        alias: 'hr',
-        desc: 'Initializes a Heroku app and generates a WAR file that is ready to push to Heroku'
+        desc: 'Deploy the current application to Heroku'
     },
     'import-jdl': {
-        alias: 'ijdl',
         argument: ['jdlFiles...'],
-        desc: 'Creates entities from the passed JDL file location'
+        desc: 'Create entities from the JDL file passed in argument'
     },
     info: {
-        alias: 'i',
         desc: 'Display information about your current project and system'
     },
     kubernetes: {
-        alias: 'k8',
-        desc: 'Creates all required Kubernetes deployment configuration for the selected applications'
+        desc: 'Deploy the current application to Kubernetes'
     },
     languages: {
-        alias: 'ln',
         argument: ['languages...'],
         desc: 'Select languages from a list of available languages. The i18n files will be copied to the /webapp/i18n folder'
     },
     openshift: {
-        alias: 'os',
-        desc: 'Creates all required OpenShift deployment configuration for the selected applications'
+        desc: 'Deploy the current application to OpenShift'
     },
     'rancher-compose': {
-        alias: 'rc',
-        desc: 'Creates all required rancher-compose deployment configuration for the selected applications'
+        desc: 'Deploy the current application to Rancher'
     },
     server: {
-        desc: 'Creates a new JHipster server side application based on the selected options'
+        desc: 'Create a new JHipster server-side application'
     },
     service: {
         argument: ['name'],
-        desc: 'Creates a new JHipster service: this is a simple transactional Spring service bean'
+        desc: 'Create a new Spring service bean'
     },
     upgrade: {
-        alias: 'up',
-        desc: 'Updates JHipster version, and upgrades generated application'
+        desc: 'Upgrade the JHipster version, and upgrade the generated application'
     }
 };
