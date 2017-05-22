ca28f77145dd5127853afceffa7fefef1edc8b51
Markus Öllinger <oellinger.markus@gmail.com>
2017-05-23 01:16:43 +0200

    add CSRF protection

--- generators/server/templates/src/main/java/package/config/_MicroserviceSecurityConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_MicroserviceSecurityConfiguration.java
@@ -110,6 +110,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -126,7 +127,8 @@ public class MicroserviceSecurityConfiguration extends ResourceServerConfigurerA
     public void configure(HttpSecurity http) throws Exception {
         http
             .csrf()
-            .disable()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and()
             .headers()
             .frameOptions()
             .disable()
