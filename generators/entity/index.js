cb84c1570c6802c50d327b5b3786c6f16e419573
Sendil Kumar N <sendilkumarn@live.com>
2017-05-24 09:20:13 +0800

    Update index.js

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -466,6 +466,7 @@ module.exports = EntityGenerator.extend({
             this.fieldsContainOneToMany = false;
             this.fieldsContainManyToOne = false;
             this.differentTypes = [];
+            this.differentRelationships = [];
             if (!this.relationships) {
                 this.relationships = [];
             }
