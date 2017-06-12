145adba49c54b452a95468a2d1422d53e34780a3
sendilkumarn <sendilkumarn@live.com>
2017-06-12 15:55:09 +0800

    aot

---
+++ generators/client/templates/angular/src/main/webapp/app/_vendor-aot.ts
@@ -0,0 +1,10 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import '@angular/forms';
+
+// RxJS
+import 'rxjs';
