464a52fbd3f420b0050c19f42af9070f3d980954
telaoumatenyanis <telaoumatenyanis@outlook.fr>
2017-06-16 15:13:43 +0100

    Replaced tabs with spaces

--- generators/client/templates/angular/src/test/javascript/_karma.conf.js
+++ generators/client/templates/angular/src/test/javascript/_karma.conf.js
@@ -145,10 +145,10 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
-		// Ensure all browsers can run the test written in .ts files
-		mime: {
-		    'text/x-typescript': ['ts','tsx']
-		},
+        // Ensure all browsers can run tests written in .ts files
+        mime: {
+            'text/x-typescript': ['ts','tsx']
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
