53c9683da66d446b8c6f32f5fa56eaede3d29aae
gimoh <gimoh@bitmessage.ch>
2017-06-23 09:49:54 +0100

    Use the real fs when reading entity JSON files
    Normally `this.fs` is yeoman's in-memory filesystem (mem-fs) so once
    file is read from disk it's not re-read.
    
    Using jhipster-core function `readEntityJSON` means we always read from
    disk and have most up to date and consistent state.
    
    This came up when implementing a change in `import-jdl` generator where
    entities needed to be re-read after they've been changed by the import.

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -29,6 +29,7 @@ const exec = require('child_process').exec;
 const os = require('os');
 const https = require('https');
 const pluralize = require('pluralize');
+const jhiCore = require('jhipster-core');
 const packagejs = require('../package.json');
 const jhipsterUtils = require('./util');
 const constants = require('./generator-constants');
@@ -1438,14 +1439,21 @@ module.exports = class extends Generator {
             return e1.definition.changelogDate - e2.definition.changelogDate;
         }
 
-        if (shelljs.test('-d', JHIPSTER_CONFIG_DIR)) {
-            shelljs.ls(path.join(JHIPSTER_CONFIG_DIR, '*.json')).forEach((file) => {
-                const definition = this.fs.readJSON(file);
-                entities.push({ name: path.basename(file, '.json'), definition });
-            });
+        if (!shelljs.test('-d', JHIPSTER_CONFIG_DIR)) {
+            return entities;
         }
 
-        return entities.sort(isBefore);
+        return shelljs.ls(
+            path.join(JHIPSTER_CONFIG_DIR, '*.json')
+        ).reduce((acc, file) => {
+            try {
+                const definition = jhiCore.readEntityJSON(file);
+                acc.push({ name: path.basename(file, '.json'), definition });
+            } catch (error) {
+                // not an entity file / malformed?
+            }
+            return acc;
+        }, entities).sort(isBefore);
     }
 
     /**
