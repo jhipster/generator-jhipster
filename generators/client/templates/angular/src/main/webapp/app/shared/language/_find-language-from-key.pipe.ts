b5552aeece6b7830d9f4606bcfa6168c1b221e82
SidMorad <SidMorad@users.noreply.github.com>
2017-06-24 13:03:58 +0430

    RTL support for Angular(4.x)

--- generators/client/templates/angular/src/main/webapp/app/shared/language/_find-language-from-key.pipe.ts
+++ generators/client/templates/angular/src/main/webapp/app/shared/language/_find-language-from-key.pipe.ts
@@ -21,42 +21,47 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'findLanguageFromKey'})
 export class FindLanguageFromKeyPipe implements PipeTransform {
     private languages: any = {
-        'ca': 'Català',
-        'cs': 'Český',
-        'da': 'Dansk',
-        'de': 'Deutsch',
-        'el': 'Ελληνικά',
-        'en': 'English',
-        'es': 'Español',
-        'et': 'Eesti',
-        'fa': 'فارسی',
-        'fr': 'Français',
-        'gl': 'Galego',
-        'hu': 'Magyar',
-        'hi': 'हिंदी',
-        'hy': 'Հայերեն',
-        'it': 'Italiano',
-        'ja': '日本語',
-        'ko': '한국어',
-        'mr': 'मराठी',
-        'nl': 'Nederlands',
-        'pl': 'Polski',
-        'pt-br': 'Português (Brasil)',
-        'pt-pt': 'Português',
-        'ro': 'Română',
-        'ru': 'Русский',
-        'sk': 'Slovenský',
-        'sr': 'Srpski',
-        'sv': 'Svenska',
-        'ta': 'தமிழ்',
-        'th': 'ไทย',
-        'tr': 'Türkçe',
-        'ua': 'Українська',
-        'vi': 'Tiếng Việt',
-        'zh-cn': '中文（简体）',
-        'zh-tw': '繁體中文'
+        'ca': { name: 'Català' },
+        'cs': { name: 'Český' },
+        'da': { name: 'Dansk' },
+        'de': { name: 'Deutsch' },
+        'el': { name: 'Ελληνικά' },
+        'en': { name: 'English' },
+        'es': { name: 'Español' },
+        'et': { name: 'Eesti' },
+        'fa': { name: 'فارسی', rtl: true },
+        'fr': { name: 'Français' },
+        'gl': { name: 'Galego' },
+        'hu': { name: 'Magyar' },
+        'hi': { name: 'हिंदी' },
+        'hy': { name: 'Հայերեն' },
+        'it': { name: 'Italiano' },
+        'ja': { name: '日本語' },
+        'ko': { name: '한국어' },
+        'mr': { name: 'मराठी' },
+        'nl': { name: 'Nederlands' },
+        'pl': { name: 'Polski' },
+        'pt-br': { name: 'Português (Brasil)' },
+        'pt-pt': { name: 'Português' },
+        'ro': { name: 'Română' },
+        'ru': { name: 'Русский' },
+        'sk': { name: 'Slovenský' },
+        'sr': { name: 'Srpski' },
+        'sv': { name: 'Svenska' },
+        'ta': { name: 'தமிழ்' },
+        'th': { name: 'ไทย' },
+        'tr': { name: 'Türkçe' },
+        'ua': { name: 'Українська' },
+        'vi': { name: 'Tiếng Việt' },
+        'zh-cn': { name: '中文（简体）' },
+        'zh-tw': { name: '繁體中文' }
     };
     transform(lang: string): string {
-        return this.languages[lang];
+        return this.languages[lang].name;
     }
+    <%_ if (enableRTLSupport) { _%>
+    isRTL(lang: string): boolean {
+        return this.languages[lang].rtl;
+    }
+    <%_ } _%>
 }
