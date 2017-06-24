c2c8f25b94c973307a2085ca7165b017294c4b5f
Zsombor <gzsombor@users.noreply.github.com>
2017-06-24 13:06:32 +0200

    Add the fix, helper classes, and DTO's to the core library (https://github.com/jhipster/jhipster/pull/15)
    For https://github.com/jhipster/generator-jhipster/pull/5540

---
+++ jhipster-server/src/main/java/io/github/jhipster/service/filter/ZonedDateTimeFilter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * This file is part of the JHipster project, see https://jhipster.github.io/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.jhipster.service.filter;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+/**
+ * Filter class for {@link ZonedDateTime} type attributes.
+ */
+public class ZonedDateTimeFilter extends RangeFilter<ZonedDateTime> {
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setEquals(ZonedDateTime equals) {
+        super.setEquals(equals);
+    }
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setGreaterThan(ZonedDateTime equals) {
+        super.setGreaterThan(equals);
+    }
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setGreaterOrEqualThan(ZonedDateTime equals) {
+        super.setGreaterOrEqualThan(equals);
+    }
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setLessThan(ZonedDateTime equals) {
+        super.setLessThan(equals);
+    }
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setLessOrEqualThan(ZonedDateTime equals) {
+        super.setLessOrEqualThan(equals);
+    }
+
+    @Override
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    public void setIn(List<ZonedDateTime> in) {
+        super.setIn(in);
+    }
+}
