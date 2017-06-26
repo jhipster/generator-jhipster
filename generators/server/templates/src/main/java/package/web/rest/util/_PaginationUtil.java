16966ea04a0de9c78b978c34d7e35dc142a7a075
Alexander Volf <job-github@wolf-a.ru>
2017-06-26 14:12:05 +0300

    Update _PaginationUtil.java
    Added missed imports.

--- generators/server/templates/src/main/java/package/web/rest/util/_PaginationUtil.java
+++ generators/server/templates/src/main/java/package/web/rest/util/_PaginationUtil.java
@@ -21,6 +21,9 @@ package <%=packageName%>.web.rest.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriComponentsBuilder;
