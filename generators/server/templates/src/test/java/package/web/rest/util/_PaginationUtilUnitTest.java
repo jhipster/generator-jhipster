b9fe0a856e0695695b5804129bc684318b64ef7a
Alexander Volf <firewolf_ttyd0@mail.ru>
2017-06-27 15:14:52 +0300

    Update _PaginationUtilUnitTest.java for full coverage

--- generators/server/templates/src/test/java/package/web/rest/util/_PaginationUtilUnitTest.java
+++ generators/server/templates/src/test/java/package/web/rest/util/_PaginationUtilUnitTest.java
@@ -39,6 +39,27 @@ import org.springframework.http.HttpHeaders;
 public class PaginationUtilUnitTest {
 
     @Test
+    public void generatePaginationHttpHeadersTest() {
+        String baseUrl = "/api/_search/example";
+        List<String> content = new ArrayList<>();
+        Page<String> page = new PageImpl<>(content,new PageRequest(6, 50),400L);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, baseUrl);
+        List<String> strHeaders = headers.get(HttpHeaders.LINK);
+        assertNotNull(strHeaders);
+        assertTrue(strHeaders.size() == 1);
+        String headerData = strHeaders.get(0);
+        assertTrue(headerData.split(",").length == 4);
+        String expectedData = "</api/_search/example?page=7&size=50>; rel=\"next\","
+                + "</api/_search/example?page=5&size=50>; rel=\"prev\","
+                + "</api/_search/example?page=7&size=50>; rel=\"last\","
+                + "</api/_search/example?page=0&size=50>; rel=\"first\"";
+        assertEquals(expectedData, headerData);
+        List<String> xTotalCountHeaders = headers.get("X-Total-Count");
+        assertTrue(xTotalCountHeaders.size() == 1);
+        assertTrue(Long.valueOf(xTotalCountHeaders.get(0)).equals(400L));
+    }
+
+    @Test
     public void commaTest() {
         String baseUrl = "/api/_search/example";
         List<String> content = new ArrayList<>();
