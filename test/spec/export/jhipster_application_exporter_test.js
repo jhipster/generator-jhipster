945e9d245c3672ab038f247c1462f5d36573427d
MathieuAA <mathieu.aa@free.fr>
2017-07-04 21:41:02 +0200

    Fixed tests

--- test/spec/export/jhipster_application_exporter_test.js
+++ test/spec/export/jhipster_application_exporter_test.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const expect = require('chai').expect,
-  fs = require('fs'),
   fail = expect.fail,
   Exporter = require('../../../lib/export/jhipster_application_exporter'),
   parseFromFiles = require('../../../lib/reader/jdl_reader').parseFromFiles;
 
-describe.only('::exportApplications', () => {
+describe('::exportApplications', () => {
   describe('when passing invalid parameters', () => {
     describe('such as undefined', () => {
       it('throws an error', () => {
@@ -21,7 +20,20 @@ describe.only('::exportApplications', () => {
   });
   describe('when passing valid arguments', () => {
     describe('when exporting an application to JSON', () => {
-      // const input = parseFromFiles(['./test/test_files/application.jdl']);
+      const parsed = parseFromFiles(['./test/test_files/application.jdl']);
+
+      it('parses it', () => {
+        expect(parsed).not.to.be.null;
+        expect(parsed.applications.length).to.equal(1);
+        expect(parsed.applications[0]).to.deep.equal({
+          baseName: 'toto',
+          path: '../../toto',
+          packageFolder: 'com/mathieu/sample',
+          packageName: 'com.mathieu.sample',
+          enableTranslation: false,
+          languages: ['en', 'fr']
+        });
+      });
     });
   });
 });
