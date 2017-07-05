d058d2a4efad1321e66a647ac6581e96ad2c8210
cbornet <cbornet@hotmail.com>
2017-07-05 16:17:59 +0000

    Use the ID as primary key for User equality checking
    Fix #5631

--- generators/server/templates/src/main/java/package/domain/_User.java
+++ generators/server/templates/src/main/java/package/domain/_User.java
@@ -45,6 +45,7 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 import java.time.Instant;
 
@@ -183,7 +184,7 @@ public class User<% if (databaseType === 'sql' || databaseType === 'mongodb') { 
 
     //Lowercase the login before saving it in database
     public void setLogin(String login) {
-        this.login = login.toLowerCase(Locale.ENGLISH);
+        this.login = login == null ? null : login.toLowerCase(Locale.ENGLISH);
     }
 
     public String getPassword() {
@@ -293,13 +294,12 @@ public class User<% if (databaseType === 'sql' || databaseType === 'mongodb') { 
         }
 
         User user = (User) o;
-
-        return login.equals(user.login);
+        return !(user.getId() == null || getId() == null) && Objects.equals(getId(), user.getId());
     }
 
     @Override
     public int hashCode() {
-        return login.hashCode();
+        return Objects.hashCode(getId());
     }
 
     @Override
