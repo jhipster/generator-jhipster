c13131d177a40e3f5d1a7fe67b1316109865440c
Trần Quang Tiến <tientq@dkt.com.vn>
2017-07-12 06:21:27 +0700

    bring jhipster cli back

--- generators/upgrade/index.js
+++ generators/upgrade/index.js
@@ -33,6 +33,7 @@ util.inherits(UpgradeGenerator, BaseGenerator);
 const GENERATOR_JHIPSTER = 'generator-jhipster';
 const UPGRADE_BRANCH = 'jhipster_upgrade';
 const GIT_VERSION_NOT_ALLOW_MERGE_UNRELATED_HISTORIES = '2.9.0';
+const GENERATOR_JHIPSTER_CLI_VERSION = '4.5.1';
 const SERVER_MAIN_RES_DIR = constants.SERVER_MAIN_RES_DIR;
 
 module.exports = UpgradeGenerator.extend({
@@ -76,7 +77,10 @@ module.exports = UpgradeGenerator.extend({
 
     _generate(version, callback) {
         this.log(`Regenerating application with JHipster ${version}...`);
-        const generatorCommand = 'yo jhipster';
+        let generatorCommand = 'yo jhipster';
+        if (semver.gte(version, GENERATOR_JHIPSTER_CLI_VERSION)) {
+            generatorCommand = this.clientPackageManager === 'yarn' ? '$(yarn bin)/jhipster' : '$(npm bin)/jhipster';
+        }
         shelljs.exec(`${generatorCommand} --with-entities --force --skip-install`, { silent: this.silent }, (code, msg, err) => {
             if (code === 0) this.log(chalk.green(`Successfully regenerated application with JHipster ${version}`));
             else this.error(`Something went wrong while generating project! ${err}`);
