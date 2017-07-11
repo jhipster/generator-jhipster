c67ed9d6a745a67321b7747ddd3ee203749c0086
Deepu K Sasidharan <d4udts@gmail.com>
2017-07-11 23:21:55 +0200

    pass options correctly for yeoman

--- cli/utils.js
+++ cli/utils.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
 const didYouMean = require('didyoumean');
+const meow = require('meow');
 
+const packageJson = require('../package.json');
+
+const CLI_NAME = 'jhipster';
 const debug = function (msg) {
     if (this.debugEnabled) {
-        console.log(`${chalk.blue('DEBUG')}:  ${msg}`);
+        console.log(`${chalk.blue('DEBUG!')}  ${msg}`);
     }
 };
 
@@ -39,6 +43,18 @@ const logger = {
     error
 };
 
+const toString = (item) => {
+    if (typeof item == 'object') {
+        if (Array.isArray(item)) {
+            return item.map(it => toString(it)).join(', ');
+        }
+        return Object.keys(item).map(k => `${k}: ${
+            typeof item[k] != 'function' && typeof item[k] != 'object' ? toString(item[k]) : 'Object'
+        }`).join(', ');
+    }
+    return item;
+};
+
 const initHelp = (program, cliName) => {
     program.on('--help', () => {
         logger.debug('Adding additional help info');
@@ -59,7 +75,71 @@ const initHelp = (program, cliName) => {
     });
 };
 
+/**
+ * Get arguments
+*/
+const getArgs = (opts) => {
+    if (opts.argument) {
+        return `[${opts.argument.join(' ')}]`;
+    }
+    return '';
+};
+
+/**
+ * Get options from arguments
+*/
+const getOptionsFromArgs = (args) => {
+    const options = [];
+    args.forEach((item) => {
+        if (typeof item == 'string') {
+            options.push(item);
+        } else if (typeof item == 'object') {
+            if (Array.isArray(item)) {
+                options.push(...item);
+            }
+        }
+    });
+    return options;
+};
+
+/**
+ *  Get options for the command
+ */
+const getCommand = (cmd, args, opts) => {
+    let options = [];
+    if (opts && opts.argument && opts.argument.length > 0) {
+        logger.debug('Arguments found');
+        options = getOptionsFromArgs(args);
+    }
+    if (args && args.length === 1) {
+        logger.debug('No Arguments found.');
+    }
+    const cmdArgs = options.join(' ').trim();
+    logger.debug(`cmdArgs: ${cmdArgs}`);
+    return `${CLI_NAME}:${cmd}${cmdArgs ? ` ${cmdArgs}` : ''}`;
+};
+
+const getCommandOptions = () => {
+    const options = meow({ help: false, pkg: packageJson, argv: process.argv.slice(2) });
+    const flags = options ? options.flags : null;
+    if (flags) {
+        // Add un-camelized options too, for legacy
+        Object.keys(flags).forEach((key) => {
+            const legacyKey = key.replace(/[A-Z]/g, m => `-${m.toLowerCase()}`);
+            flags[legacyKey] = flags[key];
+        });
+        return flags;
+    }
+    return undefined;
+};
+
 module.exports = {
+    CLI_NAME,
+    toString,
     logger,
-    initHelp
+    initHelp,
+    getArgs,
+    getOptionsFromArgs,
+    getCommand,
+    getCommandOptions
 };
