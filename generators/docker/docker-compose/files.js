29eaea508a52c9e62177e547719f2fcfe3a43083
HOUEL Steve <shouel@ippon.fr>
2017-07-12 09:49:03 +0200

    merge common code for "docker" deployment modules

---
+++ generators/docker/docker-compose/files.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2013-2017 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see https://jhipster.github.io/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module.exports = {
+    writeFiles
+};
+
+function writeFiles() {
+    return {
+        writeDockerCompose() {
+            this.template('_docker-compose.yml', 'docker-compose.yml');
+            this.template('_README-DOCKER-COMPOSE.md', 'README-DOCKER-COMPOSE.md');
+        },
+
+        writeRegistryFiles() {
+            if (this.serviceDiscoveryType === 'eureka') {
+                this.template('_jhipster-registry.yml', 'jhipster-registry.yml');
+            }
+            if (this.serviceDiscoveryType) {
+                this.template('central-server-config/_application.yml', 'central-server-config/application.yml');
+            }
+            if (this.gatewayNb === 0 && this.microserviceNb === 0) return;
+            if (this.serviceDiscoveryType === 'consul') {
+                this.template('_consul.yml', 'consul.yml');
+            }
+        },
+
+        writeKafkaFiles() {
+            if (!this.useKafka) return;
+
+            this.template('_kafka.yml', 'kafka.yml');
+        },
+
+        writeElkFiles() {
+            if (this.monitoring !== 'elk') return;
+
+            this.template('_jhipster-console.yml', 'jhipster-console.yml');
+            this.template('log-conf/_logstash.conf', 'log-conf/logstash.conf');
+            this.template('log-data/_.gitignore', 'log-data/.gitignore');
+        },
+
+        writePrometheusFiles() {
+            if (this.monitoring !== 'prometheus') return;
+
+            // Generate a list of target apps to monitor for the prometheus config
+            const appsToMonitor = [];
+            for (let i = 0; i < this.appConfigs.length; i++) {
+                appsToMonitor.push(`             - ${this.appConfigs[i].baseName}-app:${this.appConfigs[i].serverPort}`);
+            }
+
+            // Format the application target list as a YAML array
+            this.appsToMonitorList = appsToMonitor.join('\n').replace(/'/g, '');
+
+            this.template('_prometheus.yml', 'prometheus.yml');
+            this.template('prometheus-conf/_prometheus.yml', 'prometheus-conf/prometheus.yml');
+            this.template('prometheus-conf/_alert.rules', 'prometheus-conf/alert.rules');
+            this.template('alertmanager-conf/_config.yml', 'alertmanager-conf/config.yml');
+        }
+    };
+}
---
+++ generators/docker/kubernetes/files.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2013-2017 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see https://jhipster.github.io/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module.exports = {
+    writeFiles
+};
+
+function writeFiles() {
+    return {
+        writeDeployments() {
+            for (let i = 0; i < this.appConfigs.length; i++) {
+                const appName = this.appConfigs[i].baseName.toLowerCase();
+                this.app = this.appConfigs[i];
+                this.template('_deployment.yml', `${appName}/${appName}-deployment.yml`);
+                this.template('_service.yml', `${appName}/${appName}-service.yml`);
+                // If we choose microservice with no DB, it is trying to move _no.yml as prodDatabaseType is getting tagged as 'string' type
+                if (this.app.prodDatabaseType !== 'no') {
+                    this.template(`db/_${this.app.prodDatabaseType}.yml`, `${appName}/${appName}-${this.app.prodDatabaseType}.yml`);
+                }
+                if (this.app.searchEngine === 'elasticsearch') {
+                    this.template('db/_elasticsearch.yml', `${appName}/${appName}-elasticsearch.yml`);
+                }
+                if (this.app.messageBroker === 'kafka') {
+                    this.template('db/_kafka.yml', `${appName}/${appName}-kafka.yml`);
+                }
+            }
+        },
+
+        writeRegistryFiles() {
+            if (this.serviceDiscoveryType === 'eureka') {
+                this.template('registry/_jhipster-registry.yml', 'registry/jhipster-registry.yml');
+                this.template('registry/_application-configmap.yml', 'registry/application-configmap.yml');
+            } else if (this.serviceDiscoveryType === 'consul') {
+                this.template('registry/_consul.yml', 'registry/consul.yml');
+                this.template('registry/_consul-config-loader.yml', 'registry/consul-config-loader.yml');
+                this.template('registry/_application-configmap.yml', 'registry/application-configmap.yml');
+            }
+        }
+    };
+}
