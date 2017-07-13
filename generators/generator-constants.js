0572ab23916082742d3225d98900dad9aa497098
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-07-13 22:47:20 +0200

    Upgrade NodeJS, NPM and Yarn
    - NodeJS: 6.11.1
    - NPM: 5.3.0
    - Yarn: 0.27.5

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -43,9 +43,9 @@ const DOCKER_GRAFANA = 'grafana/grafana:4.3.2';
 const DOCKER_JENKINS = 'jenkins:latest';
 
 // version of Node, Yarn, NPM
-const NODE_VERSION = '6.11.0';
-const YARN_VERSION = '0.24.6';
-const NPM_VERSION = '5.0.3';
+const NODE_VERSION = '6.11.1';
+const YARN_VERSION = '0.27.5';
+const NPM_VERSION = '5.3.0';
 
 // all constants used throughout all generators
 
