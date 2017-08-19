0f5a497ab39a04c5c0257d21da9fb3d830d4a824
ctayeb <tayebchlyah@gmail.com>
2017-08-19 16:06:46 +0200

    Added Custom converters for Couchbase

--- generators/server/templates/src/main/java/package/config/_DatabaseConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_DatabaseConfiguration.java
@@ -17,9 +17,9 @@
  limitations under the License.
 -%>
 package <%=packageName%>.config;
-<%_ if (databaseType === 'sql') { _%>
 
 import io.github.jhipster.config.JHipsterConstants;
+<%_ if (databaseType === 'sql') { _%>
 import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
 
 import liquibase.integration.spring.SpringLiquibase;
@@ -38,9 +38,9 @@ import com.mongodb.MongoClient;
 import com.couchbase.client.java.Bucket;
 import com.github.couchmove.Couchmove;
 import <%=packageName%>.repository.CustomN1qlCouchbaseRepository;
+import org.apache.commons.codec.binary.Base64;
 <%_ } _%>
-<%_ if (databaseType === 'mongodb' || databaseType === 'couchbase') { _%>
-import io.github.jhipster.config.JHipsterConstants;
+<%_ if (databaseType === 'mongodb') { _%>
 import io.github.jhipster.domain.util.JSR310DateConverters.DateToZonedDateTimeConverter;
 import io.github.jhipster.domain.util.JSR310DateConverters.ZonedDateTimeToDateConverter;
 <%_ } _%>
@@ -66,11 +66,14 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;<% } %><% if (databaseType === 'couchbase') { %>
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.couchbase.config.BeanNames;
 import org.springframework.data.couchbase.core.convert.CustomConversions;
 import org.springframework.data.couchbase.core.mapping.event.ValidatingCouchbaseEventListener;
 import org.springframework.data.couchbase.repository.auditing.EnableCouchbaseAuditing;
-import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;<% } %><% if (databaseType === 'mongodb' || databaseType === 'couchbase') { %>
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+import org.springframework.util.StringUtils;<% } %><% if (databaseType === 'mongodb' || databaseType === 'couchbase') { %>
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;<% } %>
 <%_ if (databaseType === 'sql') { _%>
 import org.springframework.core.task.TaskExecutor;
@@ -82,11 +85,20 @@ import javax.sql.DataSource;
 <%_ if (devDatabaseType === 'h2Disk' || devDatabaseType === 'h2Memory') { _%>
 import java.sql.SQLException;
 <%_ } } _%>
-<%_ if (databaseType === 'mongodb' || databaseType === 'couchbase') { _%>
+<%_ if (databaseType === 'mongodb') { _%>
 
 import java.util.ArrayList;
 import java.util.List;
-<%_ } _%>
+<%_ } else if (databaseType === 'couchbase') { _%>
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+<%_ }_%>
 
 @Configuration<% if (databaseType === 'sql') { %>
 @EnableJpaRepositories("<%=packageName%>.repository")
@@ -162,9 +174,16 @@ public class DatabaseConfiguration {
     @Bean<% if (databaseType === 'couchbase') { %>(name = BeanNames.COUCHBASE_CUSTOM_CONVERSIONS)<% } %>
     public CustomConversions customConversions() {
         List<Converter<?, ?>> converters = new ArrayList<>();<% if (authenticationType === 'oauth2') { %>
-        converters.add(new OAuth2AuthenticationReadConverter());<% } %>
+        converters.add(new OAuth2AuthenticationReadConverter());<% } %><% if (databaseType === 'couchbase') { %>
+        converters.add(ZonedDateTimeToLongConverter.INSTANCE);
+        converters.add(NumberToLocalDateTimeConverter.INSTANCE);
+        converters.add(BigIntegerToStringConverter.INSTANCE);
+        converters.add(StringToBigIntegerConverter.INSTANCE);
+        converters.add(BigDecimalToStringConverter.INSTANCE);
+        converters.add(StringToBigDecimalConverter.INSTANCE);
+        converters.add(StringToByteConverter.INSTANCE);<% } else { %>
         converters.add(DateToZonedDateTimeConverter.INSTANCE);
-        converters.add(ZonedDateTimeToDateConverter.INSTANCE);
+        converters.add(ZonedDateTimeToDateConverter.INSTANCE);<% } %>
         return new CustomConversions(converters);
     }<% } %><% if (databaseType === 'mongodb') { %>
 
@@ -186,5 +205,95 @@ public class DatabaseConfiguration {
         Couchmove couchMove = new Couchmove(couchbaseBucket, "config/couchmove/changelog");
         couchMove.migrate();
         return couchMove;
+    }
+
+    /**
+     * Simple singleton to convert {@link ZonedDateTime}s to their {@link Long} representation.
+     */
+    @WritingConverter
+    public enum ZonedDateTimeToLongConverter implements Converter<ZonedDateTime, Long> {
+
+        INSTANCE;
+
+        @Override
+        public Long convert(ZonedDateTime source) {
+            return source == null ? null : Date.from(source.toInstant()).getTime();
+        }
+    }
+
+    /**
+     * Simple singleton to convert from {@link Number} {@link BigDecimal} representation.
+     */
+    @ReadingConverter
+    public enum NumberToLocalDateTimeConverter implements Converter<Number, ZonedDateTime> {
+
+        INSTANCE;
+
+        @Override
+        public ZonedDateTime convert(Number source) {
+            return source == null ? null : ZonedDateTime.ofInstant(new Date(source.longValue()).toInstant(), ZoneId.systemDefault());
+        }
+
+    }
+
+    /**
+     * Simple singleton to convert {@link BigDecimal}s to their {@link String} representation.
+     */
+    @WritingConverter
+    public enum BigDecimalToStringConverter implements Converter<BigDecimal, String> {
+        INSTANCE;
+
+        public String convert(BigDecimal source) {
+            return source == null ? null : source.toString();
+        }
+    }
+
+    /**
+     * Simple singleton to convert from {@link String} {@link BigDecimal} representation.
+     */
+    @ReadingConverter
+    public enum StringToBigDecimalConverter implements Converter<String, BigDecimal> {
+        INSTANCE;
+
+        public BigDecimal convert(String source) {
+            return StringUtils.hasText(source) ? new BigDecimal(source) : null;
+        }
+    }
+
+    /**
+     * Simple singleton to convert {@link BigInteger}s to their {@link String} representation.
+     */
+    @WritingConverter
+    public enum BigIntegerToStringConverter implements Converter<BigInteger, String> {
+        INSTANCE;
+
+        public String convert(BigInteger source) {
+            return source == null ? null : source.toString();
+        }
+    }
+
+    /**
+     * Simple singleton to convert from {@link String} {@link BigInteger} representation.
+     */
+    @ReadingConverter
+    public enum StringToBigIntegerConverter implements Converter<String, BigInteger> {
+        INSTANCE;
+
+        public BigInteger convert(String source) {
+            return StringUtils.hasText(source) ? new BigInteger(source) : null;
+        }
+    }
+
+    /**
+     * Simple singleton to convert from {@link String} {@link byte[]} representation.
+     */
+    @ReadingConverter
+    public enum StringToByteConverter implements Converter<String, byte[]> {
+        INSTANCE;
+
+        @Override
+        public byte[] convert(String source) {
+            return Base64.decodeBase64(source);
+        }
     }<% } %>
 }
