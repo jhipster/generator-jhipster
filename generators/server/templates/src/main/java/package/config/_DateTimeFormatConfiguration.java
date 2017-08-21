03a68acf4253d88990b9fa27d3b752aded506085
JoffreyDcu <joffrey.derchu@hotmail.fr>
2017-08-21 12:11:21 +0200

    a few abstract classes are now interfaces

--- generators/server/templates/src/main/java/package/config/_DateTimeFormatConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_DateTimeFormatConfiguration.java
@@ -21,10 +21,10 @@ package <%=packageName%>.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DateTimeFormatConfiguration extends WebMvcConfigurerAdapter {
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
