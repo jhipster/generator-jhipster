a2b8d835e134cd7bdd879a4ccfb77db8275b9e44
Gaël Marziou <gael.marziou@gmail.com>
2017-08-31 23:33:11 +0200

    Show error message when pom.xml parsing failed
    After `jhipster ugrade`, pom.xml was not fully merged and so contained `>>>`markers and frontend build failed without any helpful message.
    
    I made the change for angularjs and angular.

--- generators/client/templates/angularjs/gulp/_utils.js
+++ generators/client/templates/angularjs/gulp/_utils.js
@@ -36,6 +36,9 @@ function parseVersion() {
     var version = null;
     var pomXml = fs.readFileSync('pom.xml', 'utf8');
     parseString(pomXml, function (err, result) {
+        if (err) {
+            throw new Error('Failed to parse pom.xml: ' + err);
+        }
         if (result.project.version && result.project.version[0]) {
             version = result.project.version[0];
         } else if (result.project.parent && result.project.parent[0] && result.project.parent[0].version && result.project.parent[0].version[0]) {
