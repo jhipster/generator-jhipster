417c2bbd7c591a1a32ca54b009a072fa75afe0c8
Deepu K Sasidharan <d4udts@gmail.com>
2017-09-09 13:50:30 +0200

    fix openFile method

--- src/service/data-util.service.ts
+++ src/service/data-util.service.ts
@@ -47,8 +47,8 @@ export class JhiDataUtils {
     /**
      * Method to open file
      */
-    openFile(type: string, data: string) {
-        const fileURL = 'data:' + contentType + ';base64,' + field;
+    openFile(contentType: string, data: string) {
+        const fileURL = `data:${contentType};base64,${data}`;
         const win = window.open();
         win.document.write(
             '<iframe src="' + fileURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
