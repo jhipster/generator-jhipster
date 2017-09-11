c876b2e4ef14f76480f7fac4a228a0c1dd65434f
Julien Dubois <julien.dubois@gmail.com>
2017-09-11 18:38:25 +0200

    Fix SERVER_API_URL constant errors

--- generators/client/templates/angular/src/main/webapp/app/layouts/profiles/_profile.service.ts
+++ generators/client/templates/angular/src/main/webapp/app/layouts/profiles/_profile.service.ts
@@ -28,7 +28,7 @@ import { ProfileInfo } from './profile-info.model';
 @Injectable()
 export class ProfileService {
 
-    private profileInfoUrl = SERVER_API_URL + 'api/profile-info';
+    private profileInfoUrl = <% if (authenticationType === 'uaa') { %>'<% } else { %>SERVER_API_URL + '<% } %>api/profile-info';
 
     constructor(private http: Http) { }
 
