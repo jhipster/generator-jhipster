6101db82355b3cc96b888ba02811ee53f687bc48
sendilkumarn <sendilkumarn@live.com>
2017-09-17 19:58:29 +0530

    bootstrap 4 changes

--- generators/client/templates/react/src/main/webapp/app/modules/account/settings/settings.tsx
+++ generators/client/templates/react/src/main/webapp/app/modules/account/settings/settings.tsx
@@ -69,28 +69,26 @@ export class SettingsPage extends React.Component<IUserSettingsProps, IUserSetti
   render() {
     const { account } = this.state;
     return (
-      <div className="well">
         <div>
-          <h2>User settings for [{account.login}]</h2>
-        </div>
-        <form>
-          <div className="form-group">
-            <label className="control-label" >First Name</label>
-            <input type="text" className="form-control" value={account.firstName} id="firstName" name="firstName" placeholder="First Name"
-            onChange={this.setFirstName}/>
-            <label className="control-label" >Last Name</label>
-            <input type="text" className="form-control" value={account.lastName} id="lastName" name="lastName" placeholder="Last Name"
-            onChange={this.setLastName}/>
-            <label className="control-label" >Email</label>
-            <input type="text" className="form-control" value={account.email} id="email" name="email" placeholder="Email"
-                   onChange={this.setEmail}/>
-            <label>Language</label>
-            <select id="langKey" name="langKey" className="form-control">
-              {locales.map(lang => <option value="lang" key="lang">{lang}</option>)}
-            </select>
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={this.saveSettings} >Save</button>
-        </form>
+            <h2>User settings for [{account.login}]</h2>
+            <form>
+              <div className="form-control">
+                <label className="form-control-label" >First Name</label>
+                <input type="text" className="form-control" value={account.firstName} id="firstName" name="firstName" placeholder="First Name"
+                onChange={this.setFirstName}/>
+                <label className="form-control-label" >Last Name</label>
+                <input type="text" className="form-control" value={account.lastName} id="lastName" name="lastName" placeholder="Last Name"
+                onChange={this.setLastName}/>
+                <label className="form-control-label" >Email</label>
+                <input type="text" className="form-control" value={account.email} id="email" name="email" placeholder="Email"
+                       onChange={this.setEmail}/>
+                <label>Language</label>
+                <select id="langKey" name="langKey" className="form-control">
+                  {locales.map(lang => <option value="lang" key="lang">{lang}</option>)}
+                </select>
+              </div>
+              <button type="submit" className="btn btn-primary" onClick={this.saveSettings} >Save</button>
+            </form>
       </div>
     );
   }
