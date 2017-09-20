b7de5f46a2bb17075f1c28666cfb2e952e6bccad
Julien Dubois <julien.dubois@gmail.com>
2017-09-20 21:19:15 +0200

    Update JHipster website URL

--- jhipster-server/src/main/java/io/github/jhipster/config/JHipsterConstants.java
+++ jhipster-server/src/main/java/io/github/jhipster/config/JHipsterConstants.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2016-2017 the original author or authors from the JHipster project.
  *
- * This file is part of the JHipster project, see https://jhipster.github.io/
+ * This file is part of the JHipster project, see http://www.jhipster.tech/
  * for more information.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +24,7 @@ package io.github.jhipster.config;
  */
 public final class JHipsterConstants {
 
-    // Spring profiles for development, test and production, see http://jhipster.github.io/profiles/
+    // Spring profiles for development, test and production, see http://www.jhipster.tech/profiles/
     public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
     public static final String SPRING_PROFILE_TEST = "test";
     public static final String SPRING_PROFILE_PRODUCTION = "prod";
--- jhipster-server/src/main/java/io/github/jhipster/service/filter/BooleanFilter.java
+++ jhipster-server/src/main/java/io/github/jhipster/service/filter/BooleanFilter.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2017 the original author or authors.
  *
- * This file is part of the JHipster project, see https://jhipster.github.io/
+ * This file is part of the JHipster project, see http://www.jhipster.tech/
  * for more information.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
