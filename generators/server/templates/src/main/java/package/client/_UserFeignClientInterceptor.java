4ebb3e4b5fa94c007b43a752a372f00b3d513a82
anjia0532 <anjia0532@gmail.com>
2017-09-22 10:19:33 +0800

    uaa suport ropc

--- generators/server/templates/src/main/java/package/client/_UserFeignClientInterceptor.java
+++ generators/server/templates/src/main/java/package/client/_UserFeignClientInterceptor.java
@@ -18,7 +18,6 @@
 -%>
 package <%=packageName%>.client;
 
-import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,33 +26,28 @@ import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResour
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import feign.RequestInterceptor;
 import feign.RequestTemplate;
 
 
 @Component
-public class FeignClientInterceptor extends OAuth2FeignRequestInterceptor{
+public class UserFeignClientInterceptor implements RequestInterceptor{
     
     // should be added to Constants class
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_TOKEN_TYPE = "Bearer";
     
-    public FeignClientInterceptor(OAuth2ClientContext oAuth2ClientContext, OAuth2ProtectedResourceDetails resource) {
-        super(oAuth2ClientContext, resource);
-    }
 
     @Override
     public void apply(RequestTemplate template) {
-        if (!template.headers().containsKey(AUTHORIZATION_HEADER)) {
-            
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            Authentication authentication = securityContext.getAuthentication();
-            
-            if (authentication != null && authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
-                
-                OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
-                
-                template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, details.getTokenValue()));
-            }
+        
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        
+        if (authentication != null && authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
+
+            OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+            template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, details.getTokenValue()));
         }
     }
 }
