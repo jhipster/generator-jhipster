4a9e6449a706bc47a707333655ba8e83a7a30c19
Michael Cornel <Michael.Cornel@bamf.bund.de>
2017-09-26 17:11:50 +0200

    Remove blank lines
    Consecutive blank lines raise linter errors.

--- generators/client/templates/angular/src/main/webapp/app/_polyfills.ts
+++ generators/client/templates/angular/src/main/webapp/app/_polyfills.ts
@@ -59,21 +59,16 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
-
 /**
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
