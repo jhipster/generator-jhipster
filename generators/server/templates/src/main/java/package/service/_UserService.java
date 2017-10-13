944870058e205d3a511b399c21a3e27a0c2c9c2a
Julien Dubois <julien.dubois@gmail.com>
2017-10-13 21:39:22 +0200

    Fix some compilation errors

--- generators/server/templates/src/main/java/package/service/_UserService.java
+++ generators/server/templates/src/main/java/package/service/_UserService.java
@@ -257,7 +257,6 @@ public class UserService {
         log.debug("Created Information for User: {}", user);
         return user;
     }
-<%_ } _%>
 
     /**
      * Update basic information (first name, last name, email, language) for the current user.
@@ -334,6 +333,7 @@ public class UserService {
             })
             .map(UserDTO::new);
     }
+<%_ } _%>
 
     public void deleteUser(String login) {
         userRepository.findOneByLogin(login).ifPresent(user -> {
