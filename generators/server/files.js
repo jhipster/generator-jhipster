2a343ba9c46d4f4425d04d25818eb8cd9d667be9
Julien Dubois <julien.dubois@gmail.com>
2017-10-14 12:19:55 +0200

    Do not initialize the user collection for microservices - it will be automatically created when users are added from OIDC server

--- generators/server/files.js
+++ generators/server/files.js
@@ -194,7 +194,7 @@ function writeFiles() {
 
             if (this.databaseType === 'mongodb') {
                 this.template(`${SERVER_MAIN_SRC_DIR}package/config/dbmigrations/_package-info.java`, `${javaDir}config/dbmigrations/package-info.java`);
-                if (!this.skipUserManagement || this.authenticationType === 'oauth2') {
+                if (!this.skipUserManagement) {
                     this.template(`${SERVER_MAIN_SRC_DIR}package/config/dbmigrations/_InitialSetupMigration.java`, `${javaDir}config/dbmigrations/InitialSetupMigration.java`);
                 }
             }
