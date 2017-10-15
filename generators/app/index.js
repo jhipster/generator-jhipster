f0f376546023e61330d9a8b04a1b7afbc11958c2
Sophie Koonin <sophie.koonin@gmail.com>
2017-10-15 17:33:05 +0100

    Fix for custom jhiPrefix not being assigned correctly
    Fix bug where jhiPrefix would default to 'jhi' in Angular components and .angular-cli.json because this.configOptions.jhiPrefix was never assigned a value.
    
    Fix #6525

--- generators/app/index.js
+++ generators/app/index.js
@@ -108,7 +108,7 @@ module.exports = class extends BaseGenerator {
         this.skipClient = this.configOptions.skipClient = this.options['skip-client'] || this.config.get('skipClient');
         this.skipServer = this.configOptions.skipServer = this.options['skip-server'] || this.config.get('skipServer');
         this.skipUserManagement = this.configOptions.skipUserManagement = this.options['skip-user-management'] || this.config.get('skipUserManagement');
-        this.jhiPrefix = this.configOptions.jhiPrefix || this.config.get('jhiPrefix') || this.options['jhi-prefix'];
+        this.jhiPrefix = this.configOptions.jhiPrefix = this.config.get('jhiPrefix') || this.options['jhi-prefix'];
         this.withEntities = this.options['with-entities'];
         this.skipChecks = this.options['skip-checks'];
         this.useYarn = this.configOptions.useYarn = !this.options.npm;
