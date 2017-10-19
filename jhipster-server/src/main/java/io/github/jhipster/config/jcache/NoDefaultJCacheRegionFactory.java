f41b6d16bf5ec66a49df8e5d1c43fe7d023015d9
Erik Kemperman <erikkemperman@gmail.com>
2017-10-19 22:20:46 +0200

    Add unit tests for io.github.jhipster.config.jcache.NoDefaultJCacheRegionFactory

--- jhipster-server/src/main/java/io/github/jhipster/config/jcache/NoDefaultJCacheRegionFactory.java
+++ jhipster-server/src/main/java/io/github/jhipster/config/jcache/NoDefaultJCacheRegionFactory.java
@@ -26,15 +26,18 @@ import org.hibernate.cache.jcache.JCacheRegionFactory;
 import org.hibernate.cache.spi.CacheDataDescription;
 
 /**
- * Extends the default {@code JCacheRegionFactory} but makes sure all caches are already existing to prevent spontaneous
- * creation of badly configured caches (e.g. {@code new MutableConfiguration()}.
+ * Extends the default {@code JCacheRegionFactory} but makes sure all caches already exist to prevent
+ * spontaneous creation of badly configured caches (e.g. {@code new MutableConfiguration()}.
  */
+@SuppressWarnings("serial")
 public class NoDefaultJCacheRegionFactory extends JCacheRegionFactory {
+
+    public static final String EXCEPTION_MESSAGE = "All Hibernate caches should be created upfront. " +
+            "Please update CacheConfiguration.java to add";
 
     @Override
     protected Cache<Object, Object> createCache(String regionName, Properties properties, CacheDataDescription
         metadata) {
-        throw new IllegalStateException("All Hibernate caches should be created upfront. " +
-            "Please update CacheConfiguration.java to add " + regionName);
+        throw new IllegalStateException(EXCEPTION_MESSAGE + " " + regionName);
     }
 }
