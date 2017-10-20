c745bff88911015d2c07825c98b199c232408cbd
Erik Kemperman <erikkemperman@gmail.com>
2017-10-20 14:34:45 +0200

    Add unit tests for io.github.jhipster.config.apidoc.PageableParameterBuilderPlugin

--- jhipster-server/src/main/java/io/github/jhipster/config/apidoc/PageableParameterBuilderPlugin.java
+++ jhipster-server/src/main/java/io/github/jhipster/config/apidoc/PageableParameterBuilderPlugin.java
@@ -44,6 +44,20 @@ import springfox.documentation.spi.service.contexts.ParameterContext;
 
 public class PageableParameterBuilderPlugin implements OperationBuilderPlugin {
 
+    public static final String PAGE_NAME = "page";
+    public static final String PAGE_TYPE = "query";
+    public static final String PAGE_DESCRIPTION = "Page number of the requested page";
+
+    public static final String SIZE_NAME = "size";
+    public static final String SIZE_TYPE = "query";
+    public static final String SIZE_DESCRIPTION = "Size of a page";
+
+    public static final String SORT_NAME = "sort";
+    public static final String SORT_TYPE = "query";
+    public static final String SORT_DESCRIPTION = "Sorting criteria in the format: property(,asc|desc). "
+            + "Default sort order is ascending. "
+            + "Multiple sort criteria are supported.";
+
     private final TypeNameExtractor nameExtractor;
     private final TypeResolver resolver;
     private final ResolvedType pageableType;
@@ -79,26 +93,33 @@ public class PageableParameterBuilderPlugin implements OperationBuilderPlugin {
                 ModelReference stringModel = factory.apply(resolver.resolve(List.class, String.class));
 
                 parameters.add(new ParameterBuilder()
-                    .parameterType("query").name("page").modelRef(intModel)
-                    .description("Page number of the requested page").build());
+                    .name(PAGE_NAME)
+                    .parameterType(PAGE_TYPE)
+                    .modelRef(intModel)
+                    .description(PAGE_DESCRIPTION)
+                    .build());
 
                 parameters.add(new ParameterBuilder()
-                    .parameterType("query").name("size").modelRef(intModel)
-                    .description("Size of a page").build());
+                    .name(SIZE_NAME)
+                    .parameterType(SIZE_TYPE)
+                    .modelRef(intModel)
+                    .description(SIZE_DESCRIPTION)
+                    .build());
 
                 parameters.add(new ParameterBuilder()
-                    .parameterType("query").name("sort").modelRef(stringModel).allowMultiple(true)
-                    .description("Sorting criteria in the format: property(,asc|desc). "
-                        + "Default sort order is ascending. "
-                        + "Multiple sort criteria are supported.").build());
+                    .name(SORT_NAME)
+                    .parameterType(SORT_TYPE)
+                    .modelRef(stringModel)
+                    .allowMultiple(true)
+                    .description(SORT_DESCRIPTION)
+                    .build());
 
                 context.operationBuilder().parameters(parameters);
             }
         }
     }
 
-    private Function<ResolvedType, ? extends ModelReference>
-    createModelRefFactory(ParameterContext context) {
+    Function<ResolvedType, ? extends ModelReference> createModelRefFactory(ParameterContext context) {
         ModelContext modelContext = inputParam(
             context.getGroupName(),
             context.resolvedMethodParameter().getParameterType(),
@@ -107,5 +128,13 @@ public class PageableParameterBuilderPlugin implements OperationBuilderPlugin {
             context.getGenericNamingStrategy(),
             context.getIgnorableParameterTypes());
         return modelRefFactory(modelContext, nameExtractor);
+    }
+
+    TypeResolver getResolver() {
+        return resolver;
+    }
+
+    TypeNameExtractor getNameExtractor() {
+        return nameExtractor;
     }
 }
