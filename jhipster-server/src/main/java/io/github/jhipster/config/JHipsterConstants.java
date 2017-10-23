f1605739c2160cfd16d29c1ca6b00ba3a56b82d8
Erik Kemperman <erikkemperman@gmail.com>
2017-10-23 15:05:21 +0200

    Use interface instead of class if it only has constants

--- jhipster-server/src/main/java/io/github/jhipster/config/JHipsterConstants.java
+++ jhipster-server/src/main/java/io/github/jhipster/config/JHipsterConstants.java
@@ -22,23 +22,20 @@ package io.github.jhipster.config;
 /**
  * JHipster constants.
  */
-public final class JHipsterConstants {
+public interface JHipsterConstants {
 
     // Spring profiles for development, test and production, see http://www.jhipster.tech/profiles/
-    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
-    public static final String SPRING_PROFILE_TEST = "test";
-    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_TEST = "test";
+    String SPRING_PROFILE_PRODUCTION = "prod";
     // Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
-    public static final String SPRING_PROFILE_CLOUD = "cloud";
+    String SPRING_PROFILE_CLOUD = "cloud";
     // Spring profile used when deploying to Heroku
-    public static final String SPRING_PROFILE_HEROKU = "heroku";
+    String SPRING_PROFILE_HEROKU = "heroku";
     // Spring profile used to disable swagger
-    public static final String SPRING_PROFILE_SWAGGER = "swagger";
+    String SPRING_PROFILE_SWAGGER = "swagger";
     // Spring profile used to disable running liquibase
-    public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+    String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
     // Spring profile used when deploying to Kubernetes and OpenShift
-    public static final String SPRING_PROFILE_K8S = "k8s";
-
-    private JHipsterConstants() {
-    }
+    String SPRING_PROFILE_K8S = "k8s";
 }
