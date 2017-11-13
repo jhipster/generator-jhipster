040eebddf6bb69375db00923af65fe547dbf728c
lmbai <bailm1984@gmail.com>
2017-11-13 22:48:30 +0800

    6668 -  fix the logstash-appender stopping working issues (#6679)
    * fix the logstash-appender stopping working issues 
    
    In the SpringCloudConfig, Spring will refresh its configuration even the app is already running.  Spring will call the `LoggerContext` to refresh all the logger config. which will `StopAndReset` the log config. it will stop all the logappender and remove all the `LoggerContextListener`.   After that, `LoggerContext `will load the new configuration based on the configuration file (logback-spring.xml) . After that, Spring will call the configuration class ...... , but for this case, the bean is singleton and already been created. 
    In this case, the above `LoggingConfiguration`  won't be created again (even the `logstash-appender` has been stopped).
    
    We add the `@RefreshScope` here to force Spring to recreate this bean when there is configuration refresh
    
    * change the condition for enable 'RefreshScope'
    
    * import the RefreshScope when the `serviceDiscovery` is enabled

--- generators/server/templates/src/main/java/package/config/_LoggingConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_LoggingConfiguration.java
@@ -42,19 +42,24 @@ import org.springframework.beans.factory.annotation.Value;
 <%_ if (serviceDiscoveryType === "eureka") { _%>
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 <%_ } _%>
 <%_ if (serviceDiscoveryType === "consul") { _%>
 import org.springframework.cloud.consul.ConditionalOnConsulEnabled;
 import org.springframework.cloud.consul.serviceregistry.ConsulRegistration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 <%_ } _%>
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 <%_ if (serviceDiscoveryType === "eureka") { _%>
 @ConditionalOnProperty("eureka.client.enabled")
+@RefreshScope
 <%_ } _%>
 <%_ if (serviceDiscoveryType === "consul") { _%>
 @ConditionalOnConsulEnabled
+@RefreshScope
 <%_ } _%>
 public class LoggingConfiguration {
 
