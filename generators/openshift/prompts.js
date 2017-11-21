df68cf4cba3c9b31f7b5a72fecd21c58479ac684
mcmac <spengreb@gmail.com>
2017-11-21 11:09:50 +0000

    Updated openshift sub-generator to support MSSQL RDS and Gradle

--- generators/openshift/prompts.js
+++ generators/openshift/prompts.js
@@ -4,6 +4,7 @@ const dockerPrompts = require('../docker-prompts');
 module.exports = _.extend({
     askForOpenShiftNamespace,
     askForStorageType,
+    askForMSSQLConfig,
 }, dockerPrompts);
 
 function askForOpenShiftNamespace() {
@@ -14,13 +15,60 @@ function askForOpenShiftNamespace() {
         name: 'openshiftNamespace',
         message: 'What should we use for the OpenShift namespace?',
         default: this.openshiftNamespace ? this.openshiftNamespace : 'default'
+    },{
+        type: 'input',
+        name: 'openshiftUrl',
+        message: 'What is the address of your openshift console?',
+        default: this.openshiftUrl ? this.openshiftUrl : 'master.openshift.local:8443'
     }];
 
     this.prompt(prompts).then((props) => {
         this.openshiftNamespace = props.openshiftNamespace;
+        this.openshiftUrl = props.openshiftUrl;
         done();
     });
 }
+
+function askForMSSQLConfig() {
+    const done = this.async();
+    this.appConfigs.some((appConfig, index) => {
+        if (appConfig.prodDatabaseType !== 'mssql') {
+            done();
+            return;
+        }
+    });
+    //ask questions
+    const prompts = [{
+        type: 'input',
+        name: 'mssqlURL',
+        message: 'What is the address of your MSSQL Server?',
+        default: this.mssqlURL ? this.mssqlURL : 'MSSQL\SQLExpress'
+    },{
+        type: 'input',
+        name: 'mssqlUser',
+        message: 'What is your MS SQL Username?',
+        default: this.mssqlUser ? this.mssqlUser : 'sa'
+    },{
+        type: 'input',
+        name: 'mssqlPass',
+        message: 'What is the users password?',
+        default: this.mssqlPass ? this.mssqlPass : 'Password'
+    },{
+        type: 'input',
+        name: 'mssqlDb',
+        message: 'What is the database name?',
+        default: this.mssqlDb ? this.mssqlDb : 'master'
+    }];
+
+    this.prompt(prompts).then((props) => {
+        this.mssqlURL = props.mssqlURL;
+        this.mssqlUser = props.mssqlUser;
+        this.mssqlPass = props.mssqlPass;
+        this.mssqlDb = props.mssqlDb;
+        done();
+    });
+}
+
 
 function askForStorageType() {
     const done = this.async();
