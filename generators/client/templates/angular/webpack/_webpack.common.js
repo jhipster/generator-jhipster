80e363e0dd1d5622ef0255fdac474ed9d98a6d19
Sendil Kumar <sendilkumarn@live.com>
2017-11-23 08:02:33 +0100

    linting the angularX references

--- generators/client/templates/angular/webpack/_webpack.common.js
+++ generators/client/templates/angular/webpack/_webpack.common.js
@@ -114,7 +114,6 @@ module.exports = (options) => {
                 { from: './<%= MAIN_SRC_DIR %>swagger-ui/', to: 'swagger-ui' },
                 { from: './<%= MAIN_SRC_DIR %>favicon.ico', to: 'favicon.ico' },
                 { from: './<%= MAIN_SRC_DIR %>manifest.webapp', to: 'manifest.webapp' },
-                // { from: './<%= MAIN_SRC_DIR %>sw.js', to: 'sw.js' },
                 // jhipster-needle-add-assets-to-webpack - JHipster will add/remove third-party resources in this array
                 { from: './<%= MAIN_SRC_DIR %>robots.txt', to: 'robots.txt' }
             ]),
