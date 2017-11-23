89419e4949a16adea057b12818d860540311312d
Sendil Kumar <sendilkumarn@live.com>
2017-11-23 07:42:35 +0100

    fixing angularX version

--- generators/client/templates/angular/webpack/_webpack.prod.js
+++ generators/client/templates/angular/webpack/_webpack.prod.js
@@ -151,12 +151,12 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             debug: false
         }),
         new WorkboxPlugin({
-          // to cache all under target/www
-          globDirectory: utils.root('target/www'),
+          // to cache all under <%= BUILD_DIR %>www
+          globDirectory: utils.root('<%= BUILD_DIR %>www'),
           // find these files and cache them
           globPatterns: ['**/*.{html,bundle.js,css,png,svg,jpg,gif,json}'],
-          // create service worker at the target/www
-          swDest: path.resolve(utils.root('target/www'), 'sw.js'),
+          // create service worker at the <%= BUILD_DIR %>www
+          swDest: path.resolve(utils.root('<%= BUILD_DIR %>www'), 'sw.js'),
           clientsClaim: true,
           skipWaiting: true,
         }),
