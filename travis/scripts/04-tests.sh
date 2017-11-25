9336901d3e4b41b99a2c189051973d1d0ba56887
Gaël Marziou <gael.marziou@gmail.com>
2017-11-25 19:43:54 +0100

    Prevent from using JDK 9
    Recently we have seen several questions or issues due to users using JDK 9. This commit makes maven and gradle fail .
    
    Display env info at start of  Travis build.
    
    Ref  #6391

--- travis/scripts/04-tests.sh
+++ travis/scripts/04-tests.sh
@@ -2,9 +2,18 @@
 set -e
 
 #-------------------------------------------------------------------------------
-# Check Javadoc generation
+# Display environment information like JDK version
 #-------------------------------------------------------------------------------
 cd "$APP_FOLDER"
+if [ -f "mvnw" ]; then
+    ./mvnw enforcer:display-info
+elif [ -f "gradlew" ]; then
+    ./gradlew -v
+fi
+
+#-------------------------------------------------------------------------------
+# Check Javadoc generation
+#-------------------------------------------------------------------------------
 if [ -f "mvnw" ]; then
     ./mvnw javadoc:javadoc
 elif [ -f "gradlew" ]; then
