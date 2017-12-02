acbbedb8529240e80551e050cbe07e1e18f7880a
charlie <cmordant1@gmail.com>
2017-12-03 00:31:52 +0100

    clean original pom from redundant plugin configuration

--- generators/ci-cd/templates/travis/deploy.sh
+++ generators/ci-cd/templates/travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   git checkout -B "$TRAVIS_BRANCH"
-  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:prepare -P release -DdryRun=true
-  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:clean -P release
+  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:prepare -DdryRun=true
+  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:clean
   ./mvnw site
-  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:prepare -P release
-  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:perform -P release,nexus-pro
+  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:prepare
+  ./mvnw -f .mvn/parent/pom.xml --batch-mode release:perform -P nexus-pro
 fi
