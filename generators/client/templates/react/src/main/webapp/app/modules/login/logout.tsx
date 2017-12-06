80341710a7be401bfe22e4feef2e029cc15fa4f3
Deepu K Sasidharan <d4udts@gmail.com>
2017-12-06 17:09:06 +0100

    react: polish

--- generators/client/templates/react/src/main/webapp/app/modules/login/logout.tsx
+++ generators/client/templates/react/src/main/webapp/app/modules/login/logout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 
 import { logout } from '../../reducers/authentication';
 
