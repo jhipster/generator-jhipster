c26decfddaf57b5619bec8405a1b0c900bd21c0e
MathieuAA <mathieu.aa@free.fr>
2017-12-17 11:51:01 +0100

    Made the application exporting work

--- test/spec/export/jhipster_application_exporter_test.js
+++ test/spec/export/jhipster_application_exporter_test.js
@@ -17,8 +17,11 @@
  * limitations under the License.
  */
 
+/* eslint-disable no-unused-expressions */
+
 const expect = require('chai').expect;
 const fs = require('fs');
+const path = require('path');
 const Exporter = require('../../../lib/export/jhipster_application_exporter');
 const JDLParser = require('../../../lib/parser/jdl_parser');
 const parseFromFiles = require('../../../lib/reader/jdl_reader').parseFromFiles;
@@ -38,10 +41,49 @@ describe('JHipsterApplicationExporter', () => {
           }
         });
       });
+      context('such as an invalid application', () => {
+        it('throws an error', () => {
+          try {
+            Exporter.exportApplication({
+              config: {}
+            });
+            fail();
+          } catch (error) {
+            expect(error.name).to.eq('InvalidObjectException');
+          }
+        });
+      });
+      context('such as an invalid path as baseName', () => {
+        before((done) => {
+          fs.writeFile('aFile', '', {}, done);
+        });
+        after((done) => {
+          fs.unlink('aFile', done);
+        });
+
+        it('throws an error', () => {
+          try {
+            Exporter.exportApplication(
+              JDLParser.parse(
+                parseFromFiles(['./test/test_files/application_wrong_basename.jdl']),
+                'sql',
+                'monolith',
+                'toto',
+                '4.9.0'
+              ).applications.aFile
+            );
+            fail();
+          } catch (error) {
+            expect(error.name).to.eq('WrongDirException');
+          }
+        });
+      });
     });
     context('when passing valid arguments', () => {
       context('when exporting an application to JSON', () => {
-        before(() => {
+        let content = null;
+
+        before((done) => {
           Exporter.exportApplication(
             JDLParser.parse(
               parseFromFiles(['./test/test_files/application.jdl']),
@@ -51,12 +93,69 @@ describe('JHipsterApplicationExporter', () => {
               '4.9.0'
             ).applications.toto
           );
+          fs.readFile(path.join('toto', '.yo-rc.json'), { encoding: 'utf8' }, (err, data) => {
+            if (err) {
+              return done(err);
+            }
+            content = JSON.parse(data);
+            return done();
+          });
         });
+
         after((done) => {
-          fs.unlink('.yo-rc.json', done);
+          fs.unlink(path.join('toto', '.yo-rc.json'), () => {
+            fs.rmdir('toto', done);
+          });
         });
-        it('exports it', (done) => {
-          fs.readFile('.yo-rc.json', { encoding: 'utf8' }, done);
+
+        it('exports it', () => {
+          expect(content).not.to.be.undefined;
+        });
+        it('formats it', () => {
+          expect(content['generator-jhipster']).not.to.be.undefined;
+          const config = content['generator-jhipster'];
+          expect(config.jwtSecretKey).not.to.be.undefined;
+          delete config.jwtSecretKey;
+          expect(config).to.deep.equal({
+            applicationType: 'monolith',
+            authenticationType: 'jwt',
+            baseName: 'toto',
+            buildTool: 'maven',
+            clientFramework: 'angular1',
+            clientPackageManager: 'yarn',
+            clusteredHttpSession: 'no',
+            databaseType: 'sql',
+            devDatabaseType: 'h2Memory',
+            enableSocialSignIn: false,
+            enableSwaggerCodegen: false,
+            enableTranslation: false,
+            hibernateCache: 'no',
+            jhiPrefix: 'jhi',
+            jhipsterVersion: '4.9.0',
+            languages: [
+              'en',
+              'fr'
+            ],
+            messageBroker: false,
+            nativeLanguage: 'en',
+            packageFolder: 'com/mathieu/sample',
+            packageName: 'com.mathieu.sample',
+            path: '../../toto',
+            prodDatabaseType: 'mysql',
+            promptValues: {
+              packageName: 'com.mathieu.sample'
+            },
+            searchEngine: false,
+            serverPort: 8080,
+            serviceDiscoveryType: false,
+            skipClient: false,
+            skipServer: false,
+            skipUserManagement: false,
+            testFrameworks: [],
+            useCompass: false,
+            useSass: false,
+            websocket: false
+          });
         });
       });
     });
@@ -75,11 +174,11 @@ describe('JHipsterApplicationExporter', () => {
       });
     });
     context('when passing valid arguments', () => {
-      context('when exporting an application to JSON', () => {
-        before(() => {
+      context('when exporting applications to JSON', () => {
+        before('common setup for both applications', () => {
           Exporter.exportApplications(
             JDLParser.parse(
-              parseFromFiles(['./test/test_files/application.jdl']),
+              parseFromFiles([path.join('test', 'test_files', 'applications.jdl')]),
               'sql',
               'monolith',
               'toto',
@@ -87,11 +186,145 @@ describe('JHipsterApplicationExporter', () => {
             ).applications
           );
         });
-        after((done) => {
-          fs.unlink('.yo-rc.json', done);
+
+        context('for the first application', () => {
+          let content = null;
+
+          before('setup for the first application', (done) => {
+            fs.readFile(path.join('toto', '.yo-rc.json'), { encoding: 'utf8' }, (err, data) => {
+              if (err) {
+                return done(err);
+              }
+              content = JSON.parse(data);
+              return done();
+            });
+          });
+
+          after('cleanup for the fist application', () => {
+            fs.unlinkSync(path.join('toto', '.yo-rc.json'));
+            fs.rmdirSync('toto');
+          });
+
+          it('exports it', (done) => {
+            fs.readFile(path.join('toto', '.yo-rc.json'), { encoding: 'utf8' }, done);
+          });
+
+          it('formats it', () => {
+            expect(content['generator-jhipster']).not.to.be.undefined;
+            const config = content['generator-jhipster'];
+            expect(config.jwtSecretKey).not.to.be.undefined;
+            delete config.jwtSecretKey;
+            expect(config).to.deep.equal({
+              applicationType: 'monolith',
+              authenticationType: 'jwt',
+              baseName: 'toto',
+              buildTool: 'maven',
+              clientFramework: 'angular1',
+              clientPackageManager: 'yarn',
+              clusteredHttpSession: 'no',
+              databaseType: 'sql',
+              devDatabaseType: 'h2Memory',
+              enableSocialSignIn: false,
+              enableSwaggerCodegen: false,
+              enableTranslation: false,
+              hibernateCache: 'no',
+              jhiPrefix: 'jhi',
+              jhipsterVersion: '4.9.0',
+              languages: [
+                'en',
+                'fr'
+              ],
+              messageBroker: false,
+              nativeLanguage: 'en',
+              packageFolder: 'com/mathieu/toto',
+              packageName: 'com.mathieu.toto',
+              path: '../../toto',
+              prodDatabaseType: 'mysql',
+              promptValues: {
+                packageName: 'com.mathieu.toto'
+              },
+              searchEngine: false,
+              serverPort: 8080,
+              serviceDiscoveryType: false,
+              skipClient: false,
+              skipServer: false,
+              skipUserManagement: false,
+              testFrameworks: [],
+              useCompass: false,
+              useSass: false,
+              websocket: false
+            });
+          });
         });
-        it('exports it', (done) => {
-          fs.readFile('.yo-rc.json', { encoding: 'utf8' }, done);
+
+        context('for the second application', () => {
+          let content = null;
+
+          before('setup for the first application', (done) => {
+            fs.readFile(path.join('titi', '.yo-rc.json'), { encoding: 'utf8' }, (err, data) => {
+              if (err) {
+                return done(err);
+              }
+              content = JSON.parse(data);
+              return done();
+            });
+          });
+
+          after('cleanup for the fist application', () => {
+            fs.unlinkSync(path.join('titi', '.yo-rc.json'));
+            fs.rmdirSync('titi');
+          });
+
+          it('exports it', (done) => {
+            fs.readFile(path.join('titi', '.yo-rc.json'), { encoding: 'utf8' }, done);
+          });
+
+          it('formats it', () => {
+            expect(content['generator-jhipster']).not.to.be.undefined;
+            const config = content['generator-jhipster'];
+            expect(config.jwtSecretKey).not.to.be.undefined;
+            delete config.jwtSecretKey;
+            expect(config).to.deep.equal({
+              applicationType: 'monolith',
+              authenticationType: 'jwt',
+              baseName: 'titi',
+              buildTool: 'maven',
+              clientFramework: 'angular1',
+              clientPackageManager: 'yarn',
+              clusteredHttpSession: 'no',
+              databaseType: 'sql',
+              devDatabaseType: 'h2Memory',
+              enableSocialSignIn: false,
+              enableSwaggerCodegen: false,
+              enableTranslation: false,
+              hibernateCache: 'no',
+              jhiPrefix: 'jhi',
+              jhipsterVersion: '4.9.0',
+              languages: [
+                'en',
+                'fr'
+              ],
+              messageBroker: false,
+              nativeLanguage: 'en',
+              packageFolder: 'com/mathieu/titi',
+              packageName: 'com.mathieu.titi',
+              path: '../../titi',
+              prodDatabaseType: 'mysql',
+              promptValues: {
+                packageName: 'com.mathieu.titi'
+              },
+              searchEngine: false,
+              serverPort: 8080,
+              serviceDiscoveryType: false,
+              skipClient: false,
+              skipServer: false,
+              skipUserManagement: false,
+              testFrameworks: [],
+              useCompass: false,
+              useSass: false,
+              websocket: false
+            });
+          });
         });
       });
     });
