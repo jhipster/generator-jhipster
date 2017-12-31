d676838e5501e60fb99cf9d6dd66a2895582271d
Deepu K Sasidharan <d4udts@gmail.com>
2017-12-31 14:02:03 +0100

    react: fix i18n merge error

--- generators/client/templates/react/src/main/webapp/app/shared/util/date-utils.ts
+++ generators/client/templates/react/src/main/webapp/app/shared/util/date-utils.ts
@@ -3,4 +3,4 @@ import * as moment from 'moment';
 import { APP_LOCAL_DATETIME_FORMAT } from '../../config/constants';
 
 export const convertDateTimeFromServer = date =>
-    date ? moment(date).format(APP_LOCAL_DATETIME_FORMAT) : null;
+  date ? moment(date).format(APP_LOCAL_DATETIME_FORMAT) : null;
