780a1fd1def006433d7cd4b8de56727e467fcc94
Deepu K Sasidharan <d4udts@gmail.com>
2017-12-31 12:28:41 +0100

    react: polish

--- generators/entity-client/templates/react/src/main/webapp/app/entities/_entity.tsx
+++ generators/entity-client/templates/react/src/main/webapp/app/entities/_entity.tsx
@@ -20,6 +20,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
+// TODO import TextFormat only when fieldContainsDate
 import { Translate, ICrudGetAction, TextFormat } from 'react-jhipster';
 import { FaPlus, FaEye, FaPencil, FaTrash } from 'react-icons/lib/fa';
 
