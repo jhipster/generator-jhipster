86f534c601e5916ca61cc03b270be00c154c0200
Julien Dubois <julien.dubois@gmail.com>
2018-01-06 01:13:03 +0100

    Allow again the use of e-mail addresses in login fields
    Fix #6923

--- generators/server/templates/src/main/java/package/config/_Constants.java
+++ generators/server/templates/src/main/java/package/config/_Constants.java
@@ -24,7 +24,7 @@ package <%=packageName%>.config;
 public final class Constants {
 
     // Regex for acceptable logins
-    public static final String LOGIN_REGEX = "^[_'.A-Za-z0-9-]*$";
+    public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
 
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String ANONYMOUS_USER = "anonymoususer";
