f1b711b9e0517b74d74239404bfebf3f6f9152cb
Nils Wild <nils.wild@sinnovate.de>
2018-01-06 11:54:26 +0100

    lint fix

--- generators/client/templates/angular/src/test/javascript/spec/app/shared/user/_user.service.spec.ts
+++ generators/client/templates/angular/src/test/javascript/spec/app/shared/user/_user.service.spec.ts
@@ -16,7 +16,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -%>
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { JhiDateUtils } from 'ng-jhipster';
 
 import { UserService, User } from './../../../../../../main/webapp/app/shared';
