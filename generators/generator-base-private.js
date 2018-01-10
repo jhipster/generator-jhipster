c7cc7bdd65a3952e3042d1f689580ae9804425ec
MathieuAA <mathieu.aa@free.fr>
2018-01-10 10:34:03 +0100

    Improved code after review

--- generators/generator-base-private.js
+++ generators/generator-base-private.js
@@ -339,14 +339,14 @@ module.exports = class extends Generator {
      * @returns formatted api description
      */
     formatAsApiDescription(text) {
-        if (!text || text === '') {
+        if (!text) {
             return text;
         }
         const rows = text.split('\n');
         let description = rows[0];
         for (let i = 1; i < rows.length; i++) {
             // discard empty rows
-            if (rows[i] !== '' && rows[i].trim() !== '') {
+            if (rows[i].trim() !== '') {
                 // if simple text then put space between row strings
                 if (!description.endsWith('>') && !rows[i].startsWith('<')) {
                     description += ' ';
@@ -358,7 +358,7 @@ module.exports = class extends Generator {
     }
 
     formatLineForJavaStringUse(text) {
-        if (!text || text === '') {
+        if (!text) {
             return text;
         }
         return text.replace(/"/g, '\\"');
