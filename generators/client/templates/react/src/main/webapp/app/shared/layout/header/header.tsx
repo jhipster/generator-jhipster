e51d13d9aa3d2f2a1764c524096796eb4e5f055c
Deepu K Sasidharan <d4udts@gmail.com>
2018-01-13 15:57:05 +0100

    add prettier for React

--- generators/client/templates/react/src/main/webapp/app/shared/layout/header/header.tsx
+++ generators/client/templates/react/src/main/webapp/app/shared/layout/header/header.tsx
@@ -9,7 +9,9 @@ import {
 import {
   FaHome, FaThList, FaUserPlus, FaUser, FaFlag, FaHeart,
   FaList, FaTasks, FaDashboard, FaBook, FaWrench, FaSignIn, FaSignOut,
-  FaClockO, FaHddO, FaAsterisk
+  FaClockO, FaHddO,
+  // tslint:disable-next-line
+  FaAsterisk
 } from 'react-icons/lib/fa';
 import { NavLink as Link } from 'react-router-dom';
 import LoadingBar from 'react-redux-loading-bar';
