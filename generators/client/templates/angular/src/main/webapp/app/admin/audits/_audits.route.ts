2fb48ee821a32ed8c73e057f1d8ecbf11037e061
Hippolyte Durix <hdurix@softwaymedical.fr>
2018-01-22 14:01:28 +0100

    Add timestamp sort by default instead of id

--- generators/client/templates/angular/src/main/webapp/app/admin/audits/_audits.route.ts
+++ generators/client/templates/angular/src/main/webapp/app/admin/audits/_audits.route.ts
@@ -30,7 +30,7 @@ export class AuditResolvePagingParams implements Resolve<any> {
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const page = route.queryParams['page'] ? route.queryParams['page'] : '1';
-        const sort = route.queryParams['sort'] ? route.queryParams['sort'] : 'id,asc';
+        const sort = route.queryParams['sort'] ? route.queryParams['sort'] : 'auditEventDate,desc';
         return {
             page: this.paginationUtil.parsePage(page),
             predicate: this.paginationUtil.parsePredicate(sort),
