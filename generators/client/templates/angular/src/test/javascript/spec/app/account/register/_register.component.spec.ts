6f32f852887723a9dd2e56f4b5c7367f3af34027
Julien Dubois <julien.dubois@gmail.com>
2018-02-06 22:42:27 +0100

    Fix Karma tests
    See #7085

--- generators/client/templates/angular/src/test/javascript/spec/app/account/register/_register.component.spec.ts
+++ generators/client/templates/angular/src/test/javascript/spec/app/account/register/_register.component.spec.ts
@@ -91,9 +91,7 @@ describe('Component Tests', () => {
                 fakeAsync((service: Register) => {
                     spyOn(service, 'save').and.returnValue(Observable.throw({
                         status: 400,
-                        json() {
-                            return {type : LOGIN_ALREADY_USED_TYPE};
-                        }
+                        error: { type: LOGIN_ALREADY_USED_TYPE }
                     }));
                     comp.registerAccount.password = comp.confirmPassword = 'password';
 
@@ -112,9 +110,7 @@ describe('Component Tests', () => {
                 fakeAsync((service: Register) => {
                     spyOn(service, 'save').and.returnValue(Observable.throw({
                         status: 400,
-                        json() {
-                            return {type : EMAIL_ALREADY_USED_TYPE};
-                        }
+                        error: { type: EMAIL_ALREADY_USED_TYPE }
                     }));
                     comp.registerAccount.password = comp.confirmPassword = 'password';
 
