3f01ca5a42d8dd21da608d7655a6c63e2cd94e31
tibor strausz <tibor@strausz.nl>
2018-02-11 16:43:57 +0100

    Error message not shown with passwrod reset #7098
    revert change because travis test fail

--- generators/client/templates/angular/src/main/webapp/app/account/password-reset/init/_password-reset-init.component.ts
+++ generators/client/templates/angular/src/main/webapp/app/account/password-reset/init/_password-reset-init.component.ts
@@ -20,7 +20,6 @@ import { Component, OnInit, AfterViewInit, Renderer, ElementRef } from '@angular
 
 import { PasswordResetInitService } from './password-reset-init.service';
 import { EMAIL_NOT_FOUND_TYPE } from '../../../shared';
-import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -54,7 +53,7 @@ export class PasswordResetInitComponent implements OnInit, AfterViewInit {
 
         this.passwordResetInitService.save(this.resetAccount.email).subscribe(() => {
             this.success = 'OK';
-        }, (response: HttpErrorResponse) => {
+        }, (response) => {
             this.success = null;
             if (response.status === 400 && response.error.type === EMAIL_NOT_FOUND_TYPE) {
                 this.errorEmailNotExists = 'ERROR';
