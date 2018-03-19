98458fd1cac9bd48381632af65701e72b4334604
chrisdns <cdionisio@ippon.fr>
2018-03-19 18:18:29 +0100

    add infinite scroll for entities + debug

--- generators/client/files-react.js
+++ generators/client/files-react.js
@@ -290,6 +290,7 @@ const files = {
                 // util
                 'shared/util/date-utils.ts',
                 'shared/util/pagination.constants.ts',
+                'shared/util/parsing-utils.ts',
                 // components
                 // model
                 'shared/model/user.model.ts'
