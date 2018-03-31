8357e2f11a8d4aa48d3b7a3e37320cced3bda91a
Julien Dubois <julien.dubois@gmail.com>
2018-03-31 15:24:19 +0200

    Fix lint
    See @7326

--- generators/entity-client/files.js
+++ generators/entity-client/files.js
@@ -211,18 +211,14 @@ function writeFiles() {
                 // write client side files for angular 2.x +
                 this.writeFilesToDisk(angularFiles, this, false, CLIENT_NG2_TEMPLATES_DIR);
                 this.addEntityToModule(this.entityInstance, this.entityClass, this.entityAngularName, this.entityFolderName, this.entityFileName, this.enableTranslation, this.clientFramework, this.microserviceName);
-
             } else {
                 // write client side files for react
                 this.writeFilesToDisk(reactFiles, this, false, CLIENT_REACT_TEMPLATES_DIR);
                 this.addEntityToModule(this.entityInstance, this.entityClass, this.entityAngularName, this.entityFolderName, this.entityFileName, this.enableTranslation, this.clientFramework);
-
             }
-
             if (this.applicationType === 'gateway' && !_.isUndefined(this.microserviceName)) {
                 this.addEntityToWebpack(this.microserviceName, this.clientFramework);
             }
-
             this.addEntityToMenu(this.entityStateName, this.enableTranslation, this.clientFramework, this.entityTranslationKeyMenu);
         }
     };
