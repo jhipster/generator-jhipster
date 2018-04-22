e406d400b18007f6f86063d1c7535d10358323f6
Pablo Gomes Ludermir <gomesp@gmail.com>
2018-04-22 23:17:04 +1200

    Upgrading font-awesome from 4.7.0 to 5.0.10
    The new font-awesome package uses the SVG with JS icon set. The previous
    icon set has now all been renamed to the new icons present in FA5.
    
    This change has been applied to the angular codebase only.

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -83,7 +83,7 @@ module.exports = class extends PrivateBase {
                     needle: 'jhipster-needle-add-element-to-menu',
                     splicable: [`<li class="nav-item" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">
                                 <a class="nav-link" routerLink="${routerName}" (click)="collapseNavbar()">
-                                    <i class="fa fa-${glyphiconName}"></i>&nbsp;
+                                    <i class="fas fa-${glyphiconName}"></i>&nbsp;
                                     <span${enableTranslation ? ` jhiTranslate="global.menu.${routerName}"` : ''}>${_.startCase(routerName)}</span>
                                 </a>
                             </li>`
@@ -143,7 +143,7 @@ module.exports = class extends PrivateBase {
                     needle: 'jhipster-needle-add-element-to-admin-menu',
                     splicable: [`<li>
                         <a class="dropdown-item" routerLink="${routerName}" routerLinkActive="active" (click)="collapseNavbar()">
-                            <i class="fa fa-${glyphiconName}" aria-hidden="true"></i>&nbsp;
+                            <i class="fas fa-${glyphiconName}" aria-hidden="true"></i>&nbsp;
                             <span${enableTranslation ? ` jhiTranslate="global.menu.admin.${routerName}"` : ''}>${_.startCase(routerName)}</span>
                         </a>
                     </li>`
@@ -189,7 +189,7 @@ module.exports = class extends PrivateBase {
                     splicable: [
                         this.stripMargin(`|<li>
                              |                        <a class="dropdown-item" routerLink="${routerName}" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }" (click)="collapseNavbar()">
-                             |                            <i class="fa fa-fw fa-asterisk" aria-hidden="true"></i>
+                             |                            <i class="fas fa-fw fa-asterisk" aria-hidden="true"></i>
                              |                            <span${enableTranslation ? ` jhiTranslate="global.menu.entities.${entityTranslationKeyMenu}"` : ''}>${_.startCase(routerName)}</span>
                              |                        </a>
                              |                    </li>`)
