63b324175faaaf535cd3e3a9808723a9956fb26c
Julien Dubois <julien.dubois@gmail.com>
2018-06-20 12:04:15 +0200

    Update to JHipster Registry 3.3.0

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -18,7 +18,7 @@
  */
 
 // version of docker images
-const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:develop';
+const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:3.3.0';
 const DOCKER_JAVA_JRE = 'openjdk:8-jre-alpine';
 const DOCKER_MYSQL = 'mysql:5.7.20';
 const DOCKER_MARIADB = 'mariadb:10.3.7';
