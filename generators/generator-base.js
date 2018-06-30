133897ab102d6c8b971b3ae530c08c3f54d08ccd
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-06-30 09:12:02 +0200

    checks: add warning when generating project in home directory

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -2059,10 +2059,20 @@ module.exports = class extends PrivateBase {
         this.log(chalk.white.bold('                            https://www.jhipster.tech\n'));
         this.log(chalk.white('Welcome to JHipster ') + chalk.yellow(`v${packagejs.version}`));
         this.log(chalk.white(`Application files will be generated in folder: ${chalk.yellow(process.cwd())}`));
+        if (process.cwd() === this.getUserHome()) {
+            this.log(chalk.red.bold(`️⚠️  WARNING ⚠️  You are in your HOME folder!`))
+        }
         this.log(chalk.green(' _______________________________________________________________________________________________________________\n'));
         this.log(chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://www.jhipster.tech/creating-an-app/')}`));
         this.log(chalk.white(`  If you find JHipster useful, consider sponsoring the project at ${chalk.yellow('https://opencollective.com/generator-jhipster')}`));
         this.log(chalk.green(' _______________________________________________________________________________________________________________\n'));
+    }
+
+    /**
+     * Return the user home
+     */
+    getUserHome() {
+        return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
     }
 
     /**
