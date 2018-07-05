2406551d561c1b8ab97e0a6c002101ef247bb941
Julien Dubois <julien.dubois@gmail.com>
2018-07-05 15:48:26 +0200

    wording

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -2059,7 +2059,7 @@ module.exports = class extends PrivateBase {
         if (process.cwd() === this.getUserHome()) {
             this.log(chalk.red.bold('\n️⚠️  WARNING ⚠️  You are in your HOME folder!'));
             this.log(chalk.red('This can cause problems, you should always create a new directory and run the jhipster command from here.'));
-            this.log(chalk.white(`See the Troubleshooting part at ${chalk.yellow('https://www.jhipster.tech/installation/')}`));
+            this.log(chalk.white(`See the troubleshooting section at ${chalk.yellow('https://www.jhipster.tech/installation/')}`));
         }
         this.log(chalk.green(' _______________________________________________________________________________________________________________\n'));
         this.log(chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://www.jhipster.tech/creating-an-app/')}`));
