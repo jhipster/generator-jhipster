c1b2d20bdd3f00b11683aa47f34c76a55f49b23f
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-07-31 08:41:54 +0200

    Upgrade to Yarn 1.9.2 and NPM 6.2.0

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -59,8 +59,8 @@ const SCALA_VERSION = '2.12.6';
 
 // version of Node, Yarn, NPM
 const NODE_VERSION = '8.11.3';
-const YARN_VERSION = '1.6.0';
-const NPM_VERSION = '6.1.0';
+const YARN_VERSION = '1.9.2';
+const NPM_VERSION = '6.2.0';
 
 // all constants used throughout all generators
 
