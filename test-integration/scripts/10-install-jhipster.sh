8a518f71f1fbb2ebf3ef5b3bca8276c508d68af2
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-23 20:36:30 +0200

    Azure: fix install jhipster

--- test-integration/scripts/10-install-jhipster.sh
+++ test-integration/scripts/10-install-jhipster.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
 set -e
+source $(dirname $0)/00-init-env.sh
 
 #-------------------------------------------------------------------------------
 # Install JHipster Dependencies and Server-side library
 #-------------------------------------------------------------------------------
 cd "$HOME"
 if [[ "$JH_REPO" == *"/jhipster" ]]; then
-    echo "JH_REPO=$JH_REPO"
-    echo "No need to clone jhipster: use local version"
-    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+    echo "No need to clone jhipster: use local version at JH_REPO=$JH_REPO"
 
     cd "$JH_HOME"
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
     ./mvnw clean install -Dgpg.skip=true
 
 elif [[ "$JH_LIB_BRANCH" == "release" ]]; then
@@ -40,12 +42,11 @@ fi
 #-------------------------------------------------------------------------------
 cd "$HOME"
 if [[ "$JH_REPO" == *"/generator-jhipster" ]]; then
-    echo "JH_REPO=$JH_REPO"
-    echo "No need to clone generator-jhipster: use local version"
-    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+    echo "No need to clone generator-jhipster: use local version at JH_REPO=$JH_REPO"
 
     cd "$JH_HOME"
-    ls -al
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
     npm install
     npm install -g "$JH_HOME"
     if [[ "$JH_APP" == "" || "$JH_APP" == "ngx-default" ]]; then
@@ -53,9 +54,11 @@ if [[ "$JH_REPO" == *"/generator-jhipster" ]]; then
     fi
 
 elif [[ "$JH_GEN_BRANCH" == "release" ]]; then
+    echo "No need to clone generator-jhipster: use release version"
     npm install -g generator-jhipster
 
 else
+    echo "Use generator-jhipster: JH_GEN_REPO=$JH_GEN_REPO with JH_GEN_BRANCH=$JH_GEN_BRANCH"
     git clone "$JH_GEN_REPO" generator-jhipster
     cd generator-jhipster
     if [ "$JH_GEN_BRANCH" == "latest" ]; then
