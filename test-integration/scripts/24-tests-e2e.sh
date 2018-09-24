ac0bd983e65a7de25124cd9955c97499e5f926f7
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-24 08:19:34 +0200

    Azure: add separate script for packaging

--- test-integration/scripts/24-tests-e2e.sh
+++ test-integration/scripts/24-tests-e2e.sh
@@ -60,43 +60,6 @@ launchCurlOrProtractor() {
 }
 
 #-------------------------------------------------------------------------------
-# Package UAA
-#-------------------------------------------------------------------------------
-if [[ "$JH_APP" == *"uaa"* ]]; then
-    cd "$JH_FOLDER_UAA"
-    ./mvnw verify -DskipTests -P"$JH_PROFILE"
-fi
-
-#-------------------------------------------------------------------------------
-# Decrease Angular timeout for Protractor tests
-#-------------------------------------------------------------------------------
-if [ "$JH_PROTRACTOR" == 1 ] && [ -e "src/main/webapp/app/shared/shared-libs.module.ts" ]; then
-    sed -e 's/alertAsToast: false,/alertAsToast: false, alertTimeout: 1,/1;' src/main/webapp/app/shared/shared-libs.module.ts > src/main/webapp/app/shared/shared-libs.module.ts.sed
-    mv -f src/main/webapp/app/shared/shared-libs.module.ts.sed src/main/webapp/app/shared/shared-libs.module.ts
-    cat src/main/webapp/app/shared/shared-libs.module.ts | grep alertTimeout
-fi
-
-#-------------------------------------------------------------------------------
-# Package the application
-#-------------------------------------------------------------------------------
-cd "$JH_FOLDER_APP"
-
-if [ -f "mvnw" ]; then
-    ./mvnw verify -DskipTests -P"$JH_PROFILE"
-    mv target/*.war app.war
-elif [ -f "gradlew" ]; then
-    ./gradlew bootWar -P"$JH_PROFILE" -x test
-    mv build/libs/*.war app.war
-else
-    echo "No mvnw or gradlew"
-    exit 0
-fi
-if [ $? -ne 0 ]; then
-    echo "Error when packaging"
-    exit 1
-fi
-
-#-------------------------------------------------------------------------------
 # Run the application
 #-------------------------------------------------------------------------------
 if [ "$JH_RUN_APP" == 1 ]; then
