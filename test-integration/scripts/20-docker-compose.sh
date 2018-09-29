76d5692448cd762b070cdec074c6868ace9ff78e
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-29 22:08:49 +0200

    Azure: start elasticsearch before database

--- test-integration/scripts/20-docker-compose.sh
+++ test-integration/scripts/20-docker-compose.sh
@@ -10,6 +10,12 @@ cd "$JH_FOLDER_APP"
 if [ -a src/main/docker/jhipster-registry.yml ]; then
     docker-compose -f src/main/docker/jhipster-registry.yml up -d
 fi
+if [ -a src/main/docker/elasticsearch.yml ]; then
+    docker-compose -f src/main/docker/elasticsearch.yml up -d
+fi
+if [ -a src/main/docker/kafka.yml ]; then
+    docker-compose -f src/main/docker/kafka.yml up -d
+fi
 if [ -a src/main/docker/consul.yml ]; then
     docker-compose -f src/main/docker/consul.yml up -d
 fi
@@ -30,14 +36,8 @@ fi
 if [ -a src/main/docker/postgresql.yml ]; then
     docker-compose -f src/main/docker/postgresql.yml up -d
 fi
-if [ -a src/main/docker/elasticsearch.yml ]; then
-    docker-compose -f src/main/docker/elasticsearch.yml up -d
-fi
 if [ -a src/main/docker/mariadb.yml ]; then
     docker-compose -f src/main/docker/mariadb.yml up -d
-fi
-if [ -a src/main/docker/kafka.yml ]; then
-    docker-compose -f src/main/docker/kafka.yml up -d
 fi
 if [ -a src/main/docker/keycloak.yml ]; then
     docker-compose -f src/main/docker/keycloak.yml up -d
