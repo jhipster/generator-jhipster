24b92161e7ed03df920d42955f85111a2010892f
Anthony Viard <anth.viard@gmail.com>
2018-10-01 00:29:37 +0200

    Store timestamps in UTC with hibernate configuration
    store DateTime in UTC by using hibernate.jdbc.time_zone property
    is an easier way to handle timestamps in databases.
    All timestamps will be serialized in UTC before being added
    
    #8284

--- generators/server/files.js
+++ generators/server/files.js
@@ -1208,6 +1208,24 @@ const serverFiles = {
             ]
         },
         {
+            condition: generator => generator.databaseType === 'sql',
+            path: SERVER_TEST_SRC_DIR,
+            templates: [
+                {
+                    file: 'package/config/timezone/HibernateTimeZoneTest.java',
+                    renameTo: generator => `${generator.testDir}config/timezone/HibernateTimeZoneTest.java`
+                },
+                {
+                    file: 'package/config/timezone/DateTimeWrapper.java',
+                    renameTo: generator => `${generator.testDir}config/timezone/DateTimeWrapper.java`
+                },
+                {
+                    file: 'package/config/timezone/DateTimeWrapperRepository.java',
+                    renameTo: generator => `${generator.testDir}config/timezone/DateTimeWrapperRepository.java`
+                }
+            ]
+        },
+        {
             path: SERVER_TEST_RES_DIR,
             templates: ['config/application.yml', 'logback.xml']
         },
