a352d8cdcf415caa3f3391842bc65cd9d5f22318
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-10-02 18:57:55 +0200

    Put default alertTimeout to 5000ms

--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -69,7 +69,7 @@ fi
 # Decrease Angular timeout for Protractor tests
 #-------------------------------------------------------------------------------
 if [ "$PROTRACTOR" == 1 ] && [ -e "src/main/webapp/app/shared/shared-libs.module.ts" ]; then
-    sed -e 's/alertTimeout: 5/alertTimeout: 1/1;' src/main/webapp/app/shared/shared-libs.module.ts > src/main/webapp/app/shared/shared-libs.module.ts.sed
+    sed -e 's/alertTimeout: 5000/alertTimeout: 1/1;' src/main/webapp/app/shared/shared-libs.module.ts > src/main/webapp/app/shared/shared-libs.module.ts.sed
     mv -f src/main/webapp/app/shared/shared-libs.module.ts.sed src/main/webapp/app/shared/shared-libs.module.ts
     cat src/main/webapp/app/shared/shared-libs.module.ts | grep alertTimeout
 fi
