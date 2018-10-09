a5fd1658bff925c1da401631a83f68852632bab1
William Marques <wmarques@ippon.fr>
2018-10-09 12:23:18 +0200

    angular: Remove principal service and merge it with account service

--- generators/client/files-angular.js
+++ generators/client/files-angular.js
@@ -392,7 +392,6 @@ const files = {
             templates: [
                 'core/auth/csrf.service.ts',
                 'core/auth/state-storage.service.ts',
-                'core/auth/principal.service.ts',
                 'shared/auth/has-any-authority.directive.ts',
                 'core/auth/account.service.ts',
                 'core/auth/user-route-access-service.ts'
@@ -427,7 +426,6 @@ const files = {
                 'spec/app/admin/metrics/metrics-modal.component.spec.ts',
                 'spec/helpers/spyobject.ts',
                 'spec/helpers/mock-account.service.ts',
-                'spec/helpers/mock-principal.service.ts',
                 'spec/helpers/mock-route.service.ts',
                 'spec/helpers/mock-login.service.ts',
                 'spec/helpers/mock-event-manager.service.ts',
