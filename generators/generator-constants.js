38e93b6913f5213f5da59fa5c3d05b7ae4e55b0e
Julien Dubois <julien.dubois@gmail.com>
2018-11-02 15:16:20 +0100

    Update to the latest JHipster Registry v4.0.6

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -18,7 +18,7 @@
  */
 
 // version of docker images
-const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:v4.0.5';
+const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:v4.0.6';
 const DOCKER_JAVA_JRE = 'openjdk:8-jre-alpine';
 const DOCKER_MYSQL = 'mysql:5.7.20';
 const DOCKER_MARIADB = 'mariadb:10.3.7';
