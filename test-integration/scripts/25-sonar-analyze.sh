2efbd9ae180c38630699863d0ec0ab2de8870244
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-11-03 20:28:47 +0100

    Sonar: try to fix analyze

--- test-integration/scripts/25-sonar-analyze.sh
+++ test-integration/scripts/25-sonar-analyze.sh
@@ -9,21 +9,21 @@ cd "$JHI_FOLDER_APP"
 if [ "$JHI_APP" == "ngx-default" ]; then
     # specitic to Travis here
     if [ "$TRAVIS_REPO_SLUG" = "jhipster/generator-jhipster" ]; then
-        if [ "$TRAVIS_BRANCH" = "master" ]; then
-            echo "*** Sonar analyze for master branch"
-            ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
-                -Dsonar.host.url=https://sonarcloud.io \
-                -Dsonar.login=$SONAR_TOKEN
-        else
+        if [ "${TRAVIS_PULL_REQUEST:-false}" != "false" ]; then
             echo "*** Sonar analyze for $TRAVIS_PULL_REQUEST"
             ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
                 -Dsonar.host.url=https://sonarcloud.io \
                 -Dsonar.login=$SONAR_TOKEN \
                 -Dsonar.pullrequest.provider=github \
                 -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG \
-                -Dsonar.pullrequest.base=master \
+                -Dsonar.pullrequest.base=$TRAVIS_BRANCH \
                 -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
                 -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH
+        else
+            echo "*** Sonar analyze for master branch"
+            ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
+                -Dsonar.host.url=https://sonarcloud.io \
+                -Dsonar.login=$SONAR_TOKEN
         fi
     fi
 fi
