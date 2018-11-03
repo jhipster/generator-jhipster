5cd2a21eb18bca6b32d6345a37cf1373b6719fde
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-11-03 18:35:49 +0100

    Sonar: try to analyze pull request

--- test-integration/scripts/25-sonar-analyze.sh
+++ test-integration/scripts/25-sonar-analyze.sh
@@ -8,7 +8,22 @@ source $(dirname $0)/00-init-env.sh
 cd "$JHI_FOLDER_APP"
 if [ "$JHI_APP" == "ngx-default" ]; then
     # specitic to Travis here
-    if [ "$TRAVIS_REPO_SLUG" = "jhipster/generator-jhipster" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-        ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
+    if [ "$TRAVIS_REPO_SLUG" = "jhipster/generator-jhipster" ]; then
+        if [ "$TRAVIS_BRANCH" = "master" ]; then
+            echo "*** Sonar analyze for master branch"
+            ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
+                -Dsonar.host.url=https://sonarcloud.io \
+                -Dsonar.login=$SONAR_TOKEN
+        else
+            echo "*** Sonar analyze for $TRAVIS_PULL_REQUEST"
+            ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
+                -Dsonar.host.url=https://sonarcloud.io \
+                -Dsonar.login=$SONAR_TOKEN \
+                -Dsonar.pullrequest.provider=github \
+                -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG
+                -Dsonar.pullrequest.base=master \
+                -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
+                -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
+        fi
     fi
 fi
