4f095f2d119f0c10552ae22b8ea0a6e7a95065f9
Frederik Hahne <atomfrede@gmail.com>
2018-11-08 20:26:43 +0100

    reduce timeout in the correct module file

--- test-integration/scripts/23-package.sh
+++ test-integration/scripts/23-package.sh
@@ -15,10 +15,10 @@ fi
 # Decrease Angular timeout for Protractor tests
 #-------------------------------------------------------------------------------
 cd "$JHI_FOLDER_APP"
-if [ "$JHI_PROTRACTOR" == 1 ] && [ -e "src/main/webapp/app/shared/shared-libs.module.ts" ]; then
-    sed -e 's/alertTimeout: 5000/alertTimeout: 1/1;' src/main/webapp/app/shared/shared-libs.module.ts > src/main/webapp/app/shared/shared-libs.module.ts.sed
-    mv -f src/main/webapp/app/shared/shared-libs.module.ts.sed src/main/webapp/app/shared/shared-libs.module.ts
-    cat src/main/webapp/app/shared/shared-libs.module.ts | grep alertTimeout
+if [ "$JHI_PROTRACTOR" == 1 ] && [ -e "src/main/webapp/app/app.module.ts" ]; then
+    sed -e 's/alertTimeout: 5000/alertTimeout: 1/1;' src/main/webapp/app/app.module.ts > src/main/webapp/app/aap.module.ts.sed
+    mv -f src/main/webapp/app/app.module.ts.sed src/main/webapp/app/app.module.ts
+    cat src/main/webapp/app/app.module.ts | grep alertTimeout
 fi
 
 #-------------------------------------------------------------------------------
