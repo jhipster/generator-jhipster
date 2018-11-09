dd3e86091f02951fbcd25ae92ea62b110e399bb2
Clément Dessoude <clement.dessoude@gmail.com>
2018-11-09 10:23:57 +0100

    add cache & datasources metrics components + create metrics module

---
+++ src/component/metrics/jhi-jvm-memory.component.ts
@@ -0,0 +1,46 @@
+/*
+ Copyright 2013-2017 the original author or authors from the JHipster project.
+
+ This file is part of the JHipster project, see https://jhipster.github.io/
+ for more information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'jhi-jvm-memory',
+    template: `
+            <b jhiTranslate="metrics.jvm.memory.title">Memory</b>
+            <div *ngFor="let entry of jvmMetrics | keys">
+                <p><span>{{entry.key}}</span> ({{entry.value.used / 1048576 | number:'1.0-0'}}M / {{entry.value.max / 1048576 | number:'1.0-0'}}M)</p>
+                <ngb-progressbar type="success" [value]="100 * entry.value.used/entry.value.max" [striped]="true" [animated]="false">
+                    <span>{{entry.value.used * 100 / entry.value.max | number:'1.0-0'}}%</span>
+                </ngb-progressbar>
+        </div>`
+})
+export class JhiJvmMemoryComponent implements OnInit {
+
+    /**
+     * the boolean input value
+     */
+    @Input() jvmMetrics: {};
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
---
+++ src/component/metrics/jhi-jvm-threads.component.ts
@@ -0,0 +1,67 @@
+/*
+ Copyright 2013-2017 the original author or authors from the JHipster project.
+
+ This file is part of the JHipster project, see https://jhipster.github.io/
+ for more information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'jhi-jvm-threads',
+    template: `
+        <b jhiTranslate="metrics.jvm.threads.title">Threads</b> (Total: {{threadStats.threadDumpAll}}) <a class="hand" (click)="refreshThreadDumpData()" data-toggle="modal"
+                                                                                                          data-target="#threadDump">
+            <fa-icon [icon]="'eye'"></fa-icon>
+        </a>
+        <p><span jhiTranslate="metrics.jvm.threads.runnable">Runnable</span> {{threadStats.threadDumpRunnable}}</p>
+        <ngb-progressbar [value]="threadStats.threadDumpRunnable" [max]="threadStats.threadDumpAll" [striped]="true" [animated]="false" type="success">
+            <span>{{threadStats.threadDumpRunnable * 100 / threadStats.threadDumpAll | number:'1.0-0'}}%</span>
+        </ngb-progressbar>
+        <p><span jhiTranslate="metrics.jvm.threads.timedwaiting">Timed Waiting</span> ({{threadStats.threadDumpTimedWaiting}})</p>
+        <ngb-progressbar [value]="threadStats.threadDumpTimedWaiting" [max]="threadStats.threadDumpAll" [striped]="true" [animated]="false" type="warning">
+            <span>{{threadStats.threadDumpTimedWaiting * 100 / threadStats.threadDumpAll | number:'1.0-0'}}%</span>
+        </ngb-progressbar>
+        <p><span jhiTranslate="metrics.jvm.threads.waiting">Waiting</span> ({{threadStats.threadDumpWaiting}})</p>
+        <ngb-progressbar [value]="threadStats.threadDumpWaiting" [max]="threadStats.threadDumpAll" [striped]="true" [animated]="false" type="warning">
+            <span>{{threadStats.threadDumpWaiting * 100 / threadStats.threadDumpAll | number:'1.0-0'}}%</span>
+        </ngb-progressbar>
+        <p><span jhiTranslate="metrics.jvm.threads.blocked">Blocked</span> ({{threadStats.threadDumpBlocked}})</p>
+        <ngb-progressbar [value]="threadStats.threadDumpBlocked" [max]="threadStats.threadDumpAll" [striped]="true" [animated]="false" type="success">
+            <span>{{threadStats.threadDumpBlocked * 100 / threadStats.threadDumpAll | number:'1.0-0'}}%</span>
+        </ngb-progressbar>`
+})
+export class JhiJvmThreadsComponent implements OnInit {
+
+    /**
+     * the boolean input value
+     */
+    @Input() threadStats: {
+        threadDumpAll: number;
+        threadDumpRunnable: number;
+        threadDumpTimedWaiting: number;
+        threadDumpWaiting: number;
+        threadDumpBlocked: number;
+    };
+
+    @Input() refreshThreadDumpData: Function;
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
