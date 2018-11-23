d98dd7d24cfb5965c016ba07acd9be16107236a5
Gaël Marziou <gael.marziou@gmail.com>
2018-11-23 01:01:51 +0100

    Added Angular Material

--- jhipster-material/src/main/webapp/app/home/home.module.ts
+++ jhipster-material/src/main/webapp/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { JhipsterSharedModule } from 'app/shared';
+import { JhipsterMaterialModule } from 'app/material.module';
 import { HOME_ROUTE, HomeComponent } from './';
 
 @NgModule({
-    imports: [JhipsterSharedModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [JhipsterSharedModule, JhipsterMaterialModule, RouterModule.forChild([HOME_ROUTE])],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
