7678ce7be8e5b54537d49de7c30407464d5da7a3
Aurélien Mino <aurelien.mino@gmail.com>
2018-12-14 23:26:13 +0100

    Warn instead of throw an error if common is invoked from yeoman
    This is necessary for now, otherwise 'yo jhipster:app' is ending with an error

--- generators/common/index.js
+++ generators/common/index.js
@@ -17,6 +17,7 @@
  * limitations under the License.
  */
 /* eslint-disable consistent-return */
+const chalk = require('chalk');
 const BaseBlueprintGenerator = require('../generator-base-blueprint');
 const writeFiles = require('./files').writeFiles;
 const constants = require('../generator-constants');
@@ -55,7 +56,11 @@ module.exports = class extends BaseBlueprintGenerator {
         return {
             validateFromCli() {
                 if (!this.options['from-cli']) {
-                    this.error('This JHipster subgenerator is not intented for standalone use.');
+                    this.warning(
+                        `Deprecated: JHipster seems to be invoked using Yeoman command. Please use the JHipster CLI. Run ${chalk.red(
+                            'jhipster <command>'
+                        )} instead of ${chalk.red('yo jhipster:<command>')}`
+                    );
                 }
             },
 
