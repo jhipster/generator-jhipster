fa55ce90c01c8912a8a78b88f004c9e7e3653dc9
Jeff Johnson <jeffcjohson@gmail.com>
2019-01-14 18:28:24 -0500

    Merge branch 'cucumber-context-configuration' of github.com:jeffcjohnson/generator-jhipster into cucumber-context-configuration
    * 'cucumber-context-configuration' of github.com:jeffcjohnson/generator-jhipster:
      ensure only one glue class configures the Spring context
    
    # Conflicts:
    #	generators/server/files.js

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -660,7 +660,7 @@ const expectedFiles = {
     cucumber: [
         `${TEST_DIR}features/user/user.feature`,
         `${TEST_DIR}features/gitkeep`,
-        `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/cucumber/stepdefs/CucumberContextConfiguration.java`,
+        `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/cucumber/CucumberContextConfiguration.java`,
         `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/cucumber/stepdefs/UserStepDefs.java`,
         `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/cucumber/stepdefs/StepDefs.java`,
         `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/cucumber/CucumberTest.java`
