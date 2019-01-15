1b0765ecb9d4892fcedba5a1679bcf21dd4aed65
Panayiotis Vlissidis <pvlissidis@gmail.com>
2019-01-15 17:02:21 +0200

    fix failing docker compose for elastic
    - removed commented out line

--- test-integration/scripts/20-no-memory-limit-elasticsearch.sh
+++ test-integration/scripts/20-no-memory-limit-elasticsearch.sh
@@ -5,7 +5,6 @@ source $(dirname $0)/00-init-env.sh
 
 cd "$JHI_FOLDER_APP"
 if [ -a src/main/docker/elasticsearch.yml ]; then
-    #sed -i -e 's/        environment:/        # environment:/1;' src/main/docker/elasticsearch.yml
     sed -i -e 's/            - \"ES_JAVA_OPTS=-Xms1024m -Xmx1024m\"/        #     - \"ES_JAVA_OPTS=-Xms1024m -Xmx1024m\"/1;' src/main/docker/elasticsearch.yml
     cat src/main/docker/elasticsearch.yml
 fi
