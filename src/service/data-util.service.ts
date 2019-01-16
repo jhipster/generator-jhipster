0b10c600473c74ca46ad1aaf4ad5d074afc84232
Vivek More <vivek.more@gfs.com>
2019-01-16 17:32:54 -0500

    remove redundant units from style

--- src/service/data-util.service.ts
+++ src/service/data-util.service.ts
@@ -68,7 +68,7 @@ export class JhiDataUtils {
             const fileURL = `data:${contentType};base64,${data}`;
             const win = window.open();
             win.document.write(
-                '<iframe src="' + fileURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+                '<iframe src="' + fileURL + '" frameborder="0" style="border:0; top:0; left:0; bottom:0; right:0; width:100%; height:100%;" allowfullscreen></iframe>');
         }
     }
 
