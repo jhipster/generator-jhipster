652d9409708510ddcfdf3c73c92205345ef3710c
Anthony Viard <anth.viard@gmail.com>
2019-01-17 14:45:36 +0100

    Use needleNase API in clientBase

--- generators/needle/needle-client-base.js
+++ generators/needle/needle-client-base.js
@@ -3,7 +3,9 @@ const needleBase = require('./needle-base');
 module.exports = class extends needleBase {
     addStyle(style, comment, filePath, needle) {
         const styleBlock = this.mergeStyleAndComment(style, comment);
-        this.addBlockContentToFile(filePath, styleBlock, needle);
+        const rewriteFileModel = this.generateFileModel(filePath, needle, styleBlock);
+
+        this.addBlockContentToFile(rewriteFileModel, 'Style not added to JHipster app.\n');
     }
 
     mergeStyleAndComment(style, comment) {
