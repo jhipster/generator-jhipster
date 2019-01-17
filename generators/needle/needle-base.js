71f21303b3aff0e7cf4e3f52fb5c1e95984b9970
Anthony Viard <anth.viard@gmail.com>
2019-01-17 14:44:43 +0100

    Define constructor and add generateFileModel

--- generators/needle/needle-base.js
+++ generators/needle/needle-base.js
@@ -2,23 +2,44 @@ const chalk = require('chalk');
 const jhipsterUtils = require('../utils');
 
 module.exports = class {
-    addBlockContentToFile(fullPath, content, needleTag) {
+    constructor(generator) {
+        this.generator = generator;
+    }
+
+    addBlockContentToFile(rewriteFileModel, errorMessage) {
         try {
             jhipsterUtils.rewriteFile(
                 {
-                    file: fullPath,
-                    needle: needleTag,
-                    splicable: [content]
+                    file: rewriteFileModel.file,
+                    needle: rewriteFileModel.needle,
+                    splicable: rewriteFileModel.splicable
                 },
-                this
+                this.generator
             );
         } catch (e) {
-            this.log(
-                chalk.yellow('\nUnable to find ') +
-                fullPath +
-                chalk.yellow(' or missing required jhipster-needle. Content not added to JHipster app.\n')
-            );
-            this.debug('Error:', e);
+            this.logNeedleNotFound(e, errorMessage, rewriteFileModel.file);
         }
+    }
+
+    logNeedleNotFound(exception, message, fullPath) {
+        if (!message) {
+            message = 'Content not added to file';
+        }
+        this.generator.log(chalk.yellow('\nUnable to find ') +
+         fullPath +
+         chalk.yellow(` or missing required jhipster-needle. ${message}\n`));
+        this.generator.debug('Error:', exception);
+    }
+
+    generateFileModelWithPath(aPath, aFile, needleTag, ...content) {
+        return Object.assign(this.generateFileModel(aFile, needleTag, ...content), { path: aPath });
+    }
+
+    generateFileModel(aFile, needleTag, ...content) {
+        return {
+            file: aFile,
+            needle: needleTag,
+            splicable: content
+        };
     }
 };
