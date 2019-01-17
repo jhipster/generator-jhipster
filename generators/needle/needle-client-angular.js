66d2fa98a4dc7cdddbc4e9ecd237c16148285094
Anthony Viard <anth.viard@gmail.com>
2019-01-17 14:45:06 +0100

    Fix scss and css path and needle

--- generators/needle/needle-client-angular.js
+++ generators/needle/needle-client-angular.js
@@ -5,12 +5,12 @@ const CLIENT_MAIN_SRC_DIR = constants.CLIENT_MAIN_SRC_DIR;
 
 module.exports = class extends needleClientBase {
     addGlobalCSSStyle(style, comment) {
-        const filePath = `${CLIENT_MAIN_SRC_DIR}content/scss/global.scss`;
+        const filePath = `${CLIENT_MAIN_SRC_DIR}content/css/global.css`;
         this.addStyle(style, comment, filePath, 'jhipster-needle-css-add-global');
     }
 
     addGlobalSCSSStyle(style, comment) {
-        const filePath = `${CLIENT_MAIN_SRC_DIR}content/css/global.css`;
+        const filePath = `${CLIENT_MAIN_SRC_DIR}content/scss/global.scss`;
         this.addStyle(style, comment, filePath, 'jhipster-needle-scss-add-global');
     }
 
