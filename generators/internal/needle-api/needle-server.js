f38e9dad7f110837b31bbdf01df2e238d2e08c16
Anthony Viard <anth.viard@gmail.com>
2019-01-18 10:49:48 +0100

    revert wrong modifications on needle-server

--- generators/internal/needle-api/needle-server.js
+++ generators/internal/needle-api/needle-server.js
@@ -1,21 +1,3 @@
-const NeedleBase = require('./needle-base');
-const NeedleClientAngular = require('./needle-client-angular');
-const NeedleClientReact = require('./needle-client-react');
-const NeedleClientWebpack = require('./needle-client-webpack');
-const NeedleClientI18n = require('./needle-client-i18n');
-const NeedleServerMaven = require('./needle-server-maven');
-const NeedleServerCache = require('./needle-server-cache');
-const NeedleServerLiquibase = require('./needle-server-liquibase');
+const needleBase = require('./needle-base');
 
-module.exports = class {
-    constructor(generator) {
-        this.needleBase = new NeedleBase(generator);
-        this.needleServerMaven = new NeedleServerMaven(generator);
-        this.needleServerCache = new NeedleServerCache(generator);
-        this.needleServerLiquibase = new NeedleServerLiquibase(generator);
-        this.needleClientAngular = new NeedleClientAngular(generator);
-        this.needleClientReact = new NeedleClientReact(generator);
-        this.needleClientWebpack = new NeedleClientWebpack(generator);
-        this.needleClientI18n = new NeedleClientI18n(generator);
-    }
-};
+module.exports = class extends needleBase {};
