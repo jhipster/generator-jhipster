6f236d676cfa6c58bdf64e803e7c606dc8172b87
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-01-26 12:15:40 -0800

    update manifest start_url as stated in react deployment docs

--- generators/entity-client/files.js
+++ generators/entity-client/files.js
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const _ = require('lodash');
 const constants = require('../generator-constants');
 
 /* Constants use throughout */
--- generators/internal/needle-api/needle-client-webpack.js
+++ generators/internal/needle-api/needle-client-webpack.js
@@ -16,8 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const chalk = require('chalk');
-
 const needleClient = require('./needle-client');
 const constants = require('../../generator-constants');
 
