b07b364a4c48954089aac735ac1870a35e51a704
Sahbi KTIFA <sahbi.ktifa@gmail.com>
2019-02-05 11:42:14 +0100

    Health Modal component tests

--- generators/client/files.js
+++ generators/client/files.js
@@ -308,6 +308,7 @@ const vueFiles = {
                 'spec/app/admin/configuration/configuration.component.spec.ts',
                 'spec/app/admin/docs/docs.component.spec.ts',
                 'spec/app/admin/health/health.component.spec.ts',
+                'spec/app/admin/health/health-modal.component.spec.ts',
                 'spec/app/admin/health/health.service.spec.ts',
                 'spec/app/admin/logs/logs.component.spec.ts',
                 'spec/app/admin/audits/audits.component.spec.ts',
