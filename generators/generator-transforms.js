ad27a82ce8574110ff8b2bf549672c8cccc14cb1
Anthony Viard <anth.viard@gmail.com>
2019-02-07 19:09:42 +0100

    Add condition when applying prettierTransform
    Files mark as 'deleted' has not content defined.
    We have to skip them.
    
    #9211

--- generators/generator-transforms.js
+++ generators/generator-transforms.js
@@ -31,18 +31,20 @@ const prettierOptions = {
 
 const prettierTransform = function(defaultOptions) {
     const transform = (file, encoding, callback) => {
-        /* resolve from the projects config */
-        prettier.resolveConfig(file.relative).then(options => {
-            const str = file.contents.toString('utf8');
-            if (!options || Object.keys(options).length === 0) {
-                options = defaultOptions;
-            }
-            // for better errors
-            options.filepath = file.relative;
-            const data = prettier.format(str, options);
-            file.contents = Buffer.from(data);
-            callback(null, file);
-        });
+        if(file.state !== 'deleted') {
+            /* resolve from the projects config */
+            prettier.resolveConfig(file.relative).then(options => {
+                const str = file.contents.toString('utf8');
+                if (!options || Object.keys(options).length === 0) {
+                    options = defaultOptions;
+                }
+                // for better errors
+                options.filepath = file.relative;
+                const data = prettier.format(str, options);
+                file.contents = Buffer.from(data);
+                callback(null, file);
+            });
+        }
     };
     return through.obj(transform);
 };
