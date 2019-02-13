05a3bf4cbc0d5d231f6139069a126c2a0399b2e8
Anthony Viard <anth.viard@gmail.com>
2019-02-13 09:21:28 +0100

    Add client webpack needle api unit tests

---
+++ test/needle-api/needle-client-webpack.spec.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const ClientGenerator = require('../../generators/client');
+const constants = require('../../generators/generator-constants');
+
+const CLIENT_MAIN_SRC_DIR = constants.CLIENT_MAIN_SRC_DIR;
+const CLIENT_WEBPACK_DIR = constants.CLIENT_WEBPACK_DIR;
+const microserviceName = 'myMicroService';
+const assetFrom = 'source';
+const assetTo = 'target';
+
+const mockBlueprintSubGen = class extends ClientGenerator {
+    constructor(args, opts) {
+        super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
+
+        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+
+        if (!jhContext) {
+            this.error('This is a JHipster blueprint and should be used only like jhipster --blueprint myblueprint');
+        }
+
+        this.configOptions = jhContext.configOptions || {};
+        // This sets up options for this sub generator and is being reused from JHipster
+        jhContext.setupEntityOptions(this, jhContext, this);
+    }
+
+    get initializing() {
+        return super._initializing();
+    }
+
+    get prompting() {
+        return super._prompting();
+    }
+
+    get configuring() {
+        return super._configuring();
+    }
+
+    get default() {
+        return super._default();
+    }
+
+    get writing() {
+        const phaseFromJHipster = super._writing();
+        const customPhaseSteps = {
+            webpackPhase() {
+                this.addEntityToWebpack(microserviceName);
+                this.copyExternalAssetsInWebpack(assetFrom, assetTo);
+            }
+        };
+        return { ...phaseFromJHipster, ...customPhaseSteps };
+    }
+};
+
+describe('needle API Webpack: JHipster client generator with blueprint', () => {
+    const blueprintNames = ['generator-jhipster-myblueprint', 'myblueprint'];
+
+    blueprintNames.forEach(blueprintName => {
+        describe(`generate client with blueprint option '${blueprintName}'`, () => {
+            before(done => {
+                helpers
+                    .run(path.join(__dirname, '../../generators/client'))
+                    .withOptions({
+                        'from-cli': true,
+                        build: 'maven',
+                        auth: 'jwt',
+                        db: 'mysql',
+                        skipInstall: true,
+                        blueprint: blueprintName,
+                        skipChecks: true
+                    })
+                    .withGenerators([[mockBlueprintSubGen, 'jhipster-myblueprint:client']])
+                    .withPrompts({
+                        baseName: 'jhipster',
+                        clientFramework: 'angularX',
+                        useSass: true,
+                        enableTranslation: true,
+                        nativeLanguage: 'en',
+                        languages: ['en', 'fr']
+                    })
+                    .on('end', done);
+            });
+
+            it('Assert microservice name is added to webpack.dev.js in lowercase', () => {
+                assert.fileContent(`${CLIENT_WEBPACK_DIR}/webpack.dev.js`, `'/${microserviceName.toLowerCase()}',`);
+            });
+
+            it('Assert external asset is added to webpack.common.js', () => {
+                const from = `${CLIENT_MAIN_SRC_DIR}content/${assetFrom}/`;
+                const to = `content/${assetTo}/`;
+
+                assert.fileContent(`${CLIENT_WEBPACK_DIR}/webpack.common.js`, `{ from: './${from}', to: '${to}' },`);
+            });
+        });
+    });
+});
