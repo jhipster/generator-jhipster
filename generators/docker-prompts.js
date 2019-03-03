66bd896dd09ef994f3c7421bab9f72d77a8e485a
Pierre Besson <pbesson@ippon.fr>
2019-03-03 16:56:39 +0100

    test only that the .yo-rc.json is present to detect an app for the docker prompts

--- generators/docker-prompts.js
+++ generators/docker-prompts.js
@@ -455,9 +455,7 @@ function getAppFolders(input, deploymentApplicationType) {
     files.forEach(file => {
         if (file.isDirectory()) {
             if (
-                shelljs.test('-f', `${destinationPath}/${file.name}/.yo-rc.json`) &&
-                shelljs.test('-f', `${destinationPath}/${file.name}/src/main/docker/app.yml`)
-            ) {
+                shelljs.test('-f', `${destinationPath}/${file.name}/.yo-rc.json`)) {
                 try {
                     const fileData = this.fs.readJSON(`${destinationPath}/${file.name}/.yo-rc.json`);
                     if (
