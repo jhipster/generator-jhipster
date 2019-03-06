bb0750e2c823c2ccb6a872d8ea44e040cecec7b3
Pierre Besson <pbesson@ippon.fr>
2019-03-06 18:10:04 +0100

    update prometheus support for kubernetes

--- generators/kubernetes/files.js
+++ generators/kubernetes/files.js
@@ -88,8 +88,7 @@ function writeFiles() {
 
         writePrometheusGrafanaFiles() {
             if (this.monitoring === 'prometheus') {
-                this.template('monitoring/jhipster-prometheus-crd.yml.ejs', 'monitoring/jhipster-prometheus-crd.yml');
-                this.template('monitoring/jhipster-prometheus-cr.yml.ejs', 'monitoring/jhipster-prometheus-cr.yml');
+                this.template('monitoring/prometheus.yml.ejs', 'monitoring/prometheus.yml');
                 this.template('monitoring/jhipster-grafana.yml.ejs', 'monitoring/jhipster-grafana.yml');
                 this.template('monitoring/jhipster-grafana-dashboard.yml.ejs', 'monitoring/jhipster-grafana-dashboard.yml');
             }
