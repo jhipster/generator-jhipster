d374351ba78c7a8d10aefedf84e6fca2dc89478f
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-05 18:41:56 +0530

    Minor fix

--- cli/utils.js
+++ cli/utils.js
@@ -88,7 +88,7 @@ const initHelp = (program, cliName) => {
     });
 
     program.on('command:*', name => {
-        console.log(`${chalk.yellow(name)} is not a known command. See '${chalk.white(`${cliName} --help`)}'.`);
+        console.error(`${chalk.yellow(name)} is not a known command. See '${chalk.white(`${cliName} --help`)}'.`);
 
         const cmd = Object.values(name).join('');
         const availableCommands = program.commands.map(c => c._name);
