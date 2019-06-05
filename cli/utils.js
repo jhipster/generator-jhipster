d13bf9d71bb64937979840ca9efd8ad8708ae9da
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-05 17:58:42 +0530

    Minor fix
    avoid side effects by reverting the change made to logger.error()

--- cli/utils.js
+++ cli/utils.js
@@ -46,7 +46,7 @@ const error = function(msg, trace) {
     if (trace) {
         console.log(trace);
     }
-    return; // eslint-disable-line
+    process.exit(1);
 };
 
 const init = function(program) {
@@ -88,7 +88,7 @@ const initHelp = (program, cliName) => {
     });
 
     program.on('command:*', name => {
-        logger.error(`${chalk.yellow(name)} is not a known command. See '${chalk.white(`${cliName} --help`)}'.`);
+        console.log(`${chalk.yellow(name)} is not a known command. See '${chalk.white(`${cliName} --help`)}'.`);
 
         const cmd = Object.values(name).join('');
         const availableCommands = program.commands.map(cmd => {
