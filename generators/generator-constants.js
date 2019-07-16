6e4c57eefc846bebc3ca3995755fb17cdc26073f
DanielFran <dandrfranco@gmail.com>
2019-07-16 16:06:49 +0100

    Update JIB version to 1.4.0
    See:
    https://github.com/GoogleContainerTools/jib/releases/tag/v1.4.0-maven
    https://github.com/GoogleContainerTools/jib/releases/tag/v1.4.0-gradle

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -70,7 +70,7 @@ const YARN_VERSION = '1.17.3';
 const NPM_VERSION = '6.9.2';
 
 // Libraries version
-const JIB_VERSION = '1.3.0';
+const JIB_VERSION = '1.4.0';
 
 // all constants used throughout all generators
 
