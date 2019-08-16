3faac5a550744896a8afcfcbb9c8ace76154429c
DanielFran <dandrfranco@gmail.com>
2019-08-16 18:54:39 +0100

    Update npm version to 6.10.3

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -67,7 +67,7 @@ const JAVA_VERSION = '1.8'; // Java version is forced to be 1.8. We keep the var
 // version of Node, Yarn, NPM
 const NODE_VERSION = '10.16.3';
 const YARN_VERSION = '1.17.3';
-const NPM_VERSION = '6.10.2';
+const NPM_VERSION = '6.10.3';
 
 // Libraries version
 const JIB_VERSION = '1.4.0';
