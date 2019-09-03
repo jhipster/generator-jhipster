3563c5e84898f0e227834addae7792c5fc339c89
DanielFran <dandrfranco@gmail.com>
2019-09-03 21:41:52 +0100

    Update apache maven wrapper to 3.6.2
    Used command mvn -N io.takari:maven:0.7.6:wrapper -Dmaven=3.6.2

--- generators/server/templates/.mvn/wrapper/MavenWrapperDownloader.java
+++ generators/server/templates/.mvn/wrapper/MavenWrapperDownloader.java
@@ -20,12 +20,12 @@ import java.util.Properties;
 
 public class MavenWrapperDownloader {
 
-    private static final String WRAPPER_VERSION = "0.5.4";
+    private static final String WRAPPER_VERSION = "0.5.5";
     /**
      * Default URL to download the maven-wrapper.jar from, if no 'downloadUrl' is provided.
      */
     private static final String DEFAULT_DOWNLOAD_URL = "https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/"
-        + WRAPPER_VERSION + "/maven-wrapper-" + WRAPPER_VERSION + " .jar";
+        + WRAPPER_VERSION + "/maven-wrapper-" + WRAPPER_VERSION + ".jar";
 
     /**
      * Path to the maven-wrapper.properties file, which might contain a downloadUrl property to
