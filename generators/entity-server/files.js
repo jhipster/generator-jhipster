4c9c27acad49249304319bb8b059abff71fe09cb
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-09-08 19:41:12 -0300

    Fix reproducibility for date and enum fields
    Conflicts:
    	generators/entity-server/files.js

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -34,6 +34,40 @@ const SERVER_TEST_SRC_DIR = constants.SERVER_TEST_SRC_DIR;
 // In order to have consistent results with Faker, the seed is fixed.
 faker.seed(42);
 
+/*
+ * Current faker version is 4.1.0 and was release in 2017
+ * It is outdated
+ * https://github.com/Marak/faker.js/blob/10bfb9f467b0ac2b8912ffc15690b50ef3244f09/lib/date.js#L73-L96
+ * Needed for reproducible builds
+ */
+/* eslint-disable-next-line */
+const recentDate = function (days, refDate) {
+    /* eslint-disable */
+    var date = new Date();
+    if (typeof refDate !== "undefined") {
+        date = new Date(Date.parse(refDate));
+    }
+
+    var range = {
+            min: 1000,
+            max: (days || 1) * 24 * 3600 * 1000
+    };
+
+    var future = date.getTime();
+    future -= faker.random.number(range); // some time from now to N days ago, in milliseconds
+    date.setTime(future);
+
+    return date;
+};
+
+const recentForLiquibase = function (days, changelogDate) {
+    let formatedDate;
+    if (changelogDate !== undefined) {
+        formatedDate = changelogDate.substring(0, 4) + '-' + changelogDate.substring(4, 6) + '-' + changelogDate.substring(6, 8);
+    }
+    return recentDate(1, formatedDate);
+}
+
 /**
  * The default is to use a file path string. It implies use of the template method.
  * For any other config an object { file:.., method:.., template:.. } can be used
@@ -54,6 +88,8 @@ const serverFiles = {
                     options: {
                         interpolate: INTERPOLATE_REGEX,
                         context: {
+                            recentDate,
+                            recentForLiquibase,
                             faker,
                             randexp
                         }
