f559ca83109394c1114645b7304a74638a771555
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-02 19:50:17 -0300

    Add client plugin test

---
+++ test/templates/plugins/client/generators/client/index.js
@@ -0,0 +1,48 @@
+module.exports = function(Generator) {
+    return class NewGenerator extends Generator {
+        constructor(args, opts) {
+            super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
+            const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+            if (!jhContext) {
+                this.error("This is a JHipster blueprint and should be used only like 'jhipster --blueprint myblueprint')}");
+            }
+            this.configOptions = jhContext.configOptions || {};
+            // This sets up options for this sub generator and is being reused from JHipster
+            jhContext.setupClientOptions(this, jhContext);
+        }
+
+        get initializing() {
+            return super._initializing();
+        }
+
+        get prompting() {
+            return super._prompting();
+        }
+
+        get configuring() {
+            return super._configuring();
+        }
+
+        get default() {
+            return super._default();
+        }
+
+        get writing() {
+            const phaseFromJHipster = super._writing();
+            const customPhaseSteps = {
+                addDummyProperty() {
+                    this.addNpmDependency('dummy-blueprint-property', '2.0');
+                }
+            };
+            return { ...phaseFromJHipster, ...customPhaseSteps };
+        }
+
+        get install() {
+            return super._install();
+        }
+
+        get end() {
+            return super._end();
+        }
+    };
+};
