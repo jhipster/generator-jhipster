4aed0fae125090f0117385d09a2e4c3a29e9b56a
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-24 17:06:47 -0200

    Improve interceptor

--- generators/interceptor.js
+++ generators/interceptor.js
@@ -1,15 +1,28 @@
-const debug = require('debug')('interceptor');
+/* eslint-disable global-require */
 
-const printDiff = function() {
-    if (!debug.enabled) return;
-    const self = this;
-    const initialSelf = this.initialSelf;
-    const diff = Object.getOwnPropertyNames(this)
+const getDebug = function(name) {
+    let debug = require('debug')(`interceptor:${name}`);
+    if (debug.enabled) {
+        return { debug, pretty: true, pattern: '%O' };
+    }
+    debug = require('debug')(`interceptor-ugly:${name}`);
+    if (debug.enabled) {
+        return { debug, pretty: false, pattern: '%o' };
+    }
+    return undefined;
+};
+
+const printDiff = function(generator) {
+    const d = getDebug('diff');
+    if (!d) return;
+
+    const initialSelf = generator.initialSelf;
+    const diff = Object.getOwnPropertyNames(generator)
         .filter(key => !key.startsWith('_'))
         .sort()
         .reduce((diff, key) => {
             if (['args', 'arguments', 'async', 'initialSelf'].includes(key)) return diff;
-            const pd = Object.getOwnPropertyDescriptor(self, key);
+            const pd = Object.getOwnPropertyDescriptor(generator, key);
             if (typeof pd.value === 'object' && !Array.isArray(pd.value)) return diff;
             const otherPd = Object.getOwnPropertyDescriptor(initialSelf, key);
             if (!otherPd || otherPd.value !== pd.value) {
@@ -17,15 +30,17 @@ const printDiff = function() {
             }
             return diff;
         }, {});
-    debug('Namespace: %O', this.options.namespace);
-    debug('%O', diff);
+    const debug = d.debug;
+    debug('Namespace: %s', generator.options.namespace);
+    debug(d.pattern, diff);
 };
 
 const registerDiff = function(generator) {
     generator.initialSelf = { ...generator };
-    generator.queueMethod(printDiff.bind(generator), 'printDiff', 'writing');
+    generator.queueMethod(printDiff.bind(generator, generator), 'printDiff', 'writing');
 };
 
 module.exports = {
-    registerDiff
+    registerDiff,
+    printDiff
 };
