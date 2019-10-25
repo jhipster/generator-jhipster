b9864eeee398a1d3313bf02d8cedc8ce77eee344
Marcelo Boveto Shima <marcelo@shimasolucoes.com.br>
2019-10-25 17:36:14 -0200

    Try to fix ci

--- test-integration/scripts/11.9-generate-project-base.sh
+++ test-integration/scripts/11.9-generate-project-base.sh
@@ -11,6 +11,8 @@ if [ "$JHI_FOLDER_APP" == "$HOME/app" ]; then
     cp "$JHI_INTEG"/configstore/*.json "$HOME"/.config/configstore/
 fi
 
+export DIVERGE_VERSION="mshima/generator-jhipster#before_jhipster7"
+
 if [[ "$JHI_ENTITY" == "jdl" ]]; then
     #-------------------------------------------------------------------------------
     # Generate with JDL
@@ -18,7 +20,11 @@ if [[ "$JHI_ENTITY" == "jdl" ]]; then
     mkdir -p "$JHI_FOLDER_APP"
     cd "$JHI_FOLDER_APP"
     cp -f "$JHI_SAMPLES"/"$JHI_APP"/*.jdl ./
+
+    npm install "$DIVERGE_VERSION"
     jhipster import-jdl *.jdl --no-insight --creation-timestamp 2019-10-25
+    rm -rf src/ webpack/ .jhipster/ .mvn/ node_modules/
+    rm * .* || true
 
 else
     #-------------------------------------------------------------------------------
@@ -29,12 +35,10 @@ else
         cd "$JHI_FOLDER_UAA"
 
         cp -f "$JHI_SAMPLES"/uaa/.yo-rc.json ./
+        npm install "$DIVERGE_VERSION"
         jhipster --force --no-insight --with-entities --skip-checks --from-cli --creation-timestamp 2019-10-25
-        ls -al "$JHI_FOLDER_UAA"
-
-        echo "========= Start git diff ========="
-        git --no-pager diff HEAD -- . ':(exclude)package-lock.json'
-        echo "========= End git diff ========="
+        rm -rf src/ webpack/ .jhipster/ .mvn/ node_modules/
+        rm * .* || true
     fi
 
     #-------------------------------------------------------------------------------
@@ -42,18 +46,16 @@ else
     #-------------------------------------------------------------------------------
     mkdir -p "$JHI_FOLDER_APP"
     cd "$JHI_FOLDER_APP"
-
     cp -f "$JHI_SAMPLES"/"$JHI_APP"/.yo-rc.json ./
-    jhipster --force --no-insight --skip-checks --with-entities --from-cli --creation-timestamp 2019-10-25
 
+    npm install "$DIVERGE_VERSION"
+    jhipster --force --no-insight --skip-checks --with-entities --from-cli --creation-timestamp 2019-10-25
+    rm -rf src/ webpack/ .jhipster/ .mvn/ node_modules/
+    rm * .* || true
 fi
 
 #-------------------------------------------------------------------------------
 # Check folder where the app is generated
 #-------------------------------------------------------------------------------
-echo "========= Start git diff ========="
-git --no-pager diff HEAD -- . ':(exclude)package-lock.json'
-echo "========= End git diff ========="
-
 ls -al "$JHI_FOLDER_APP"
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
