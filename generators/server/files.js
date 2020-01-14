bad59a60699942fcf399df42a4c7249a01ee043f
Matt Raible <matt.raible@okta.com>
2020-01-13 19:47:22 -0700

    Add OAuth 2.0 / OIDC Auth for Reactive

--- generators/server/files.js
+++ generators/server/files.js
@@ -424,20 +424,6 @@ const serverFiles = {
         },
         {
             condition: generator =>
-                !generator.reactive &&
-                (generator.applicationType === 'uaa' ||
-                    generator.authenticationType === 'uaa' ||
-                    generator.authenticationType === 'oauth2'),
-            path: SERVER_MAIN_SRC_DIR,
-            templates: [
-                {
-                    file: 'package/config/MethodSecurityConfiguration.java',
-                    renameTo: generator => `${generator.javaDir}config/MethodSecurityConfiguration.java`
-                }
-            ]
-        },
-        {
-            condition: generator =>
                 !shouldSkipUserManagement(generator) && generator.authenticationType === 'session' && !generator.reactive,
             path: SERVER_MAIN_SRC_DIR,
             templates: [
@@ -761,7 +747,7 @@ const serverFiles = {
             ]
         },
         {
-            condition: generator => generator.authenticationType === 'oauth2',
+            condition: generator => !generator.reactive && generator.authenticationType === 'oauth2',
             path: SERVER_MAIN_SRC_DIR,
             templates: [
                 {
