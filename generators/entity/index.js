4d4c09d9f40b965c19f722cfdfa6d5f13915d342
Aurélien Mino <aurelien.mino@gmail.com>
2020-03-03 22:59:26 +0100

    Prettier format

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -237,7 +237,11 @@ class EntityGenerator extends BaseBlueprintGenerator {
 
             validateReactiveCompatibility() {
                 if (this.context.reactive && !['mongodb', 'cassandra', 'couchbase'].includes(this.context.databaseType)) {
-                    this.error(chalk.red(`The entity generator doesn't support reactive apps with databases of type ${this.context.databaseType} at the moment`));
+                    this.error(
+                        chalk.red(
+                            `The entity generator doesn't support reactive apps with databases of type ${this.context.databaseType} at the moment`
+                        )
+                    );
                 }
             },
 
