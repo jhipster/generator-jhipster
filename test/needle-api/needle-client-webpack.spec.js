a09b8f03b3d9a47181c7688e8cbe30284d48b038
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-03-04 08:12:20 -0300

    --blueprint cli option is deprecated, replace messages with --blueprints

--- test/needle-api/needle-client-webpack.spec.js
+++ test/needle-api/needle-client-webpack.spec.js
@@ -16,7 +16,7 @@ const mockBlueprintSubGen = class extends ClientGenerator {
         const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
 
         if (!jhContext) {
-            this.error('This is a JHipster blueprint and should be used only like jhipster --blueprint myblueprint');
+            this.error('This is a JHipster blueprint and should be used only like jhipster --blueprints myblueprint');
         }
 
         this.configOptions = jhContext.configOptions || {};
