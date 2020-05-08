79ca7257d3cd058c650f1063ba6876ca3c243cd4
Frederik Hahne <atomfrede@gmail.com>
2020-05-08 07:23:17 +0200

    make sure ejs replace does not break escaping for regex

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -108,7 +108,7 @@ const HELM_MOGODB_REPLICASET = '^3.10.1';
 
 // all constants used throughout all generators
 
-const LOGIN_REGEX = '^[a-zA-Z0-9!#$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*|[_.@A-Za-z0-9-]+$';
+const LOGIN_REGEX = '^[a-zA-Z0-9!#$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*|[_.@A-Za-z0-9-]+$';
 const MAIN_DIR = 'src/main/';
 const TEST_DIR = 'src/test/';
 
