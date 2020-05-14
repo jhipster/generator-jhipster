aaae364fac804d03c88db39cffd1e38715e5a30c
François Delbrayelle <fdelbrayelle@gmail.com>
2020-05-14 11:41:14 +0200

    fix: eslint

--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -163,11 +163,10 @@ function askForMoreModules() {
 }
 
 function askModulesToBeInstalled(done, generator) {
-
     const jHipsterMajorVersion = packagejs.version.match(/^(\d+)/g);
 
     generator.httpsGet(
-        'https://api.npms.io/v2/search?q=keywords:jhipster-module+jhipster-' + jHipsterMajorVersion + '&from=0&size=50',
+        `https://api.npms.io/v2/search?q=keywords:jhipster-module+jhipster-${jHipsterMajorVersion}&from=0&size=50`,
         body => {
             try {
                 const moduleResponse = JSON.parse(body);
