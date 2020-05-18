5e8c92b4fc663e391d4f41f4db41cde3e6d4f184
xetys <xetys.0@gmail.com>
2020-05-18 20:51:01 +0200

    fix failing test for authority-relationship

--- test/entity.spec.js
+++ test/entity.spec.js
@@ -380,25 +380,9 @@ describe('JHipster generator for entity', () => {
                     helpers
                         .run(require.resolve('../generators/entity'))
                         .inTmpDir(dir => {
-                            fse.copySync(path.join(__dirname, '../test/templates/default-ng2'), dir);
+                            fse.copySync(path.join(__dirname, '../test/templates/authority-relationship'), dir);
                         })
                         .withArguments(['foo'])
-                        .withPrompts({
-                            fieldAdd: false,
-                            relationshipAdd: true,
-                        })
-                        .withPrompts({
-                            relationshipAdd: false,
-                            otherEntityName: 'authority',
-                            relationshipName: 'authority',
-                            relationshipType: 'many-to-one',
-                            ownerSide: true,
-                            otherEntityRelationshipName: 'foo',
-                            otherEntityField: 'name',
-                            dto: 'no',
-                            service: 'no',
-                            pagination: 'no',
-                        })
                         .on('end', done);
                 });
 
@@ -409,28 +393,15 @@ describe('JHipster generator for entity', () => {
                     assert.file(expectedFiles.fakeData);
                 });
 
-                // it('creates correct entity files for authority', () => {
-                //     // entity related files exist
-                //     assert.file(`${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.module.ts`);
-                //     assert.file(`${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.component.html`);
-                //     assert.file('.jhipster/Foo.json');
-                //     assert.fileContent('.jhipster/Foo.json', 'many-to-one');
-                //     assert.fileContent('.jhipster/Foo.json', {
-                //         relationships: [
-                //             {
-                //                 relationshipType: 'many-to-one',
-                //                 otherEntityName: 'authority',
-                //                 otherEntityRelationshipName: 'foo',
-                //                 relationshipName: 'authority',
-                //                 otherEntityField: 'name',
-                //             },
-                //         ],
-                //     });
-                //     assert.fileContent(
-                //         `${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.component.html`,
-                //         "<a [routerLink]=\"['/authority', foo.authority?.name, 'view']\" >{{ foo.authority?.name }}</a>"
-                //     );
-                // });
+                it('creates correct entity files for authority', () => {
+                    // entity related files exist
+                    assert.file(`${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.module.ts`);
+                    assert.file(`${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.component.html`);
+                    assert.fileContent(
+                        `${CLIENT_MAIN_SRC_DIR}app/entities/foo/foo.component.html`,
+                        "<a [routerLink]=\"['/authority', foo.authority?.name, 'view']\" >{{ foo.authority?.name }}</a>"
+                    );
+                });
             });
         });
 
