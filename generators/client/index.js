fd18206079c4e56d1a3a0ca85e49761c73a1ba2e
Vivek <vivekmore@users.noreply.github.com>
2020-05-21 22:51:43 -0400

    :rewind: revert breaking changes
    Co-authored-by: Aurélien Mino <aurelien.mino@gmail.com>

--- generators/client/index.js
+++ generators/client/index.js
@@ -189,7 +189,7 @@ module.exports = class extends BaseBlueprintGenerator {
         return {
             askForModuleName: prompts.askForModuleName,
             askForClient: prompts.askForClient,
-            askForI18n: prompts.askForI18n,
+            askFori18n: prompts.askForI18n,
             askForClientTheme: prompts.askForClientTheme,
             askForClientThemeVariant: prompts.askForClientThemeVariant,
 
