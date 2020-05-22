3f94b951b98213ab68f372f0c862cdc92db1d089
Vivek <vivekmore@users.noreply.github.com>
2020-05-21 22:51:54 -0400

    :rewind: revert breaking changes
    Co-authored-by: Aurélien Mino <aurelien.mino@gmail.com>

--- generators/server/index.js
+++ generators/server/index.js
@@ -348,7 +348,7 @@ module.exports = class extends BaseBlueprintGenerator {
             askForModuleName: prompts.askForModuleName,
             askForServerSideOpts: prompts.askForServerSideOpts,
             askForOptionalItems: prompts.askForOptionalItems,
-            askForI18n: prompts.askForI18n,
+            askFori18n: prompts.askForI18n,
 
             setSharedConfigOptions() {
                 this.configOptions.packageName = this.packageName;
