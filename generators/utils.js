9ce94d2e4be9dbd09351a00c2969d37c7b9508a8
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-07-11 12:59:15 -0300

    Export parseBlueprintInfo

--- generators/utils.js
+++ generators/utils.js
@@ -69,6 +69,7 @@ module.exports = {
     parseBluePrints,
     normalizeBlueprintName,
     packageNameToNamespace,
+    parseBlueprintInfo,
     stringHashCode,
     RandexpWithFaker,
     gitExec,
