b63956bb5d7d2bf77a29ba7bb33202d06c10593e
Marcelo Shima <marceloshima@gmail.com>
2020-07-12 09:43:40 -0300

    Apply suggestions from code review

--- test/app-required-config.js
+++ test/app-required-config.js
@@ -3,7 +3,7 @@ const helpers = require('yeoman-test');
 const { requiredDefaultConfig, defaultConfig } = require('../generators/generator-defaults');
 const EnvironmentBuilder = require('../cli/environment-builder');
 
-describe('JHipster generator with require configuration', () => {
+describe('JHipster generator with required configuration', () => {
     before(() => {
         return helpers
             .create('jhipster:app', {}, { createEnv: EnvironmentBuilder.createEnv })
@@ -15,7 +15,7 @@ describe('JHipster generator with require configuration', () => {
             .run();
     });
 
-    it('Writes additional default config to .yo-rc.json', () => {
+    it('writes additional default config to .yo-rc.json', () => {
         assert.JSONFileContent('.yo-rc.json', {
             'generator-jhipster': { ...defaultConfig, languages: [defaultConfig.nativeLanguage] },
         });
