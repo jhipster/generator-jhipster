ffae3ecf5fbc4dd23abddc818c9eb450adc20dbe
Anthony Viard <anth.viard@gmail.com>
2020-07-21 17:24:10 +0200

    Remove Yarn from test-integration folder

--- test-integration/scripts/22-tests-frontend.sh
+++ test-integration/scripts/22-tests-frontend.sh
@@ -7,11 +7,7 @@ source $(dirname $0)/00-init-env.sh
 # Launch frontend tests
 #-------------------------------------------------------------------------------
 cd "$JHI_FOLDER_APP"
-if [[ $(grep yarn .yo-rc.json) != "" ]]; then
-    JHI_CLIENT_PACKAGE_MANAGER=yarn
-else
     JHI_CLIENT_PACKAGE_MANAGER=npm
-fi
 
 if [ -f "tsconfig.json" ]; then
     if [ -f "src/main/webapp/app/app.tsx" ]; then
