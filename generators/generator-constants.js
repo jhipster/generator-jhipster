2f9d733634b3a00aac8936b7a8064b07fae197fd
Aurélien Mino <aurelien.mino@gmail.com>
2020-08-24 22:20:45 +0200

    Update jhipster-lib version to 3.10.0-SNAPSHOT

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -30,7 +30,7 @@ const GRADLE_VERSION = '6.5';
 const JIB_VERSION = '2.5.0';
 
 // Libraries version
-const JHIPSTER_DEPENDENCIES_VERSION = '3.8.0-SB2.3-SNAPSHOT';
+const JHIPSTER_DEPENDENCIES_VERSION = '3.10.0-SNAPSHOT';
 // The spring-boot version should match the one managed by https://mvnrepository.com/artifact/io.github.jhipster/jhipster-dependencies/JHIPSTER_DEPENDENCIES_VERSION
 const SPRING_BOOT_VERSION = '2.3.3.RELEASE';
 const LIQUIBASE_VERSION = '3.10.2';
