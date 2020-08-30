f1b0a539c3d245acf4d077da37ec4339f0d1acbf
nicolas63 <nicolas_raymond@orange.fr>
2020-08-30 15:56:49 +0200

    Remove unused import

--- generators/client/files-vue.js
+++ generators/client/files-vue.js
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const mkdirp = require('mkdirp');
 const constants = require('../generator-constants');
 const utils = require('../utils');
 
