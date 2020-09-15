8e631f44bc5ff8e68a7b5d96af09025ecfba7f3e
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-09-15 19:52:03 +0200

    [vue] fix protractor tests with toast

--- generators/client/files-vue.js
+++ generators/client/files-vue.js
@@ -314,6 +314,7 @@ const vueFiles = {
                 'e2e/util/utils.ts',
                 'e2e/page-objects/navbar-page.ts',
                 'e2e/page-objects/signin-page.ts',
+                'e2e/page-objects/alert-page.ts',
                 'e2e/page-objects/administration-page.ts',
                 'protractor.conf.js',
             ],
