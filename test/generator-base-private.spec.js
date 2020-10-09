ab46ddcd75ff53eddd0bf4faeaa711b1d596fd6d
Frederik Hahne <atomfrede@gmail.com>
2020-10-09 16:30:26 +0200

    fix h2 unit test assertions

--- test/generator-base-private.spec.js
+++ test/generator-base-private.spec.js
@@ -327,15 +327,15 @@ export * from './entityFolderName/entityFileName.state';`;
         describe('when called for h2Disk', () => {
             it('return r2dbc:h2:file:./build/h2db/db/test;DB_CLOSE_DELAY=-1', () => {
                 expect(BaseGenerator.getR2DBCUrl('h2Disk', { databaseName: 'test', localDirectory: './build/h2db/db' })).to.equal(
-                    'r2dbc:h2:file:./build/h2db/db/test;DB_CLOSE_DELAY=-1'
+                    'r2dbc:h2:file://./build/h2db/db/test;DB_CLOSE_DELAY=-1'
                 );
             });
         });
         describe('when called for h2Disk with skipExtraOptions enabled', () => {
-            it('return r2dbc:h2:file:./build/h2db/db/test', () => {
+            it('return r2dbc:h2:file:://./build/h2db/db/test', () => {
                 expect(
                     BaseGenerator.getR2DBCUrl('h2Disk', { databaseName: 'test', localDirectory: './build/h2db/db', skipExtraOptions: true })
-                ).to.equal('r2dbc:h2:file:./build/h2db/db/test');
+                ).to.equal('r2dbc:h2:file://./build/h2db/db/test');
             });
         });
         describe('when called for h2Disk with missing `localDirectory` option', () => {
