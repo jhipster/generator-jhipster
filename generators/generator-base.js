62dfa506561f1a06bcbdac0741defc9a0cca9278
Sendil Kumar N <sendilkumarn@live.com>
2020-10-21 18:56:51 +0000

    chore: revert the generator base changes

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -117,10 +117,16 @@ module.exports = class JHipsterBaseGenerator extends PrivateBase {
             this.config.set(this.options.localConfig);
         }
 
+        if (this.options.skipGeneratedFlag !== undefined) {	
+            this.jhipsterConfig.skipGeneratedFlag = this.options.skipGeneratedFlag;	
+        }
+
         // Load common runtime options.
         this.parseCommonRuntimeOptions();
 
-        this.registerGeneratedAnnotationTransform();
+        if (!this.jhipsterConfig.skipGeneratedFlag) {
+            this.registerGeneratedAnnotationTransform();
+        }
 
         // Register .yo-resolve file
         if (!this.options.skipYoResolve) {
