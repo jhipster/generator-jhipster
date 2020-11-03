7bd1c84642574d8bf271e127e7ddc8dedfc47ff6
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-11-03 15:45:33 -0300

    Don't remove id fields for backend.

--- generators/entity-server/index.js
+++ generators/entity-server/index.js
@@ -31,8 +31,6 @@ module.exports = class extends BaseBlueprintGenerator {
         super(args, opts);
 
         this.entity = opts.context;
-        // Remove fields with custom ids, drop once templates supports them
-        this.entity = { ...this.entity, fields: this.entity.fieldsNoId };
 
         utils.copyObjectProps(this, this.entity);
         this.jhipsterContext = opts.jhipsterContext || opts.context;
