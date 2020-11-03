60e5329a8fa0b186938368764704c0d117019c6f
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-11-03 14:14:07 -0300

    Only set User fieldValidateRulesMaxlength if type is string

--- generators/generator-base-private.js
+++ generators/generator-base-private.js
@@ -1722,12 +1722,13 @@ module.exports = class JHipsterBasePrivateGenerator extends Generator {
 
         const userIdType =
             user.authenticationType === 'oauth2' || user.databaseType !== 'sql' ? 'String' : this.getPkType(user.databaseType);
+        const fieldValidateRulesMaxlength = userIdType === 'String' ? 100 : undefined;
 
         user.fields = [
             {
                 fieldName: 'id',
                 fieldType: userIdType,
-                fieldValidateRulesMaxlength: 100,
+                fieldValidateRulesMaxlength,
                 options: {
                     fieldNameHumanized: 'ID',
                     id: true,
