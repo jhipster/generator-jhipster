ccb2432163a527e53b052762bd0044420d07ac4f
nicolas63 <nicolas_raymond@orange.fr>
2020-11-10 22:47:07 +0100

    Fix tests

--- jdl/validators/jdl-with-application-validator.js
+++ jdl/validators/jdl-with-application-validator.js
@@ -53,7 +53,7 @@ function createValidator(jdlObject, logger = console) {
             checkForApplicationErrors();
             jdlObject.forEachApplication(jdlApplication => {
                 const blueprints = jdlApplication.getConfigurationOptionValue('blueprints');
-                if (!blueprints || blueprints.length > 0) {
+                if (blueprints && blueprints.length > 0) {
                     logger.warn('Generating application with blueprint, skipping jdl validation');
                     return;
                 }
