a82fbba8f5e2cbaff5c673f5b6e9d8302a7bb22a
Daniel Franco <dandrfranco@gmail.com>
2020-12-11 09:33:01 +0000

    Update hibernate version to 5.4.25.Final

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -43,7 +43,7 @@ const SPRING_BOOT_VERSION = '2.3.7.RELEASE';
 const LIQUIBASE_VERSION = '4.1.1';
 const liquibaseSemVer = semver.parse(LIQUIBASE_VERSION);
 const LIQUIBASE_DTD_VERSION = `${liquibaseSemVer.major}.${liquibaseSemVer.minor}`;
-const HIBERNATE_VERSION = '5.4.23.Final';
+const HIBERNATE_VERSION = '5.4.25.Final';
 
 const JACOCO_VERSION = '0.8.6';
 const KAFKA_VERSION = '5.5.2';
