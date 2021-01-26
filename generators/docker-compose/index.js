fff9666b345c8b06d6866d88163b247896e43122
Kaido Hallik <kaidohallik@gmail.com>
2021-01-26 10:14:10 +0200

    just in case generate polished YAML already before running Prettier

--- generators/docker-compose/index.js
+++ generators/docker-compose/index.js
@@ -198,7 +198,7 @@ module.exports = class extends BaseDockerGenerator {
           this.authenticationType = appConfig.authenticationType;
 
           // Dump the file
-          let yamlString = jsyaml.dump(parentConfiguration, { indent: 4, lineWidth: -1 });
+          let yamlString = jsyaml.dump(parentConfiguration, { indent: 2, lineWidth: -1 });
 
           // Add extra indentation for each lines
           const yamlArray = yamlString.split('\n');
