db77b86f4757809bb85c1440cf9898f87ec4d7ce
Marcelo Shima <marceloshima@gmail.com>
2021-02-04 08:57:54 -0300

    Implement json data generation and change angular to use it.

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -499,6 +499,19 @@ class EntityGenerator extends BaseBlueprintGenerator {
   // Public API method used by the getter and also by Blueprints
   _preparing() {
     return {
+      loadRelationships() {
+        this.context.relationships.forEach(relationship => {
+          const otherEntityName = this._.upperFirst(relationship.otherEntityName);
+          const otherEntity = this.configOptions.sharedEntities[otherEntityName];
+          if (!otherEntity) {
+            throw new Error(`Error looking for otherEntity ${otherEntityName}`);
+          }
+          relationship.otherEntity = otherEntity;
+          otherEntity.otherRelationships = otherEntity.otherRelationships || [];
+          otherEntity.otherRelationships.push(relationship);
+        });
+      },
+
       prepareEntityForTemplates() {
         const entity = this.context;
         prepareEntityForTemplates(entity, this);
@@ -561,19 +574,6 @@ class EntityGenerator extends BaseBlueprintGenerator {
           }
         });
       },
-
-      loadRelationships() {
-        this.context.relationships.forEach(relationship => {
-          const otherEntityName = this._.upperFirst(relationship.otherEntityName);
-          const otherEntity = this.configOptions.sharedEntities[otherEntityName];
-          if (!otherEntity) {
-            throw new Error(`Error looking for otherEntity ${otherEntityName}`);
-          }
-          relationship.otherEntity = otherEntity;
-          otherEntity.otherRelationships = otherEntity.otherRelationships || [];
-          otherEntity.otherRelationships.push(relationship);
-        });
-      },
     };
   }
 
@@ -620,7 +620,12 @@ class EntityGenerator extends BaseBlueprintGenerator {
       },
 
       processDerivedPrimaryKey() {
-        if (!this.context.derivedPrimaryKey) {
+        if (!this.context.primaryKey) {
+          return;
+        }
+        if (!this.context.primaryKey.derived) {
+          const derivedFields = this.context.primaryKey.derivedFields;
+          this.context.fields.unshift(...derivedFields);
           return;
         }
         const idFields = this.context.primaryKey.derivedFields;
