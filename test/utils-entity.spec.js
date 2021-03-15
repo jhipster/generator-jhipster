570d79327a132ad1e34e3e69ff06083ff1ef7821
Marcelo Shima <marceloshima@gmail.com>
2021-03-15 06:53:03 -0300

    Add tests for prepareEntityPrimaryKeyForTemplates.

--- test/utils-entity.spec.js
+++ test/utils-entity.spec.js
@@ -47,7 +47,19 @@ describe('entity utilities', () => {
             id: true,
           });
         });
+
+        it('should contains correct structure', () => {
+          expect(entity.primaryKey).to.deep.include({
+            name: 'id',
+            nameCapitalized: 'Id',
+            type: 'CustomType',
+            derived: false,
+            composite: false,
+          });
+          expect(entity.primaryKey.fields[0]).to.equal(entity.fields[0]);
+        });
       });
+
       describe('with @Id', () => {
         let entity = {
           ...entityDefaultConfig,
@@ -65,6 +77,164 @@ describe('entity utilities', () => {
           expect(entity.fields[0]).to.eql({
             fieldName: 'id',
             fieldType: 'CustomType',
+          });
+        });
+      });
+
+      describe('with multiple @Id relationships and field', () => {
+        let entity1;
+        let entity2;
+        let entity3;
+        let entity4;
+
+        beforeEach(() => {
+          entity1 = {
+            ...entityDefaultConfig,
+            name: 'Entity1',
+            entityClass: 'Entity1',
+            entityInstance: 'entity1',
+            fields: [{ fieldName: 'id', fieldNameCapitalized: 'Id', columnName: 'id', fieldType: 'String', id: true }],
+          };
+          entity2 = {
+            ...entityDefaultConfig,
+            name: 'Entity2',
+            entityClass: 'Entity2',
+            entityInstance: 'entity2',
+            fields: [
+              { fieldName: 'uuid', fieldNameCapitalized: 'Uuid', columnName: 'uuid', fieldType: 'UUID', id: true, autoGenerate: true },
+            ],
+          };
+          entity3 = {
+            ...entityDefaultConfig,
+            name: 'Entity3',
+            entityClass: 'Entity3',
+            entityInstance: 'entity3',
+            relationships: [
+              {
+                relationshipName: 'entity2',
+                relationshipNameCapitalized: 'Entity2',
+                relationshipType: 'one-to-one',
+                id: true,
+                otherEntity: entity2,
+              },
+            ],
+          };
+          entity4 = {
+            ...entityDefaultConfig,
+            name: 'Entity4',
+            entityClass: 'Entity4',
+            entityInstance: 'entity4',
+            fields: [{ fieldName: 'uuid', fieldType: 'UUID', columnName: 'uuid', id: true, autoGenerate: false }],
+            relationships: [
+              {
+                relationshipName: 'otherEntity1',
+                relationshipNameCapitalized: 'OtherEntity1',
+                id: true,
+                otherEntity: entity1,
+                relationshipType: 'many-to-one',
+              },
+              {
+                relationshipName: 'otherEntity3',
+                relationshipNameCapitalized: 'OtherEntity3',
+                id: true,
+                otherEntity: entity3,
+                relationshipType: 'many-to-one',
+              },
+            ],
+          };
+
+          entity1 = prepareEntityPrimaryKeyForTemplates(entity1, defaultGenerator, true);
+          entity2 = prepareEntityPrimaryKeyForTemplates(entity2, defaultGenerator, true);
+          entity3 = prepareEntityPrimaryKeyForTemplates(entity3, defaultGenerator, true);
+          entity4 = prepareEntityPrimaryKeyForTemplates(entity4, defaultGenerator, true);
+        });
+
+        it('should prepare correct primaryKey for entity1', () => {
+          expect(entity1.primaryKey.fields).to.have.lengthOf(1);
+          expect(entity1.primaryKey).to.deep.include({
+            name: 'id',
+            nameCapitalized: 'Id',
+            type: 'String',
+            tsType: 'string',
+            derived: false,
+            composite: false,
+          });
+          expect(entity1.primaryKey.fields[0]).to.equal(entity1.fields[0]);
+        });
+
+        it('should prepare correct primaryKey for entity2', () => {
+          expect(entity2.primaryKey.fields).to.have.lengthOf(1);
+          expect(entity2.primaryKey).to.deep.include({
+            name: 'uuid',
+            nameCapitalized: 'Uuid',
+            type: 'UUID',
+            tsType: 'string',
+            derived: false,
+            composite: false,
+          });
+          expect(entity2.primaryKey.fields[0]).to.equal(entity2.fields[0]);
+        });
+
+        it('should prepare correct primaryKey for one-to-one relationship id', () => {
+          expect(entity3.primaryKey.fields).to.have.lengthOf(1);
+          expect(entity3.primaryKey).to.deep.include({
+            name: 'uuid',
+            nameCapitalized: 'Uuid',
+            type: 'UUID',
+            derived: true,
+            composite: false,
+          });
+          expect(entity3.primaryKey.fields[0]).to.deep.include({
+            fieldName: 'uuid',
+            autoGenerate: true,
+          });
+        });
+
+        it('should prepare correct primaryKey for entity4', () => {
+          expect(entity4.primaryKey.fields).to.have.lengthOf(3);
+          expect(entity4.primaryKey).to.deep.include({
+            name: 'id',
+            nameCapitalized: 'Id',
+            type: 'Entity4Id',
+            composite: true,
+            autoGenerate: false,
+          });
+        });
+
+        it('should prepare correct own id', () => {
+          expect(entity4.primaryKey.fields[0]).to.deep.include({
+            fieldName: 'uuid',
+          });
+        });
+
+        it('should prepare correct relationship id', () => {
+          const field = entity4.primaryKey.fields[1];
+          expect(field).to.deep.include({
+            ...entity1.primaryKey.fields[0],
+            fieldName: 'entity1Id',
+            fieldNameCapitalized: 'Entity1Id',
+            columnName: 'other_entity1_id',
+            derivedPath: ['otherEntity1', 'id'],
+            path: ['otherEntity1', 'id'],
+
+            derivedEntity: entity1,
+            reference: field.reference,
+          });
+        });
+
+        it('should prepare correct relationship id with derived primaryKey', () => {
+          const field = entity4.primaryKey.fields[2];
+          expect(field).to.deep.include({
+            ...entity3.primaryKey.fields[0],
+            derived: true,
+            fieldName: 'entity3Uuid',
+            fieldNameCapitalized: 'Entity3Uuid',
+            columnName: 'other_entity3_uuid',
+            derivedPath: ['otherEntity3', 'uuid'],
+            path: ['otherEntity3', 'entity2', 'uuid'],
+
+            derivedEntity: entity3,
+            reference: field.reference,
           });
         });
       });
