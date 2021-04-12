166332a33a30e1294c71ed44ebb1d60b92bed85a
Marcelo Boveto Shima <marceloshima@gmail.com>
2021-04-12 15:20:55 -0300

    Adjusts.

--- generators/workspaces/index.js
+++ generators/workspaces/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013-2020 the original author or authors from the JHipster project.
+ * Copyright 2013-2021 the original author or authors from the JHipster project.
  *
  * This file is part of the JHipster project, see https://www.jhipster.tech/
  * for more information.
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 const fs = require('fs');
 const path = require('path');
 
@@ -61,7 +62,7 @@ module.exports = class extends BaseBlueprintGenerator {
         const applications = importState.exportedApplicationsWithEntities;
         const packages = Object.keys(applications);
         const clientPackageManager = applications[packages[0]].config.clientPackageManager;
-        const dockerCompose = !!importState.exportedDeployments.find(
+        const dockerCompose = importState.exportedDeployments.some(
           deployment => deployment[GENERATOR_JHIPSTER].deploymentType === DOCKERCOMPOSE
         );
 
