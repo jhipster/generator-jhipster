9a109a2e87ae324a6adbb33b39acd6e1ce6c956c
dandrfranco <dandrfranco@gmail.com>
2021-04-19 11:10:48 +0100

    Update spring-boot version to 2.4.5

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -35,7 +35,7 @@ const JIB_VERSION = '2.8.0';
 // Libraries version
 const JHIPSTER_DEPENDENCIES_VERSION = '7.0.2-SNAPSHOT';
 // The spring-boot version should match the one managed by https://mvnrepository.com/artifact/tech.jhipster/jhipster-dependencies/JHIPSTER_DEPENDENCIES_VERSION
-const SPRING_BOOT_VERSION = '2.4.4';
+const SPRING_BOOT_VERSION = '2.4.5';
 const LIQUIBASE_VERSION = '4.3.2';
 const LIQUIBASE_DTD_VERSION = LIQUIBASE_VERSION.split('.', 3).slice(0, 2).join('.');
 const HIBERNATE_VERSION = '5.4.29.Final';
