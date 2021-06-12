74060c0819fcf41d2ed00d71171b0e7c3b6a8dc2
ko5tik <konstantin.pribluda@gmail.com>
2021-06-12 17:59:35 +0300

    removed test code

--- sandbox/admin/src/main/java/de/vc/admin/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package de.vc.admin.service.dto;
--- sandbox/admin/src/test/java/de/vc/admin/cucumber/stepdefs/StepDefs.java
+++
@@ -1,8 +0,0 @@
-package de.vc.admin.cucumber.stepdefs;
-
-import org.springframework.test.web.servlet.ResultActions;
-
-public abstract class StepDefs {
-
-    protected ResultActions actions;
-}
