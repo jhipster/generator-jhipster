566518d042d869ff5d2bf73b50adcae03b7e78bd
Marcelo Boveto Shima <marceloshima@gmail.com>
2021-06-24 10:51:20 -0300

    Improve maven up-to-date.

--- generators/server/index.js
+++ generators/server/index.js
@@ -252,6 +252,7 @@ module.exports = class JHipsterServerGenerator extends BaseBlueprintGenerator {
       loadSharedConfig() {
         this.loadAppConfig();
         this.loadClientConfig();
+        this.loadDerivedClientConfig();
         this.loadServerConfig();
         this.loadTranslationConfig();
       },
