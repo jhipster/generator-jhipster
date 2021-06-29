0fce25b74ff2464ec8eafcd889c90c3ecaee0655
Marcelo Shima <marceloshima@gmail.com>
2021-06-29 16:15:49 -0300

    Add comment to camelcase function.

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -61,6 +61,7 @@ const { GATLING, CUCUMBER, PROTRACTOR, CYPRESS } = require('../jdl/jhipster/test
 const { GATEWAY, MICROSERVICE, MONOLITH } = require('../jdl/jhipster/application-types');
 const { ELASTICSEARCH } = require('../jdl/jhipster/search-engine-types');
 
+/* Taken from commander.js source code, options camelCase must match */
 function camelcase(str) {
   return str.split('-').reduce((str, word) => {
     return str + word[0].toUpperCase() + word.slice(1);
