8fa34e47718186e15ddc23920312f7d119151833
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-07-03 18:35:22 +0200

    Polish maven spec

--- test/maven.spec.js
+++ test/maven.spec.js
@@ -3,17 +3,19 @@ const assert = require('yeoman-assert');
 const { skipPrettierHelpers: helpers } = require('./utils/utils');
 
 describe('JHipster Maven generator', () => {
-  describe('generate init', () => {
+  describe('with package name', () => {
     before(async () => {
       await helpers.run(path.join(__dirname, '../generators/init')).withPrompts({
         projectName: 'jhipster project',
         baseName: 'jhipster',
+        prettierDefaultIndent: 2,
+        prettierJavaIndent: 4,
       });
       await helpers.run(path.join(__dirname, '../generators/maven')).withPrompts({
         packageName: 'tech.jhipster',
       });
     });
-    it('creates expected files for init generator', () => {
+    it('creates expected files for maven generator', () => {
       assert.file('.mvn/wrapper/maven-wrapper.jar');
       assert.file('.mvn/wrapper/maven-wrapper.properties');
       assert.file('.mvn/wrapper/MavenWrapperDownloader.java');
