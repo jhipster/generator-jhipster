98193434902ef2ef22826df467f9c947f3db8731
George Saad <georgekysaad@gmail.com>
2021-08-22 22:45:48 -0400

    Update generators/cleanup.js
    Co-authored-by: Marcelo Shima <marceloshima@gmail.com>

--- generators/cleanup.js
+++ generators/cleanup.js
@@ -488,10 +488,10 @@ function cleanupOldServerFiles(generator, javaDir, testDir, mainResourceDir, tes
     generator.removeFile(`${javaDir}config/CloudDatabaseConfiguration.java`);
   }
   if (generator.isJhipsterVersionLessThan('7.1.1')) {
-    generator.removeFile(`${javaDir}repository/N1qlCouchbaseRepository.java`);
+    generator.removeFile(`${javaDir}repository/ReactiveN1qlCouchbaseRepository.java `);
     generator.removeFile(`${testDir}config/DatabaseConfigurationIT.java`);
     if (generator.searchEngine !== 'couchbase') {
-      generator.removeFile(`${javaDir}Frepository/CustomN1qlCouchbaseRepository.java`);
+      generator.removeFile(`${javaDir}repository/CustomN1qlCouchbaseRepository.java`);
     }
   }
 }
