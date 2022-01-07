c953df273ef55c1f29938739eec42ecc93e8f703
colameo <serano.colameo@gmail.com>
2022-01-07 08:56:48 +0100

    fixes issue #17512

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -1871,7 +1871,7 @@ module.exports = class JHipsterBaseGenerator extends PrivateBase {
     if (this.destinationPath) {
       keystoreFolder = this.destinationPath(keystoreFolder);
     }
-    const keyStoreFile = `${keystoreFolder}/keystore.p12`;
+    const keyStoreFile = `"${keystoreFolder}/keystore.p12"`;
 
     if (this.fs.exists(keyStoreFile)) {
       this.log(chalk.cyan(`\nKeyStore '${keyStoreFile}' already exists. Leaving unchanged.\n`));
