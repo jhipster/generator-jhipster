3b4023550bc09722cdc114bc07ad7e4169087567
Tayeb Chlyah <tayebchlyah@gmail.com>
2022-02-07 12:22:54 +0100

    (Couchbase) Move scope to properties

--- generators/server/files-couchbase.js
+++ generators/server/files-couchbase.js
@@ -71,34 +71,16 @@ const couchbaseFiles = {
   serverResource: [
     {
       path: SERVER_MAIN_RES_DIR,
-      templates: [
-        'config/couchmove/changelog/V0__create_collections.n1ql',
-        'config/couchmove/changelog/V0.2__create_indexes.n1ql',
-      ],
+      templates: ['config/couchmove/changelog/V0__create_collections.n1ql', 'config/couchmove/changelog/V0.2__create_indexes.n1ql'],
     },
     {
       condition: generator => !generator.skipUserManagement || generator.authenticationType === OAUTH2,
       path: SERVER_MAIN_RES_DIR,
       templates: [
-        {
-          file: 'config/couchmove/changelog/V0.1__initial_setup/scope/authority/ROLE_ADMIN.json',
-          renameTo: generator =>
-            `config/couchmove/changelog/V0.1__initial_setup/${generator.camelCase(generator.baseName)}/authority/ROLE_ADMIN.json`,
-        },
-        {
-          file: 'config/couchmove/changelog/V0.1__initial_setup/scope/authority/ROLE_USER.json',
-          renameTo: generator =>
-            `config/couchmove/changelog/V0.1__initial_setup/${generator.camelCase(generator.baseName)}/authority/ROLE_USER.json`,
-        },
-        {
-          file: 'config/couchmove/changelog/V0.1__initial_setup/scope/user/admin.json',
-          renameTo: generator =>
-            `config/couchmove/changelog/V0.1__initial_setup/${generator.camelCase(generator.baseName)}/user/admin.json`,
-        },
-        {
-          file: 'config/couchmove/changelog/V0.1__initial_setup/scope/user/user.json',
-          renameTo: generator => `config/couchmove/changelog/V0.1__initial_setup/${generator.camelCase(generator.baseName)}/user/user.json`,
-        },
+        'config/couchmove/changelog/V0.1__initial_setup/authority/ROLE_ADMIN.json',
+        'config/couchmove/changelog/V0.1__initial_setup/authority/ROLE_USER.json',
+        'config/couchmove/changelog/V0.1__initial_setup/user/admin.json',
+        'config/couchmove/changelog/V0.1__initial_setup/user/user.json',
       ],
     },
   ],
