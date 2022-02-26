9890f1bdcfc84b452b44d682cb5b3032c0f30c15
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-02-26 10:12:22 +0100

    Bump jhipster-registry to 7.3.0

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -49,7 +49,7 @@ const JACKSON_DATABIND_NULLABLE_VERSION = '0.2.2';
 // Version of docker images
 const DOCKER_COMPOSE_FORMAT_VERSION = '3.8';
 // const DOCKER_JHIPSTER_REGISTRY = 'ghcr.io/jhipster/jhipster-registry:main';
-const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:v7.2.0';
+const DOCKER_JHIPSTER_REGISTRY = 'jhipster/jhipster-registry:v7.3.0';
 const DOCKER_JHIPSTER_CONTROL_CENTER = 'jhipster/jhipster-control-center:v0.5.0';
 const DOCKER_JAVA_JRE = 'eclipse-temurin:11-jre-focal';
 const DOCKER_MYSQL = 'mysql:8.0.28';
