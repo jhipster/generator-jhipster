ab46cd2021d5ceda5e34c7e6a4b8c909a5438c22
Marcelo Shima <marceloshima@gmail.com>
2022-03-17 10:52:10 -0300

    Update 03-system.sh

--- test-integration/scripts/03-system.sh
+++ test-integration/scripts/03-system.sh
@@ -2,4 +2,6 @@
 
 sudo /etc/init.d/mysql stop
 
-echo "127.0.0.1 keycloak\n127.0.0.1 kafka" | sudo tee -a /etc/hosts
+echo "127.0.0.1 keycloak" | sudo tee -a /etc/hosts
+echo "127.0.0.1 kafka" | sudo tee -a /etc/hosts
+cat /etc/hosts
