cc4dda7d166ba591c03d88dbc97004640d98fcd9
Daniel Franco <dandrfranco@gmail.com>
2022-03-24 17:21:54 +0000

    Update hibernate version to 5.6.7.Final

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -40,7 +40,7 @@ const JHIPSTER_DEPENDENCIES_VERSION = '7.7.1-SNAPSHOT';
 const SPRING_BOOT_VERSION = '2.6.5';
 const LIQUIBASE_VERSION = '4.6.1';
 const LIQUIBASE_DTD_VERSION = LIQUIBASE_VERSION.split('.', 3).slice(0, 2).join('.');
-const HIBERNATE_VERSION = '5.6.5.Final';
+const HIBERNATE_VERSION = '5.6.7.Final';
 
 const JACOCO_VERSION = '0.8.7';
 const KAFKA_VERSION = '5.5.7';
