e2650ab1ba2c62206655be2943b9a86a004bae83
Daniel Franco <dandrfranco@gmail.com>
2022-04-28 10:27:08 +0100

    Update spring-boot version to 2.7.0-RC1

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -37,15 +37,15 @@ const JIB_VERSION = '3.2.1';
 // Libraries version
 const JHIPSTER_DEPENDENCIES_VERSION = '7.8.2-SNAPSHOT';
 // The spring-boot version should match the one managed by https://mvnrepository.com/artifact/tech.jhipster/jhipster-dependencies/JHIPSTER_DEPENDENCIES_VERSION
-const SPRING_BOOT_VERSION = '2.7.0';
-const LIQUIBASE_VERSION = '4.11.0';
+const SPRING_BOOT_VERSION = '2.7.0-RC1';
+const LIQUIBASE_VERSION = '4.6.1';
 const LIQUIBASE_DTD_VERSION = LIQUIBASE_VERSION.split('.', 3).slice(0, 2).join('.');
-const HIBERNATE_VERSION = '5.6.9.Final';
+const HIBERNATE_VERSION = '5.6.8.Final';
 
 const JACOCO_VERSION = '0.8.8';
 const KAFKA_VERSION = '7.1.1';
 const JACKSON_DATABIND_NULLABLE_VERSION = '0.2.2';
-const ELASTICSEARCH_VERSION = '7.17.3'; // The version should be coherent with the one from spring-data-elasticsearch project
+const ELASTICSEARCH_VERSION = '7.15.2'; // The version should be coherent with the one from spring-data-elasticsearch project
 // Version of docker images
 const DOCKER_COMPOSE_FORMAT_VERSION = '3.8';
 // const DOCKER_JHIPSTER_REGISTRY = 'ghcr.io/jhipster/jhipster-registry:main';
