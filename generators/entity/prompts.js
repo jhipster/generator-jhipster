c5f40434fbcf509907f7225177c31712c8ef6b5e
Murray Wilson <murray.wilson@epsilon.com>
2022-05-02 19:39:22 -0400

    Increase name limit for oracle to 128
    Fix #18424

--- generators/entity/prompts.js
+++ generators/entity/prompts.js
@@ -279,10 +279,7 @@ function askForTableName() {
     skipCheckLengthOfIdentifier ||
     !this.entityConfig.relationships ||
     this.entityConfig.relationships.length === 0 ||
-    /* All versions of Oracle with a 30 character name limit have gone end-of-life
-    !((prodDatabaseType === ORACLE && entityTableName.length > 14) || entityTableName.length > 30)
-    */
-    // Current versions of Oracle support 128 character names
+    // All versions of Oracle with a 30 character name limit have gone end-of-life, the limit is now 128
     !(entityTableName.length > (prodDatabaseType === ORACLE ? 112 : 30))
     ) {
     return undefined;
@@ -299,13 +296,12 @@ function askForTableName() {
         if (input === '') {
           return 'The table name cannot be empty';
         }
-        /* All versions of Oracle with a 30 character name limit have gone end-of-life
-        if (prodDatabaseType === 'oracle' && input.length > 14 && !skipCheckLengthOfIdentifier) {
+        // All versions of Oracle with a 30 character name limit have gone end-of-life, the limit is now 128
+        if (prodDatabaseType === 'oracle' && input.length > 112 && !skipCheckLengthOfIdentifier) {
           return 'The table name is too long for Oracle, try a shorter name';
         }
-        */
         // Current versions of Oracle support 128 character names [- 16 (since previous code checked for 14, 16 less than 30) = 112]
-        if (input.length > (prodDatabaseType === 'oracle' ? 112 : 30) && !skipCheckLengthOfIdentifier) {
+        if (input.length > (prodDatabaseType === 'oracle' ? 128 : 30) && !skipCheckLengthOfIdentifier) {
           return 'The table name is too long, try a shorter name';
         }
         return true;
