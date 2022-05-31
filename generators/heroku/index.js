7b4be0dd2249c1e944cebdc7c53e7eea868ea67a
Frederik Hahne <atomfrede@gmail.com>
2022-05-31 06:35:40 +0200

    enforce es version for maven when deploying to heroku
    closes #18650

--- generators/heroku/index.js
+++ generators/heroku/index.js
@@ -490,10 +490,9 @@ module.exports = class extends BaseBlueprintGenerator {
         this.log(chalk.bold('\nProvisioning addons'));
         if (this.searchEngine === ELASTICSEARCH) {
           this.log(chalk.bold('\nProvisioning bonsai elasticsearch addon'));
-          ChildProcess.exec(
-            `heroku addons:create bonsai:sandbox-6 --as BONSAI --app ${this.herokuAppName}`, (err, stdout, stderr) => {
-              addonCreateCallback.bind('Elasticsearch', err, stdout, stderr);
-            });
+          ChildProcess.exec(`heroku addons:create bonsai:sandbox-6 --as BONSAI --app ${this.herokuAppName}`, (err, stdout, stderr) => {
+            addonCreateCallback.bind('Elasticsearch', err, stdout, stderr);
+          });
         }
 
         if (this.useOkta) {
