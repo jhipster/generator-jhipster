c858fd3bb28b8883ef9af5ff045fe8c9285c5341
Daniel Franco <dandrfranco@gmail.com>
2022-06-02 19:42:01 +0100

    r2dbc:mysql connection uri should start by r2dbc:mariadb://

--- generators/sql-constants.js
+++ generators/sql-constants.js
@@ -25,7 +25,7 @@ const databaseData = {
     protocolSuffix: 'sqlserver://',
     port: ':1433;database=',
     r2dbc: {
-      protocolSuffix: 'mssql://',
+      protocolSuffix: 'mariadb://',
       port: ':1433/',
     },
   },
