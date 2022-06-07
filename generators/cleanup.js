964af563e2ece485d971cdc8527a1b745c9098ef
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-06-07 13:24:44 +0200

    Revert changing filename in cleanup

--- generators/cleanup.js
+++ generators/cleanup.js
@@ -298,7 +298,7 @@ function cleanupOldFiles(generator) {
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/util/entity-utils.spec.ts`);
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/auth/private-route.spec.tsx`);
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/error/error-boundary.spec.tsx`);
-      generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/error/error-boundary-routes.spec.tsx`);
+      generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/error/error-boundary-route.spec.tsx`);
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/layout/header/header.spec.tsx`);
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/shared/layout/menus/account.spec.tsx`);
       generator.removeFile(`${CLIENT_TEST_SRC_DIR}spec/app/modules/administration/administration.reducer.spec.ts`);
