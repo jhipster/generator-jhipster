75e16258b74b543249b1b7c3ff487e4a3b07d433
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-06-07 15:07:47 +0200

    Move error-boundary-route cleanup

--- generators/client/files-react.js
+++ generators/client/files-react.js
@@ -372,6 +372,10 @@ function cleanup() {
   if (this.isJhipsterVersionLessThan('7.7.1')) {
     this.removeFile(`${CLIENT_MAIN_SRC_DIR}app/entities/index.tsx`);
   }
+  if (this.isJhipsterVersionLessThan('7.8.2')) {
+    this.removeFile(`${CLIENT_MAIN_SRC_DIR}shared/error/error-boundary-route.tsx`);
+    this.removeFile(`${CLIENT_MAIN_SRC_DIR}shared/error/error-boundary-route.spec.tsx`);
+  }
 }
 
 function writeFiles() {
