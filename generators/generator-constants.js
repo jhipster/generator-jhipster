4ffb31f64e89bb740170cd76029d9ae1353e686d
Daniel Franco <dandrfranco@gmail.com>
2022-07-27 19:50:02 +0100

    Remove override of H2 and H2 R2DBC version

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -43,10 +43,6 @@ const HIBERNATE_VERSION = '5.6.10.Final';
 const JACOCO_VERSION = '0.8.8';
 const JACKSON_DATABIND_NULLABLE_VERSION = '0.2.3';
 const JIB_VERSION = '3.2.1';
-// TODO upgrade to h2 v2 once liquibase issues has been addressed
-const H2_VERSION = '2.1.214';
-// Version 0.9.x is not compatible with h2 v1
-const H2_R2DBC_VERSION = '0.9.1.RELEASE';
 
 // Version of docker images
 const DOCKER_COMPOSE_FORMAT_VERSION = '3.8';
@@ -416,8 +412,6 @@ const constants = {
   JIB_VERSION,
   JACOCO_VERSION,
   JACKSON_DATABIND_NULLABLE_VERSION,
-  H2_VERSION,
-  H2_R2DBC_VERSION,
 
   // Docker
   DOCKER_COMPOSE_FORMAT_VERSION,
