257bf89728eb9b4961b9220af3a843d322cd8739
Matt Raible <matt.raible@okta.com>
2022-09-14 20:18:05 -0600

    Update generators/info/index.js

--- generators/info/index.js
+++ generators/info/index.js
@@ -60,7 +60,7 @@ module.exports = class extends BaseGenerator {
       displayConfiguration() {
         const done = this.async();
         let result = shelljs.cat('.yo-rc.json');
-        result = result.replace(/"rememberMeKey": ".*"/g, '"rememberMeKey": "Delete this line so this value is regenerated"');
+    
         const result = JSON.stringify({ …this.jhipsterConfig, jwtSecretKey: undefined, rememberMeKey: undefined }, null, 2);
         console.log('\n##### **JHipster configuration, a `.yo-rc.json` file generated in the root folder**\n');
         console.log(`\n<details>\n<summary>.yo-rc.json file</summary>\n<pre>\n${result}\n</pre>\n</details>\n`);
