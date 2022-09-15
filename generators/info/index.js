663352891f95571e34a4d504f6a591fc5a59588d
Matt Raible <matt.raible@okta.com>
2022-09-14 20:19:33 -0600

    Update generators/info/index.js

--- generators/info/index.js
+++ generators/info/index.js
@@ -59,7 +59,6 @@ module.exports = class extends BaseGenerator {
 
       displayConfiguration() {
         const done = this.async();
-        let result = shelljs.cat('.yo-rc.json');
     
         const result = JSON.stringify({ …this.jhipsterConfig, jwtSecretKey: undefined, rememberMeKey: undefined }, null, 2);
         console.log('\n##### **JHipster configuration, a `.yo-rc.json` file generated in the root folder**\n');
