e054b49436847735bb757f420e55bde95a28b60d
mustafa.yildirim <mustafayildirim@aurorabilisim.com>
2022-09-24 11:12:46 +0300

    formatDateForChangelog metodunda okunurluk arttırıldı.

--- utils/liquibase.js
+++ utils/liquibase.js
@@ -55,26 +55,32 @@ function formatDateForChangelog(now) {
     now.getUTCSeconds()
   );
   const year = `${nowUTC.getFullYear()}`;
+
   let month = `${nowUTC.getMonth() + 1}`;
   if (month.length === 1) {
     month = `0${month}`;
   }
+
   let day = `${nowUTC.getDate()}`;
   if (day.length === 1) {
     day = `0${day}`;
   }
+
   let hour = `${nowUTC.getHours()}`;
   if (hour.length === 1) {
     hour = `0${hour}`;
   }
+
   let minute = `${nowUTC.getMinutes()}`;
   if (minute.length === 1) {
     minute = `0${minute}`;
   }
+
   let second = `${nowUTC.getSeconds()}`;
   if (second.length === 1) {
     second = `0${second}`;
   }
+
   return `${year}${month}${day}${hour}${minute}${second}`;
 }
 
