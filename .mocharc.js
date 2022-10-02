bb532e01d8dfc4dae2dc7a7c10e2d8356a25fdb1
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-10-02 18:11:16 +0200

    Don't use @esbuild-kit/cjs-loader
    With this loader running mocha fails.

--- .mocharc.js
+++ .mocharc.js
@@ -7,6 +7,6 @@ module.exports = {
   extension: ['js', 'spec.cjs', 'spec.mjs', 'spec.ts', 'spec.mts'],
   require: ['mocha-expect-snapshot/old-format', 'test/mocha.config.js', 'ts-node/register'],
   // Resolve absolute path for test with fork and different cwd.
-  loader: [require.resolve('@esbuild-kit/esm-loader'), require.resolve('@esbuild-kit/cjs-loader')],
+  loader: require.resolve('@esbuild-kit/esm-loader'),
   parallel: true,
 };
