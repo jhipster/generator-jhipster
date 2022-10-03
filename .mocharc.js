6272699150b367618962e27cc1710189168ed2bd
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-10-03 17:50:30 +0200

    Set up ts-node for running mocha tests

--- .mocharc.js
+++ .mocharc.js
@@ -5,7 +5,7 @@ module.exports = {
   timeout: 30000,
   ui: 'bdd',
   extension: ['js', 'spec.cjs', 'spec.mjs', 'spec.ts', 'spec.mts'],
-  require: ['mocha-expect-snapshot/old-format', 'test/mocha.config.js'],
+  require: ['mocha-expect-snapshot/old-format', 'test/mocha.config.js', 'ts-node/register'],
   // Resolve absolute path for test with fork and different cwd.
   loader: require.resolve('@esbuild-kit/esm-loader'),
   parallel: true,
