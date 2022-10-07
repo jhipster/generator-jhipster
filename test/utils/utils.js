fed4a12f108356ed877a0052c65960f3b4b74185
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-10-07 19:10:05 +0200

    Run JHipster cli in tests with tsx instead of node

--- test/utils/utils.js
+++ test/utils/utils.js
@@ -68,7 +68,7 @@ function shouldBeV3DockerfileCompatible(databaseType) {
 
 function getJHipsterCli() {
   const cmdPath = path.join(__dirname, '../../cli/jhipster');
-  let cmd = `node ${cmdPath} `;
+  let cmd = `tsx ${cmdPath} `;
   if (os.platform() === 'win32') {
     // corrected test for windows user
     cmd = cmd.replace(/\\/g, '/');
