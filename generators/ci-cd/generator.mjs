be665f4f258afffbf9c4507c3e478bdb8dbe0c7e
Daniel Franco <dandrfranco@gmail.com>
2022-11-01 17:56:20 +0000

    Use dependabot for jenkins docker image

--- generators/ci-cd/generator.mjs
+++ generators/ci-cd/generator.mjs
@@ -128,7 +128,6 @@ export default class CiCdGenerator extends BaseGenerator {
       getConstants() {
         this.DOCKER_DIR = constants.DOCKER_DIR;
         this.SERVER_MAIN_RES_DIR = constants.SERVER_MAIN_RES_DIR;
-        this.DOCKER_JENKINS = constants.DOCKER_JENKINS;
         this.ANGULAR = constants.SUPPORTED_CLIENT_FRAMEWORKS.ANGULAR;
         this.JAVA_VERSION = constants.JAVA_VERSION;
       },
