20b9fe304cfa4483889a99c2b973e7a3f76618c9
Daniel Franco <dandrfranco@gmail.com>
2022-11-01 17:39:04 +0000

    Use dependabot for consul docker image

--- generators/base-docker/generator.mjs
+++ generators/base-docker/generator.mjs
@@ -75,7 +75,6 @@ export default class BaseDockerGenerator extends BlueprintBaseGenerator {
         // Make constants available in templates
         this.DOCKER_JHIPSTER_REGISTRY = constants.DOCKER_JHIPSTER_REGISTRY;
         this.DOCKER_JHIPSTER_CONTROL_CENTER = constants.DOCKER_JHIPSTER_CONTROL_CENTER;
-        this.DOCKER_CONSUL = constants.DOCKER_CONSUL;
         this.DOCKER_CONSUL_CONFIG_LOADER = constants.DOCKER_CONSUL_CONFIG_LOADER;
         this.DOCKER_PROMETHEUS = constants.DOCKER_PROMETHEUS;
         this.DOCKER_PROMETHEUS_ALERTMANAGER = constants.DOCKER_PROMETHEUS_ALERTMANAGER;
