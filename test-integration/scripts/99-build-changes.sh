38d6b5f6bdeeb29f2959f78cc6494dfe3e179a85
Marcelo Shima <marceloshima@gmail.com>
2022-11-21 22:40:11 -0300

    add couchbase to build changes.

--- test-integration/scripts/99-build-changes.sh
+++ test-integration/scripts/99-build-changes.sh
@@ -90,6 +90,7 @@ echo "::endgroup::"
 echo "::group::Check Server"
 git -c color.ui=always diff --exit-code @~1 -- \
   'generators/bootstrap-application-server' \
+  'generators/couchbase' \
   'generators/gradle' \
   'generators/kafka' \
   'generators/maven' \
