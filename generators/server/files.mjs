e4d05cd1fcf92228ec7c0e0b9464264224e1150f
frascu <francescoscuccimarri@gmail.com>
2023-02-06 23:30:16 +0100

    create SpaWebFilterIT for reactive app

--- generators/server/files.mjs
+++ generators/server/files.mjs
@@ -603,6 +603,12 @@ export const baseServerFiles = {
       renameTo: moveToJavaPackageSrcDir,
       templates: ['web/filter/SpaWebFilter_reactive.java'],
     },
+    {
+      condition: generator => !generator.skipClient && generator.reactive,
+      path: `${SERVER_TEST_SRC_DIR}package/`,
+      renameTo: moveToJavaPackageTestDir,
+      templates: ['web/filter/SpaWebFilterIT_reactive.java'],
+    },
   ],
   serverJavaWebsocket: [
     {
