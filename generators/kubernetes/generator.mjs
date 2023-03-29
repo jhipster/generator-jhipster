96139ca7a7916e04d8194e314f3fa3f46195f5e8
Matt Raible <matt@raibledesigns.com>
2023-03-29 16:18:11 -0600

    Add support for Apple Silicon

--- generators/kubernetes/generator.mjs
+++ generators/kubernetes/generator.mjs
@@ -180,9 +180,9 @@ export default class KubernetesGenerator extends BaseDockerGenerator {
             const appConfig = this.appConfigs[index];
             let runCommand = '';
             if (appConfig.buildTool === MAVEN) {
-              runCommand = `./mvnw -ntp -Pprod verify jib:build -Djib.to.image=${appConfig.targetImageName}`;
+              runCommand = `./mvnw -ntp -Pprod verify jib:build${process.arch === 'arm64' ? ' -Djib-maven-plugin.architecture=arm64' : ''} -Djib.to.image=${appConfig.targetImageName}`;
             } else {
-              runCommand = `./gradlew bootJar -Pprod jib -Djib.to.image=${appConfig.targetImageName}`;
+              runCommand = `./gradlew bootJar -Pprod jib${process.arch === 'arm64' ? ' -PjibArchitecture=arm64' : ''} -Djib.to.image=${appConfig.targetImageName}`;
             }
             this.logger.info(`  ${chalk.cyan(`${runCommand}`)} in ${this.destinationPath(this.directoryPath + appsFolder)}`);
           });
