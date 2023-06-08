620e7e04322ee32012d4316ac7abb4924b2f605c
Matt Raible <matt@raibledesigns.com>
2023-06-08 00:20:05 -0500

    Fix port

--- generators/docker/templates/src/main/docker/config/keycloak-health-check.sh
+++ generators/docker/templates/src/main/docker/config/keycloak-health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-exec 3<>/dev/tcp/localhost/38217
+exec 3<>/dev/tcp/localhost/9080
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:38217\n" >&3
+echo -e "GET /health/ready HTTP/1.1\nhost: localhost:9080\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
