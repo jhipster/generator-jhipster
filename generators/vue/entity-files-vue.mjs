215df24c47af2a14bce9002142cad794a67715a0
Dinu k Sasidharan <102976558+dinu0000@users.noreply.github.com>
2023-08-20 12:18:17 +0530

    Update generators/vue/entity-files-vue.mjs
    Co-authored-by: Marcelo Shima <marceloshima@gmail.com>

--- generators/vue/entity-files-vue.mjs
+++ generators/vue/entity-files-vue.mjs
@@ -97,7 +97,7 @@ export async function postWriteEntityFiles({ application, entities }) {
 export function cleanupEntitiesFiles({ application, entities }) {
   for (const entity of entities.filter(entity => !entity.skipClient && !entity.builtIn)) {
     const { entityFolderName, entityFileName } = entity;
-    if (this.isJhipsterVersionLessThan('8.0.0')) {
+    if (this.isJhipsterVersionLessThan('8.0.0-beta.3')) {
       this.removeFile(`${application.clientTestDir}/spec/app/entities/${entityFolderName}/${entityFileName}.component.spec.ts`);
       this.removeFile(`${application.clientTestDir}/spec/app/entities/${entityFolderName}/${entityFileName}-detail.component.spec.ts`);
       this.removeFile(`${application.clientTestDir}/spec/app/entities/${entityFolderName}/${entityFileName}-update.component.spec.ts`);
