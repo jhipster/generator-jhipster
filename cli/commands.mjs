0abfc6851695f772f0ccaf3f140d5c506fa4ccfa
Marcelo Shima <marceloshima@gmail.com>
2023-09-06 06:35:01 -0300

    fix format

--- cli/commands.mjs
+++ cli/commands.mjs
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import chalk from "chalk";
+import chalk from 'chalk';
 
 const removedV8 = chalk.yellow(`
 
