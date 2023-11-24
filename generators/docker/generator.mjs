2673bd8c828916533ef12908d42d12c3f6ed3f95
Marcelo Shima <marceloshima@gmail.com>
2023-11-24 14:45:40 -0300

    increase cassandra sleep

--- generators/docker/generator.mjs
+++ generators/docker/generator.mjs
@@ -251,7 +251,7 @@ export default class DockerGenerator extends BaseApplicationGenerator {
             });
             if (application.databaseTypeCassandra) {
               // Wait for migration
-              postServicesSleep = 10;
+              postServicesSleep = 60;
             }
           } else {
             scriptsStorage.set(
