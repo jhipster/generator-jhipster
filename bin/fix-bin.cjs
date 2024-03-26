a4824ab3d4660830991494bf592e7fbb983f024b
Marcelo Shima <marceloshima@gmail.com>
2024-03-26 12:51:15 -0300

    add fix-bin helper (#25630)
    * add fix-bin helper

---
+++ bin/fix-bin.cjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const { chmod } = require('node:fs/promises');
+const { join } = require('node:path');
+const process = require('node:process');
+
+(async () => {
+  if (process.platform !== 'win32') {
+    await chmod(join(__dirname, '../dist/cli/jhipster.cjs'), 0o755);
+  }
+})();
