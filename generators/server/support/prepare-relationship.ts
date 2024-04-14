abf022212f3fd395092ed8f404641e28b3e4681d
Marcelo Shima <marceloshima@gmail.com>
2024-04-13 21:39:07 -0300

    add missing propertyApiDescription

--- generators/server/support/prepare-relationship.ts
+++ generators/server/support/prepare-relationship.ts
@@ -1,8 +1,13 @@
+import { mutateData } from '../../base/support/config.js';
 import { formatDocAsApiDescription, formatDocAsJavaDoc } from '../../java/support/doc.js';
 
 export function prepareRelationship({ relationship }: { relationship: any; entity: any }) {
   if (relationship.documentation) {
-    relationship.relationshipJavadoc = formatDocAsJavaDoc(relationship.documentation, 4);
-    relationship.relationshipApiDescription = formatDocAsApiDescription(relationship.documentation);
+    mutateData(relationship, {
+      __override__: false,
+      relationshipJavadoc: formatDocAsJavaDoc(relationship.documentation, 4),
+      relationshipApiDescription: formatDocAsApiDescription(relationship.documentation),
+      propertyApiDescription: ({ relationshipApiDescription }) => relationshipApiDescription,
+    });
   }
 }
