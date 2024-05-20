87943cd53ad3b696170e5e2bdb6389b85cbe5d70
Charlie Mordant <cmordant1@gmail.com>
2024-05-20 19:32:20 +0200

    Update jdl/converters/parsed-jdl-to-jdl-object/parsed-jdl-to-jdl-object-converter.spec.ts
    Co-authored-by: Marcelo Shima <marceloshima@gmail.com>

--- jdl/converters/parsed-jdl-to-jdl-object/parsed-jdl-to-jdl-object-converter.spec.ts
+++ jdl/converters/parsed-jdl-to-jdl-object/parsed-jdl-to-jdl-object-converter.spec.ts
@@ -64,7 +64,7 @@ describe('jdl - ParsedJDLToJDLObjectConverter', () => {
       describe('because there is no parsedContent', () => {
         it('should fail', () => {
           expect(() => {
-            // @ts-ignore
+            // @ts-expect-error
             ParsedJDLToJDLObjectConverter.parseFromConfigurationObject({});
           }).to.throw(/^The parsed JDL content must be passed\.$/);
         });
