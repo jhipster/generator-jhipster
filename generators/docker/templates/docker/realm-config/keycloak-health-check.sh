fcd4851617839bcb6dbd0dbac0581e9a8c0c4517
Quentin <quentin.monmert@gmail.com>
2024-06-12 17:35:51 +0200

    Upgrade Keycloak 25.0.0

--- generators/docker/templates/docker/realm-config/keycloak-health-check.sh
+++ generators/docker/templates/docker/realm-config/keycloak-health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-exec 3<>/dev/tcp/localhost/9080
+exec 3<>/dev/tcp/localhost/9990
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:9080\n" >&3
+echo -e "GET /health/ready HTTP/1.1\nhost: localhost:9990\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
