4360bd192acf9b15789de2c5451560386d499d6f
Marcelo Shima <marceloshima@gmail.com>
2024-06-28 10:49:29 -0300

    fix fromMatrix for a single config

--- testing/support/matrix-utils.ts
+++ testing/support/matrix-utils.ts
@@ -1,38 +1,33 @@
 import sortKeys from 'sort-keys';
 
-const appendTitle = (title, config, value) => {
+const appendTitle = (title: string, config: string, value: boolean | string | number) => {
   if (Array.isArray(value)) value = value[0];
   if (value === undefined) return title;
   const newTitle = typeof value === 'string' && value !== 'no' ? value : `${config}(${value})`;
   return `${title}${title.length === 0 ? '' : '-'}${newTitle}`;
 };
 
-export const fromMatrix = configMatrix => {
+export const fromMatrix = (configMatrix: Record<string, any[]>) => {
   const configEntries = Object.entries(configMatrix);
-  const samples = configEntries.reduce((previousValue: any, currentValue: [string, any]) => {
-    const [config, configValues] = currentValue;
-    if (previousValue.length === 0) {
-      return configValues.map(value => [
-        value,
-        {
-          [config]: value,
-        },
-      ]);
-    }
-    return previousValue
-      .map(([previousName, previousConfig]) =>
-        configValues.map(value => {
-          return [
-            appendTitle(previousName, config, value),
-            {
-              ...previousConfig,
-              [config]: value,
-            },
-          ];
-        }),
-      )
-      .flat();
-  }, []);
+  const samples = configEntries.reduce(
+    (previousValue: any, currentValue: [string, any]) => {
+      const [config, configValues] = currentValue;
+      return previousValue
+        .map(([previousName, previousConfig]) =>
+          configValues.map(value => {
+            return [
+              appendTitle(previousName, config, value),
+              {
+                ...previousConfig,
+                [config]: value,
+              },
+            ];
+          }),
+        )
+        .flat();
+    },
+    [['', {}]],
+  );
   return Object.fromEntries(samples);
 };
 
