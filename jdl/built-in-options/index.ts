9318ee6ff0e289ddf8cf05aa39b0a6266603986b
Marcelo Shima <marceloshima@gmail.com>
2024-09-02 15:00:44 -0300

    cleanup

--- jdl/built-in-options/index.ts
+++ jdl/built-in-options/index.ts
@@ -19,6 +19,8 @@ export {
   /** @deprecated */
   searchEngineTypes,
   /** @deprecated */
+  entityOptions,
+  /** @deprecated */
   clientFrameworkTypes,
   /** @deprecated */
   serviceDiscoveryTypes,
