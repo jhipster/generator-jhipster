5deae080b1dcb37f3765ee4f2e6ca6bf11662b7a
Marcelo Shima <marceloshima@gmail.com>
2024-09-12 09:23:38 -0300

    adjust containers supported by composer-compose integration

--- .blueprint/github-build-matrix/support/testcontainers.ts
+++ .blueprint/github-build-matrix/support/testcontainers.ts
@@ -1,6 +1,7 @@
 import { extendMatrix, fromMatrix } from '../../../lib/testing/index.js';
 import { convertToCliArgs } from './cli-args.js';
 
+// Supported containers: https://github.com/spring-projects/spring-boot/tree/main/spring-boot-project/spring-boot-docker-compose/src/main/java/org/springframework/boot/docker/compose/service/connection
 export const testcontainersMatrix = Object.fromEntries(
   Object.entries(
     extendMatrix(
@@ -22,9 +23,9 @@ export const testcontainersMatrix = Object.fromEntries(
       {
         build: ['maven', 'gradle'],
         searchEngine: ['no', 'elasticsearch'],
-        auth: ['jwt', 'oauth2'],
-        serviceDiscoveryType: ['no', 'eureka', 'consul'],
-        messageBroker: ['no', 'kafka'],
+        // auth: ['jwt', 'oauth2'],
+        // serviceDiscoveryType: ['no', 'eureka', 'consul'],
+        // messageBroker: ['no', 'kafka'],
       },
     ),
   ).map(([key, value]) => [key, { args: convertToCliArgs(value) }]),
