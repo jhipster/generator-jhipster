e9cb68168391ce89f8b333a339082bec7ca53cec
Marcelo Shima <marceloshima@gmail.com>
2024-09-12 13:17:45 -0300

    move docker-compose dep back to profile

--- generators/spring-boot/generator.ts
+++ generators/spring-boot/generator.ts
@@ -589,7 +589,7 @@ public void set${javaBeanCase(propertyName)}(${propertyType} ${propertyName}) {
               },
             ],
           });
-          source.addMavenDependency!({ ...dockerComposeArtifact, optional: true });
+          source.addMavenDependency!({ inProfile: 'docker-compose', ...dockerComposeArtifact, optional: true });
         }
       },
     });
