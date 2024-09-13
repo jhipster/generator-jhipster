6d52440161bb7f80fd68eadc0eaedb304697ab25
Marcelo Shima <marceloshima@gmail.com>
2024-09-13 13:13:44 -0300

    prettier adjusts

--- .blueprint/github-build-matrix/samples/docker-compose-integration.ts
+++ .blueprint/github-build-matrix/samples/docker-compose-integration.ts
@@ -3,35 +3,36 @@ import { convertOptionsToJDL } from '../support/jdl.js';
 
 // Supported containers: https://github.com/spring-projects/spring-boot/tree/main/spring-boot-project/spring-boot-docker-compose/src/main/java/org/springframework/boot/docker/compose/service/connection
 export const dockerComposeMatrix = Object.fromEntries(
-  [...Object.entries(
-    extendMatrix(
-      {
-        ...fromMatrix({
-          databaseType: ['cassandra', 'mongodb', 'neo4j'],
-          reactive: [false, true],
-        }),
-        ...extendMatrix(
-          fromMatrix({
-            prodDatabaseType: ['postgresql', 'mysql', 'mariadb'],
-            reactive: [false],
+  [
+    ...Object.entries(
+      extendMatrix(
+        {
+          ...fromMatrix({
+            databaseType: ['cassandra', 'mongodb', 'neo4j'],
+            reactive: [false, true],
           }),
-          { cacheProvider: ['no', 'redis', 'memcached'] },
-        ),
-        ...fromMatrix({
-          prodDatabaseType: ['postgresql', 'mysql', 'mariadb'],
-          reactive: [true],
-        }),
-      },
-      {
-        buildTool: ['maven', 'gradle'],
-        searchEngine: ['no', 'elasticsearch'],
-        authenticationType: ['jwt', 'oauth2'],
-        serviceDiscoveryType: ['no', 'eureka', 'consul'],
-        messageBroker: ['no', 'kafka'],
-      },
+          ...extendMatrix(
+            fromMatrix({
+              prodDatabaseType: ['postgresql', 'mysql', 'mariadb'],
+              reactive: [false],
+            }),
+            { cacheProvider: ['no', 'redis', 'memcached'] },
+          ),
+          ...fromMatrix({
+            prodDatabaseType: ['postgresql', 'mysql', 'mariadb'],
+            reactive: [true],
+          }),
+        },
+        {
+          buildTool: ['maven', 'gradle'],
+          searchEngine: ['no', 'elasticsearch'],
+          authenticationType: ['jwt', 'oauth2'],
+          serviceDiscoveryType: ['no', 'eureka', 'consul'],
+          messageBroker: ['no', 'kafka'],
+        },
+      ),
     ),
-  ),
-     ['h2', { devDatabaseType: 'h2Disk' }],
+    ['h2', { devDatabaseType: 'h2Disk' }],
   ].map(([key, value]) => [
     key,
     {
