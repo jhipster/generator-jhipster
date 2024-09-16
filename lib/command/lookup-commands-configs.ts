d21557ab19eb82488da5f0d8f0daef49f032e751
Marcelo Shima <marceloshima@gmail.com>
2024-09-16 11:20:04 -0300

    add filter to lookupCommandsConfigs

--- lib/command/lookup-commands-configs.ts
+++ lib/command/lookup-commands-configs.ts
@@ -1,7 +1,7 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { glob } from 'glob';
-import type { JHipsterConfigs } from './types.js';
+import type { JHipsterConfig, JHipsterConfigs } from './types.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,17 +9,20 @@ const __dirname = dirname(__filename);
 const cwd = join(__dirname, '../..');
 let jdlConfigs: JHipsterConfigs;
 
-export const lookupCommandsConfigs = async (): Promise<JHipsterConfigs> => {
+export const lookupCommandsConfigs = async (options?: { filter: (config: JHipsterConfig) => boolean }): Promise<JHipsterConfigs> => {
   if (jdlConfigs) {
     return jdlConfigs;
   }
+  const { filter = () => true } = options ?? {};
   jdlConfigs = {};
   const files = [...(await glob('generators/*/index.{j,t}s', { cwd })), ...(await glob('generators/*/generators/*/index.{j,t}s', { cwd }))];
   for (const file of files) {
     const index = await import(`${cwd}/${file}`);
     const configs: JHipsterConfigs = index.command?.configs ?? {};
     for (const [key, value] of Object.entries(configs)) {
-      jdlConfigs[key] = value;
+      if (filter(value)) {
+        jdlConfigs[key] = value;
+      }
     }
   }
   return jdlConfigs;
