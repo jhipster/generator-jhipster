4892d773e0917083865c758347475c1aad7a2c45
Matt Raible <matt@raibledesigns.com>
2024-09-20 11:29:08 -0600

    Add support for Java 23

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -26,7 +26,7 @@ export const JHIPSTER_DEPENDENCIES_VERSION = '8.7.1';
 // Version of Java
 export const JAVA_VERSION = '17';
 // Supported Java versions, https://www.oracle.com/java/technologies/java-se-support-roadmap.html
-export const JAVA_COMPATIBLE_VERSIONS = ['17', '21', '22'];
+export const JAVA_COMPATIBLE_VERSIONS = ['17', '21', '22', '23'];
 // Force spring milestone repository. Spring Boot milestones are detected.
 export const ADD_SPRING_MILESTONE_REPOSITORY = false;
 
