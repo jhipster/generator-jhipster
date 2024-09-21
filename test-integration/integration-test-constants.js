4aaed0bebf54548ed5485e02243aaedeed74a7fb
Marcelo Shima <marceloshima@gmail.com>
2024-09-21 18:10:04 -0300

    use jhipster-framework release (#27342)
    * use jhipster-framework release
    
    * force build jhipster-bom

--- test-integration/integration-test-constants.js
+++ test-integration/integration-test-constants.js
@@ -3,7 +3,7 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { JHIPSTER_DEPENDENCIES_VERSION } from '../generators/generator-constants.js';
 
-const FORCE_BUILD_JHIPSTER_BOM = false;
+const FORCE_BUILD_JHIPSTER_BOM = true;
 export const JHIPSTER_BOM_BRANCH = 'main';
 export const JHIPSTER_BOM_CICD_VERSION = '0.0.0-CICD';
 export const BUILD_JHIPSTER_BOM = FORCE_BUILD_JHIPSTER_BOM || JHIPSTER_DEPENDENCIES_VERSION.includes('-SNAPSHOT');
