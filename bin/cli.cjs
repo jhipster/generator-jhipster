e8707b3939dace2e357d26e5e99bf61eee32a1f5
Marcelo Shima <marceloshima@gmail.com>
2024-10-16 04:44:35 -0300

    dynamic register ts node loader (#27587)

--- bin/cli.cjs
+++
@@ -1,6 +0,0 @@
-// Cleanup loaders options, we don't want to pass to child processes.
-delete process.env.NODE_OPTIONS;
-
-process.env.JHIPSTER_DEV_BLUEPRINT = true;
-
-require('../cli/jhipster.cjs');
