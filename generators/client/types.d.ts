e647b9c15aee3612768a705dc6d9ff42ce9d0ec7
Charlie Mordant <cmordant1@gmail.com>
2024-11-02 13:22:03 +0100

    ensure retrocompatibility

--- generators/client/types.d.ts
+++ generators/client/types.d.ts
@@ -3,11 +3,12 @@ import type { ExportApplicationPropertiesFromCommand } from '../../lib/command/i
 import type { CypressApplication } from '../cypress/types.js';
 import type { JavaScriptApplication, JavaScriptSourceType } from '../javascript/types.js';
 import type { PostWritingEntitiesTaskParam } from '../../lib/types/application/tasks.js';
+import type { PartialAngularApplication } from '../angular/types-deprecated.js';
 import type Command from './command.ts';
 
 type ApplicationClientProperties = ExportApplicationPropertiesFromCommand<typeof Command>;
 
-export type ClientApplication = ApplicationClientProperties &
+export type FrontendApplication = ApplicationClientProperties &
   JavaScriptApplication &
   CypressApplication & {
     webappLoginRegExp: string;
@@ -15,6 +16,11 @@ export type ClientApplication = ApplicationClientProperties &
     webappEnumerationsDir?: string;
     clientFrameworkBuiltIn?: boolean;
   };
+
+/**
+ * Deprecated in favor of frontend application.
+ */
+export type ClientApplication = JavaScriptApplication & PartialAngularApplication;
 
 export type ClientResources = {
   /**
