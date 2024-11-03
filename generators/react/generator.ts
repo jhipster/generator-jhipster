6ce4ca6e5f82b72333ead6db404e7e1f4883d927
Marcelo Shima <marceloshima@gmail.com>
2024-11-03 10:07:01 -0300

    react: use mock in mf test (#27775)

--- generators/react/generator.ts
+++ generators/react/generator.ts
@@ -155,9 +155,16 @@ export default class ReactGenerator extends BaseApplicationGenerator {
 
   get writing() {
     return this.asWritingTaskGroup({
-      async cleanup({ control }) {
+      async cleanup({ control, application }) {
         await control.cleanupFiles({
           '8.6.1': ['.eslintrc.json', '.eslintignore'],
+          '8.7.4': [
+            [
+              Boolean(application.microfrontend && application.applicationTypeGateway),
+              `${application.srcMainWebapp}microfrontends/entities-menu.tsx`,
+              `${application.srcMainWebapp}microfrontends/entities-routes.tsx`,
+            ],
+          ],
         });
       },
       cleanupOldFilesTask,
