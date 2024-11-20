f4a0aedceb66428b00625eb39fca423ce25ed8b1
Marcelo Shima <marceloshima@gmail.com>
2024-11-20 15:34:40 -0300

    from-issue: improve parseIssue (#27945)

---
+++ lib/testing/github.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'esmocha';
+import { parseIssue } from './github.js';
+
+describe('github', () => {
+  it('should parse full issue spec', () => {
+    expect(parseIssue('jhipster/generator-jhipster#12345')).toEqual({
+      owner: 'jhipster',
+      repository: 'generator-jhipster',
+      issue: '12345',
+    });
+  });
+  it('should parse repo with issue', () => {
+    expect(parseIssue('generator-jhipster-entity-audit#12345')).toEqual({
+      repository: 'generator-jhipster-entity-audit',
+      issue: '12345',
+    });
+  });
+  it('should parse issue', () => {
+    expect(parseIssue('12345')).toEqual({ issue: '12345' });
+  });
+  it('should not parse with missing repo', () => {
+    expect(() => parseIssue('jhipster/12345')).toThrowError('Invalid issue format: jhipster/12345');
+  });
+  it('should not parse with invalid issue number', () => {
+    expect(() => parseIssue('a')).toThrowError('Invalid issue format: a');
+  });
+});
