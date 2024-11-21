790840a17fd98d649aedb00e918c61588ce8f496
Marcelo Shima <marceloshima@gmail.com>
2024-11-21 15:00:23 -0300

    Update Spring Boot to v3.3.6 (#27959)

--- generators/java/types.d.ts
+++ generators/java/types.d.ts
@@ -75,6 +75,7 @@ export type JavaApplication = JavaBootstrapStorageProperties &
 
     addOpenapiGeneratorPlugin: boolean;
     useNpmWrapper: boolean;
+    graalvmReachabilityMetadata: string;
   };
 
 export type ConditionalJavaDefinition = JavaDefinition & { condition?: boolean };
