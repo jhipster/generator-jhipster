e31ce4b506a0ced25c60698a920633e6653b49ed
Marcelo Boveto Shima <marceloshima@gmail.com>
2024-11-29 16:56:31 -0300

    languages: fix languages prompts

--- generators/languages/prompts.ts
+++ generators/languages/prompts.ts
@@ -54,6 +54,7 @@ export async function askForLanguages(this: LanguagesGenerator, { control }) {
   const currentLanguages = this.jhipsterConfig.languages ?? [];
   const answers = await this.prompt([
     {
+      when: this.jhipsterConfigWithDefaults.enableTranslation,
       type: 'checkbox',
       name: 'languages',
       message: 'Please choose additional languages to install',
@@ -67,9 +68,11 @@ export async function askForLanguages(this: LanguagesGenerator, { control }) {
       default: () => this.jhipsterConfigWithDefaults.languages,
     },
   ]);
-  if (control.existingProject) {
-    this.languagesToApply.push(...answers.languages.filter(newLang => !currentLanguages.includes(newLang)));
-  } else {
-    this.languagesToApply.push(...answers.languages);
+  if (answers.languages) {
+    if (control.existingProject) {
+      this.languagesToApply.push(...answers.languages.filter(newLang => !currentLanguages.includes(newLang)));
+    } else {
+      this.languagesToApply.push(...answers.languages);
+    }
   }
 }
