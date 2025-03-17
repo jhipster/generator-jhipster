0db317c5e11f480e8bacf36823ee6dd0b5adc73b
Marcelo Shima <marceloshima@gmail.com>
2025-03-17 17:59:48 -0300

    Revert "use nullable_specification branch"
    This reverts commit ec022e8da7f0172e1dbed866239bd57abc506801.

--- test-integration/integration-test-constants.js
+++ test-integration/integration-test-constants.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { JHIPSTER_DEPENDENCIES_VERSION } from '../generators/generator-constants.js';
 
 const FORCE_BUILD_JHIPSTER_BOM = true;
-export const JHIPSTER_BOM_BRANCH = 'nullable_specification';
+export const JHIPSTER_BOM_BRANCH = 'main';
 export const JHIPSTER_BOM_CICD_VERSION = '0.0.0-CICD';
 export const BUILD_JHIPSTER_BOM = FORCE_BUILD_JHIPSTER_BOM || JHIPSTER_DEPENDENCIES_VERSION.includes('-SNAPSHOT');
 
