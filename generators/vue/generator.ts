fa8dd6c9091a7ac36f440fc040d25b872b0b313c
Marcelo Shima <marceloshima@gmail.com>
2025-05-16 09:10:54 -0300

    internal: use control from task instead of generator api

--- generators/vue/generator.ts
+++ generators/vue/generator.ts
@@ -113,7 +113,7 @@ export default class VueGenerator extends BaseApplicationGenerator {
           javaNodeBuildPaths?.push('vite.config.mts');
         }
       },
-      prepareForTemplates({ application, source }) {
+      prepareForTemplates({ application, control, source }) {
         application.addPrettierExtensions?.(['html', 'vue', 'css', 'scss']);
 
         source.addWebpackConfig = args => {
@@ -121,7 +121,7 @@ export default class VueGenerator extends BaseApplicationGenerator {
             throw new Error('This application is not webpack based');
           }
           const webpackPath = `${application.clientRootDir}webpack/webpack.common.js`;
-          const ignoreNonExisting = this.control.ignoreNeedlesError && 'Webpack configuration file not found';
+          const ignoreNonExisting = control.ignoreNeedlesError && 'Webpack configuration file not found';
           this.editFile(
             webpackPath,
             { ignoreNonExisting },
