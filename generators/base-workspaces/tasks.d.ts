fe68d7b6d44129ed03f1a9e5bcfcbd824e9f2e7f
Charlie Mordant <cmordant1@gmail.com>
2025-06-09 22:57:20 +0200

    Update generators/base-workspaces/tasks.d.ts
    Co-authored-by: Marcelo Shima <marceloshima@gmail.com>

--- generators/base-workspaces/tasks.d.ts
+++ generators/base-workspaces/tasks.d.ts
@@ -18,10 +18,8 @@
  */
 import type { Merge } from 'type-fest';
 import type { TaskTypes as BaseTaskTypes, TaskParamWithControl, TaskParamWithSource, TaskTypes } from '../base/tasks.js';
-import type {
-  Application as BaseSimpleApplicationApplication,
-  Source as BaseSimpleApplicationSource,
-} from '../base-simple-application/types.d.ts';
+import type { Application as BaseSimpleApplicationApplication } from '../base-simple-application/types.d.ts';
+import type { Source as BaseSource } from '../base/types.d.ts';
 import type { Workspaces } from './types.js';
 
 export type TaskParamWithApplications<
