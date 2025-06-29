86ae8e7b57aaace1584935a0f5da8e3d1025bdd9
Charlie Mordant <cmordant1@gmail.com>
2025-06-29 18:48:56 +0200

    first part of use of writing api for helm

--- generators/kubernetes-helm/files.ts
+++ generators/kubernetes-helm/files.ts
@@ -17,18 +17,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  applicationTypes,
-  authenticationTypes,
-  kubernetesPlatformTypes,
-  monitoringTypes,
-  searchEngineTypes,
-  serviceDiscoveryTypes,
-} from '../../lib/jhipster/index.js';
-import { asWritingTask } from '../base-application/support/index.js';
+import { authenticationTypes, kubernetesPlatformTypes, monitoringTypes, serviceDiscoveryTypes } from '../../lib/jhipster/index.js';
+import { asWriteFilesSection, asWritingTask } from '../base-application/support/index.js';
+import type { WriteFileSection } from '../base-core/api.js';
 
-const { ELASTICSEARCH } = searchEngineTypes;
-const { GATEWAY, MONOLITH } = applicationTypes;
 const { JWT } = authenticationTypes;
 const { PROMETHEUS } = monitoringTypes;
 const { CONSUL, EUREKA } = serviceDiscoveryTypes;
@@ -36,35 +28,90 @@ const { ServiceTypes } = kubernetesPlatformTypes;
 
 const { INGRESS } = ServiceTypes;
 
+const applicationFiles: WriteFileSection = asWriteFilesSection((suffix: string) => ({
+  common: [
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/${data.app.baseName.toLowerCase()}-deployment.yml`,
+      templates: ['deployment.yml'],
+    },
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/${data.app.baseName.toLowerCase()}-service.yml`,
+      templates: ['service.yml'],
+    },
+  ],
+  elasticsearch: [
+    {
+      condition: data => data.app.searchEngineElasticsearch,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/${data.app.baseName.toLowerCase()}-elasticsearch.yml`,
+      templates: ['db/elasticsearch.yml'],
+    },
+  ],
+  gateway: [
+    {
+      condition: data => (data.app.applicationTypeGateway || data.app.applicationTypeMonolith) && data.istio,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/${data.app.baseName.toLowerCase()}-gateway.yml`,
+      templates: ['istio/gateway.yml'],
+    },
+    {
+      condition: data =>
+        (data.app.applicationTypeGateway || data.app.applicationTypeMonolith) && !data.istio && data.kubernetesServiceType === INGRESS,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/${data.app.baseName.toLowerCase()}-ingress.yml`,
+      templates: ['ingress.yml'],
+    },
+  ],
+}));
+const helmApplicationFiles: WriteFileSection = asWriteFilesSection((suffix: string) => ({
+  common: [
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/values.yaml`,
+      templates: ['app/values.yml'],
+    },
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/Chart.yaml`,
+      templates: ['app/Chart.yml'],
+    },
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/requirements.yaml`,
+      templates: ['app/requirements.yml'],
+    },
+    {
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/_helpers.tpl`,
+      templates: ['app/helpers.tpl'],
+    },
+  ],
+}));
+const writeDeploymentFiles = (suffix = '') => ({
+  namespace: [
+    {
+      condition: data => data.kubernetesNamespace !== 'default',
+      templates: ['namespace.yml'],
+    },
+  ],
+});
+
 export const writeFiles = asWritingTask(async function writeFiles() {
   const suffix = 'helm';
-  // app charts
+
+  await this.writeFiles({
+    sections: writeDeploymentFiles(suffix),
+    context: this,
+    rootTemplatesPath: this.fetchFromInstalledJHipster('kubernetes/templates'),
+  });
+
   const kubernetesSubgenPath = this.fetchFromInstalledJHipster('kubernetes/templates');
-  if (this.kubernetesNamespace !== 'default') {
-    await this.writeFile(`${kubernetesSubgenPath}/namespace.yml.ejs`, 'namespace.yml');
-  }
   for (let i = 0; i < this.appConfigs.length; i++) {
     const appName = this.appConfigs[i].baseName.toLowerCase();
     const appOut = appName.concat('-', suffix);
     this.app = this.appConfigs[i];
-
-    await this.writeFile(`${kubernetesSubgenPath}/deployment.yml.ejs`, `${appOut}/templates/${appName}-deployment.yml`);
-    await this.writeFile(`${kubernetesSubgenPath}/service.yml.ejs`, `${appOut}/templates/${appName}-service.yml`);
-    await this.writeFile('app/values.yml.ejs', `${appOut}/values.yaml`);
-    await this.writeFile('app/Chart.yml.ejs', `${appOut}/Chart.yaml`);
-    await this.writeFile('app/requirements.yml.ejs', `${appOut}/requirements.yaml`);
-    await this.writeFile('app/helpers.tpl.ejs', `${appOut}/templates/_helpers.tpl`);
-
-    if (this.app.searchEngine === ELASTICSEARCH) {
-      await this.writeFile(`${kubernetesSubgenPath}/db/elasticsearch.yml.ejs`, `${appOut}/templates/${appName}-elasticsearch.yml`);
-    }
-    if (this.app.applicationType === GATEWAY || this.app.applicationType === MONOLITH) {
-      if (this.istio) {
-        await this.writeFile(`${kubernetesSubgenPath}/istio/gateway.yml.ejs`, `${appOut}/templates/${appName}-gateway.yml`);
-      } else if (this.kubernetesServiceType === INGRESS) {
-        await this.writeFile(`${kubernetesSubgenPath}/ingress.yml.ejs`, `${appOut}/templates/${appName}-ingress.yml`);
-      }
-    }
+    await this.writeFiles({
+      sections: applicationFiles(suffix),
+      context: this,
+      rootTemplatesPath: this.fetchFromInstalledJHipster('kubernetes/templates'),
+    });
+    await this.writeFiles({
+      sections: helmApplicationFiles(suffix),
+      context: this,
+    });
     if (!this.app.serviceDiscoveryAny && this.app.authenticationType === JWT) {
       await this.writeFile(`${kubernetesSubgenPath}/secret/jwt-secret.yml.ejs`, `${appOut}/templates/jwt-secret.yml`);
     }
--- generators/kubernetes/files.ts
+++ generators/kubernetes/files.ts
@@ -25,14 +25,14 @@ import type { WriteFileSection } from '../base-core/api.js';
 const { GATEWAY, MONOLITH } = applicationTypes;
 const { JWT } = authenticationTypes;
 
-const applicationFiles: WriteFileSection = asWriteFilesSection({
+const applicationFiles: WriteFileSection = asWriteFilesSection((suffix: string) => ({
   common: [
     {
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-deployment.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-deployment.yml`,
       templates: ['deployment.yml'],
     },
     {
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-service.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-service.yml`,
       templates: ['service.yml'],
     },
   ],
@@ -41,64 +41,66 @@ const applicationFiles: WriteFileSection = asWriteFilesSection({
       // If we choose microservice with no DB, it is trying to move _no.yml as prodDatabaseType is getting tagged as 'string' type
       condition: data => !data.app.databaseTypeNo,
       renameTo: data =>
-        `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-${data.app.prodDatabaseType ?? data.app.databaseType}.yml`,
+        `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-${data.app.prodDatabaseType ?? data.app.databaseType}.yml`,
       templates: [{ sourceFile: data => `db/${data.app.prodDatabaseType ?? data.app.databaseType}.yml` }],
     },
     {
       condition: data => data.app.databaseTypeCouchbase,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/templates/couchbase-secret.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/templates/couchbase-secret.yml`,
       templates: ['secret/couchbase-secret.yml'],
     },
   ],
   searchEngine: [
     {
       condition: data => data.app.searchEngineElasticsearch,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-elasticsearch.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-elasticsearch.yml`,
       templates: ['db/elasticsearch.yml'],
     },
   ],
   gateway: [
     {
       condition: data => (data.app.applicationType === GATEWAY || data.app.applicationType === MONOLITH) && data.istio,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-gateway.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-gateway.yml`,
       templates: ['istio/gateway.yml'],
     },
     {
       condition: data =>
-        (data.app.applicationType === GATEWAY || data.app.applicationType === MONOLITH) && data.kubernetesServiceType === 'Ingress',
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-ingress.yml`,
+        (data.app.applicationType === GATEWAY || data.app.applicationType === MONOLITH) &&
+        !data.istio &&
+        data.kubernetesServiceType === 'Ingress',
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-ingress.yml`,
       templates: ['ingress.yml'],
     },
   ],
   serviceDiscovery: [
     {
       condition: data => !data.app.serviceDiscoveryAny && data.app.authenticationType === JWT,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-jwt-secret.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-jwt-secret.yml`,
       templates: ['secret/jwt-secret.yml'],
     },
   ],
   monitoring: [
     {
       condition: data => data.monitoringPrometheus,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-prometheus-sm.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-prometheus-sm.yml`,
       templates: ['monitoring/jhipster-prometheus-sm.yml'],
     },
   ],
   istio: [
     {
       condition: data => data.istio,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-destination-rule.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-destination-rule.yml`,
       templates: ['istio/destination-rule.yml'],
     },
     {
       condition: data => data.istio,
-      renameTo: data => `${data.app.baseName.toLowerCase()}-k8s/${data.app.baseName.toLowerCase()}-virtual-service.yml`,
+      renameTo: data => `${data.app.baseName.toLowerCase()}-${suffix}/${data.app.baseName.toLowerCase()}-virtual-service.yml`,
       templates: ['istio/virtual-service.yml'],
     },
   ],
-});
+}));
 
-const writeDeploymentFiles = asWriteFilesSection({
+const writeDeploymentFiles = asWriteFilesSection((suffix = '') => ({
   common: [
     {
       templates: [
@@ -118,16 +120,16 @@ const writeDeploymentFiles = asWriteFilesSection({
   messageBroker: [
     {
       condition: data => data.useKafka,
-      templates: [{ sourceFile: 'messagebroker/kafka.yml', destinationFile: 'messagebroker-k8s/kafka.yml' }],
+      templates: [{ sourceFile: 'messagebroker/kafka.yml', destinationFile: `messagebroker-${suffix}/kafka.yml` }],
     },
   ],
   authentication: [
     {
       condition: data => data.useKeycloak,
       templates: [
-        { sourceFile: 'keycloak/keycloak-configmap.yml.ejs', destinationFile: 'keycloak-k8s/keycloak-configmap.yml' },
-        { sourceFile: 'keycloak/keycloak-postgresql.yml.ejs', destinationFile: 'keycloak-k8s/keycloak-postgresql.yml' },
-        { sourceFile: 'keycloak/keycloak.yml.ejs', destinationFile: 'keycloak-k8s/keycloak.yml' },
+        { sourceFile: 'keycloak/keycloak-configmap.yml.ejs', destinationFile: `keycloak-${suffix}/keycloak-configmap.yml` },
+        { sourceFile: 'keycloak/keycloak-postgresql.yml.ejs', destinationFile: `keycloak-${suffix}/keycloak-postgresql.yml` },
+        { sourceFile: 'keycloak/keycloak.yml.ejs', destinationFile: `keycloak-${suffix}/keycloak.yml` },
       ],
     },
     {
@@ -180,16 +182,16 @@ const writeDeploymentFiles = asWriteFilesSection({
     {
       condition: data => data.serviceDiscoveryTypeEureka,
       templates: [
-        { sourceFile: 'registry/jhipster-registry.yml.ejs', destinationFile: 'registry-k8s/jhipster-registry.yml' },
-        { sourceFile: 'registry/application-configmap.yml.ejs', destinationFile: 'registry-k8s/application-configmap.yml' },
+        { sourceFile: 'registry/jhipster-registry.yml.ejs', destinationFile: `registry-${suffix}/jhipster-registry.yml` },
+        { sourceFile: 'registry/application-configmap.yml.ejs', destinationFile: `registry-${suffix}/application-configmap.yml` },
       ],
     },
     {
       condition: data => data.serviceDiscoveryTypeConsul,
       templates: [
-        { sourceFile: 'registry/consul.yml.ejs', destinationFile: 'registry-k8s/consul.yml' },
-        { sourceFile: 'registry/consul-config-loader.yml.ejs', destinationFile: 'registry-k8s/consul-config-loader.yml' },
-        { sourceFile: 'registry/application-configmap.yml.ejs', destinationFile: 'registry-k8s/application-configmap.yml' },
+        { sourceFile: 'registry/consul.yml.ejs', destinationFile: `registry-${suffix}/consul.yml` },
+        { sourceFile: 'registry/consul-config-loader.yml.ejs', destinationFile: `registry-${suffix}/consul-config-loader.yml` },
+        { sourceFile: 'registry/application-configmap.yml.ejs', destinationFile: `registry-${suffix}/application-configmap.yml` },
       ],
     },
   ],
@@ -197,25 +199,25 @@ const writeDeploymentFiles = asWriteFilesSection({
     {
       condition: data => data.istio,
       templates: [
-        { sourceFile: 'istio/gateway/grafana-gateway.yml.ejs', destinationFile: 'istio-k8s/grafana-gateway.yml' },
-        { sourceFile: 'istio/gateway/zipkin-gateway.yml.ejs', destinationFile: 'istio-k8s/zipkin-gateway.yml' },
-        { sourceFile: 'istio/gateway/kiali-gateway.yml.ejs', destinationFile: 'istio-k8s/kiali-gateway.yml' },
-        { sourceFile: 'kustomize/patch/istio-label.yml.ejs', destinationFile: 'patch-k8s/istio-label.yml' },
-        { sourceFile: 'kustomize/patch/istio-namespace.yml.ejs', destinationFile: 'patch-k8s/istio-namespace.yml' },
+        { sourceFile: 'istio/gateway/grafana-gateway.yml.ejs', destinationFile: `istio-${suffix}/grafana-gateway.yml` },
+        { sourceFile: 'istio/gateway/zipkin-gateway.yml.ejs', destinationFile: `istio-${suffix}/zipkin-gateway.yml` },
+        { sourceFile: 'istio/gateway/kiali-gateway.yml.ejs', destinationFile: `istio-${suffix}/kiali-gateway.yml` },
+        { sourceFile: 'kustomize/patch/istio-label.yml.ejs', destinationFile: `patch-${suffix}/istio-label.yml` },
+        { sourceFile: 'kustomize/patch/istio-namespace.yml.ejs', destinationFile: `patch-${suffix}/istio-namespace.yml` },
       ],
     },
   ],
-});
+}));
 export const writeFiles = asWritingTask(async function writeFiles() {
   for (let i = 0; i < this.appConfigs.length; i++) {
     this.app = this.appConfigs[i];
     await this.writeFiles({
-      sections: applicationFiles,
+      sections: applicationFiles('k8s'),
       context: this,
     });
   }
   await this.writeFiles({
-    sections: writeDeploymentFiles,
+    sections: writeDeploymentFiles('k8s'),
     context: this,
   });
 });
