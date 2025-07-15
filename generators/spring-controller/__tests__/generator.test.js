20195442812373a1a69c920923fff761f42100ed
Vinayak M V <147400242+VINAYAK28x@users.noreply.github.com>
2025-07-15 13:24:50 +0530

    Create generator.test.js
    created a sample test for the spring REST controller generator

---
+++ generators/spring-controller/__tests__/generator.test.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+
+describe('Spring Controller Generator', () => {
+  it('generates a controller with expected content', async () => {
+    await helpers.run(path.join(__dirname, '/generators/spring-controller')).withPrompts({
+      packageName: 'com.example.demo',
+      controllerName: 'HelloController',
+      endpoint: '/api/hello',
+      httpMethod: 'GET',
+      methodName: 'sayHello',
+    });
+
+    const expectedPath = 'src/main/java/com/example/demo/web/rest/HelloController.java';
+
+    assert.file(expectedPath);
+    assert.fileContent(expectedPath, 'public class HelloController');
+    assert.fileContent(expectedPath, '@GetMapping');
+    assert.fileContent(expectedPath, 'public String sayHello()');
+  });
+});
