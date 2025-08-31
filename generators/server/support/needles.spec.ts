f6cd700662899b30f3b3d279428d1812991a4bdd
Quentin <quentin.monmert@gmail.com>
2025-08-31 22:43:24 +0200

    Fix typo: should not be add=>should not add

--- generators/server/support/needles.spec.ts
+++ generators/server/support/needles.spec.ts
@@ -141,7 +141,7 @@ public class ApplicationProperties {
           expect(runResult.getSnapshot((file: any) => fileRegexp.test(file.path))).toEqual(snapshot);
         });
 
-        it('should not be add new content with prettier differences and new lines', () => {
+        it('should not add new content with prettier differences and new lines', () => {
           const application = runResult.application!;
           insertContentIntoApplicationProperties.call(runResult.generator, application, {
             property: `  private Foo getFoo() {
