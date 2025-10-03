2e78e92934c51a729f22c793ed34c5ce4e3389d8
Quentin <quentin.monmert@gmail.com>
2025-10-03 09:34:09 +0200

    Fix bump keycloak/keycloak from 26.3.4 to 26.4.0 (#30942)

--- generators/docker/templates/docker/realm-config/keycloak-health-check.sh
+++ generators/docker/templates/docker/realm-config/keycloak-health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 3<>/dev/tcp/localhost/9990
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:9990\n" >&3
+echo -e "GET /health/ready HTTP/1.1\r\nHost: localhost:9990\r\nConnection: close\r\n\r\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
