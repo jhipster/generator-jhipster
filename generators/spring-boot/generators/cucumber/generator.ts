0f59c858314033cfe23db10aff3e329e085c63de
Quentin <quentin.monmert@gmail.com>
2025-11-06 00:28:41 +0100

    Move cucumber into spring-boot generator (#31269)

--- generators/spring-boot/generators/cucumber/generator.ts
+++ generators/spring-boot/generators/cucumber/generator.ts
@@ -18,9 +18,9 @@
  */
 import { gt } from 'semver';
 
-import { SERVER_TEST_RES_DIR, SERVER_TEST_SRC_DIR } from '../generator-constants.js';
-import { JavaApplicationGenerator } from '../java/generator.ts';
-import { moveToJavaPackageTestDir } from '../java/support/files.ts';
+import { SERVER_TEST_RES_DIR, SERVER_TEST_SRC_DIR } from '../../../generator-constants.js';
+import { JavaApplicationGenerator } from '../../../java/generator.ts';
+import { moveToJavaPackageTestDir } from '../../../java/support/files.ts';
 
 export default class CucumberGenerator extends JavaApplicationGenerator {
   async beforeQueue() {
