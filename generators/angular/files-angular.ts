46e16d02baf2e2898b6a93f8c0fd912a4fe3cbb1
Marcelo Shima <marceloshima@gmail.com>
2025-11-07 21:34:38 -0300

    revert jest.config to js

--- generators/angular/files-angular.ts
+++ generators/angular/files-angular.ts
@@ -46,7 +46,7 @@ export const files = asWriteFilesSection({
   jest: [
     clientRootTemplatesBlock({
       condition: ctx => ctx.clientTestFrameworkJest,
-      templates: ['jest.conf.ts'],
+      templates: ['jest.conf.js'],
     }),
   ],
   vitest: [
