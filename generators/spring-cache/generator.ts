51a9386d731b2b6515483c88539d6978990bf1c6
Marcelo Shima <marceloshima@gmail.com>
2026-01-02 12:48:03 -0300

    spring-boot: modularize hazelcast CacheConfiguration

--- generators/spring-cache/generator.ts
+++ generators/spring-cache/generator.ts
@@ -138,7 +138,7 @@ export default class SpringCacheGenerator extends BaseApplicationGenerator<
         });
       },
       async writeTask({ application }) {
-        const useModularedCacheConfiguration = application.cacheProviderInfinispan || application.cacheProviderEhcache || application.cacheProviderCaffeine;
+        const useModularedCacheConfiguration = application.cacheProviderInfinispan || application.cacheProviderEhcache || application.cacheProviderCaffeine || application.cacheProviderHazelcast;
         await this.writeFiles({
           sections: {
             cacheFiles: [
