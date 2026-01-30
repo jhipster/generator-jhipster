454d2d84cf81188e95e9067284de62d5378e06e2
Quentin <quentin.monmert@gmail.com>
2026-01-30 21:28:22 +0100

    Apply Prettier

--- generators/heroku/generator.ts
+++ generators/heroku/generator.ts
@@ -476,7 +476,7 @@ export default class HerokuGenerator extends BaseApplicationGenerator<HerokuEnti
             }
 
             const { stdout: buildpackData } = await this.spawnHeroku(['buildpacks', '--app', this.herokuAppName!]);
-            if (!(buildpackData).includes(buildpack)) {
+            if (!buildpackData.includes(buildpack)) {
               await this.spawnHeroku(['buildpacks:add', buildpack, '--app', this.herokuAppName!]);
             }
 
