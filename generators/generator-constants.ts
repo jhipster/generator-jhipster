df709de17020f1aa3bf1fe55e4cee29b93dae652
Matt Raible <matt@raibledesigns.com>
2026-03-10 23:13:43 -0600

    Prepare for next release

--- generators/generator-constants.ts
+++ generators/generator-constants.ts
@@ -25,7 +25,7 @@ export * from '../lib/constants/jhipster.ts';
 
 export const BLUEPRINT_API_VERSION = 'jhipster-8';
 // jhipster-bom version
-export const JHIPSTER_DEPENDENCIES_VERSION = '9.0.0';
+export const JHIPSTER_DEPENDENCIES_VERSION = '9.0.1-SNAPSHOT';
 // Version of Java
 export const RECOMMENDED_JAVA_VERSION = '21';
 // Supported Java versions, https://www.oracle.com/java/technologies/java-se-support-roadmap.html
