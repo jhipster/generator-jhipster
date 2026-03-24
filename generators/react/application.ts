cf9ee2a39b847eca2c569606d816a700ee1be0f7
Marcelo Shima <marceloshima@gmail.com>
2026-03-24 13:24:58 -0300

    simplify react templates

---
+++ generators/react/application.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2013-2026 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see https://www.jhipster.tech/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { MutateDataParam, MutateDataPropertiesWithRequiredProperties } from '../../lib/utils/object.ts';
+
+import type { Application as ReactApplication } from './types.d.ts';
+
+export type ReactAddedApplicationProperties = {
+  reactBootstrapBsTheme: string;
+  reactBootstrapVariant: string;
+};
+
+export const mutateApplication = {
+  __override__: false,
+  /**
+   * Color mode https://getbootstrap.com/docs/5.3/customize/color-modes/
+   */
+  reactBootstrapBsTheme: ({ clientThemeNone, clientThemeVariantLight }) => (clientThemeNone || !clientThemeVariantLight ? 'dark' : 'light'),
+  /**
+   * https://react-bootstrap.github.io/components/navbar/#navbar-props
+   */
+  reactBootstrapVariant: ({ clientThemeVariant }) => clientThemeVariant ?? 'dark',
+} as const satisfies MutateDataPropertiesWithRequiredProperties<MutateDataParam<ReactApplication>, ReactAddedApplicationProperties>;
