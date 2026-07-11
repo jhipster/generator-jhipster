0a94a63185e10fb37134e41e73ba68af21710fce
Quentin <quentin.monmert@gmail.com>
2026-07-11 22:53:16 +0200

    Fix typo: is=>are

--- generators/base/generator.ts
+++ generators/base/generator.ts
@@ -291,7 +291,7 @@ export default class BaseGenerator<
     // Use started counter or use stored creationTimestamp if creationTimestamp option is passed
     const creationTimestamp = this.options.creationTimestamp ? this.config.get('creationTimestamp') : undefined;
     let now = new Date();
-    // Milliseconds is ignored for changelogDate.
+    // Milliseconds are ignored for changelogDate.
     now.setMilliseconds(0);
     // Run reproducible timestamp when regenerating the project with reproducible option or a specific timestamp.
     if (reproducible || creationTimestamp) {
