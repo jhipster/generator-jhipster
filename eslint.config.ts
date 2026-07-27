c9e0d7d11778c8cf552f8e4c0c1544da2ea6a6fe
Quentin <quentin.monmert@gmail.com>
2026-07-27 16:03:03 +0200

    ESLint: enable no-import-type-side-effects (#34290)

--- eslint.config.ts
+++ eslint.config.ts
@@ -62,6 +62,7 @@ export default defineConfig(
       '@typescript-eslint/no-unnecessary-type-arguments': 'off',
       '@typescript-eslint/prefer-readonly': 'error',
       '@typescript-eslint/no-redundant-type-constituents': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
