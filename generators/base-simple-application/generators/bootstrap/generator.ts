842fbb23e5911442d334518f7e8707d3f64abc68
Marcelo Shima <marceloshima@gmail.com>
2026-07-28 15:56:46 -0300

    internal: add the removeNeedles property to the bootstrap generator

--- generators/base-simple-application/generators/bootstrap/generator.ts
+++ generators/base-simple-application/generators/bootstrap/generator.ts
@@ -70,6 +70,14 @@ export default class BaseSimpleApplicationBootstrapGenerator extends BaseSimpleA
 
   get preparing() {
     return this.asPreparingTaskGroup({
+      async removeNeedles({ application }) {
+        if (application.removeNeedles) {
+          // This sets removeNeedles to true in the bootstrap generator so it removes needles from generated files.
+          // Needles will be removed from every generated file. To do this for the generated project files only, we need https://github.com/SBoudrias/mem-fs/issues/67.
+          const bootstrapGenerator = await this.dependsOnJHipster('bootstrap');
+          bootstrapGenerator.removeNeedles = true;
+        }
+      },
       preparing({ applicationDefaults }) {
         if (this.useVersionPlaceholders) {
           applicationDefaults({
