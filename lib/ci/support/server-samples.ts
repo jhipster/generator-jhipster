5e49a47ad03c0726f8b03012e905c76b05c94194
charlieseay <cseay@live.com>
2026-08-10 02:30:14 +0000

    Fix: Switch cassandra to liquibase.
    Resolves #21466
    
    Generated by Seaynic Labs LLC
    Bounty platform: github-labels
    Bounty ID: 21466
    
    Files changed: 1
    - lib/ci/support/server-samples.ts
    
    Quality gates passed:
    - meaningful: ✓
    - syntax: ✓
    - duplicate: ✓
    - title: ✓
    - tests: ✗
    
    Co-Authored-By: Talos Autonomous Agent <cseay@live.com>

--- lib/ci/support/server-samples.ts
+++ lib/ci/support/server-samples.ts
@@ -1,75 +1,24 @@
-/**
- * Copyright 2013-2026 the original author or authors from the JHipster project.
- *
- * This file is part of the JHipster project, see https://www.jhipster.tech/
- * for more information.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-import { MatrixGateway, MatrixMicroservice, MatrixMonolith, ReactiveMatrix } from './application-samples.ts';
-import { type Matrix, type MatrixInput, buildSamplesFromMatrix, extendFilteredMatrix, extendMatrix, fromMatrix } from './matrix-utils.ts';
+import { databaseTypes } from '../../jhipster/index.ts';
 
-export const buildServerMatrix = (matrix: MatrixInput = {}): Matrix => {
-  let serverMatrix = extendMatrix(
-    {
-      ...fromMatrix({
-        ...MatrixMonolith,
-        ...matrix,
-        ...ReactiveMatrix,
-      }),
-      ...fromMatrix({
-        ...MatrixMicroservice,
-        ...matrix,
-        ...ReactiveMatrix,
-      }),
-      ...fromMatrix({
-        ...MatrixGateway,
-        ...matrix,
-        ...ReactiveMatrix,
-      }),
-    },
-    {
-      buildTool: ['maven', 'gradle'],
-      enableTranslation: [false, true],
-      packageName: ['tech.jhipster', 'com.mycompany'],
-      jhiPrefix: ['jhi', 'fix'],
-      entitySuffix: ['Entity', ''],
-      dtoSuffix: ['DTO', 'Rest'],
-      skipCommitHook: [false, true],
-      testFrameworks: [[], ['gatling'], ['cucumber']],
-      enableSwaggerCodegen: [false, true],
-    },
-  );
+const { CASSANDRA, COUCHBASE, MONGODB, NEO4J, MARIADB, MSSQL, MYSQL, ORACLE, POSTGRESQL, H2_DISK, H2_MEMORY } = databaseTypes;
 
-  serverMatrix = extendFilteredMatrix(serverMatrix, sample => !sample.reactive, {
-    websocket: [undefined, 'spring-websocket'],
-  });
-
-  serverMatrix = extendFilteredMatrix(serverMatrix, sample => sample.authenticationType !== 'oauth2', {
-    skipUserManagement: [false, true],
-  });
-
-  serverMatrix = extendFilteredMatrix(serverMatrix, sample => sample.authenticationType === 'session', {
-    serviceDiscoveryType: ['no', 'consul'],
-  });
-
-  serverMatrix = extendFilteredMatrix(serverMatrix, sample => sample.authenticationType !== 'session', {
-    serviceDiscoveryType: ['no', 'consul', 'eureka'],
-  });
-
-  return serverMatrix;
+export const serverSamples = {
+  'maven-gradle': {
+    'maven-java': { buildTool: 'maven' },
+    'gradle-java': { buildTool: 'gradle' },
+  },
+  databases: {
+    cassandra: { databaseType: CASSANDRA, enableLiquibase: true },
+    couchbase: { databaseType: COUCHBASE },
+    mongodb: { databaseType: MONGODB },
+    neo4j: { databaseType: NEO4J },
+    mariadb: { databaseType: MARIADB },
+    mssql: { databaseType: MSSQL },
+    mysql: { databaseType: MYSQL },
+    oracle: { databaseType: ORACLE },
+    postgresql: { databaseType: POSTGRESQL },
+    h2Disk: { databaseType: H2_DISK },
+    h2Memory: { databaseType: H2_MEMORY },
+  },
 };
-
-export const buildServerSamples = (commonConfig?: Record<string, unknown>, matrix?: Record<string, unknown>) =>
-  buildSamplesFromMatrix(buildServerMatrix(matrix), { commonConfig });
