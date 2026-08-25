0901d7ea258f239d249b4613fc16df4d366cdd71
Marcelo Shima <marceloshima@gmail.com>
2026-08-25 15:50:06 -0300

    generate-blueprint: set skipLibCheck option to false (#33638)

--- generators/generate-blueprint/generators/standalone/generator.ts
+++ generators/generate-blueprint/generators/standalone/generator.ts
@@ -57,6 +57,7 @@ export default class StandaloneBlueprintGenerator extends GenerateBlueprintBaseG
       async preparing({ applicationDefaults }) {
         applicationDefaults({
           blueprintCliName: data => `cli.c${data.javascriptBlueprint ? 'j' : 't'}s`,
+          typescriptEslint: data => !data.javascriptBlueprint,
         });
       },
     });
@@ -181,6 +182,7 @@ export default class StandaloneBlueprintGenerator extends GenerateBlueprintBaseG
             globals: mainDependencies.globals,
             vitest: mainDependencies.vitest,
             prettier: mainDependencies.prettier,
+            ...(application.typescriptEslint ? { 'typescript-eslint': mainDependencies['typescript-eslint'] } : {}),
             /*
              * yeoman-test version is loaded through generator-jhipster peer dependency.
              * generator-jhipster uses a fixed version, blueprints must set a compatible range.
