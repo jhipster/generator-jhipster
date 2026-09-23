bc962908786d27903eb279fa1a1dba8880705730
Quentin <quentin.monmert@gmail.com>
2026-09-23 23:45:09 +0200

    ESLint: extend js recommended config for ejs templates (#35151)

--- eslint.config.ts
+++ eslint.config.ts
@@ -159,13 +159,13 @@ export default defineConfig(
       experimental: true,
       allowedGlobals: ['wrapMono', 'generateEntityClientEnumImports', 'getOriginalField', 'generateFakeData'],
     },
+    js.configs.recommended,
     stylistic.configs.customize({
       jsx: false,
       semi: true,
     }),
     {
       rules: {
-        ...js.configs.recommended.rules,
         ...jsRules,
         'no-negated-condition': 'error',
         'no-nested-ternary': 'error',
