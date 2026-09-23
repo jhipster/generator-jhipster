834c56aebab5dd401b96116581f2430f98d7322b
Quentin <quentin.monmert@gmail.com>
2026-09-23 22:08:30 +0200

    ESLint: enable no-nested-ternary for ejs templates

--- eslint.config.ts
+++ eslint.config.ts
@@ -169,6 +169,7 @@ export default defineConfig(
         ...js.configs.recommended.rules,
         ...jsRules,
         'no-negated-condition': 'error',
+        'no-nested-ternary': 'error',
         'no-useless-concat': 'error',
         'no-extra-parens': ['error', 'all', { nestedBinaryExpressions: false, ternaryOperandBinaryExpressions: false }],
         '@stylistic/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: 'never' }],
